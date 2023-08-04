1860e456e1cf0a16eaf0d4568decb4cec67a1d6b
Hans Pagel <mail@hanspagel.com>
2023-08-04 12:14:31 +0200

    fix: json syntax highlighting for request body

--- api-client/src/components/ApiClient/Request/RequestBody.vue
+++ api-client/src/components/ApiClient/Request/RequestBody.vue
@@ -29,7 +29,7 @@ defineProps<{
             )
           : '{}'
       "
-      :languages="['javascript']"
+      :languages="['json']"
       :lineNumbers="true" />
   </CollapsibleSection>
 </template>
