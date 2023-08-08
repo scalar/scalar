1fbd7c140accea28ec1ca840bc52118e331f38cd
Hans Pagel <mail@hanspagel.com>
2023-08-09 00:17:46 +0200

    chore: remove the Go swagger parser package

--- packages/swagger-parser/src/main.ts
+++
@@ -1,3 +0,0 @@
-export { loadWasmFile } from './helpers/loadWasmFile'
-export { parseSwaggerString } from './helpers/parseSwaggerString'
-export { validateSwaggerString } from './helpers/validateSwaggerString'
