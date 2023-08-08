31f53b56a22742e4d6cfca53cbac7d4e0eab1dcf
Hans Pagel <mail@hanspagel.com>
2023-08-08 14:51:47 +0200

    refactor: move sidebar styles to component

--- packages/api-reference/src/components/Sidebar.vue
+++ packages/api-reference/src/components/Sidebar.vue
@@ -93,3 +93,191 @@ const {
     </div>
   </div>
 </template>
+
+<style>
+.sidebar {
+  --theme-sidebar-indent-base: 6px;
+  background: var(--sidebar-background-1, var(--theme-background-1));
+}
+
+/* ----------------------------------------------------- */
+/* Main sidebar styles */
+
+.sidebar {
+  flex: 1;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background: var(--sidebar-background-1, var(--theme-background-1));
+  --sidebar-level: 0;
+}
+
+.pages {
+  padding-top: 9px;
+  padding-bottom: 9px;
+}
+
+.sidebar-group {
+  list-style: none;
+  width: 100%;
+}
+
+.sidebar-heading {
+  display: flex;
+  gap: 6px;
+
+  color: var(--sidebar-color-2, var(--theme-color-2));
+  font-size: var(--theme-mini);
+  font-weight: var(--theme-semibold);
+  word-break: break-word;
+  line-height: 1.385;
+  display: flex;
+  align-items: center;
+  max-width: 100%;
+  position: relative;
+  cursor: pointer;
+  border-radius: 0 var(--theme-radius) var(--theme-radius) 0;
+  flex: 1;
+  padding-right: 12px;
+  user-select: none;
+}
+
+/* Folder/page collapse icon */
+.toggle-nested-icon {
+  border: none;
+  position: absolute !important;
+  left: 2px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: currentColor;
+}
+
+.toggle-nested-icon:hover,
+.toggle-nested-icon:focus-visible {
+  color: currentColor;
+  filter: drop-shadow(0 0.125px 0 currentColor)
+    drop-shadow(0 -0.125px 0 currentColor);
+}
+
+/* We indent each level of nesting further */
+.sidebar-indent-nested .sidebar-heading {
+  padding-left: calc(
+    (var(--sidebar-level) * var(--theme-sidebar-indent-base)) + 24px
+  ) !important;
+}
+
+/* Collapse/expand icons must also be offset */
+.sidebar-indent-nested .sidebar-heading .toggle-nested-icon {
+  left: calc(
+    (var(--sidebar-level) * var(--theme-sidebar-indent-base)) + 2px
+  ) !important;
+}
+
+.sidebar-heading-link {
+  padding-right: 12px;
+  padding: 6px 0;
+  display: flex;
+  align-items: center;
+}
+
+/* Sidebar link icon */
+.link-icon {
+  position: relative;
+  left: 4px;
+}
+
+.sidebar-icon {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 6px;
+
+  width: 13px;
+  height: 13px;
+}
+
+.sidebar-icon > svg {
+  width: 13px;
+  height: 13px;
+}
+
+.sidebar-heading:hover {
+  color: var(--sidebar-color-1, var(--theme-color-1));
+  background: var(--sidebar-item-hover-background, var(--theme-background-3));
+}
+
+.active_page.sidebar-heading:hover,
+.active_page.sidebar-heading,
+.marc_active .sidebar-heading {
+  background: var(
+    --sidebar-item-active-background,
+    var(--theme-background-3)
+  ) !important;
+  color: var(--sidebar-color-active, var(--theme-color-accent)) !important;
+}
+.sidebar-group-item {
+  position: relative;
+}
+
+/* Change font colors and weights for nested items */
+.sidebar-indent-nested .sidebar-heading {
+  color: var(--sidebar-color-1, var(--theme-color-1));
+}
+.sidebar-indent-nested .sidebar-indent-nested .sidebar-heading {
+  color: var(--sidebar-color-2, var(--theme-color-2));
+}
+.sidebar-indent-nested > div:has(.active_page) .sidebar-heading {
+  font-weight: var(--theme-bold);
+}
+
+.sidebar-mobile-header {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  height: 100%;
+  width: 100%;
+  padding: 0 6px;
+}
+
+.sidebar-mobile-breadcrumbs {
+  flex: 1;
+  min-width: 0;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  font-size: var(--theme-small);
+  font-weight: var(--theme-semibold);
+}
+
+.sidebar-mobile-actions {
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+  height: 24px;
+  align-items: center;
+  padding: 0 4px;
+}
+
+.sidebar-mobile-actions .sidebar-mobile-darkmode-toggle {
+  height: 16px;
+  width: 16px;
+}
+
+@media (max-width: 1000px) {
+  .sidebar {
+    min-height: 0;
+  }
+
+  .pages {
+    padding-top: 12px;
+  }
+}
+
+@media (max-width: 500px) {
+  .header-item-link.header-item-active,
+  .sidebar-section,
+  .sidebar-heading {
+    font-size: var(--theme-mini);
+  }
+}
+</style>
