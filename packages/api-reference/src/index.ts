c1a2d5eb0e198e237f3eaa8f55ef480196db42cd
Hans Pagel <mail@hanspagel.com>
2023-08-14 10:59:58 +0200

    chore: rename SwaggerEditor to ApiReference, rename GuideComponent to ApiGuide

--- packages/api-reference/src/index.ts
+++ packages/api-reference/src/index.ts
@@ -1,5 +1,5 @@
 export { default as ApiClientModal } from './components/ApiClientModal.vue'
-export { default as SwaggerEditor } from './components/SwaggerEditor.vue'
+export { default as ApiReference } from './components/ApiReference.vue'
 export { default as Sidebar } from './components/Sidebar.vue'
 export { default as RenderedReference } from './components/Content/Content.vue'
 
