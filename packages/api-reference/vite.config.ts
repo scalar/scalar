a9793af00fe41838d6a80a75e29d9996bbc47546
Hans Pagel <mail@hanspagel.com>
2023-08-16 16:24:48 +0200

    chore: clean up

--- packages/api-reference/vite.config.ts
+++ packages/api-reference/vite.config.ts
@@ -1,7 +1,5 @@
 import vue from '@vitejs/plugin-vue'
-import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
-import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
   plugins: [vue()],
--- packages/use-keyboard-event/vite.config.ts
+++ packages/use-keyboard-event/vite.config.ts
@@ -1,6 +1,5 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
   plugins: [vue()],
