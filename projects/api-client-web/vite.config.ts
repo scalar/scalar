587d7a5799d77d8c6f2ed1315ffba3f37ea30a41
Hans Pagel <mail@hanspagel.com>
2023-08-17 11:06:29 +0200

    chore: use package source locally

--- projects/api-client-web/vite.config.ts
+++ projects/api-client-web/vite.config.ts
@@ -1,10 +1,19 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 5050,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@scalar/api-client',
+        replacement: path.resolve(__dirname, '../../packages/api-client/src/index.ts'),
+      },
+    ],
   },
   plugins: [vue()],
 })
