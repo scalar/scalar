34adf2f042aad2a70728570fdafb17bc6cd8fdf4
Hans Pagel <mail@hanspagel.com>
2023-08-17 11:55:45 +0200

    feat: add start page

--- projects/api-client-web/src/pages/StartPage.vue
+++ projects/api-client-web/src/pages/StartPage.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    <router-link :to="{ name: 'api-client' }">
-      API Client
+  <div class="flex flex-row gap-10 p-10 h-screen content-center bg-gray-100">
+    <router-link
+      :to="{ name: 'api-client' }"
+      class="basis-1/2 text-center border-solid border-gray-300 hover:border-gray-400 flex flex-col justify-center rounded-lg bg-white shadow-xl text-lg p-4"
+    >
+      <h2 class="m-4 font-bold">
+        API Client
+      </h2>
+      <div class="">
+        Your drop-in API Client.
+      </div>
     </router-link>
-    <router-link :to="{ name: 'api-reference' }">
-      API Reference
+    <router-link
+      :to="{ name: 'api-reference' }"
+      class="basis-1/2 text-center border-solid border-gray-300 hover:border-gray-400 flex flex-col justify-center rounded-lg bg-white shadow-xl text-lg p-4"
+    >
+      <h2 class="m-4 font-bold">
+        API Reference
+      </h2>
+      <div>
+        Interactive API documentations from Swagger files.
+      </div>
     </router-link>
   </div>
 </template>
---
+++ projects/api-client-web/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
