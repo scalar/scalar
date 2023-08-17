d155ae2ef54e055b4dded83d999de8a68ebf07e6
Hans Pagel <mail@hanspagel.com>
2023-08-17 12:33:31 +0200

    chore: update vite config to split css file

--- packages/api-reference/vite.config.ts
+++ packages/api-reference/vite.config.ts
@@ -4,15 +4,25 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
+    cssCodeSplit: true,
     minify: false,
     lib: {
-      entry: 'src/index.ts',
+      entry: ['src/index.ts', 'src/assets/css/variables.css'],
       name: '@scalar/api-reference',
       fileName: 'index',
       formats: ['es'],
     },
     rollupOptions: {
       external: ['vue'],
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'index.css') {
+            return 'style.css'
+          }
+
+          return assetInfo.name ?? 'default'
+        },
+      },
     },
   },
 })
