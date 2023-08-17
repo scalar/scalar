c2dcc1ea0677de10fbf7d9adae75c3995891f6bd
Hans Pagel <mail@hanspagel.com>
2023-08-17 12:16:37 +0200

    fix: node polyfills for the swagger-parser

--- projects/api-client-web/vite.config.ts
+++ projects/api-client-web/vite.config.ts
@@ -1,9 +1,27 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import path from 'path'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    vue(),
+    nodePolyfills({
+      // To exclude specific polyfills, add them to this list.
+      exclude: [
+        'fs', // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   server: {
     port: 5050,
   },
@@ -17,5 +35,4 @@ export default defineConfig({
       },
     ],
   },
-  plugins: [vue()],
 })
