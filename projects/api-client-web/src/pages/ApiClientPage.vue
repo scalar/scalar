3fe1cf04e7ceccc4325995fef530e326ef0d1033
Hans Pagel <mail@hanspagel.com>
2023-08-30 11:47:57 +0200

    feat: store active request in local storage (just for the demo)

--- projects/api-client-web/src/pages/ApiClientPage.vue
+++ projects/api-client-web/src/pages/ApiClientPage.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
 import { ApiClient } from '@scalar/api-client'
 import { useApiClientRequestStore } from '@scalar/api-client'
+import { watch } from 'vue'
 
-// import '@scalar/api-client/style.css'
-
-const { readOnly } = useApiClientRequestStore()
+const { readOnly, activeRequest, setActiveRequest } = useApiClientRequestStore()
 
 readOnly.value = false
+
+// Store active request in local storage
+watch(activeRequest, () => {
+  const activeRequestFromStorage = window.localStorage.getItem('activeRequest')
+
+  if (JSON.stringify(activeRequest) !== activeRequestFromStorage) {
+    window.localStorage.setItem('activeRequest', JSON.stringify(activeRequest))
+  }
+})
+
+// Restore active request from local storage
+const activeRequestFromStorage = window.localStorage.getItem('activeRequest')
+
+if (activeRequestFromStorage) {
+  setActiveRequest(JSON.parse(activeRequestFromStorage))
+}
 </script>
 
 <template>
