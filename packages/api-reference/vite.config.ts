32e8c45c56575de2ddfe9a69923618ec8db195a9
Hans Pagel <mail@hanspagel.com>
2023-08-31 14:57:23 +0200

    refactor: remove vite-plugin-top-level-await, add node polyfills to api-reference package

--- packages/api-reference/vite.config.ts
+++ packages/api-reference/vite.config.ts
@@ -1,12 +1,27 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import topLevelAwait from 'vite-plugin-top-level-await'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [vue(), topLevelAwait()],
+  plugins: [
+    vue(),
+    nodePolyfills({
+      // To exclude specific polyfills, add them to this list.
+      exclude: [
+        'fs', // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: false,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   build: {
     cssCodeSplit: false,
-    minify: false,
     lib: {
       entry: ['src/index.ts', 'src/assets/css/variables.css'],
       name: '@scalar/api-reference',
