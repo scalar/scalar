d000794feed18d6f2fb0581e0af1a7297dd6a30a
Hans Pagel <mail@hanspagel.com>
2023-09-05 11:19:30 +0200

    feat: show how to generate a Swagger file and pass it directly to the fastify plugin

--- projects/fastify-api-reference/src/index.ts
+++ projects/fastify-api-reference/src/index.ts
@@ -1,28 +1,76 @@
-import fastifyApiReference from '@scalar/fastify-api-reference'
+// import fastifyApiReference from '@scalar/fastify-api-reference'
 import Fastify from 'fastify'
 
+// Init Fastify
 const fastify = Fastify({
   logger: false,
 })
 
-fastify.get('/scalar.json', async (request, reply) => {
-  reply.send(await import('./scalar.json'))
-})
-
-fastify.register(fastifyApiReference, {
-  prefix: '/api-reference',
-  apiReference: {
-    specUrl: '/scalar.json',
-    // spec: {
-    //   openapi: '3.1.0',
-    //   info: {
-    //     title: 'Example',
-    //   },
-    //   paths: {},
-    // },
+// Register Swagger
+await fastify.register(require('@fastify/swagger'), {
+  swagger: {
+    info: {
+      title: 'Example API',
+    },
   },
 })
 
-fastify.listen({ port: 0 }, function (err, address) {
+// Add some routes
+fastify.put(
+  '/example-route/:id',
+  {
+    schema: {
+      description: 'post some data',
+      tags: ['user', 'code'],
+      summary: 'qwerty',
+      params: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            description: 'user id',
+          },
+        },
+      },
+      body: {
+        type: 'object',
+        properties: {
+          hello: { type: 'string' },
+          obj: {
+            type: 'object',
+            properties: {
+              some: { type: 'string' },
+            },
+          },
+        },
+      },
+      response: {
+        201: {
+          description: 'Successful response',
+          type: 'object',
+          properties: {
+            hello: { type: 'string' },
+          },
+        },
+      },
+    },
+  },
+  (req, reply) => {},
+)
+
+// Add the plugin
+await fastify.register(require('@scalar/fastify-api-reference'), {
+  prefix: '/api-reference',
+  apiReference: {
+    // Pass the generated Swagger spec:
+    // @ts-ignore
+    spec: fastify.swagger(),
+    // Or pass the URL to the spec:
+    // specUrl: '/scalar.json',
+  },
+})
+
+// Start the server
+fastify.listen({ port: 64100 }, function (err, address) {
   console.log(`⚡️ Fastify Plugin running on ${address}/api-reference`)
 })
