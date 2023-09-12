52566aa6d682893cb4a85f5559dcdba4e44be046
Hans Pagel <mail@hanspagel.com>
2023-09-12 15:26:17 +0200

    style: highlight variables in base urls

--- packages/api-reference/src/components/Content/Introduction/BaseUrl.vue
+++ packages/api-reference/src/components/Content/Introduction/BaseUrl.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
 import { useClipboard } from '@scalar/use-clipboard'
+import { computed } from 'vue'
 
-defineProps<{
+const props = defineProps<{
   server?: {
     url: string
     description?: string
@@ -9,23 +10,39 @@ defineProps<{
 }>()
 
 const { copyToClipboard } = useClipboard()
+
+const formattedServerUrl = computed(() => {
+  const url = props.server?.url ?? ''
+  const urlWithoutHtml = url.replace(/(<([^>]+)>)/gi, '')
+
+  /* Replace all variables (example: {{ baseurl }} with an HTML tag) */
+  return urlWithoutHtml.replace(
+    /{{\s*([\w.-]+)\s*}}/g,
+    '<span class="base-url-variable">{{ $1 }}</span>',
+  )
+})
 </script>
 <template>
   <template v-if="server">
     <a
       class="base-url"
       :title="server.description"
-      @click="copyToClipboard(server.url)">
-      {{ server.url }}
-    </a>
+      @click="copyToClipboard(server.url)"
+      v-html="formattedServerUrl" />
   </template>
 </template>
+
+<style>
+.base-url-variable {
+  color: var(--theme-color-disabled);
+}
+</style>
+
 <style scoped>
 .base-url {
   color: var(--theme-color-2);
   font-size: var(--theme-mini);
   cursor: pointer;
-  display: flex;
   font-family: var(--theme-font-code);
   flex-direction: column;
 }
