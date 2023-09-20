9550ea01dd3994c3f5d96b37dd917f35eb0db5dd
Hans Pagel <mail@hanspagel.com>
2023-09-20 10:15:19 +0200

    chore: add storybook

---
+++ packages/api-reference/.storybook/main.js
@@ -0,0 +1,17 @@
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+}
+export default config
