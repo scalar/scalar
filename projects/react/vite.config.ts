493cde2b5023524fbd429359077b1403dc4c9d83
Hans Pagel <mail@hanspagel.com>
2023-09-22 12:19:14 +0200

    feat: add react demo

--- projects/react/vite.config.ts
+++ projects/react/vite.config.ts
@@ -1,10 +1,49 @@
-import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import veauryVitePlugins from 'veaury/vite/index.js'
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    veauryVitePlugins({
+      type: 'react',
+      // Configuration of @vitejs/plugin-vue
+      // vueOptions: {...},
+      // Configuration of @vitejs/plugin-react
+      // reactOptions: {...},
+      // Configuration of @vitejs/plugin-vue-jsx
+      // vueJsxOptions: {...}
+    }),
+    nodePolyfills({
+      // To exclude specific polyfills, add them to this list.
+      exclude: [
+        'fs', // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   server: {
     port: 5054,
+  },
+  resolve: {
+    alias: [
+      // Resolve the uncompiled source code for all @scalar packages
+      // It’s working with the alias, too. It’s just required to enable HMR.
+      {
+        // Resolve the uncompiled source code for all @scalar packages
+        // @scalar/* -> packages/*/
+        // (not @scalar/*/style.css)
+        find: /^@scalar\/([^/]+)$/,
+        replacement: path.resolve(__dirname, '../../packages/$1/src/index.ts'),
+      },
+    ],
   },
 })
