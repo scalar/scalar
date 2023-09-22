799f9166a70facfa0c0cd717b4c814582705d47f
Hans Pagel <hanspagel@users.noreply.github.com>
2023-09-22 18:04:19 +0200

    feat: React demo (#98)
    * refactor: move dev command to bash script, add react project
    
    * feat: add react demo
    
    * chore: update README

--- patches/README.md
+++ patches/README.md
@@ -1,5 +1,3 @@
 # Patches
 
 Patch fixes for pnpm modules. This currently used to patch ono for swagger-parser polyfills.
-
-NOTE: This should not be moved outside of packages as the dockerfiles typically only copy over packages and then any core code.
---
+++ projects/react/src/App.tsx
@@ -0,0 +1,16 @@
+import { ApiReference as VueComponent } from '@scalar/api-reference'
+import { applyVueInReact } from 'veaury'
+
+const ApiReference = applyVueInReact(VueComponent)
+
+function App() {
+  return (
+    <>
+      {/* Explanation: https://github.com/devilwjp/veaury#typescript-jsx-types-conflict-caused-by-vue-and-react-at-the-same-time
+      // @ts-ignore */}
+      <ApiReference isEditable={true} />
+    </>
+  )
+}
+
+export default App
