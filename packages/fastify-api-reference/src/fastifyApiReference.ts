d67b66348de9a1707b2e0b0cdc9097a8eea6848c
Hans Pagel <mail@hanspagel.com>
2023-09-25 16:04:31 +0200

    refactor: use ejs templating engine instead of just template literals

--- packages/fastify-api-reference/src/fastifyApiReference.ts
+++ packages/fastify-api-reference/src/fastifyApiReference.ts
@@ -1,5 +1,7 @@
+import ejs from 'ejs'
 import type { FastifyPluginAsync } from 'fastify'
 import fp from 'fastify-plugin'
+import path from 'path'
 
 export type ApiReferenceOptions = {
   /**
@@ -13,34 +15,6 @@ export type ApiReferenceOptions = {
     spec?: Record<string, any> | (() => Record<string, any>)
     title?: string
   }
-}
-
-const getHtmlMarkup = (options: ApiReferenceOptions) => {
-  const htmlTag = options.apiReference?.specUrl
-    ? `<div data-spec-url="${options.apiReference?.specUrl}" />`
-    : typeof options.apiReference?.spec === 'object'
-    ? `<div data-spec='${JSON.stringify(options.apiReference?.spec)}' />`
-    : options.apiReference?.spec
-    ? `<div data-spec='${JSON.stringify(options.apiReference?.spec())}' />`
-    : ''
-
-  return `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>${options.apiReference.title || 'API Reference'}</title>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1" />
-  </head>
-  <body>
-    <!-- Add your own OpenAPI/Swagger spec file URL here: -->
-    ${htmlTag}
-    <script src="https://www.unpkg.com/@scalar/api-reference"></script>
-  </body>
-</html>
-`
 }
 
 const fastifyApiReference: FastifyPluginAsync<ApiReferenceOptions> = async (
@@ -61,7 +35,14 @@ const fastifyApiReference: FastifyPluginAsync<ApiReferenceOptions> = async (
   })
 
   fastify.get(options.routePrefix ?? '/', async (_, reply) => {
-    reply.send(getHtmlMarkup(options))
+    const html = await ejs.renderFile(
+      path.resolve(`${__dirname}/templates/index.ejs`),
+      {
+        options,
+      },
+    )
+
+    reply.send(html)
   })
 }
 
