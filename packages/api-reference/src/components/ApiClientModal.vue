fff8bfb9b8c846ba3f9510eb564d0674a7d6b2d5
Hans Pagel <mail@hanspagel.com>
2023-09-26 15:28:22 +0200

    fix: z-index for api client modal

--- packages/api-reference/src/components/ApiClientModal.vue
+++ packages/api-reference/src/components/ApiClientModal.vue
@@ -176,7 +176,7 @@ TODO: Markup is missing
   position: fixed;
   bottom: 4px;
   left: 4px;
-  z-index: 9999;
+  z-index: 2;
   opacity: 0;
   animation: apiclientfadein 0.35s forwards;
 }
@@ -198,7 +198,7 @@ TODO: Markup is missing
   height: 100vh;
   background: rgba(0, 0, 0, 0.44);
   transition: all 0.3s ease-in-out;
-  z-index: 9998;
+  z-index: 1;
   cursor: pointer;
   animation: drawerexitfadein 0.35s forwards;
 }
