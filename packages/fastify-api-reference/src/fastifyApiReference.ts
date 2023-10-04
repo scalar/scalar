b03da7b7fb073e42fb7cf44909702545f01be58f
Hans Pagel <mail@hanspagel.com>
2023-10-04 11:04:13 +0200

    fix: hide fastify plugin routes from the swagger spec

--- packages/fastify-api-reference/src/fastifyApiReference.ts
+++ packages/fastify-api-reference/src/fastifyApiReference.ts
@@ -18,6 +18,12 @@ export type ApiReferenceOptions = {
   }
 }
 
+// This Schema is used to hide the route from the documentation.
+// https://github.com/fastify/fastify-swagger#hide-a-route
+const schemaToHideRoute = {
+  hide: true,
+}
+
 const fastifyApiReference: FastifyPluginAsync<ApiReferenceOptions> = async (
   fastify,
   options,
@@ -36,39 +42,50 @@ const fastifyApiReference: FastifyPluginAsync<ApiReferenceOptions> = async (
     return
   }
 
-  fastify.get(options.routePrefix ?? '/', async (_, reply) => {
-    reply.header('Content-Type', 'text/html; charset=utf-8')
+  fastify.route({
+    method: 'GET',
+    url: options.routePrefix ?? '/',
+    // We don’t know whether @fastify/swagger is registered, but it doesn’t hurt to add a schema anyway.
+    // @ts-ignore
+    schema: schemaToHideRoute,
+    async handler(_, reply) {
+      reply.header('Content-Type', 'text/html; charset=utf-8')
 
-    let mergedOptions = options
+      let mergedOptions = options
 
-    if (
-      !options.apiReference?.spec &&
-      !options.apiReference?.specUrl &&
-      hasSwaggerPlugin
-    ) {
-      mergedOptions = {
-        ...options,
-        apiReference: {
-          ...options.apiReference,
-          // @ts-ignore
-          spec: () => fastify.swagger(),
-        },
+      if (
+        !options.apiReference?.spec &&
+        !options.apiReference?.specUrl &&
+        hasSwaggerPlugin
+      ) {
+        mergedOptions = {
+          ...options,
+          apiReference: {
+            ...options.apiReference,
+            // @ts-ignore
+            spec: () => fastify.swagger(),
+          },
+        }
       }
-    }
 
-    const html = await ejs.renderFile(
-      path.resolve(`${__dirname}/../dist/templates/index.ejs`),
-      {
-        options: mergedOptions,
-      },
-    )
+      const html = await ejs.renderFile(
+        path.resolve(`${__dirname}/../dist/templates/index.ejs`),
+        {
+          options: mergedOptions,
+        },
+      )
 
-    reply.send(html)
+      reply.send(html)
+    },
   })
 
-  fastify.get(
-    (options.routePrefix ?? '/') + '/fastify-api-reference.js',
-    async (_, reply) => {
+  fastify.route({
+    method: 'GET',
+    url: (options.routePrefix ?? '/') + '/fastify-api-reference.js',
+    // We don’t know whether @fastify/swagger is registered, but it doesn’t hurt to add a schema anyway.
+    // @ts-ignore
+    schema: schemaToHideRoute,
+    async handler(_, reply) {
       reply.header('Content-Type', 'application/javascript; charset=utf-8')
 
       const content = fs.readFileSync(
@@ -78,7 +95,7 @@ const fastifyApiReference: FastifyPluginAsync<ApiReferenceOptions> = async (
 
       reply.send(content)
     },
-  )
+  })
 }
 
 export default fp(fastifyApiReference)
