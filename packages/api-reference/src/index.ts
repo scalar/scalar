675d524f1b6885f1d7f911a3a001704ab9be1ee0
marclave <marc@scalar.com>
2023-10-10 02:43:19 -0700

    fix: add SearchModal to exports

--- packages/api-reference/src/index.ts
+++ packages/api-reference/src/index.ts
@@ -6,6 +6,7 @@ export { default as ApiReference } from './components/ApiReference.vue'
 export { default as Sidebar } from './components/Sidebar.vue'
 export { default as RenderedReference } from './components/Content/Content.vue'
 export { default as DarkModeToggle } from './components/DarkModeToggle.vue'
+export { default as SearchModal } from './components/SearchModal.vue'
 
 export { useDarkModeState } from './hooks'
 export * from './types'
