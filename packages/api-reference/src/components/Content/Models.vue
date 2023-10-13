d16d829d5c0c0752a8b623ae0a301877d22184c0
marclave <marc@scalar.com>
2023-10-13 04:44:08 -0700

    fix: add missing models.vue

---
+++ packages/api-reference/src/components/Content/Models.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { type Components, type Definitions } from '../../types'
+
+defineProps<{
+  definitions?: Definitions
+  components?: Components
+}>()
+</script>
+<template>
+  <div>
+    <div v-if="definitions">{{ definitions }}</div>
+    <div v-if="components">{{ components }}</div>
+  </div>
+</template>
