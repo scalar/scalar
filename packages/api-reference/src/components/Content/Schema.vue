309c18a90440256feb2a7003ccd7054698cdf138
Hans Pagel <mail@hanspagel.com>
2023-10-13 16:00:30 +0200

    chore: render the models (wip)

---
+++ packages/api-reference/src/components/Content/Schema.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+// TODO: value.xml isn’t rendered yet
+import Schema from './Schema.vue'
+
+defineProps<{ value: Record<string, any> }>()
+</script>
+<template>
+  {{ value }}
+  type: {{ value.type }}
+  <table v-if="value.properties">
+    <tr
+      v-for="property in Object.keys(value.properties)"
+      :key="property">
+      <td>
+        {{ property }}
+
+        <div v-if="value?.required?.includes(property)">required</div>
+        <div v-else>optional</div>
+      </td>
+      <td>
+        <Schema
+          v-if="value.properties[property]"
+          :value="value.properties[property]" />
+      </td>
+    </tr>
+  </table>
+</template>
