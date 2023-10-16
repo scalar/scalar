708a68a68e9931ff5b353122158ccf8c56c1e2e8
Hans Pagel <mail@hanspagel.com>
2023-10-16 12:46:28 +0200

    fix: make analyze and preflight functions sync

--- packages/swagger-parser/src/helpers/analyze.ts
+++ packages/swagger-parser/src/helpers/analyze.ts
@@ -3,7 +3,7 @@ import type { SwaggerSpec } from '../types'
 /**
  * This function analyzes a spec and returns a report of the analysis.
  */
-export const analyze = async (spec: SwaggerSpec) => {
+export const analyze = (spec: SwaggerSpec) => {
   const hasTitle = spec.info?.title !== undefined
 
   const hasDescription = spec.info?.description !== undefined
