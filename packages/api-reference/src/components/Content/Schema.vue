aab4c3582398589e7a572317c5ca9e15d0bd86c6
Hans Pagel <mail@hanspagel.com>
2023-10-17 12:38:34 +0200

    feat: add better schema support

--- packages/api-reference/src/components/Content/Schema.vue
+++ packages/api-reference/src/components/Content/Schema.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-// TODO: value.xml isn’t rendered yet
 import Schema from './Schema.vue'
 
 withDefaults(
@@ -11,6 +10,39 @@ withDefaults(
     level: 0,
   },
 )
+
+const descriptions: Record<string, Record<string, string>> = {
+  number: {
+    _default: 'Any numbers.',
+    float: 'Floating-point numbers.',
+    double: 'Floating-point numbers with double precision.',
+  },
+  integer: {
+    _default: 'Integer numbers.',
+    int32: 'Signed 32-bit integers (commonly used integer type).',
+    int64: 'Signed 64-bit integers (long type).',
+  },
+  string: {
+    'date':
+      'full-date notation as defined by RFC 3339, section 5.6, for example, 2017-07-21',
+    'date-time':
+      'the date-time notation as defined by RFC 3339, section 5.6, for example, 2017-07-21T17:32:28Z',
+    'password': 'a hint to UIs to mask the input',
+    'byte': 'base64-encoded characters, for example, U3dhZ2dlciByb2Nrcw==',
+    'binary': 'binary data, used to describe files',
+  },
+}
+
+const generatePropertyDescription = (property: {
+  type: string
+  format?: string
+}) => {
+  if (descriptions[property.type]) {
+    return descriptions[property.type][property.format || '_default']
+  }
+}
+
+const rules = ['oneOf', 'anyOf', 'allOf', 'not']
 </script>
 <template>
   <div class="schema">
@@ -24,7 +56,7 @@ withDefaults(
           <template v-if="value.type === 'object'"> {} </template>
           <template v-if="value.type === 'array'"> [] </template>
         </span>
-        <template v-if="value?.xml?.name">
+        <template v-if="value?.xml?.name && value?.xml?.name !== '##default'">
           &lt;{{ value?.xml?.name }} /&gt;
         </template>
         <template v-else>
@@ -48,10 +80,35 @@ withDefaults(
             v-if="value.properties[property].type"
             class="property-type">
             <template v-if="value.properties[property].type !== 'object'">
-              {{ value.properties[property].type }}
+              <template
+                v-if="
+                  value.properties[property]?.items &&
+                  !['object'].includes(value.properties[property].items.type)
+                ">
+                {{ value.properties[property].type }}
+                {{ value.properties[property].items.type }}[]
+              </template>
+              <template v-else>
+                {{ value.properties[property].type }}
+              </template>
+              <template
+                v-if="
+                  value.properties[property].minItems ||
+                  value.properties[property].maxItems
+                ">
+                {{ value.properties[property].minItems }}..{{
+                  value.properties[property].maxItems
+                }}
+              </template>
+              <template v-if="value.properties[property].uniqueItems">
+                unique!
+              </template>
             </template>
             <template v-if="value.properties[property].format">
               &middot; {{ value.properties[property].format }}
+            </template>
+            <template v-if="value.properties[property].enum">
+              &middot; enum
             </template>
           </div>
           <div
@@ -61,12 +118,41 @@ withDefaults(
               example: {{ value.properties[property].example }}
             </code>
           </div>
+          <template
+            v-for="rule in rules"
+            :key="rule">
+            <div
+              v-if="value.properties[property][rule]"
+              class="property-rule">
+              {{ rule }}
+            </div>
+          </template>
         </div>
+        <!-- Description -->
         <div
           v-if="value.properties[property].description"
           class="property-description">
           {{ value.properties[property].description }}
         </div>
+        <div
+          v-if="generatePropertyDescription(value.properties[property])"
+          class="property-description">
+          {{ generatePropertyDescription(value.properties[property]) }}
+        </div>
+        <!-- Enum -->
+        <div
+          v-if="value.properties[property].enum"
+          class="property-enum">
+          <ul class="property-enum-values">
+            <li
+              v-for="enumValue in value.properties[property].enum"
+              :key="enumValue"
+              class="property-enum-value">
+              {{ enumValue }}
+            </li>
+          </ul>
+        </div>
+        <!-- Object -->
         <div
           v-if="value.properties[property].properties"
           class="children">
@@ -75,7 +161,31 @@ withDefaults(
             :level="level + 1"
             :value="value.properties[property]" />
         </div>
-        <!-- TODO: .items for array -->
+        <!-- Array of objects -->
+        <template v-if="value.properties[property].items">
+          <div
+            v-if="['object'].includes(value.properties[property].items.type)"
+            class="children">
+            <Schema
+              v-if="level < 3"
+              :level="level + 1"
+              :value="value.properties[property].items" />
+          </div>
+        </template>
+        <!-- oneOf -->
+        <template
+          v-for="rule in rules"
+          :key="rule">
+          <div
+            v-if="value.properties[property][rule]"
+            class="rule">
+            <Schema
+              v-for="(schema, index) in value.properties[property][rule]"
+              :key="index"
+              :level="level + 1"
+              :value="schema" />
+          </div>
+        </template>
       </div>
     </div>
   </div>
@@ -117,6 +227,12 @@ withDefaults(
 .property-description {
   margin-top: 12px;
   color: var(--theme-color-2, var(--default-theme-color-2));
+}
+
+.property-rule {
+  font-family: var(--theme-font-code, var(--default-theme-font-code));
+  background: var(--theme-color-orange, var(--default-theme-color-orange));
+  padding: 0 6px;
 }
 
 .property:not(:last-of-type) {
@@ -163,11 +279,31 @@ withDefaults(
   padding: 12px 12px;
   background: var(--theme-background-4, var(--default-theme-background-4));
   padding: 2px 5px;
-  border-radius: var(--theme-radius, var(--default-theme-radius));
   font-family: var(--theme-font-code, var(--default-theme-font-code));
   font-size: var(
     --default-theme-font-size-5,
     var(--default-default-theme-font-size-5)
   );
+}
+
+.rule {
+  margin-top: 12px;
+  padding: 0 12px 12px;
+  border-radius: var(--theme-radius-lg, var(--default-theme-radius-lg));
+  background: var(--theme-background-4, var(--default-theme-background-4));
+  border: 2px dotted
+    var(--theme-border-color, var(--default-theme-border-color));
+}
+
+.property-enum-value {
+  padding: 3px 0;
+}
+.property-enum-value::before {
+  content: '▶';
+  margin-right: 6px;
+  color: var(--theme-color-3, var(--default-theme-color-3));
+}
+.property-enum-values {
+  margin-top: 12px;
 }
 </style>
