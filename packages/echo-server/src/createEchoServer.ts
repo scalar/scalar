03235b62e0a324c868a13e082c66fb2aa4bc65e2
Hans Pagel <mail@hanspagel.com>
2023-10-18 13:20:27 +0200

    feat: parse authorization header

--- packages/echo-server/src/createEchoServer.ts
+++ packages/echo-server/src/createEchoServer.ts
@@ -20,8 +20,39 @@ export const createEchoServer = () => {
   app.all('/*', async (req, res) => {
     console.log(`${req.method} ${req.path}`)
 
+    let authentication = {}
+
+    const authorizationHeader = req.headers.authorization
+
+    if (authorizationHeader) {
+      // if value starts with "Basic "
+      if (authorizationHeader.startsWith('Basic ')) {
+        const token = authorizationHeader.split(' ')[1]
+
+        authentication = {
+          authentication: {
+            type: 'http.basic',
+            token,
+            value: Buffer.from(token, 'base64').toString('utf8'),
+          },
+        }
+      }
+
+      if (authorizationHeader.startsWith('Bearer ')) {
+        const token = authorizationHeader.split(' ')[1]
+
+        authentication = {
+          authentication: {
+            type: 'http.bearer',
+            token,
+          },
+        }
+      }
+    }
+
     res.send({
       headers: req.headers,
+      ...authentication,
       cookies: req.cookies,
       method: req.method,
       path: req.path,
