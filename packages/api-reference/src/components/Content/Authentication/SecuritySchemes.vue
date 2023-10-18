0134c8840094ce83e892f11a3e844334c19d948f
Hans Pagel <mail@hanspagel.com>
2023-10-18 14:46:04 +0200

    chore: copy select styles (wip)

--- packages/api-reference/src/components/Content/Authentication/SecuritySchemes.vue
+++ packages/api-reference/src/components/Content/Authentication/SecuritySchemes.vue
@@ -31,6 +31,19 @@ onMounted(() => {
 <template>
   <div class="security-schemes">
     <div class="security-schemes-selector">
+      <!-- <span>
+        {{ authentication.securitySchemeKey }}
+        <svg
+          fill="none"
+          height="100%"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="m19.5 10-7.5 7.5-7.5-7.5"
+            xmlns="http://www.w3.org/2000/svg"></path>
+        </svg>
+      </span> -->
       <select
         @input="handleAuthenticationTypeInput"
         @value="authentication.securitySchemeKey">
@@ -74,4 +87,36 @@ onMounted(() => {
   gap: 12px;
   flex-direction: column;
 }
+
+/* .security-schemes-selector span {
+  background: transparent;
+  padding: 2px 0;
+  border-radius: 3px;
+  font-size: var(--theme-small, var(--default-theme-small));
+  pointer-events: none;
+  color: var(--theme-color-2, var(--default-theme-color-2));
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+}
+.security-schemes-selector:hover span {
+  color: var(--theme-color-1, var(--default-theme-color-1));
+  border-color: currentColor;
+}
+.security-schemes-selector span svg {
+  width: 15px;
+  height: 15px;
+  margin-left: 3px;
+}
+
+.security-schemes-selector select {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  cursor: pointer;
+} */
 </style>
