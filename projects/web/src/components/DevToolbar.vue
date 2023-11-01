1c1870f74cbc4d4bf8ff3589ec2b4cd2a1c3d847
Brynn Hawker <brynn@hawker.me>
2023-11-01 12:28:02 -0700

    Hide toolbar button

--- projects/web/src/components/DevToolbar.vue
+++ projects/web/src/components/DevToolbar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { type ReferenceConfiguration } from '@scalar/api-reference'
 import { availableThemes } from '@scalar/themes'
-import { computed } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 const props = defineProps<{
   modelValue: ReferenceConfiguration
@@ -15,9 +15,23 @@ const configuration = computed({
   get: () => props.modelValue,
   set: (v: ReferenceConfiguration) => emit('update:modelValue', v),
 })
+const showToolbar = ref(true)
+
+const docStyle = document.documentElement.style
+
+watch(
+  showToolbar,
+  (show) => {
+    if (show) docStyle.setProperty('--theme-header-height', '40px')
+    else docStyle.removeProperty('--theme-header-height')
+  },
+  { immediate: true },
+)
 </script>
 <template>
-  <header class="references-dev-header">
+  <header
+    v-if="showToolbar"
+    class="references-dev-header">
     <div class="references-dev-title">Dev Toolbar</div>
     <div class="references-dev-options">
       <div>
@@ -43,13 +57,24 @@ const configuration = computed({
           </option>
         </select>
       </div>
+      <button
+        class="references-dev-hide-toolbar"
+        type="button"
+        @click="showToolbar = false">
+        Hide
+      </button>
     </div>
   </header>
+
+  <button
+    v-else
+    class="references-dev-show-toolbar"
+    type="button"
+    @click="showToolbar = true">
+    Dev Toolbar
+  </button>
 </template>
-<style>
-:root {
-  --theme-header-height: 40px;
-}
+<style scoped>
 .references-dev-header {
   display: flex;
   justify-content: space-between;
@@ -58,8 +83,6 @@ const configuration = computed({
   width: 100%;
   height: var(--refs-header-height);
 
-  padding: 0px 8px;
-
   background: var(--default-theme-background-1);
   border-bottom: 1px solid var(--default-theme-border-color);
 }
@@ -67,6 +90,7 @@ const configuration = computed({
   font-weight: bold;
   display: flex;
   align-items: center;
+  padding: 0px 8px;
 
   min-width: 0;
   flex: 1;
@@ -86,5 +110,28 @@ const configuration = computed({
   display: flex;
   align-items: center;
   gap: 4px;
+}
+
+.references-dev-show-toolbar {
+  position: fixed;
+  z-index: 1000;
+  top: 8px;
+  right: 8px;
+
+  padding: 4px 8px;
+
+  font-size: var(--default-theme-small);
+  font-weight: 500;
+
+  color: var(--default-theme-color-1);
+  background: var(--default-theme-background-1);
+  border: 1px solid var(--default-theme-border-color);
+  border-radius: var(--default-theme-radius);
+  box-shadow: var(--default-theme-shadow-1);
+}
+
+.references-dev-show-toolbar:hover,
+.references-dev-hide-toolbar:hover {
+  background: var(--default-theme-background-2);
 }
 </style>
