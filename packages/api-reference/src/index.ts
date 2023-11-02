03d7afc6f14486d36c787b24dcbfd45338574af9
Amrit <amrit@hockey-community.com>
2023-11-02 14:37:17 -0700

    feat: export ApiReferenceLayout

--- packages/api-reference/src/index.ts
+++ packages/api-reference/src/index.ts
@@ -3,6 +3,7 @@ export {
   useApiClientStore,
 } from './components/ApiClientModal.vue'
 export { default as ApiReference } from './components/ApiReference.vue'
+export { default as ApiReferenceLayout } from './components/ApiReferenceLayout.vue'
 export { default as Sidebar } from './components/Sidebar.vue'
 export { default as RenderedReference } from './components/Content/Content.vue'
 export { default as DarkModeToggle } from './components/DarkModeToggle.vue'
