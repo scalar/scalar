339fe00efc50ea3a49a7ed4c10317a330ece2a37
Geoff Scott <geoffgscott@gmail.com>
2023-11-03 15:21:27 -0700

    feat: Cleanup swagger packages

--- packages/swagger-editor/vite.config.ts
+++ packages/swagger-editor/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['vue', 'yjs', '@codemirror/state', '@hocuspocus/provider'],
+      external: ['vue', 'yjs', '@hocuspocus/provider'],
       output: {
         assetFileNames: (assetInfo) => {
           if (assetInfo.name === 'index.css') {
