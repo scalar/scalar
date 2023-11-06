a08c9ed07a8aab8a8af57e479c95f521b97d5d50
Hans Pagel <mail@hanspagel.com>
2023-11-06 11:12:52 +0100

    chore: add ssg example

---
+++ projects/ssg/src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { RenderedReference, Sidebar, type Spec } from '@scalar/api-reference'
+import { reactive } from 'vue'
+
+const reactiveSpec = reactive<Spec>({
+  info: {
+    title: '',
+    description: '',
+    termsOfService: '',
+    version: '',
+    license: {
+      name: '',
+      url: '',
+    },
+    contact: {
+      email: '',
+    },
+  },
+  externalDocs: {
+    description: '',
+    url: '',
+  },
+  servers: [],
+  tags: [],
+})
+</script>
+
+<template>
+  <div>
+    <Sidebar
+      :isDarkMode="true"
+      :parsedSpec="reactiveSpec"
+      @toggleDarkMode="() => {}" />
+    <RenderedReference
+      class="references-rendered"
+      :parsedSpec="reactiveSpec"
+      :rawSpec="''"
+      :ready="true" />
+  </div>
+</template>
