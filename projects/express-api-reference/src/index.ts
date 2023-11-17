d69d0e58aebb9744829ab3dd6b2711383810d041
Hans Pagel <mail@hanspagel.com>
2023-11-17 13:59:59 +0100

    chore: add Express demo

---
+++ projects/express-api-reference/src/index.ts
@@ -0,0 +1,43 @@
+import Express from 'express'
+import swaggerJsdoc from 'swagger-jsdoc'
+
+// Initialize Express
+const app = Express()
+
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Get a mysterious string.
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
+app.get('/foobar', (req, res) => {
+  res.send('Hello World!')
+})
+
+const OpenApiSpecification = swaggerJsdoc({
+  failOnErrors: true,
+  definition: {
+    openapi: '3.1.0',
+    info: {
+      title: 'Example',
+      description:
+        'The `@scalar/express-api-reference` middleware renders a beautiful API reference based on your OpenAPI specification.',
+      version: '1.0.0',
+    },
+  },
+  // Files containing annotations
+  apis: ['./src/*.ts'],
+})
+
+// TODO: Remove later
+app.get('/swagger.json', (req, res) => {
+  res.json(OpenApiSpecification)
+})
+
+// Listen
+app.listen(5056, () => {
+  console.log('💻 Express listening on http://localhost:5056')
+})
---
+++ projects/express-api-reference/src/routes/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
