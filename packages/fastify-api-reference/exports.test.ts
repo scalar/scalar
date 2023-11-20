b68cfa2b52a66742cdfafa6214fc0676077a7f28
Marc Laventure <marc@scalar.com>
2023-11-20 10:51:43 -0800

    chore: remove legacy file (#518)
    * chore: remove legacy file
    
    * chore: rename files

--- packages/fastify-api-reference/exports.test.ts
+++ packages/fastify-api-reference/exports.test.ts
@@ -11,7 +11,7 @@ describe('exports', () => {
       fastify.register(import('./dist/index.js'), {
         routePrefix: '/foobar',
         apiReference: {
-          spec: { url: '/scalar.json' },
+          spec: { url: '/swagger.json' },
         },
       })
 
@@ -33,7 +33,7 @@ describe('exports', () => {
       fastify.register(require('./dist/index.cjs'), {
         routePrefix: '/foobar',
         apiReference: {
-          spec: { url: '/scalar.json' },
+          spec: { url: '/swagger.json' },
         },
       })
 
