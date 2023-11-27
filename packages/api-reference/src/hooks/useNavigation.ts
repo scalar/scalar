9f5dbf1b191f2711abb5c1b4a6a910b02282be25
Hans Pagel <mail@hanspagel.com>
2023-11-27 14:58:47 +0100

    refactor: add comments to useNavigation

--- packages/api-reference/src/hooks/useNavigation.ts
+++ packages/api-reference/src/hooks/useNavigation.ts
@@ -14,25 +14,7 @@ import {
 import { useTemplateStore } from '../stores/template'
 import type { Spec, Tag, TransformedOperation } from '../types'
 
-export type SidebarIdVisibility = Record<string, boolean>
-
-const sidebarIdVisibility = reactive<SidebarIdVisibility>({})
-
-function setItemIdVisibility(id: string, visible: boolean) {
-  sidebarIdVisibility[id] = visible
-}
-
-// Expand/collapse sidebar items
-const { setCollapsedSidebarItem } = useTemplateStore()
-
-const { state } = useApiClientStore()
-
-const moreThanOneDefaultTag = (tags?: Tag[]) =>
-  tags?.length !== 1 || tags[0].name !== 'default' || tags[0].description !== ''
-
-const isVisible = (id: string) => sidebarIdVisibility[id] ?? false
-
-type SidebarEntry = {
+export type SidebarEntry = {
   id: string
   title: string
   type: 'Page' | 'Folder'
@@ -41,8 +23,31 @@ type SidebarEntry = {
   httpVerb?: string
 }
 
+// Track the parsed spec
 const parsedSpec = ref<Spec | undefined>(undefined)
 
+// Track which sidebar items are visible
+type SidebarIdVisibility = Record<string, boolean>
+
+const sidebarIdVisibility = reactive<SidebarIdVisibility>({})
+
+function setItemIdVisibility(id: string, visible: boolean) {
+  sidebarIdVisibility[id] = visible
+}
+
+const isVisible = (id: string) => sidebarIdVisibility[id] ?? false
+
+// Expand/collapse sidebar items
+const { setCollapsedSidebarItem } = useTemplateStore()
+
+// Check whether the API client is visible
+const { state } = useApiClientStore()
+
+// Check whether there is more than one default tag
+const moreThanOneDefaultTag = (tags?: Tag[]) =>
+  tags?.length !== 1 || tags[0].name !== 'default' || tags[0].description !== ''
+
+// Track headings in the spec description
 const headings = ref<any[]>([])
 
 const updateHeadings = async (description: string) => {
@@ -51,6 +56,96 @@ const updateHeadings = async (description: string) => {
 
   return newHeadings.filter((heading) => heading.depth === lowestLevel)
 }
+
+// Create the list of sidebar items from the given spec
+const items = computed((): SidebarEntry[] => {
+  // Introduction
+  const headingEntries: SidebarEntry[] = headings.value.map((heading) => {
+    return {
+      id: getHeadingId(heading),
+      title: heading.value.toUpperCase(),
+      type: 'Page',
+    }
+  })
+
+  // Tags & Operations
+  const firstTag = parsedSpec.value?.tags?.[0]
+
+  const operationEntries: SidebarEntry[] | undefined =
+    firstTag &&
+    moreThanOneDefaultTag(parsedSpec.value?.tags) &&
+    firstTag.operations?.length > 0
+      ? parsedSpec.value?.tags?.map((tag: Tag) => {
+          return {
+            id: getTagSectionId(tag),
+            title: tag.name.toUpperCase(),
+            type: 'Folder',
+            children: tag.operations?.map((operation: TransformedOperation) => {
+              return {
+                id: getOperationSectionId(operation, tag),
+                title: operation.name,
+                type: 'Page',
+                httpVerb: operation.httpVerb,
+                select: () => {
+                  if (state.showApiClient) {
+                    showItemInClient(operation)
+                  }
+                },
+              }
+            }),
+          }
+        })
+      : firstTag?.operations?.map((operation) => {
+          return {
+            id: getOperationSectionId(operation, firstTag),
+            title: operation.name,
+            type: 'Page',
+            httpVerb: operation.httpVerb,
+            select: () => {
+              if (state.showApiClient) {
+                showItemInClient(operation)
+              }
+            },
+          }
+        })
+
+  // Models
+  const modelEntries: SidebarEntry[] = hasModels(parsedSpec.value)
+    ? [
+        {
+          id: getModelSectionId(),
+          title: 'MODELS',
+          type: 'Folder',
+          children: Object.keys(
+            parsedSpec.value?.components?.schemas ?? {},
+          ).map((name) => {
+            return {
+              id: getModelSectionId(name),
+              title: name,
+              type: 'Page',
+            }
+          }),
+        },
+      ]
+    : []
+
+  return [...headingEntries, ...(operationEntries ?? []), ...modelEntries]
+})
+
+// Track the active sidebar item
+const activeItemId = computed(() => {
+  const flattenedItems = items.value.reduce((acc, item) => {
+    acc.push(item)
+
+    if (item.children) {
+      acc.push(...item.children)
+    }
+
+    return acc
+  }, [] as SidebarEntry[])
+
+  return flattenedItems.find((item) => isVisible(item.id))?.id ?? null
+})
 
 export function useNavigation(options?: { parsedSpec: Spec }) {
   if (options?.parsedSpec) {
@@ -82,104 +177,15 @@ export function useNavigation(options?: { parsedSpec: Spec }) {
         return (headings.value = await updateHeadings(description))
       },
     )
-  }
 
-  const items = computed((): SidebarEntry[] => {
-    // Introduction
-    const headingEntries: SidebarEntry[] = headings.value.map((heading) => {
-      return {
-        id: getHeadingId(heading),
-        title: heading.value.toUpperCase(),
-        type: 'Page',
+    // Watch the active item and update the URL
+    watch(activeItemId, (id) => {
+      if (id) {
+        const newUrl = `${window.location.origin}${window.location.pathname}#${id}`
+        window.history.replaceState({}, '', newUrl)
       }
     })
-
-    // Tags & Operations
-    const firstTag = parsedSpec.value?.tags?.[0]
-
-    const operationEntries: SidebarEntry[] | undefined =
-      firstTag &&
-      moreThanOneDefaultTag(parsedSpec.value?.tags) &&
-      firstTag.operations?.length > 0
-        ? parsedSpec.value?.tags?.map((tag: Tag) => {
-            return {
-              id: getTagSectionId(tag),
-              title: tag.name.toUpperCase(),
-              type: 'Folder',
-              children: tag.operations?.map(
-                (operation: TransformedOperation) => {
-                  return {
-                    id: getOperationSectionId(operation, tag),
-                    title: operation.name,
-                    type: 'Page',
-                    httpVerb: operation.httpVerb,
-                    select: () => {
-                      if (state.showApiClient) {
-                        showItemInClient(operation)
-                      }
-                    },
-                  }
-                },
-              ),
-            }
-          })
-        : firstTag?.operations?.map((operation) => {
-            return {
-              id: getOperationSectionId(operation, firstTag),
-              title: operation.name,
-              type: 'Page',
-              httpVerb: operation.httpVerb,
-              select: () => {
-                if (state.showApiClient) {
-                  showItemInClient(operation)
-                }
-              },
-            }
-          })
-
-    // Models
-    const modelEntries: SidebarEntry[] = hasModels(parsedSpec.value)
-      ? [
-          {
-            id: getModelSectionId(),
-            title: 'MODELS',
-            type: 'Folder',
-            children: Object.keys(
-              parsedSpec.value?.components?.schemas ?? {},
-            ).map((name) => {
-              return {
-                id: getModelSectionId(name),
-                title: name,
-                type: 'Page',
-              }
-            }),
-          },
-        ]
-      : []
-
-    return [...headingEntries, ...(operationEntries ?? []), ...modelEntries]
-  })
-
-  const activeItemId = computed(() => {
-    const flattenedItems = items.value.reduce((acc, item) => {
-      acc.push(item)
-
-      if (item.children) {
-        acc.push(...item.children)
-      }
-
-      return acc
-    }, [] as SidebarEntry[])
-
-    return flattenedItems.find((item) => isVisible(item.id))?.id ?? null
-  })
-
-  watch(activeItemId, (id) => {
-    if (id) {
-      const newUrl = `${window.location.origin}${window.location.pathname}#${id}`
-      window.history.replaceState({}, '', newUrl)
-    }
-  })
+  }
 
   return {
     items,
