daa69284542ff0d41b3b78e69b65a8c86a4a4ced
Hans Pagel <hanspagel@users.noreply.github.com>
2023-12-04 16:18:21 +0100

    chore: use fastify-html to send HTML (#621)
    * chore: use fastify-html to send HTML
    
    * docs(changeset): chore: use fastify-html to send HTML
    
    * fix: ignore missing types of fastify-html
    
    * chore: disable CommonJS build
    
    * docs(changeset): chore: disable CommonJS build, don’t support require() anymore

---
+++ .changeset/empty-poets-walk.md
@@ -0,0 +1,5 @@
+---
+'@scalar/fastify-api-reference': patch
+---
+
+chore: disable CommonJS build, don’t support require() anymore
--- packages/fastify-api-reference/vite.config.ts
+++ packages/fastify-api-reference/vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
       entry: 'src/index.ts',
       name: '@scalar/fastify-api-reference',
       fileName: 'index',
-      formats: ['es', 'cjs'],
+      formats: ['es'],
     },
     // We don’t have any production dependencies.
     // rollupOptions: {
