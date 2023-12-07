45f9e5aac387e523fe5b54568d3ef7cf7f540381
marclave <marc@scalar.com>
2023-12-07 06:17:08 +0100

    fix: handle root path in request generation

--- packages/api-reference/src/helpers/getUrlFromServerState.ts
+++ packages/api-reference/src/helpers/getUrlFromServerState.ts
@@ -2,10 +2,16 @@ import type { ServerState } from '../types'
 import { replaceVariables } from './replaceVariables'
 
 export function getUrlFromServerState(state: ServerState) {
-  const url =
+  let url =
     state.selectedServer === null
       ? state?.servers?.[0]?.url ?? undefined
       : state?.servers?.[state.selectedServer]?.url
+
+  if (url === '/') {
+    url = window.location.origin
+  } else if (url === '//') {
+    url = window.location.origin
+  }
 
   return url ? replaceVariables(url, state?.variables) : undefined
 }
