e47f08dbd511f3c3d4c0792e27f883ea2fd91b0e
Brynn Hawker <brynn@hawker.me>
2023-12-07 17:09:55 -0800

    chore: prettier formatting

--- packages/components/src/scripts/typegen.ts
+++ packages/components/src/scripts/typegen.ts
@@ -13,11 +13,11 @@ const fileNames = readdirSync(iconFolder).filter((fileName) =>
 
 // Write icons to a typescript file for exporting
 let writeStr = 'export const ICONS = [\n'
-fileNames.forEach((fileName, index) => {
+fileNames.forEach((fileName) => {
   const icon = fileName.replace(svgRegex, '')
   writeStr += `  '${icon}',\n`
 })
-writeStr += '] as const'
+writeStr += '] as const\n'
 
 writeFile(iconsFile, writeStr, (err) => {
   if (err) console.error(err)
