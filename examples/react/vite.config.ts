dab370233502ccfe5a9e1f95b9e8d1a7d7d007fb
Hans Pagel <mail@hanspagel.com>
2023-12-13 12:15:43 +0100

    chore: ignore snippetz in the alias config

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
         // Resolve the uncompiled source code for all @scalar packages
         // @scalar/* -> packages/*/
         // (not @scalar/*/style.css)
-        find: /^@scalar\/(?!(components\/style\.css|components\b))(.+)/,
+        find: /^@scalar\/(?!(snippetz|components\/style\.css|components\b))(.+)/,
         replacement: path.resolve(__dirname, '../../packages/$2/src/index.ts'),
       },
     ],
