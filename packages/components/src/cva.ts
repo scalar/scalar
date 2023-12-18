108c69705c4a0df13105aff7691feb139027dc50
Brynn Hawker <brynn@hawker.me>
2023-12-18 11:33:03 -0800

    fix: merge reset into twmerge

--- packages/components/src/cva.ts
+++ packages/components/src/cva.ts
@@ -4,13 +4,20 @@ import { extendTailwindMerge } from 'tailwind-merge'
 /**
  * Tailwind Merge Config
  *
- * By default tailwind merge only knows about the default tailwind clasess
+ * By default tailwind merge only knows about the default tailwind classes
  * this is because it does not load in the tailwind config at runtime (perf reasons)
  * we must specify any custom classes if they are getting overwritten
  *
  * https://github.com/dcastil/tailwind-merge/blob/v2.0.0/docs/configuration.md#class-groups
  */
-const tw = extendTailwindMerge({})
+const tw = extendTailwindMerge<'scalar-reset'>({
+  extend: {
+    classGroups: {
+      // Add scalar-reset as a custom set of classes to be deduped by tailwind-merge
+      'scalar-reset': ['scalar-reset'],
+    },
+  },
+})
 
 /**
  * CVA Config
@@ -19,7 +26,7 @@ const tw = extendTailwindMerge({})
  */
 const { cva, cx, compose } = defineConfig({
   hooks: {
-    onComplete: (className) => `${tw(className)} scalar-reset`,
+    onComplete: (className) => `${tw(className, 'scalar-reset')}`,
   },
 })
 
