d43c81bb20356e3a4e7d0b9b9025edf0348974bb
marclave <marc@scalar.com>
2024-01-05 16:29:48 -0800

    feat: add consistent select styling

--- packages/api-reference/src/components/Content/ReferenceEndpoint/RequestBody.vue
+++ packages/api-reference/src/components/Content/ReferenceEndpoint/RequestBody.vue
@@ -5,6 +5,7 @@ import type { ContentType, RequestBody } from '../../../types'
 import Schema from '../Schema.vue'
 
 const prop = defineProps<{ requestBody?: RequestBody }>()
+
 const selectedContentType = ref<ContentType>('application/json')
 if (prop.requestBody?.content) {
   const keys = Object.keys(prop?.requestBody?.content)
@@ -17,17 +18,19 @@ if (prop.requestBody?.content) {
   <div v-if="prop?.requestBody">
     <div class="request-body-title">
       <slot name="title" />
-      <select
-        v-if="prop?.requestBody"
-        v-model="selectedContentType"
-        style="display: block">
-        <option
-          v-for="(value, key) in prop.requestBody?.content"
-          :key="key"
-          :value="key">
-          {{ key }}
-        </option>
-      </select>
+      <div class="request-body-title-select">
+        <span>{{ selectedContentType }}</span>
+        <select
+          v-if="prop?.requestBody"
+          v-model="selectedContentType">
+          <option
+            v-for="(value, key) in prop.requestBody?.content"
+            :key="key"
+            :value="key">
+            {{ key }}
+          </option>
+        </select>
+      </div>
     </div>
     <div
       v-if="prop?.requestBody.content?.[selectedContentType]"
@@ -42,6 +45,8 @@ if (prop.requestBody?.content) {
 
 <style scoped>
 .request-body-title {
+  display: flex;
+  align-items: center;
   font-size: var(--theme-heading-4, var(--default-theme-heading-4));
   font-weight: var(--theme-semibold, var(--default-theme-semibold));
   color: var(--theme-color-1, var(--default-theme-color-1));
@@ -50,5 +55,51 @@ if (prop.requestBody?.content) {
   padding-bottom: 12px;
   border-bottom: 1px solid
     var(--theme-border-color, var(--default-theme-border-color));
+}
+.request-body-title-select {
+  position: relative;
+  padding-left: 9px;
+  height: fit-content;
+  color: var(--theme-color-2, var(--default-theme-color-2));
+  font-size: var(--theme-font-size-3, var(--default-theme-font-size-3));
+  display: flex;
+  align-items: center;
+}
+.request-body-title-select span {
+  display: flex;
+  align-items: center;
+}
+.request-body-title-select:after {
+  content: '';
+  width: 7px;
+  height: 7px;
+  transform: rotate(45deg) translate3d(-2px, -4px, 0);
+  display: block;
+  margin-left: 7px;
+  box-shadow: 1px 1px 0 currentColor;
+}
+.request-body-title-select select {
+  border: none;
+  outline: none;
+  cursor: pointer;
+  background: var(--theme-background-3, var(--default-theme-background-3));
+  box-shadow: -2px 0 0 0
+    var(--theme-background-3, var(--default-theme-background-3));
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+}
+.request-body-title-select:hover {
+  color: var(--theme-color-1, var(--default-theme-color-1));
+}
+@media (max-width: 460px) {
+  .request-body-title-select {
+    margin-left: auto;
+    padding-right: 3px;
+  }
 }
 </style>
