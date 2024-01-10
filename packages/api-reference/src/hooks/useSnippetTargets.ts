a75a279b81b56a5d058ec7b65136cedea5e9b17c
Brynn Hawker <brynn@hawker.me>
2024-01-10 18:55:33 +1300

    feat: allow configuration of excluded clients

--- packages/api-reference/src/hooks/useSnippetTargets.ts
+++ packages/api-reference/src/hooks/useSnippetTargets.ts
@@ -1,53 +1,62 @@
 import { availableTargets as allTargets } from 'httpsnippet-lite'
 import { computed, readonly, ref } from 'vue'
 
-const DEFAULT_EXCLUDED_CLIENTS = ['fetch', 'unirest'] as const
+const DEFAULT_EXCLUDED_CLIENTS = ['unirest'] as const
 
 const excludedClients = ref<string[]>([...DEFAULT_EXCLUDED_CLIENTS])
 
 export function useSnippetTargets() {
   const availableTargets = computed(() =>
-    allTargets().map((target) => {
-      // Remove excluded clients
-      target.clients = target.clients.filter(
-        (client) => !excludedClients.value.includes(client.key),
-      )
+    allTargets()
+      .map((target) => {
+        // Remove fetch because we want a different one
+        target.clients = target.clients.filter(
+          (client) => client.key !== 'fetch',
+        )
 
-      // Node.js
-      if (target.key === 'node') {
-        target.default = 'undici'
+        // Node.js
+        if (target.key === 'node') {
+          target.default = 'undici'
 
-        target.clients.unshift({
-          description:
-            'A browser-compatible implementation of the fetch() function.',
-          key: 'fetch',
-          link: 'https://nodejs.org/dist/latest/docs/api/globals.html#fetch',
-          title: 'fetch',
-        })
+          target.clients.unshift({
+            description:
+              'A browser-compatible implementation of the fetch() function.',
+            key: 'fetch',
+            link: 'https://nodejs.org/dist/latest/docs/api/globals.html#fetch',
+            title: 'fetch',
+          })
 
-        target.clients.unshift({
-          description: 'An HTTP/1.1 client, written from scratch for Node.js.',
-          key: 'undici',
-          link: 'https://github.com/nodejs/undici',
-          title: 'undici',
-        })
-      }
+          target.clients.unshift({
+            description:
+              'An HTTP/1.1 client, written from scratch for Node.js.',
+            key: 'undici',
+            link: 'https://github.com/nodejs/undici',
+            title: 'undici',
+          })
+        }
 
-      // JS
-      if (target.key === 'javascript') {
-        target.default = 'fetch'
+        // JS
+        if (target.key === 'javascript') {
+          target.default = 'fetch'
 
-        target.clients.unshift({
-          description:
-            'A browser-compatible implementation of the fetch() function.',
-          key: 'fetch',
-          link: 'https://nodejs.org/dist/latest/docs/api/globals.html#fetch',
-          title: 'fetch',
-        })
-      }
+          target.clients.unshift({
+            description:
+              'A browser-compatible implementation of the fetch() function.',
+            key: 'fetch',
+            link: 'https://nodejs.org/dist/latest/docs/api/globals.html#fetch',
+            title: 'fetch',
+          })
+        }
 
-      return target
-    }),
+        // Filter out excluded clients
+        target.clients = target.clients.filter(
+          (client) => !excludedClients.value.includes(client.key),
+        )
+
+        return target
+      })
+      // Remove targets with no clients
+      .filter((target) => target.clients.length),
   )
 
   return {
