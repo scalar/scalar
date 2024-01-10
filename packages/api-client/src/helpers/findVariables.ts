bbc963c3020ab9b11a2ef8cff6360bb35ac16c38
Brynn Hawker <brynn@hawker.me>
2024-01-11 10:43:30 +1300

    fix: remove regex lookbehind

--- packages/api-client/src/helpers/findVariables.ts
+++ packages/api-client/src/helpers/findVariables.ts
@@ -6,7 +6,7 @@ export const findVariables = (value: string) => {
   // Wrapped in single or double curly braces.
   // Ignores whitespace
   // Works with lowercase, uppercase, numbers, dashes, underscores
-  const regex = /(?<=\{)\s*\w+\s*(?=\})/g
+  const regex = /(?=\{)\s*\w+\s*(?=\})/g
 
   return value.match(regex)?.map((variable) => variable.trim()) || []
 }
