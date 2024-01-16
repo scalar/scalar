0c5c09a19937f909f05799c19b68f6f58fe73f81
Hans Pagel <mail@hanspagel.com>
2024-01-16 13:53:58 +0100

    refactor: there’s only one body parameter, no need to merge

--- packages/api-reference/src/helpers/getRequestBodyFromOperation.ts
+++ packages/api-reference/src/helpers/getRequestBodyFromOperation.ts
@@ -29,23 +29,41 @@ export function getRequestBodyFromOperation(operation: TransformedOperation) {
 
   // TODO:
   // * Add support for formData
-
+  /**
+   * Body Parameters (Swagger 2.0)
+   *
+   * ”The payload that's appended to the HTTP request. Since there can only be one payload, there can only
+   * be one body parameter. The name of the body parameter has no effect on the parameter itself and is used
+   * for documentation purposes only. Since Form parameters are also in the payload, body and form
+   * parameters cannot exist together for the same operation.”
+   */
   const bodyParameters = getParametersFromOperation(operation, 'body', false)
 
   if (bodyParameters.length > 0) {
-    const allBodyParameters = mergeAllObjects(
-      bodyParameters.map(
-        (parameter) => parameter.value as Record<string, any>[],
-      ),
-    )
-
     return {
       postData: {
         mimeType: 'application/json',
-        text: prettyPrintJson(allBodyParameters),
+        text: prettyPrintJson(bodyParameters[0]),
       },
     }
   }
+
+  /**
+   * FormData Parameters (Swagger 2.0)
+   *
+   * “Form - Used to describe the payload of an HTTP request when either application/x-www-form-urlencoded,
+   * multipart/form-data or both are used as the content type of the request (in Swagger's definition, the
+   * consumes property of an operation). This is the only parameter type that can be used to send files,
+   * thus supporting the file type. Since form parameters are sent in the payload, they cannot be declared
+   * together with a body parameter for the same operation. Form parameters have a different format based on
+   * the content-type used (for further details, consult http://www.w3.org/TR/html401/interact/forms.html#h-17.13.4):
+   * - application/x-www-form-urlencoded - Similar to the format of Query parameters but as a payload.
+   *   For example, foo=1&bar=swagger - both foo and bar are form parameters. This is normally used for simple
+   *   parameters that are being transferred.
+   * - multipart/form-data - each parameter takes a section in the payload with an internal header.
+   *   For example, for the header Content-Disposition: form-data; name="submit-name" the name of the parameter is
+   *   submit-name. This type of form parameters is more commonly used for file transfers.”
+   */
 
   // If no mime type is supported, exit early
   if (!mimeType) {
