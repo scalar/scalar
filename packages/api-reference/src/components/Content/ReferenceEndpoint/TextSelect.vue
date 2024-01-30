e9eefff409e8736e21f36a976a5b002bc1c8ea63
Hans Pagel <hanspagel@users.noreply.github.com>
2024-01-30 17:21:50 +0100

    feat: improve custom example block, fix #899 (#900)
    * feat: show path for custom examples
    
    * feat: show try request button for custom examples
    
    * refactor: move language select to a generic text select component
    
    * docs(changeset): feat: improve custom example block with try request button, path and improved select

---
+++ packages/api-reference/src/components/Content/ReferenceEndpoint/TextSelect.vue
@@ -0,0 +1,110 @@
+<script lang="ts" setup>
+defineProps<{
+  modelValue: any
+  options: {
+    value: string
+    label: string
+    options?: {
+      value: string
+      label: string
+    }[]
+  }[]
+}>()
+
+defineEmits<{
+  (event: 'update:modelValue', value: any): void
+}>()
+</script>
+
+<template>
+  <div
+    class="text-select"
+    :class="options.length === 1 ? 'text-select--single-option' : ''">
+    <span>
+      <slot />
+    </span>
+    <select
+      :value="modelValue"
+      @input="
+        (event) =>
+          $emit('update:modelValue', (event.target as HTMLSelectElement).value)
+      ">
+      <template
+        v-for="option in options"
+        :key="option.value">
+        <template v-if="option.options">
+          <optgroup :label="option.label">
+            <option
+              v-for="child in option.options"
+              :key="child.value"
+              :value="child.value">
+              {{ child.label }}
+            </option>
+          </optgroup>
+        </template>
+        <template v-else>
+          <option
+            :key="option.value"
+            :value="option.value">
+            {{ option.label }}
+          </option>
+        </template>
+      </template>
+    </select>
+  </div>
+</template>
+
+<style>
+.text-select {
+  position: relative;
+  padding-right: 9px;
+  height: fit-content;
+  padding-left: 12px;
+  border-right: 1px solid
+    var(--theme-border-color, var(--default-theme-border-color));
+}
+.text-select--single-option {
+  pointer-events: none;
+}
+.text-select select {
+  border: none;
+  outline: none;
+  cursor: pointer;
+  background: var(--theme-background-3, var(--default-theme-background-3));
+  box-shadow: -2px 0 0 0
+    var(--theme-background-3, var(--default-theme-background-3));
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+}
+.text-select span {
+  font-size: var(--theme-mini, var(--default-theme-mini));
+  color: var(--theme-color-2, var(--default-theme-color-2));
+  font-weight: var(--theme-semibold, var(--default-theme-semibold));
+  white-space: nowrap;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.text-select:hover span {
+  color: var(--theme-color-1, var(--default-theme-color-1));
+}
+.text-select:not(.text-select--single-option) span::after {
+  content: '';
+  width: 7px;
+  height: 7px;
+  transform: rotate(45deg) translate3d(-2px, -2px, 0);
+  display: block;
+  margin-left: 6px;
+  box-shadow: 1px 1px 0 currentColor;
+}
+.text-select span:hover {
+  background: var(--theme-background-2, var(--default-theme-background-2));
+}
+</style>
