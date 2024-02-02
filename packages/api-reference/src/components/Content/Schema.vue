8f1cfb0e44dadc74638515ad71651bf1a3433ac4
Brynn Hawker <brynn@hawker.me>
2024-02-01 16:57:47 -0800

    feat: style compact schema view

--- packages/api-reference/src/components/Content/Schema.vue
+++ packages/api-reference/src/components/Content/Schema.vue
@@ -45,56 +45,78 @@ const handleClick = (e: MouseEvent) =>
   <Disclosure
     v-if="typeof value === 'object' && Object.keys(value).length"
     v-slot="{ open }"
-    as="div"
-    class="schema-card"
-    :class="[
-      `schema-card--level-${level}`,
-      { 'schema-card--compact': compact },
-    ]"
     :defaultOpen="noncollapsible">
     <div
-      class="schema-properties"
-      :class="{ 'schema-properties-open': open }">
-      <DisclosureButton
-        v-show="!(noncollapsible && compact)"
-        :as="noncollapsible ? 'div' : 'button'"
-        class="schema-card-title"
-        :style="{
-          top: `calc(var(--refs-header-height) +  calc(var(--schema-title-height) * ${level}))`,
-        }"
-        @click.capture="handleClick">
-        <ScalarIcon
-          v-if="shouldShowToggle"
-          class="schema-card-title-icon"
-          :icon="open ? 'ChevronDown' : 'ChevronRight'"
-          size="md" />
-        <SchemaHeading
-          :name="name"
-          :value="mergedSchema" />
-      </DisclosureButton>
-      <DisclosurePanel :static="noncollapsible">
-        <template v-if="mergedSchema?.properties">
-          <SchemaProperty
-            v-for="property in Object.keys(mergedSchema?.properties)"
-            :key="property"
-            :compact="compact"
-            :level="level"
-            :name="property"
-            :required="
-              mergedSchema.required &&
-              mergedSchema.required.length &&
-              mergedSchema.required.includes(property)
-            "
-            :value="mergedSchema.properties?.[property]" />
-        </template>
-        <template v-else>
-          <SchemaProperty
-            :compact="compact"
-            :level="level"
-            :name="mergedSchema?.name"
-            :value="mergedSchema" />
-        </template>
-      </DisclosurePanel>
+      class="schema-card"
+      :class="[
+        `schema-card--level-${level}`,
+        { 'schema-card--compact': compact, 'schema-card--open': open },
+      ]">
+      <div
+        class="schema-properties"
+        :class="{ 'schema-properties-open': open }">
+        <DisclosureButton
+          v-show="!(noncollapsible && compact)"
+          :as="noncollapsible ? 'div' : 'button'"
+          class="schema-card-title"
+          :class="{ 'schema-card-title--compact': compact }"
+          :style="{
+            top: `calc(var(--refs-header-height) +  calc(var(--schema-title-height) * ${level}))`,
+          }"
+          @click.capture="handleClick">
+          <template v-if="compact">
+            <svg
+              v-if="shouldShowToggle"
+              class="schema-card-title-icon"
+              :class="{ 'schema-card-title-icon--open': open }"
+              fill="currentColor"
+              height="14"
+              viewBox="0 0 14 14"
+              width="14"
+              xmlns="http://www.w3.org/2000/svg">
+              <polygon
+                fill-rule="nonzero"
+                points="14 8 8 8 8 14 6 14 6 8 0 8 0 6 6 6 6 0 8 0 8 6 14 6" />
+            </svg>
+            <template v-if="open">Hide details</template>
+            <template v-else>Show details</template>
+          </template>
+          <template v-else>
+            <ScalarIcon
+              v-if="shouldShowToggle"
+              class="schema-card-title-icon"
+              :class="{ 'schema-card-title-icon--open': open }"
+              icon="ChevronRight"
+              size="md" />
+            <SchemaHeading
+              :name="name"
+              :value="mergedSchema" />
+          </template>
+        </DisclosureButton>
+        <DisclosurePanel :static="noncollapsible">
+          <template v-if="mergedSchema?.properties">
+            <SchemaProperty
+              v-for="property in Object.keys(mergedSchema?.properties)"
+              :key="property"
+              :compact="compact"
+              :level="level"
+              :name="property"
+              :required="
+                mergedSchema.required &&
+                mergedSchema.required.length &&
+                mergedSchema.required.includes(property)
+              "
+              :value="mergedSchema.properties?.[property]" />
+          </template>
+          <template v-else>
+            <SchemaProperty
+              :compact="compact"
+              :level="level"
+              :name="mergedSchema?.name"
+              :value="mergedSchema" />
+          </template>
+        </DisclosurePanel>
+      </div>
     </div>
   </Disclosure>
 </template>
@@ -136,7 +158,9 @@ button.schema-card-title:hover {
 .schema-card-title-icon {
   margin-left: -4px;
 }
-
+.schema-card-title-icon--open {
+  transform: rotate(90deg);
+}
 .schema-properties-open > .schema-card-title {
   z-index: 1;
   position: sticky;
@@ -144,7 +168,8 @@ button.schema-card-title:hover {
 
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
-  border-color: var(--theme-border-color, var(--default-theme-border-color));
+  border-bottom: 1px solid
+    var(--theme-border-color, var(--default-theme-border-color));
 }
 
 .schema-card .property:last-of-type {
@@ -159,6 +184,23 @@ button.schema-card-title:hover {
   border-radius: var(--theme-radius-lg, var(--default-theme-radius-lg));
 }
 
+.schema-card-title--compact {
+  padding: 6px 8px;
+  height: auto;
+  border-bottom: none;
+}
+
+.schema-card-title--compact > .schema-card-title-icon {
+  width: 9px;
+  height: 9px;
+  margin: 0;
+}
+.schema-card-title--compact > .schema-card-title-icon--open {
+  transform: rotate(45deg);
+}
+.schema-properties-open > .schema-card-title--compact {
+  position: static;
+}
 .schema-card--compact.schema-card--level-0 > .schema-properties {
   border: none;
 }
