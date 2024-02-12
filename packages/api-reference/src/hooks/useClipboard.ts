9e7e09cd3f390d4295fd08ac2a47bc44751cdb59
Hans Pagel <mail@hanspagel.com>
2024-02-12 15:44:41 +0100

    chore: use vueuse useClipboard

--- packages/api-reference/src/hooks/useClipboard.ts
+++ packages/api-reference/src/hooks/useClipboard.ts
@@ -1,13 +1,31 @@
 import { useToasts } from '@scalar/use-toasts'
+import { useClipboard as useClipboardFromVueUse } from '@vueuse/core'
+import { watch } from 'vue'
 
+const { copy, copied } = useClipboardFromVueUse()
 const { addToast } = useToasts()
 
+/**
+ * A hook to copy text to the clipboard and trigger a toast notification.
+ */
 export const useClipboard = () => {
-  const copyToClipboard = (value: string) => {
-    navigator.clipboard.writeText(value).then(() => {
-      addToast({ title: 'Copied to the clipboard' }, { timeout: 2000 })
+  const copyToClipboard = (text: string) => {
+    copy(text)
+
+    watch(copied, (value: boolean) => {
+      if (!value) {
+        return
+      }
+
+      addToast(
+        {
+          title: 'Copied to the clipboard',
+        },
+        { timeout: 2000 },
+      )
     })
   }
+
   return {
     copyToClipboard,
   }
