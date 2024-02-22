089a38f8f312a94b004f8a520491f7829bdf4449
Hans Pagel <mail@hanspagel.com>
2024-02-22 11:15:06 +0100

    chore: remove codemirror languages

--- packages/use-codemirror/src/hooks/useCodeMirror.ts
+++ packages/use-codemirror/src/hooks/useCodeMirror.ts
@@ -1,27 +1,6 @@
-import { css } from '@codemirror/lang-css'
 import { html } from '@codemirror/lang-html'
-import { java } from '@codemirror/lang-java'
-import { javascript } from '@codemirror/lang-javascript'
 import { json } from '@codemirror/lang-json'
-import { php } from '@codemirror/lang-php'
-import { python } from '@codemirror/lang-python'
-import { rust } from '@codemirror/lang-rust'
 import { type LanguageSupport, StreamLanguage } from '@codemirror/language'
-import {
-  c,
-  csharp,
-  kotlin,
-  objectiveC,
-} from '@codemirror/legacy-modes/mode/clike'
-import { clojure } from '@codemirror/legacy-modes/mode/clojure'
-import { go } from '@codemirror/legacy-modes/mode/go'
-import { http } from '@codemirror/legacy-modes/mode/http'
-import { oCaml } from '@codemirror/legacy-modes/mode/mllike'
-import { powerShell } from '@codemirror/legacy-modes/mode/powershell'
-import { r } from '@codemirror/legacy-modes/mode/r'
-import { ruby } from '@codemirror/legacy-modes/mode/ruby'
-import { shell } from '@codemirror/legacy-modes/mode/shell'
-import { swift } from '@codemirror/legacy-modes/mode/swift'
 import * as yamlMode from '@codemirror/legacy-modes/mode/yaml'
 import { type Extension, StateEffect } from '@codemirror/state'
 import {
@@ -223,28 +202,8 @@ export const useCodeMirror = (
 const syntaxHighlighting: {
   [lang in CodeMirrorLanguage]: LanguageSupport | StreamLanguage<any>
 } = {
-  c: StreamLanguage.define(c),
-  clojure: StreamLanguage.define(clojure),
-  csharp: StreamLanguage.define(csharp),
-  css: css(),
-  go: StreamLanguage.define(go),
-  http: StreamLanguage.define(http),
   html: html(),
-  java: java(),
-  javascript: javascript(),
   json: json(),
-  kotlin: StreamLanguage.define(kotlin),
-  node: javascript(),
-  objc: StreamLanguage.define(objectiveC),
-  ocaml: StreamLanguage.define(oCaml),
-  powershell: StreamLanguage.define(powerShell),
-  python: python(),
-  php: php(),
-  r: StreamLanguage.define(r),
-  ruby: StreamLanguage.define(ruby),
-  rust: rust(),
-  shell: StreamLanguage.define(shell),
-  swift: StreamLanguage.define(swift),
   yaml: StreamLanguage.define(yamlMode.yaml),
 }
 
