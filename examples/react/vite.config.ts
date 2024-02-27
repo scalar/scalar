b3c411e4a7bf2423fb09ed57b4dfc1d65bf7f0ec
Hans Pagel <mail@hanspagel.com>
2024-02-27 18:41:49 +0100

    chore: remove node polyfills

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path'
 import veauryVitePlugins from 'veaury/vite/index.js'
 import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,20 +13,6 @@ export default defineConfig({
       // reactOptions: {...},
       // Configuration of @vitejs/plugin-vue-jsx
       // vueJsxOptions: {...}
-    }),
-    nodePolyfills({
-      // To exclude specific polyfills, add them to this list.
-      exclude: [
-        'fs', // Excludes the polyfill for `fs` and `node:fs`.
-      ],
-      // Whether to polyfill specific globals.
-      globals: {
-        Buffer: true, // can also be 'build', 'dev', or false
-        global: true,
-        process: true,
-      },
-      // Whether to polyfill `node:` protocol imports.
-      protocolImports: true,
     }),
   ],
   server: {
--- examples/web/vite.config.ts
+++ examples/web/vite.config.ts
@@ -1,27 +1,10 @@
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    nodePolyfills({
-      // To exclude specific polyfills, add them to this list.
-      exclude: [
-        'fs', // Excludes the polyfill for `fs` and `node:fs`.
-      ],
-      // Whether to polyfill specific globals.
-      globals: {
-        Buffer: true, // can also be 'build', 'dev', or false
-        global: true,
-        process: true,
-      },
-      // Whether to polyfill `node:` protocol imports.
-      protocolImports: true,
-    }),
-  ],
+  plugins: [vue()],
   server: {
     port: 5050,
   },
