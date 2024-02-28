96b274bf6aee8d1ba277124b2d0d5ddc75ef08a3
tmastrom <thomas.mastromonaco@gmail.com>
2024-02-28 14:10:05 -0800

    test: playwright ui test to validate cdn api reference

---
+++ packages/playwright/tests/cdn.spec.ts
@@ -0,0 +1,27 @@
+import { expect, test } from '@playwright/test'
+
+test('Renders petstore api reference from CDN', async ({ page, isMobile }) => {
+  await page.goto('http://127.0.0.1:3173')
+
+  // Check for basic elements on the page
+  // The heading
+  await expect(
+    page.getByRole('heading', { name: 'Swagger Petstore - OpenAPI' }),
+  ).toBeVisible()
+  // Body Text
+  await expect(
+    page.locator('p').filter({ hasText: 'This is a sample Pet Store' }),
+  ).toBeVisible()
+  // http client
+  await expect(page.getByText('Client Libraries')).toBeVisible()
+
+  // Check for elements that are only visible on desktop
+  if (!isMobile) {
+    // Sidebar
+    await expect(
+      page.getByRole('link', { name: 'PET', exact: true }),
+    ).toBeVisible()
+    // Search
+    await expect(page.getByRole('button', { name: 'Search ⌘k' })).toBeVisible()
+  }
+})
