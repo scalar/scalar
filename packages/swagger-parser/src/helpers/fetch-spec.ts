b5652f7868bc94e8a663e5b7490d2ed8c0631e1b
Amrit <amrit@hockey-community.com>
2024-03-08 11:42:51 -0800

    fix: broken proxy

--- packages/swagger-parser/src/helpers/fetch-spec.ts
+++ packages/swagger-parser/src/helpers/fetch-spec.ts
@@ -17,30 +17,42 @@ function jsonOrYaml(value: string) {
  */
 export const fetchSpecFromUrl = async (url: string, proxy?: string) => {
   // With Proxy
-  const response = proxy
-    ? await fetch(proxy, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          method: 'GET',
-          url,
-        }),
-      })
-    : await fetch(url)
+  if (proxy) {
+    const response = proxy
+      ? await fetch(proxy, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            method: 'GET',
+            url,
+          }),
+        })
+      : await fetch(url)
 
-  if (response.status !== 200) {
-    const proxyWarning = proxy
-      ? ''
-      : 'Trying to fetch the spec file without a proxy. The CORS headers have to be set properly, otherwise the request will fail.'
-    console.error(
-      `[fetchSpecFromUrl] Failed to fetch the spec at ${url}. ${proxyWarning}`,
-    )
+    const payload = await response.json()
+
+    if (payload.error) {
+      throw new Error(JSON.stringify(payload))
+    }
+
+    if (!payload.data) {
+      throw new Error(
+        "Didn't receive a proper answer from the Proxy: " +
+          JSON.stringify(payload),
+      )
+    }
+
+    return jsonOrYaml(payload.data)
   }
+  // Without proxy
+  else {
+    console.warn(
+      '[fetchSpecFromUrl] Trying to fetch the spec file without a proxy. The CORS headers have to be set properly, otherwise the request will fail.',
+    )
+    const response = await fetch(url)
 
-  const payload = await response.text()
-
-  // Formats the JSON if provided
-  return jsonOrYaml(payload)
+    return jsonOrYaml(await response.text())
+  }
 }
