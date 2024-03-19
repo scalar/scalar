954aa6c2c53d0cad7bf197f4943af18b62c82d61
tmastrom <thomas.mastromonaco@gmail.com>
2024-03-19 10:46:32 -0700

    chore: fetch openapi example spec

--- examples/nextjs-api-reference/app/reference/route.ts
+++ examples/nextjs-api-reference/app/reference/route.ts
@@ -1,6 +1,12 @@
 import { ApiReference } from '@scalar/nextjs-api-reference'
 
-import content from '../petstorev3.json'
+// Local example for local development
+// import content from '../../../web/src/fixtures/petstorev3.json'
+
+const openapiResponse = await fetch(
+  'https://cdn.scalar.com/spec/openapi_petstore.json',
+)
+const content = await openapiResponse.json()
 
 const config = {
   spec: {
