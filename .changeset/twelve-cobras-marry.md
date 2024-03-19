0b784b55d05622888e73376f3e0daaf1511016e4
Hans Pagel <hanspagel@users.noreply.github.com>
2024-03-19 10:25:48 -0700

    fix: cli exists on invalid specifications (#1235)

---
+++ .changeset/twelve-cobras-marry.md
@@ -0,0 +1,5 @@
+---
+"@scalar/cli": patch
+---
+
+fix: cli watcher exits when specification is invalid
--- packages/cli/src/utils/loadOpenApiFile.ts
+++ packages/cli/src/utils/loadOpenApiFile.ts
@@ -5,38 +5,42 @@ import fs from 'node:fs'
 export async function loadOpenApiFile(file: string) {
   const specification = fs.readFileSync(file, 'utf8')
 
-  const result = await openapi().load(specification).resolve()
-  const { valid, version, schema } = result
+  try {
+    const result = await openapi().load(specification).resolve()
+    const { valid, version, schema } = result
 
-  if (valid) {
-    console.log(
-      kleur.bold().white('[INFO]'),
-      kleur.bold().white(schema.info.title),
-      kleur.grey(`(OpenAPI v${version})`),
-    )
-    // Stats
-    const pathsCount = Object.keys(schema.paths).length
+    if (valid) {
+      console.log(
+        kleur.bold().white('[INFO]'),
+        kleur.bold().white(schema.info.title),
+        kleur.grey(`(OpenAPI v${version})`),
+      )
+      // Stats
+      const pathsCount = Object.keys(schema.paths).length
 
-    let operationsCount = 0
-    for (const path in schema.paths) {
-      for (const method in schema.paths[path]) {
-        operationsCount++
+      let operationsCount = 0
+      for (const path in schema.paths) {
+        for (const method in schema.paths[path]) {
+          operationsCount++
+        }
       }
+
+      console.log(
+        kleur.bold().white('[INFO]'),
+        kleur.grey(`${pathsCount} paths, ${operationsCount} operations`),
+      )
+
+      console.log()
+    } else {
+      console.warn(
+        kleur.bold().yellow('[WARN]'),
+        kleur.yellow('File doesn’t match the OpenAPI specification.'),
+      )
+      console.log()
     }
-
-    console.log(
-      kleur.bold().white('[INFO]'),
-      kleur.grey(`${pathsCount} paths, ${operationsCount} operations`),
-    )
-
-    console.log()
-  } else {
-    console.warn(
-      kleur.bold().yellow('[WARN]'),
-      kleur.yellow('File doesn’t match the OpenAPI specification.'),
-    )
+    return result
+  } catch (error) {
+    console.warn(kleur.bold().red('[ERROR]'), kleur.red(error))
     console.log()
   }
-
-  return result
 }
