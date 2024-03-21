d661b2dcc96d503d4f393970ee237b43a714fc28
Amrit <amrit@scalar.com>
2024-03-21 13:07:35 -0700

    fix: env

--- packages/components/env.d.ts
+++ packages/components/env.d.ts
@@ -3,3 +3,4 @@
 declare module '*.module.css?inline'
 declare module 'vite-plugin-scope-tailwind'
 declare module '@rise8/tailwind-pixel-perfect-preset'
+declare module 'prismjs/plugins/autoloader/prism-autoloader.js'
