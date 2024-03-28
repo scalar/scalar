248e98a22b82987106cb2426784d92353b7c82e6
Hans Pagel <mail@hanspagel.com>
2024-03-28 00:10:37 -0500

    fix: TS issues

--- packages/api-reference/src/stores/useConfigurationStore.ts
+++ packages/api-reference/src/stores/useConfigurationStore.ts
@@ -1,4 +1,9 @@
-import { reactive, readonly } from 'vue'
+import {
+  type DeepReadonly,
+  type UnwrapNestedRefs,
+  reactive,
+  readonly,
+} from 'vue'
 
 import type { ReferenceConfiguration } from '../types'
 
@@ -15,7 +20,13 @@ const setConfiguration = (newState: Partial<ReferenceConfiguration>) => {
   })
 }
 
-export const useConfigurationStore = () => ({
+// We need to be explicit about the return type here because of this error:
+//
+// The inferred type of 'useConfigurationStore' cannot be named without a reference to '.pnpm/zhead@2.2.4/node_modules/zhead/dist/shared/zhead.177ad851'. This is likely not portable. A type annotation is necessary.
+export const useConfigurationStore: () => {
+  configuration: DeepReadonly<UnwrapNestedRefs<ReferenceConfiguration>>
+  setConfiguration: (newState: Partial<ReferenceConfiguration>) => void
+} = () => ({
   configuration: readonly(configuration),
   setConfiguration,
 })
