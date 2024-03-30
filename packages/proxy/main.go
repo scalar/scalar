a9bd6e163aa50111aae0bc7b6751016faa3e4e5e
marclave <marc@scalar.com>
2024-03-29 21:54:54 -0700

    feat: add basic golang proxy

---
+++ packages/proxy/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"log"
+)
+
+// Handler that forwards requests to the target and writes the response back to the original client
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	// Parse query parameters
+	queryValues := r.URL.Query()
+	target := queryValues.Get("url") 
+
+	// we can eventually do a check on the type of request
+	// i.e. websocket vs mqtt vs grpc
+	// but let's handle just http for now :) 
+
+	proxyUrl, _ := url.Parse(target)
+
+	// Create a reverse proxy
+	proxy := httputil.NewSingleHostReverseProxy(proxyUrl)
+
+	// Modify the request to indicate it is proxied
+	r.URL.Host = proxyUrl.Host
+	r.URL.Scheme = proxyUrl.Scheme
+	r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+	r.Host = proxyUrl.Host
+
+	// ServeHttp uses the given ResponseWriter and Request to do the proxying
+	proxy.ServeHTTP(w, r)
+}
+
+
+func main() {
+	port := ":1337"
+
+	http.HandleFunc("/", handleRequest)
+
+	log.Println("Starting proxy server on", port)
+	
+	err := http.ListenAndServe(port, nil) 
+	if err != nil {
+		log.Fatal("Error starting proxy server: ", err)
+	}
+}
