0d00618656b9426ed72be89ec07dc44ad0b96816
Helge Sverre <helge.sverre@gmail.com>
2024-04-05 23:49:17 +0200

    feat: allow using an object to specify clients within specific targets to be hidden. Example: {     php: true, // Exclude all PHP clients     python: true, //  Exclude all Python clients     node: ['fetch'], // We expect the "fetch" client to still be available in the "javascript" target.   }

--- packages/api-reference/src/hooks/useHttpClients.ts
+++ packages/api-reference/src/hooks/useHttpClients.ts
@@ -12,14 +12,29 @@ const cachedTargets = ref([])
 watchEffect(() => {
   cachedTargets.value = allTargets()
     .map((target) => {
-      // Filter out excluded clients
-      target.clients = target.clients.filter(
-        (client) => !excludedClients.value.includes(client.key),
-      )
+      // NOTE: This is for backwards compatibility with the previous behavior,
+      //  If excludedClients is an array, it will exclude the matching clients from all targets.
+      if (Array.isArray(excludedClients.value)) {
+        target.clients = target.clients.filter(
+          (client) => !excludedClients.value.includes(client.key),
+        )
+      }
+
+      // Determine if the whole target (language) is to be excluded
+      if (excludedClients.value[target.key] === true) {
+        return null // Exclude the entire target by returning null
+      }
+
+      // Filter out excluded clients within the target
+      if (Array.isArray(excludedClients.value[target.key])) {
+        target.clients = target.clients.filter(
+          (client) => !excludedClients.value[target.key].includes(client.key),
+        )
+      }
 
       return target
     })
-    .filter((target) => target.clients.length)
+    .filter((target) => target?.clients.length)
 })
 
 export function useHttpClients() {
