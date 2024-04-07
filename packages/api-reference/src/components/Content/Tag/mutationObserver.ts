b1aaced7b40dce19eb96bb69eacf7f746364d1ae
anthony lio <lionnet.ant@gmail.com>
2024-04-07 19:28:37 +0300

    fix: endpoint navigation
    *updates endpoint scroll handler to observe node changes
    
    *adds mutation observer

---
+++ packages/api-reference/src/components/Content/Tag/mutationObserver.ts
@@ -0,0 +1,9 @@
+import { useMutationObserver } from '@vueuse/core'
+import { type Ref } from 'vue'
+
+export const observeMutations = (
+  target: Ref<HTMLElement | null>,
+  callback: MutationCallback,
+) => {
+  useMutationObserver(target, callback, { childList: true, subtree: true })
+}
