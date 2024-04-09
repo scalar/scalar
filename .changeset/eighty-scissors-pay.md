622f31b3d597d574a666e9bd004ba226353961e5
Anthony Lio <lionnet.ant@gmail.com>
2024-04-09 21:18:36 +0300

    fix: endpoint navigation (#1401)
    * fix: endpoint navigation
    
    *updates endpoint scroll handler to observe node changes
    
    *adds mutation observer
    
    * docs(changeset): fix: endpoint navigation

---
+++ .changeset/eighty-scissors-pay.md
@@ -0,0 +1,5 @@
+---
+"@scalar/api-reference": patch
+---
+
+fix: endpoint navigation
--- packages/api-reference/src/components/Content/Tag/Tag.vue
+++ packages/api-reference/src/components/Content/Tag/Tag.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 import { useNavState, useSidebar } from '../../../hooks'
 import type { Spec, Tag } from '../../../types'
 import { SectionContainer } from '../../Section'
 import ShowMoreButton from '../../ShowMoreButton.vue'
 import Endpoints from './Endpoints.vue'
+import { observeMutations } from './mutationObserver'
 
 const props = defineProps<{
   id?: string
   tag: Tag
   spec: Spec
 }>()
+
+const sectionContainerRef = ref<HTMLElement | null>(null)
 
 const { collapsedSidebarItems } = useSidebar()
 const { getTagId } = useNavState()
@@ -18,13 +23,43 @@ const moreThanOneDefaultTag = (tag: Tag) =>
   props.spec.tags?.length !== 1 ||
   tag?.name !== 'default' ||
   tag?.description !== ''
+
+const redirectToOperation = (operationId: string) => {
+  window.location.href = `#${operationId}`
+}
+
+const observeSectionMutations = (operationId: string) => {
+  observeMutations(sectionContainerRef, (mutations, observer) => {
+    mutations.forEach((mutation) => {
+      const operationIsAdded = Array.from(mutation.addedNodes).some((node) => {
+        return node instanceof HTMLElement && node.id === operationId
+      })
+      if (operationIsAdded) {
+        redirectToOperation(operationId)
+        observer.disconnect()
+      }
+    })
+  })
+}
+
+const observeAndNavigate = (operationId: string) => {
+  const operationIsVisible = document.getElementById(operationId)
+  if (!operationIsVisible) {
+    observeSectionMutations(operationId)
+  } else {
+    redirectToOperation(operationId)
+  }
+}
 </script>
 <template>
-  <SectionContainer class="tag-section-container">
+  <SectionContainer
+    ref="sectionContainerRef"
+    class="tag-section-container">
     <Endpoints
       v-if="moreThanOneDefaultTag(tag)"
       :id="id"
-      :tag="tag" />
+      :tag="tag"
+      @observeAndNavigate="observeAndNavigate" />
     <ShowMoreButton
       v-if="!collapsedSidebarItems[getTagId(tag)] && tag.operations?.length > 1"
       :id="id ?? ''" />
