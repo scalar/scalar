f9463660eb9eb9d7a862ab040fcf0bf6e990de21
Brynn Hawker <brynn@hawker.me>
2024-04-09 12:46:07 -0700

    fix: show example picker if the list only has one example

--- packages/api-reference/src/components/Content/Operation/PathResponses/PathResponses.vue
+++ packages/api-reference/src/components/Content/Operation/PathResponses/PathResponses.vue
@@ -41,9 +41,7 @@ const orderedStatusCodes = computed(() => {
 })
 
 const hasMultipleExamples = computed<boolean>(
-  () =>
-    !!currentJsonResponse.value.examples &&
-    Object.keys(currentJsonResponse.value.examples).length > 1,
+  () => !!currentJsonResponse.value.examples,
 )
 
 // Keep track of the current selected tab
