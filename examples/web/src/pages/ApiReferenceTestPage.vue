203d7bad999c084b6d2ad9b6f11b2128e396fbe3
Brynn Hawker <brynn@hawker.me>
2024-04-10 16:03:51 -0700

    chore: add playwright snapshot tests for all themes and layouts

---
+++ examples/web/src/pages/ApiReferenceTestPage.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {
+  ApiReference,
+  type ReferenceConfiguration,
+  type ReferenceLayoutType,
+} from '@scalar/api-reference'
+import { type ThemeId } from '@scalar/themes'
+import { reactive } from 'vue'
+import { useRoute } from 'vue-router'
+
+import content from '../fixtures/petstorev3.json'
+
+const route = useRoute()
+
+const configuration = reactive<ReferenceConfiguration>({
+  theme: (`${route.params['theme']}` as ThemeId) ?? 'default',
+  isEditable: false,
+  layout: `${route.params['layout']}` as ReferenceLayoutType,
+  darkMode: !!route.query['darkMode'],
+  spec: { content },
+})
+</script>
+<template>
+  <ApiReference :configuration="configuration" />
+</template>
