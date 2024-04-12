ef0ca901d78ec143eada310ad73141a4fecdf9a1
Amrit <amrit@scalar.com>
2024-04-11 21:55:58 -0700

    fix: tests

--- packages/api-reference/src/helpers/getUrlFromServerState.test.ts
+++ packages/api-reference/src/helpers/getUrlFromServerState.test.ts
@@ -38,27 +38,6 @@ describe('getUrlFromServerState', () => {
     expect(request).toMatchObject('https://unicorn.fantasy')
   })
 
-  it('prefixes path with the origin', () => {
-    global.window = {
-      // @ts-expect-error
-      location: {
-        origin: 'https://example.com',
-      },
-    }
-
-    const request = getUrlFromServerState({
-      ...createEmptyServerState(),
-      selectedServer: 0,
-      servers: [
-        {
-          url: 'v1',
-        },
-      ],
-    })
-
-    expect(request).toMatchObject('https://example.com/v1')
-  })
-
   it('replaces variables first, and then checks whether a prefix is necessary', () => {
     const request = getUrlFromServerState({
       ...createEmptyServerState(),
