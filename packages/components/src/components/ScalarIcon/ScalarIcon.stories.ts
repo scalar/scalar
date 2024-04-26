faa7ac18787451037bb84c6af6a77a5e593e3fdd
Brynn Hawker <brynn@hawker.me>
2024-04-26 14:12:31 -0700

    feat: adjust stroke size for different icon sizes

--- packages/components/src/components/ScalarIcon/ScalarIcon.stories.ts
+++ packages/components/src/components/ScalarIcon/ScalarIcon.stories.ts
@@ -9,7 +9,10 @@ const meta = {
   tags: ['autodocs'],
   argTypes: {
     icon: { control: 'select', options: ICONS },
-    size: { control: 'select', options: ['xs', 'sm', 'md', 'lg', 'xl'] },
+    size: {
+      control: 'select',
+      options: ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl'],
+    },
   },
   parameters: {
     docs: {
@@ -35,7 +38,28 @@ export const Base: Story = {
   args: { icon: 'Logo' },
 }
 
-export const IconList: StoryObj = {
+export const AllSizes: Story = {
+  args: { icon: 'CodeFolder' },
+  render: (args) => ({
+    components: { ScalarIcon },
+    setup() {
+      return { args }
+    },
+    template: `
+<div class="flex items-center gap-2 text-fore-2">
+  <ScalarIcon size="xs" v-bind="args"/>
+  <ScalarIcon size="sm" v-bind="args"/>
+  <ScalarIcon size="md" v-bind="args"/>
+  <ScalarIcon size="lg" v-bind="args"/>
+  <ScalarIcon size="xl" v-bind="args"/>
+  <ScalarIcon size="2xl" v-bind="args"/>
+  <ScalarIcon size="3xl" v-bind="args"/>
+</div>
+    `,
+  }),
+}
+
+export const AllIcons: StoryObj = {
   render: () => ({
     components: { IconGrid },
     setup() {
