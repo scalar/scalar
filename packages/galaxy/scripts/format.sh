dca757e6c1f99c0f9cddfe61d992437eea2b2509
Hans Pagel <mail@hanspagel.com>
2024-04-26 15:43:55 +0200

    fix: cli not available in CI

--- packages/galaxy/scripts/format.sh
+++ packages/galaxy/scripts/format.sh
@@ -2,6 +2,6 @@
 
 # The source file is just YAML. We transform it to JSON to have both formats available.
 
-npx @scalar/cli format ./dist/3.1.yaml --output ./dist/3.1.json
-npx @scalar/cli format ./dist/latest.yaml --output ./dist/latest.json
+pnpm dlx @scalar/cli format ./dist/3.1.yaml --output ./dist/3.1.json
+pnpm dlx @scalar/cli format ./dist/latest.yaml --output ./dist/latest.json
 
