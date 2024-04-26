1ed108aa99de4d811d1769bc3173cd1ddc914338
Hans Pagel <mail@hanspagel.com>
2024-04-26 14:09:39 +0200

    chore: clean up

--- packages/galaxy/scripts/format.sh
+++ packages/galaxy/scripts/format.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-./node_modules/.bin/scalar format ./dist/3.1.yaml
-./node_modules/.bin/scalar format ./dist/3.1.yaml --output ./dist/3.1.json
+# The source file is just YAML. We transform it to JSON to have both formats available.
 
-./node_modules/.bin/scalar format ./dist/latest.yaml
+./node_modules/.bin/scalar format ./dist/3.1.yaml --output ./dist/3.1.json
 ./node_modules/.bin/scalar format ./dist/latest.yaml --output ./dist/latest.json
 
