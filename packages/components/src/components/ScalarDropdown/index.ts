6cb2516929dad75ae711c7772ae6ab0f47e8de2b
Brynn Hawker <brynn@hawker.me>
2024-05-02 14:51:35 -0700

    chore: update exports

--- packages/components/src/components/ScalarDropdown/index.ts
+++ packages/components/src/components/ScalarDropdown/index.ts
@@ -1,1 +1,3 @@
 export { default as ScalarDropdown } from './ScalarDropdown.vue'
+export { default as ScalarDropdownDivider } from './ScalarDropdownDivider.vue'
+export { default as ScalarDropdownItem } from './ScalarDropdownItem.vue'
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,10 +2,17 @@ import type { App } from 'vue'
 
 import { ScalarButton } from './components/ScalarButton'
 import { ScalarCodeBlock } from './components/ScalarCodeBlock'
+import {
+  ScalarDropdown,
+  ScalarDropdownDivider,
+  ScalarDropdownItem,
+} from './components/ScalarDropdown'
+import { ScalarFloating } from './components/ScalarFloating'
 import { type Icon, ScalarIcon } from './components/ScalarIcon'
 import { ScalarIconButton } from './components/ScalarIconButton'
 import { ScalarLoading, useLoadingState } from './components/ScalarLoading'
 import { ScalarModal, useModal } from './components/ScalarModal'
+import { ScalarPopover } from './components/ScalarPopover'
 import { ScalarSearchInput } from './components/ScalarSearchInput'
 import {
   ScalarSearchResultItem,
@@ -17,10 +24,15 @@ import './tailwind/tailwind.css'
 export default {
   install: (app: App) => {
     app.component('ScalarButton', ScalarButton)
+    app.component('ScalarDropdown', ScalarDropdown)
+    app.component('ScalarDropdownDivider', ScalarDropdownDivider)
+    app.component('ScalarDropdownItem', ScalarDropdownItem)
+    app.component('ScalarFloating', ScalarFloating)
     app.component('ScalarIcon', ScalarIcon)
     app.component('ScalarIconButton', ScalarIconButton)
     app.component('ScalarLoading', ScalarLoading)
     app.component('ScalarModal', ScalarModal)
+    app.component('ScalarPopover', ScalarPopover)
     app.component('ScalarSearchInput', ScalarSearchInput)
     app.component('ScalarSearchResultItem', ScalarSearchResultItem)
     app.component('ScalarSearchResultList', ScalarSearchResultList)
@@ -31,10 +43,15 @@ export default {
 export {
   ScalarButton,
   ScalarCodeBlock,
+  ScalarDropdown,
+  ScalarDropdownDivider,
+  ScalarDropdownItem,
+  ScalarFloating,
   ScalarIcon,
   ScalarIconButton,
   ScalarLoading,
   ScalarModal,
+  ScalarPopover,
   ScalarSearchInput,
   ScalarSearchResultItem,
   ScalarSearchResultList,
