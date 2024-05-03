e5a9ddb11353d17ba651d266965f53d68fc330cd
tmastrom <thomas.mastromonaco@gmail.com>
2024-05-03 10:28:59 -0700

    feat: only run theme tests on chromium and update snapshots

--- playwright/tests/themes.spec.ts
+++ playwright/tests/themes.spec.ts
@@ -21,19 +21,27 @@ const baseUrl = `http://${HOST}:5050/test-api-reference`
 
 const testOpts = { fullPage: true, maxDiffPixelRatio: 0.02 }
 
-layouts.forEach((layout) =>
-  themes.forEach((theme) =>
-    test(`Renders the ${theme} with the ${layout} layout`, async ({ page }) => {
-      await page.goto(`${baseUrl}/${layout}/${theme}`)
-      await expect(page).toHaveScreenshot(
-        `${layout}-${theme}-light-snapshot.png`,
-        testOpts,
-      )
-      await page.goto(`${baseUrl}/${layout}/${theme}?darkMode=true`)
-      await expect(page).toHaveScreenshot(
-        `${layout}-${theme}-dark-snapshot.png`,
-        testOpts,
-      )
-    }),
-  ),
-)
+test.describe('theme tests', () => {
+  // Only run this test on Chromium desktop browser
+  test.skip(({ browserName }) => browserName !== 'chromium', 'Chromium only!')
+  test.skip(({ isMobile }) => isMobile === true, 'Desktop only!')
+
+  layouts.forEach((layout) =>
+    themes.forEach((theme) =>
+      test(`Renders the ${theme} with the ${layout} layout`, async ({
+        page,
+      }) => {
+        await page.goto(`${baseUrl}/${layout}/${theme}`)
+        await expect(page).toHaveScreenshot(
+          `${layout}-${theme}-light-snapshot.png`,
+          testOpts,
+        )
+        await page.goto(`${baseUrl}/${layout}/${theme}?darkMode=true`)
+        await expect(page).toHaveScreenshot(
+          `${layout}-${theme}-dark-snapshot.png`,
+          testOpts,
+        )
+      }),
+    ),
+  )
+})
