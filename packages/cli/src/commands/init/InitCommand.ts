7254f6eae5c7fbacac52a18e10a71979369d1d11
Anthony Lio <lionnet.ant@gmail.com>
2024-05-06 20:40:06 +0300

    feat: cli style updates + directory navigation  (#1614)
    * fix: no overwrite process exit 1 -> 0
    
    * feat: update cli init command message style
    
    * feat: add directory navigation to cli init command
    
    * docs(changeset): feat: directory navigation on init command

--- packages/cli/src/commands/init/InitCommand.ts
+++ packages/cli/src/commands/init/InitCommand.ts
@@ -1,4 +1,5 @@
 import { Command } from 'commander'
+import { glob } from 'glob'
 import kleur from 'kleur'
 import fs from 'node:fs'
 import path from 'node:path'
@@ -16,24 +17,46 @@ export function InitCommand() {
   cmd.action(async ({ file }) => {
     // Path to `scalar.config.json` file
     const configFile = path.resolve(CONFIG_FILE)
+    let validInput = false
+    let input = file
+
+    // Handle cancel from the user
+    const handleCancel = () => {
+      console.log()
+      process.exit(0)
+    }
+
+    // Function to validate file extension
+    function isValidFile(filePath) {
+      const validExtensions = ['.json', '.yaml', '.yml']
+      const extension = path.extname(filePath).toLowerCase()
+      return validExtensions.includes(extension)
+    }
+
+    if (input && isValidFile(input)) {
+      validInput = true
+    }
 
     // Check if `scalar.config.json` already exists
     if (fs.existsSync(configFile)) {
-      console.warn(kleur.yellow(`A ${CONFIG_FILE} file already exists.`))
-      console.log()
+      console.log(
+        `${kleur.green('✔')} Found Scalar configuration file: ${kleur.reset().green(`${CONFIG_FILE}`)}`,
+      )
 
-      const { overwrite } = await prompts({
-        type: 'toggle',
-        name: 'overwrite',
-        message: 'Do you want to override the file?',
-        initial: false,
-        active: 'yes',
-        inactive: 'no',
-      })
+      const { overwrite } = await prompts(
+        {
+          type: 'toggle',
+          name: 'overwrite',
+          message: 'Do you want to override the file?',
+          initial: false,
+          active: 'yes',
+          inactive: 'no',
+        },
+        { onCancel: handleCancel },
+      )
 
       if (overwrite === false) {
-        console.log()
-        process.exit(1)
+        handleCancel()
       }
     }
 
@@ -42,48 +65,154 @@ export function InitCommand() {
       references: [],
     }
 
-    const { input } = file
-      ? {
-          input: file,
-        }
-      : await prompts({
-          type: 'text',
+    // Check if the file option is provided and valid
+    if (input) {
+      const validExtensions = ['.json', '.yaml', '.yml']
+      const extension = path.extname(input).toLowerCase()
+      if (validExtensions.includes(extension)) {
+        validInput = true
+      } else {
+        console.log(
+          kleur.red('✖'),
+          `Please enter a valid file path ${validExtensions.join(', ')}.`,
+        )
+      }
+    }
+
+    while (!validInput) {
+      const response = await prompts(
+        {
+          type: 'autocomplete',
           name: 'input',
-          message: 'Where is your OpenAPI file?',
-          initial: './openapi.json',
-          validate: (input: string) => {
-            return fs.existsSync(input) ? true : 'File doesn’t exist.'
+          message: `Where is your OpenAPI file? ${kleur.reset().grey('(Use arrow keys or type)')}`,
+          initial: false,
+          choices: [
+            { title: `Current working directory`, value: process.cwd() },
+            { title: `Parent directory`, value: `../` },
+            { title: `Grand directory`, value: `../../` },
+            { title: `Home directory`, value: `~` },
+            { title: `Root directory`, value: `/` },
+            // Display more suggestions
+            { value: `/` },
+            { value: `/` },
+          ],
+          suggest: async (input, _choices) => {
+            const cwd = process.cwd()
+            const start = (
+              input.length === 0 ? cwd : path.resolve(cwd, input)
+            ).replace('~', process.env.HOME)
+            const files = await glob(`${start}/*`, {
+              cwd,
+              nodir: false,
+            })
+            const suggestions = files
+              .map((file) => {
+                const isDirectory = fs.lstatSync(file).isDirectory()
+                const ext = path.extname(file)
+                const isSpecFile =
+                  ['.json', '.yaml', '.yml'].includes(ext) && !isDirectory
+                return {
+                  title: path.relative(cwd, file) + (isDirectory ? '/' : ''),
+                  value: file,
+                  specFile: isSpecFile,
+                  isDirectory: isDirectory,
+                }
+              })
+              .filter((item) => item !== null)
+
+            // Sort to put specification file types at the top
+            suggestions.sort((a, b) => {
+              if (a.specFile === b.specFile) {
+                return a.title.localeCompare(b.title)
+              }
+              return Number(b.specFile) - Number(a.specFile)
+            })
+
+            if (
+              input.length > 0 &&
+              !suggestions.find((s) => s.value === input)
+            ) {
+              suggestions.unshift({
+                title: `${input}`,
+                value: input,
+                specFile: false,
+                isDirectory: false,
+              })
+            }
+
+            // Limit to 5 entries to leave space for the 'Cancel' option
+            const limitedSuggestions = suggestions.slice(0, 6)
+
+            limitedSuggestions.push({
+              title: 'Cancel',
+              value: 'cancel',
+              specFile: false,
+              isDirectory: false,
+            })
+
+            return limitedSuggestions
           },
-        })
+        },
+        {
+          onCancel: handleCancel,
+        },
+      )
+
+      input = response.input
+
+      // Validate the user's choice after the prompt
+      if (input) {
+        if (input === 'cancel') {
+          process.exit(0)
+        }
+
+        if (isValidFile(input)) {
+          validInput = true
+        } else {
+          console.log()
+          console.log(
+            kleur.red('✖'),
+            `Invalid file extension. Expected: ${['.json', '.yaml', '.yml'].join(', ')}.`,
+          )
+          console.log()
+        }
+      }
+    }
+
+    // Convert absolute path to relative path
+    const relativePath = path.relative(process.cwd(), input)
+    const formattedRelativePath =
+      !relativePath.startsWith('../') && !relativePath.startsWith('/')
+        ? `./${relativePath}`
+        : relativePath
 
     configuration.references.push({
       name: 'API Reference',
-      path: input,
+      path: formattedRelativePath,
     })
 
     const content = JSON.stringify(configuration, null, 2)
 
-    console.log()
-    console.log(kleur.bold().white(`    ${CONFIG_FILE}`))
+    // Create `scalar.config.json` file
+    fs.writeFileSync(configFile, content)
+
+    console.log(
+      kleur.green('✔'),
+      'Scalar configuration file created:',
+      kleur.reset().green(`${CONFIG_FILE}`),
+    )
     console.log()
     console.log(
       content
         .trim()
         .split('\n')
-        .map((line) => kleur.grey(`    ${line}`))
+        .map((line) => kleur.grey(`  ${line}`))
         .join('\n'),
     )
     console.log()
-
-    // Create `scalar.config.json` file
-    fs.writeFileSync(configFile, content)
-
-    console.log(kleur.green('Created a new project configuration.'))
     console.log(
       kleur.white(
-        `Run ${kleur
-          .grey()
-          .bold('scalar --help')} to see all available commands.`,
+        `Run ${kleur.magenta('scalar --help')} to see all available commands.`,
       ),
     )
     console.log()
