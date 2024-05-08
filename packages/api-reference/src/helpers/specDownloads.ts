d4d1780bbc8bcc6f9c1977d1286ecb14c6bd64ba
anthony lio <lionnet.ant@gmail.com>
2024-05-08 12:14:06 +0300

    feat: include spec title in downloaded spec filename

--- packages/api-reference/src/helpers/specDownloads.ts
+++ packages/api-reference/src/helpers/specDownloads.ts
@@ -1,12 +1,12 @@
 import { isJsonString } from '@scalar/oas-utils'
 import { type EventBusKey, useEventBus } from '@vueuse/core'
 
-const downloadSpecEventBusKey: EventBusKey<{ id: string }> =
+const downloadSpecEventBusKey: EventBusKey<{ id: string; specTitle?: string }> =
   Symbol('downloadSpec')
 export const downloadSpecBus = useEventBus(downloadSpecEventBusKey)
 
 /** Download the OAS file string */
-export function downloadSpecFile(spec: string) {
+export function downloadSpecFile(spec: string, specTitle?: string) {
   const isJson = isJsonString(spec)
 
   const blob = isJson
@@ -14,11 +14,17 @@ export function downloadSpecFile(spec: string) {
     : new Blob([spec], { type: 'application/x-yaml' })
 
   const data = URL.createObjectURL(blob)
+  const fileExtension = isJson ? '.json' : '.yaml'
+
+  // Default filename based on the file type
+  const defaultFilename = 'spec' + fileExtension
+
+  // Spec title as filename or default
+  const specFilename = specTitle ? specTitle + fileExtension : defaultFilename
 
   const link = document.createElement('a')
   link.href = data
-  link.download = isJson ? 'spec.json' : 'spec.yaml'
-
+  link.download = specFilename
   // this is necessary as link.click() does not work on the latest firefox
   link.dispatchEvent(
     new MouseEvent('click', {
