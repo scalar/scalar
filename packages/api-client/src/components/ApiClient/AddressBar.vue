0640c52937d324145c93fc43192fb143df85d7b2
Anthony Lio <lionnet.ant@gmail.com>
2024-05-10 22:58:32 +0300

    feat: api client address bar style (#1694)
    * fix: remove unused lastRequestTimestamp function
    
    * feat: update api client address bar style
    
    * docs(changeset): feat: api client address bar style update

--- packages/api-client/src/components/ApiClient/AddressBar.vue
+++ packages/api-client/src/components/ApiClient/AddressBar.vue
@@ -79,13 +79,6 @@ async function send() {
   loading.value = false
 }
 
-const lastRequestTimestamp = computed(() => {
-  const lastRequestKey = requestHistoryOrder.value[0]
-  return requestHistory[lastRequestKey]
-    ? humanDiff(requestHistory[lastRequestKey].sentTime)
-    : 'History'
-})
-
 const onChange = (value: string) => {
   if (readOnly.value) {
     return
@@ -132,58 +125,58 @@ const handleRequestMethodChanged = (requestMethod?: string) => {
           withoutTheme
           withVariables
           @change="onChange" />
-      </div>
-      <div
-        v-if="requestHistoryOrder.length"
-        class="history">
         <div
-          class="history-toggle"
-          @click="historyModal.show()">
+          v-if="requestHistoryOrder.length"
+          class="history">
+          <div
+            class="history-toggle"
+            @click="historyModal.show()">
+            <svg
+              fill="none"
+              height="48"
+              viewBox="0 0 14 14"
+              width="48"
+              xmlns="http://www.w3.org/2000/svg">
+              <g id="rewind-clock--back-return-clock-timer-countdown">
+                <path
+                  id="Vector 1561 (Stroke)"
+                  clip-rule="evenodd"
+                  d="M6.99999 2.75C7.4142 2.75 7.74999 3.08579 7.74999 3.5V7.5C7.74999 7.76345 7.61177 8.00758 7.38586 8.14312L4.88586 9.64312C4.53068 9.85623 4.06998 9.74106 3.85687 9.38587C3.64376 9.03069 3.75893 8.56999 4.11412 8.35688L6.24999 7.07536V3.5C6.24999 3.08579 6.58578 2.75 6.99999 2.75Z"
+                  fill="currentColor"
+                  fill-rule="evenodd"></path>
+                <path
+                  id="Union"
+                  clip-rule="evenodd"
+                  d="M12.5 7C12.5 3.96243 10.0376 1.5 7 1.5C5.24916 1.5 3.68853 2.31796 2.68066 3.59456L3.64645 4.56034C3.96143 4.87533 3.73835 5.4139 3.29289 5.4139H0.5C0.223857 5.4139 0 5.19004 0 4.9139V2.121C0 1.67555 0.53857 1.45247 0.853553 1.76745L1.61439 2.52829C2.89781 0.984301 4.83356 0 7 0C10.866 0 14 3.13401 14 7C14 10.866 10.866 14 7 14C3.68902 14 0.916591 11.702 0.187329 8.61473C0.0921059 8.21161 0.341704 7.80762 0.744824 7.7124C1.14794 7.61717 1.55193 7.86677 1.64715 8.26989C2.22013 10.6955 4.40025 12.5 7 12.5C10.0376 12.5 12.5 10.0376 12.5 7Z"
+                  fill="currentColor"
+                  fill-rule="evenodd"></path>
+              </g>
+            </svg>
+          </div>
+        </div>
+        <button
+          class="send-button"
+          :disabled="!formattedUrl.trim().length"
+          type="submit"
+          @click="send">
           <svg
             fill="none"
             height="48"
             viewBox="0 0 14 14"
             width="48"
             xmlns="http://www.w3.org/2000/svg">
-            <g id="rewind-clock--back-return-clock-timer-countdown">
+            <g id="send-email--mail-send-email-paper-airplane">
               <path
-                id="Vector 1561 (Stroke)"
+                id="Subtract"
                 clip-rule="evenodd"
-                d="M6.99999 2.75C7.4142 2.75 7.74999 3.08579 7.74999 3.5V7.5C7.74999 7.76345 7.61177 8.00758 7.38586 8.14312L4.88586 9.64312C4.53068 9.85623 4.06998 9.74106 3.85687 9.38587C3.64376 9.03069 3.75893 8.56999 4.11412 8.35688L6.24999 7.07536V3.5C6.24999 3.08579 6.58578 2.75 6.99999 2.75Z"
-                fill="currentColor"
-                fill-rule="evenodd"></path>
-              <path
-                id="Union"
-                clip-rule="evenodd"
-                d="M12.5 7C12.5 3.96243 10.0376 1.5 7 1.5C5.24916 1.5 3.68853 2.31796 2.68066 3.59456L3.64645 4.56034C3.96143 4.87533 3.73835 5.4139 3.29289 5.4139H0.5C0.223857 5.4139 0 5.19004 0 4.9139V2.121C0 1.67555 0.53857 1.45247 0.853553 1.76745L1.61439 2.52829C2.89781 0.984301 4.83356 0 7 0C10.866 0 14 3.13401 14 7C14 10.866 10.866 14 7 14C3.68902 14 0.916591 11.702 0.187329 8.61473C0.0921059 8.21161 0.341704 7.80762 0.744824 7.7124C1.14794 7.61717 1.55193 7.86677 1.64715 8.26989C2.22013 10.6955 4.40025 12.5 7 12.5C10.0376 12.5 12.5 10.0376 12.5 7Z"
+                d="M11.8215 0.0977331C12.1097 -0.0075178 12.422 -0.0287134 12.7219 0.0367172C13.0248 0.102803 13.3024 0.254481 13.5216 0.473719C13.7409 0.692957 13.8926 0.970537 13.9586 1.27346C14.0241 1.57338 14.0029 1.88566 13.8976 2.17389L10.3236 12.8859L10.3234 12.8866C10.2363 13.15 10.083 13.3867 9.87813 13.5739C9.67383 13.7606 9.42512 13.8917 9.15575 13.9549C8.88633 14.0206 8.60444 14.015 8.33777 13.9388C8.07134 13.8627 7.82929 13.7187 7.63532 13.5209L5.71798 11.6123L3.70392 12.6538C3.54687 12.735 3.3586 12.7272 3.20877 12.6333C3.05895 12.5395 2.96984 12.3734 2.97443 12.1967L3.057 9.01294L10.102 3.89553C10.3812 3.69267 10.4432 3.30182 10.2403 3.02255C10.0375 2.74327 9.64662 2.68133 9.36734 2.88419L2.20286 8.0884L0.473156 6.35869L0.473098 6.35864L0.472971 6.35851C0.285648 6.17132 0.147746 5.94054 0.0716498 5.68688C-0.00390565 5.43503 -0.016181 5.16847 0.0358684 4.91079C0.087985 4.62928 0.213827 4.36658 0.400607 4.14951C0.588668 3.93095 0.831681 3.76658 1.10453 3.67339L1.1079 3.67224L1.1079 3.67225L11.8215 0.0977331Z"
                 fill="currentColor"
                 fill-rule="evenodd"></path>
             </g>
           </svg>
-        </div>
+          <span>Send</span>
+        </button>
       </div>
-      <button
-        class="send-button"
-        :disabled="!formattedUrl.trim().length"
-        type="submit"
-        @click="send">
-        <svg
-          fill="none"
-          height="48"
-          viewBox="0 0 14 14"
-          width="48"
-          xmlns="http://www.w3.org/2000/svg">
-          <g id="send-email--mail-send-email-paper-airplane">
-            <path
-              id="Subtract"
-              clip-rule="evenodd"
-              d="M11.8215 0.0977331C12.1097 -0.0075178 12.422 -0.0287134 12.7219 0.0367172C13.0248 0.102803 13.3024 0.254481 13.5216 0.473719C13.7409 0.692957 13.8926 0.970537 13.9586 1.27346C14.0241 1.57338 14.0029 1.88566 13.8976 2.17389L10.3236 12.8859L10.3234 12.8866C10.2363 13.15 10.083 13.3867 9.87813 13.5739C9.67383 13.7606 9.42512 13.8917 9.15575 13.9549C8.88633 14.0206 8.60444 14.015 8.33777 13.9388C8.07134 13.8627 7.82929 13.7187 7.63532 13.5209L5.71798 11.6123L3.70392 12.6538C3.54687 12.735 3.3586 12.7272 3.20877 12.6333C3.05895 12.5395 2.96984 12.3734 2.97443 12.1967L3.057 9.01294L10.102 3.89553C10.3812 3.69267 10.4432 3.30182 10.2403 3.02255C10.0375 2.74327 9.64662 2.68133 9.36734 2.88419L2.20286 8.0884L0.473156 6.35869L0.473098 6.35864L0.472971 6.35851C0.285648 6.17132 0.147746 5.94054 0.0716498 5.68688C-0.00390565 5.43503 -0.016181 5.16847 0.0358684 4.91079C0.087985 4.62928 0.213827 4.36658 0.400607 4.14951C0.588668 3.93095 0.831681 3.76658 1.10453 3.67339L1.1079 3.67224L1.1079 3.67225L11.8215 0.0977331Z"
-              fill="currentColor"
-              fill-rule="evenodd"></path>
-          </g>
-        </svg>
-        <span>Send</span>
-      </button>
     </div>
     <div
       class="address-bar-close"
@@ -225,7 +218,7 @@ const handleRequestMethodChanged = (requestMethod?: string) => {
 }
 .address-bar {
   width: 100%;
-  padding: 12px 12px 10px 12px;
+  padding: 10px;
   display: flex;
   align-items: center;
   position: relative;
@@ -245,25 +238,28 @@ const handleRequestMethodChanged = (requestMethod?: string) => {
   align-items: center;
 }
 .url-form-field {
-  border-right: 0;
-  background: var(--scalar-background-2);
-  border-radius: var(--scalar-radius) 0 0 var(--scalar-radius);
-  display: flex;
   align-items: stretch;
-  width: 100%;
-  overflow: hidden;
+  border: 1px solid var(--scalar-border-color);
+  border-radius: var(--scalar-radius);
+  display: flex;
   min-height: 31px;
+  overflow: hidden;
+  padding: 2px;
+  width: 100%;
 }
+
 .url-form-input {
+  background: var(--scalar-background-1);
   color: var(--scalar-color-1);
-}
-.url-form-input {
   font-weight: var(--scalar-semibold);
   min-height: auto;
   padding-top: 0;
 }
 .url-form-input :deep(.cm-scroller) {
   overflow-y: hidden;
+}
+.url-form-input :deep(.cm-line) {
+  font-size: var(--scalar-micro);
 }
 .history {
   appearance: none;
@@ -273,7 +269,6 @@ const handleRequestMethodChanged = (requestMethod?: string) => {
   display: flex;
   align-items: center;
   border-radius: var(--scalar-radius);
-  background: var(--scalar-background-2);
 }
 
 .send-button[type='submit'] {
@@ -282,12 +277,12 @@ const handleRequestMethodChanged = (requestMethod?: string) => {
   color: var(--scalar-button-1-color);
   border: none;
   white-space: nowrap;
-  padding: 0 12px;
+  padding: 0 8px;
   cursor: pointer;
   outline: none;
   font-family: var(--scalar-font);
   font-weight: var(--scalar-semibold);
-  border-radius: 0 var(--scalar-radius) var(--scalar-radius) 0;
+  border-radius: var(--scalar-radius);
   background: var(--scalar-button-1);
   position: relative;
   display: flex;
@@ -326,13 +321,10 @@ const handleRequestMethodChanged = (requestMethod?: string) => {
   border: 1px solid var(--scalar-border-color);
 }
 .history-toggle {
-  padding: 0 12px;
+  padding: 7px;
   line-height: 30px;
   color: var(--scalar-color-3);
   font-size: var(--scalar-micro);
-  letter-spacing: 0.125px;
-  font-weight: var(--scalar-semibold);
-  text-transform: uppercase;
   height: 100%;
   display: flex;
   align-items: center;
@@ -340,8 +332,14 @@ const handleRequestMethodChanged = (requestMethod?: string) => {
   white-space: nowrap;
   border-radius: var(--scalar-radius);
   user-select: none;
+  border-radius: var(--scalar-radius);
+  margin-right: 4px;
+  transition:
+    background-color 0.15s ease-in-out,
+    color 0.15s ease-in-out;
 }
 .history-toggle:hover {
+  background-color: var(--scalar-background-2);
   color: var(--scalar-color-1);
 }
 .history-toggle svg {
