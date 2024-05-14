0978906c3c87fbc4af621a4217832cdbaf1a2813
tmastrom <thomas.mastromonaco@gmail.com>
2024-05-14 15:29:45 -0700

    test: playwright test setup in valid nuxt example

---
+++ packages/nuxt/test/example.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from '@nuxt/test-utils/playwright'
+import { fileURLToPath } from 'node:url'
+
+test.use({
+  nuxt: {
+    rootDir: fileURLToPath(new URL('..', import.meta.url)),
+    browser: true,
+    server: true,
+  },
+})
+
+test('test', async ({ page, goto }) => {
+  await goto('/', { waitUntil: 'hydration' })
+  await expect(page.getByRole('heading')).toHaveText('Welcome to Playwright!')
+})
