1de30c947c73d148d36e73cf4b1a6a57fdccbdd7
Hans Pagel <mail@hanspagel.com>
2024-05-21 15:39:34 +0200

    fix: tests

--- packages/oas-utils/src/normalizeMimeType.test.ts
+++ packages/oas-utils/src/normalizeMimeType.test.ts
@@ -18,32 +18,24 @@ describe('normalizeMimeType', () => {
   it('removes whitespace', async () => {
     const content = ' application/json '
 
-    expect(normalizeMimeType(content)).toBe({
-      'application/json': {},
-    })
+    expect(normalizeMimeType(content)).toBe('application/json')
   })
 
   it('removes mimetype variants', async () => {
     const content = 'application/problem+json'
 
-    expect(normalizeMimeType(content)).toBe({
-      'application/json': {},
-    })
+    expect(normalizeMimeType(content)).toBe('application/json')
   })
 
   it('removes mimetype variants with special characters', async () => {
     const content = 'application/vnd.api+json'
 
-    expect(normalizeMimeType(content)).toBe({
-      'application/json': {},
-    })
+    expect(normalizeMimeType(content)).toBe('application/json')
   })
 
   it('removes all the clutter', async () => {
     const content = 'application/problem-foobar+json; charset=utf-8'
 
-    expect(normalizeMimeType(content)).toBe({
-      'application/json': {},
-    })
+    expect(normalizeMimeType(content)).toBe('application/json')
   })
 })
