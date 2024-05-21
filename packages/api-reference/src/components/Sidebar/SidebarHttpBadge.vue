077fe7b895ebd9c02e4a1d5b2f41e0a1903c24d9
Cameron Rohani <cameronrohani@users.noreply.github.com>
2024-05-21 10:04:39 -0700

    fix: make request type more legible + minimal (#1802)

--- packages/api-reference/src/components/Sidebar/SidebarHttpBadge.vue
+++ packages/api-reference/src/components/Sidebar/SidebarHttpBadge.vue
@@ -19,8 +19,6 @@ defineProps<{
   display: block;
   min-width: 3.9em;
   overflow: hidden;
-  border-radius: 30px;
-  padding: 0 3px;
   line-height: 14px;
   flex-shrink: 0;
   color: white;
@@ -29,27 +27,13 @@ defineProps<{
     var(--method-color, var(--scalar-color-1)),
     transparent 0%
   );
-  background: var(--method-color, var(--scalar-background-3));
-  background: color-mix(
-    in srgb,
-    var(--method-color, var(--scalar-background-3)),
-    transparent 90%
-  );
   text-transform: uppercase;
-  font-size: 8.5px;
+  font-size: 10px;
   font-weight: bold;
-  text-align: center;
+  text-align: right;
   position: relative;
   font-family: var(--scalar-font-code);
   white-space: nowrap;
   margin-left: 3px;
-}
-.sidebar-heading-type-active {
-  background: var(--method-color, transparent);
-  color: color-mix(in srgb, var(--method-color), white 85%);
-}
-.dark-mode .sidebar-heading-type-active {
-  background: var(--method-color, transparent);
-  color: color-mix(in srgb, var(--method-color), black 80%);
 }
 </style>
