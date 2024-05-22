3b0934450e258850e74ce7f26febbf1d32c764cb
Anthony Lio <lionnet.ant@gmail.com>
2024-05-22 20:47:12 +0300

    feat: scalar cli default display (#1824)
    * feat: scalar cli error display
    
    * feat: scalar cli default display
    
    * docs(changeset): feat: scalar cli default display

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -19,14 +19,20 @@ program
   .description('CLI to work with your OpenAPI files')
   .version(version)
 
-program.addCommand(InitCommand(), {
-  isDefault: true,
-})
+/** display help in case of error */
+program.showHelpAfterError()
+
+program.addCommand(InitCommand())
 program.addCommand(FormatCommand())
 program.addCommand(ValidateCommand())
 program.addCommand(BundleCommand())
 program.addCommand(ServeCommand())
 program.addCommand(MockCommand())
 program.addCommand(ShareCommand())
+
+/** display help if no argument has been provided */
+if (process.argv.length === 2) {
+  program.help()
+}
 
 program.parse()
