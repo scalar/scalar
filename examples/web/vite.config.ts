bb35c7bc8477dd248f6095df728f055eb829019d
Amrit <amrit@scalar.com>
2024-05-24 12:13:28 -0700

    fix: remove alias from example

--- examples/web/vite.config.ts
+++ examples/web/vite.config.ts
@@ -9,18 +9,4 @@ export default defineConfig({
     port: 5050,
     open: true,
   },
-  // resolve: {
-  //   alias: [
-  //     // Resolve the uncompiled source code for all @scalar packages
-  //     // It’s working with the alias, too. It’s just required to enable HMR.
-  //     // It also does not match components since we want the built version
-  //     {
-  //       // Resolve the uncompiled source code for all @scalar packages
-  //       // @scalar/* -> packages/*/
-  //       // (not @scalar/*/style.css)
-  //       find: /^@scalar\/(?!(openapi-parser|snippetz|galaxy|components\/style\.css|components\b))(.+)/,
-  //       replacement: path.resolve(__dirname, '../../packages/$2/src/index.ts'),
-  //     },
-  //   ],
-  // },
 })
