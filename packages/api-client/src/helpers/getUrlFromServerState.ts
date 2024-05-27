f75a3c1ab7999ea36d4936084c74b895d094dc41
Hans Pagel <mail@hanspagel.com>
2024-05-27 14:44:13 +0200

    fix: replaceVariables gets wrong format

--- packages/api-client/src/helpers/getUrlFromServerState.ts
+++ packages/api-client/src/helpers/getUrlFromServerState.ts
@@ -8,10 +8,19 @@ export function getUrlFromServerState(state: ServerState) {
   // Get the selected server
   const server = state?.servers?.[state.selectedServer ?? 0]
 
+  // Make variables an key value object
+  const variables = state.variables.reduce(
+    (acc, variable) => {
+      acc[variable.name] = variable.value
+      return acc
+    },
+    {} as Record<string, string | number>,
+  )
+
   // Replace variables: {protocol}://{host}:{port}/{basePath}
   const url =
     typeof server?.url === 'string'
-      ? replaceVariables(server?.url, state.variables)
+      ? replaceVariables(server?.url, variables)
       : server?.url
 
   return url
