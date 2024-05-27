c899a6f69d2118691e0dcce29881c6ceadf5c1ae
Hans Pagel <mail@hanspagel.com>
2024-05-27 16:33:28 +0200

    fix: TS issue

--- packages/api-client/src/helpers/getRequestFromAuthentication.test.ts
+++ packages/api-client/src/helpers/getRequestFromAuthentication.test.ts
@@ -199,6 +199,8 @@ describe('getRequestFromAuthentication', () => {
           scopes: [],
           accessToken: '',
           state: '',
+          username: '',
+          password: '',
         },
       },
       [
