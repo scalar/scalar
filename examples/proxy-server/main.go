692430d1b25944836ba47b81f0a6d34d3d75187b
Marc Laventure <marc@scalar.com>
2024-05-28 12:38:36 -0700

    fix: set allowed headers before options finish (#1879)

--- examples/proxy-server/main.go
+++ examples/proxy-server/main.go
@@ -14,14 +14,13 @@ func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 		// Handle pre-flight requests
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
-
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
 		// Pass down the request to the next middleware (or final handler)
 		next.ServeHTTP(w, r)
