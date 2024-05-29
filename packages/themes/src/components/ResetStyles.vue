a839889f762a7b63db712f860be5d9fd1a7c28df
Brynn <brynn@hawker.me>
2024-05-29 15:35:26 -0600

    fix: also use cascade layers for CSS reset to avoid overriding tailwind (#1898)

--- packages/themes/src/components/ResetStyles.vue
+++ packages/themes/src/components/ResetStyles.vue
@@ -12,31 +12,31 @@ useApplyClasses('#headlessui-portal-root', reset)
   <slot :styles="reset" />
 </template>
 <style lang="postcss" module>
-/* Use :where to lower specificity to 0 */
-:where(.reset) :global {
-  /** Add some more things which are normally applied to `html`. */
-  font-family: var(--scalar-font);
-  line-height: 1.15;
-  -webkit-text-size-adjust: 100%;
-  -moz-tab-size: 4;
-  tab-size: 4;
+@layer scalar-base {
+  .reset :global {
+    /** Add some more things which are normally applied to `html`. */
+    font-family: var(--scalar-font);
+    line-height: 1.15;
+    -webkit-text-size-adjust: 100%;
+    -moz-tab-size: 4;
+    tab-size: 4;
 
-  /** Make sure box-sizing is set properly. */
-  *,
-  *:before,
-  *:after {
-    box-sizing: border-box;
-  }
+    /** Make sure box-sizing is set properly. */
+    *,
+    *:before,
+    *:after {
+      box-sizing: border-box;
+    }
 
-  /** Smooth text rendering */
-  * {
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
+    /** Smooth text rendering */
+    * {
+      text-rendering: optimizeLegibility;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
 
-  /* prettier-ignore */
-  h1, h2, h3, h4, h5, h6, i, p, ol, ul, dl, dd, dt, em, strong, button {
+    /* prettier-ignore */
+    h1, h2, h3, h4, h5, h6, i, p, ol, ul, dl, dd, dt, em, strong, button {
     margin: unset;
     padding: unset;
     color: unset;
@@ -44,40 +44,41 @@ useApplyClasses('#headlessui-portal-root', reset)
     font-style: unset;
   }
 
-  button {
-    border: unset;
-    background: unset;
-    appearance: unset;
-    font-family: var(--scalar-font);
-  }
+    button {
+      border: unset;
+      background: unset;
+      appearance: unset;
+      font-family: var(--scalar-font);
+    }
 
-  input::placeholder {
-    color: var(--scalar-color-3);
-    font-family:
-      var(--scalar-font),
-      'Inter',
-      -apple-system,
-      BlinkMacSystemFont,
-      'Segoe UI',
-      Roboto,
-      Oxygen,
-      Ubuntu,
-      Cantarell,
-      'Open Sans',
-      'Helvetica Neue',
-      sans-serif;
-  }
-  input:-ms-input-placeholder {
-    color: var(--scalar-color-3);
-    font-family: var(--scalar-font);
-  }
-  input::-webkit-input-placeholder {
-    color: var(--scalar-color-3);
-    font-family: var(--scalar-font);
-  }
-  /** Remove yellow/blue autofill indicator */
-  input:-webkit-autofill {
-    background-clip: text !important;
+    input::placeholder {
+      color: var(--scalar-color-3);
+      font-family:
+        var(--scalar-font),
+        'Inter',
+        -apple-system,
+        BlinkMacSystemFont,
+        'Segoe UI',
+        Roboto,
+        Oxygen,
+        Ubuntu,
+        Cantarell,
+        'Open Sans',
+        'Helvetica Neue',
+        sans-serif;
+    }
+    input:-ms-input-placeholder {
+      color: var(--scalar-color-3);
+      font-family: var(--scalar-font);
+    }
+    input::-webkit-input-placeholder {
+      color: var(--scalar-color-3);
+      font-family: var(--scalar-font);
+    }
+    /** Remove yellow/blue autofill indicator */
+    input:-webkit-autofill {
+      background-clip: text !important;
+    }
   }
 }
 </style>
--- packages/themes/src/components/ScrollbarStyles.vue
+++ packages/themes/src/components/ScrollbarStyles.vue
@@ -12,64 +12,65 @@ useApplyClasses('#headlessui-portal-root', scrollbars)
   <slot :styles="scrollbars" />
 </template>
 <style lang="postcss" module>
-/* Use :where to lower specificity to 0 */
-:where(.scrollbars) :global {
-  .cm-scroller,
-  .custom-scroll {
-    overflow-y: auto;
-    scrollbar-color: transparent transparent;
-    scrollbar-width: thin;
-    -webkit-overflow-scrolling: touch;
-  }
-  .custom-scroll-self-contain-overflow {
-    overscroll-behavior: contain;
-  }
-  @supports (-moz-appearance: none) {
+@layer scalar-utilities {
+  .scrollbars :global {
     .cm-scroller,
     .custom-scroll {
-      padding-right: 12px;
+      overflow-y: auto;
+      scrollbar-color: transparent transparent;
+      scrollbar-width: thin;
+      -webkit-overflow-scrolling: touch;
     }
-  }
-  .cm-scroller:hover,
-  .custom-scroll:hover {
-    scrollbar-color: var(--scalar-scrollbar-color) transparent;
-  }
-  .cm-scroller:hover::-webkit-scrollbar-thumb,
-  .custom-scroll:hover::-webkit-scrollbar-thumb {
-    background: var(--scalar-scrollbar-color);
-    background-clip: content-box;
-    border: 3px solid transparent;
-  }
-  .cm-scroller::-webkit-scrollbar-thumb:active,
-  .custom-scroll::-webkit-scrollbar-thumb:active {
-    background: var(--scalar-scrollbar-color-active);
-    background-clip: content-box;
-    border: 3px solid transparent;
-  }
-  .cm-scroller::-webkit-scrollbar-corner,
-  .custom-scroll::-webkit-scrollbar-corner {
-    background: transparent;
-  }
-  .cm-scroller::-webkit-scrollbar,
-  .custom-scroll::-webkit-scrollbar {
-    height: 12px;
-    width: 12px;
-  }
-  .cm-scroller::-webkit-scrollbar-track,
-  .custom-scroll::-webkit-scrollbar-track {
-    background: transparent;
-  }
-  .cm-scroller::-webkit-scrollbar-thumb,
-  .custom-scroll::-webkit-scrollbar-thumb {
-    border-radius: 20px;
-    background: transparent;
-    background-clip: content-box;
-    border: 3px solid transparent;
-  }
-  @media (pointer: coarse) {
-    .cm-scroller,
-    .custom-scroll {
-      padding-right: 12px;
+    .custom-scroll-self-contain-overflow {
+      overscroll-behavior: contain;
+    }
+    @supports (-moz-appearance: none) {
+      .cm-scroller,
+      .custom-scroll {
+        padding-right: 12px;
+      }
+    }
+    .cm-scroller:hover,
+    .custom-scroll:hover {
+      scrollbar-color: var(--scalar-scrollbar-color) transparent;
+    }
+    .cm-scroller:hover::-webkit-scrollbar-thumb,
+    .custom-scroll:hover::-webkit-scrollbar-thumb {
+      background: var(--scalar-scrollbar-color);
+      background-clip: content-box;
+      border: 3px solid transparent;
+    }
+    .cm-scroller::-webkit-scrollbar-thumb:active,
+    .custom-scroll::-webkit-scrollbar-thumb:active {
+      background: var(--scalar-scrollbar-color-active);
+      background-clip: content-box;
+      border: 3px solid transparent;
+    }
+    .cm-scroller::-webkit-scrollbar-corner,
+    .custom-scroll::-webkit-scrollbar-corner {
+      background: transparent;
+    }
+    .cm-scroller::-webkit-scrollbar,
+    .custom-scroll::-webkit-scrollbar {
+      height: 12px;
+      width: 12px;
+    }
+    .cm-scroller::-webkit-scrollbar-track,
+    .custom-scroll::-webkit-scrollbar-track {
+      background: transparent;
+    }
+    .cm-scroller::-webkit-scrollbar-thumb,
+    .custom-scroll::-webkit-scrollbar-thumb {
+      border-radius: 20px;
+      background: transparent;
+      background-clip: content-box;
+      border: 3px solid transparent;
+    }
+    @media (pointer: coarse) {
+      .cm-scroller,
+      .custom-scroll {
+        padding-right: 12px;
+      }
     }
   }
 }
