44495c8fa50b9ec674c70097a70b9c492ba210ac
Brynn Hawker <brynn@hawker.me>
2024-05-29 22:12:38 -0600

    chore: mandate stroke property options

--- packages/components/src/components/ScalarIcon/.svglintrc.js
+++ packages/components/src/components/ScalarIcon/.svglintrc.js
@@ -6,6 +6,7 @@ export default {
       // Don't allow certain elements
       clipPath: false,
       desc: false,
+      g: false,
     },
     attr: [
       {
@@ -16,23 +17,27 @@ export default {
         // ViewBox and namespace must be set
         'viewBox': true,
         'xmlns': 'http://www.w3.org/2000/svg',
-        // Allow fill and stroke properties
-        'fill': ['currentColor', 'none'],
-        'stroke?': 'currentColor',
-        'stroke-linecap?': 'round',
-        'stroke-linejoin?': 'round',
+        // Require fill property
+        'fill': true,
+        // Allow stroke properties
+        'stroke?': true,
+        'stroke-linecap?': true,
+        'stroke-linejoin?': true,
         // Make sure the svg is responsive
         'width': false,
         'height': false,
       },
       {
-        // Ensure that the SVG children have appropriate attributes
-        'rule::selector': 'svg *',
-        // Allow fill and stroke properties
-        'fill?': ['currentColor', 'none'],
-        'stroke?': 'currentColor',
-        'stroke-linecap?': 'round',
-        'stroke-linejoin?': 'round',
+        // Require fills to be `none` or `currentColor`
+        'rule::selector': '[fill]',
+        'fill': ['currentColor', 'none'],
+      },
+      {
+        // Require stroke properties to be set if stroke is set
+        'rule::selector': '[stroke]',
+        'stroke': 'currentColor',
+        'stroke-linecap': 'round',
+        'stroke-linejoin': 'round',
         // Make sure to not override stroke width
         'stroke-width': false,
       },
