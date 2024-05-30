e9837c05b564636213783f734e5d8d07dec84eb9
Brynn Hawker <brynn@hawker.me>
2024-05-29 21:36:46 -0600

    chore: add attribute linting for root element

--- packages/components/src/components/ScalarIcon/.svglintrc.js
+++ packages/components/src/components/ScalarIcon/.svglintrc.js
@@ -1,7 +1,29 @@
 export default {
   rules: {
     elm: {
+      // Only allow one <svg> node
       svg: 1,
     },
+    attr: [
+      {
+        // Ensure that the SVG element has the appropriate attributes
+        'rule::selector': 'svg',
+        // Don't allow extraneous attributes
+        'rule::whitelist': true,
+        // ViewBox and namespace must be set
+        'viewBox': true,
+        'xmlns': 'http://www.w3.org/2000/svg',
+        // Allow fill and stroke properties
+        'fill?': ['currentColor', 'none'],
+        'stroke?': 'currentColor',
+        'stroke-linecap?': 'round',
+        'stroke-linejoin?': 'round',
+        // Make sure to not override stroke width
+        'stroke-width': false,
+        // Make sure the svg is responsive
+        'width': false,
+        'height': false,
+      },
+    ],
   },
 }
