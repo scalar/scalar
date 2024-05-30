4a7002123e3164637663c07872411a9d561930b8
anthony lio <lionnet.ant@gmail.com>
2024-05-30 21:54:51 +0300

    fix: remove console log in use nav state

--- packages/api-reference/src/hooks/useNavState.ts
+++ packages/api-reference/src/hooks/useNavState.ts
@@ -67,7 +67,6 @@ const getTagId = ({ name }: Tag) => {
 
 // Grabs the sectionId of the hash to open the section before scrolling
 const getSectionId = (hashStr = hash.value) => {
-  console.log('hashStr', hash.value)
   const tagId = hashStr.match(/(tag\/[^/]+)/)?.[0]
   const modelId = hashStr.startsWith('model') ? 'models' : ''
   const webhookId = hashStr.startsWith('webhook') ? 'webhooks' : ''
