be64ac2b6d3c9893a4108c2a8dfd75fb93631e78
Brynn Hawker <brynn@hawker.me>
2024-05-31 16:48:21 -0600

    fix: copy fonts css to build

--- packages/themes/vite.config.ts
+++ packages/themes/vite.config.ts
@@ -23,6 +23,10 @@ export default defineConfig({
           src: 'src/presets',
           dest: './',
         },
+        {
+          src: 'src/fonts',
+          dest: './',
+        },
       ],
     }),
   ],
