f416db8a1e61c3f248dd9dd1726e6085b0abffba
anthony lio <lionnet.ant@gmail.com>
2024-06-04 10:34:34 +0300

    feat: add folder selector in action modal request

--- packages/client-app/src/components/ActionModal/ActionModalRequest.vue
+++ packages/client-app/src/components/ActionModal/ActionModalRequest.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import Request from '@/assets/ascii/request.ascii?raw'
-import { REQUEST_METHODS } from '@/components/HttpMethod/httpMethods'
+import HttpMethod from '@/components/HttpMethod/HttpMethod.vue'
 import ScalarAsciiArt from '@/components/ScalarAsciiArt.vue'
 import { useWorkspace } from '@/store/workspace'
-import { ScalarButton, ScalarIcon } from '@scalar/components'
+import { ScalarButton, ScalarIcon, ScalarListbox } from '@scalar/components'
 import {
   type RequestRef,
   createRequestInstance,
 } from '@scalar/oas-utils/entities/workspace/spec'
 import { nanoid } from 'nanoid'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 defineProps<{
@@ -24,7 +24,23 @@ const { push } = useRouter()
 
 const { requestMutators, collectionMutators, activeCollection } = useWorkspace()
 const requestName = ref('')
-const requestMethod = ref('')
+const requestMethod = ref('Get')
+const selectedFolderId = ref('')
+
+const folders = computed(() => {
+  if (!activeCollection.value) return []
+  return Object.values(activeCollection.value.folders).map((folder) => ({
+    id: folder.uid,
+    label: folder.name,
+  }))
+})
+
+const selectedFolder = computed({
+  get: () => folders.value.find(({ id }) => id === selectedFolderId.value),
+  set: (opt) => {
+    if (opt?.id) selectedFolderId.value = opt.id
+  },
+})
 
 function handleChangeMethod(method: string) {
   requestMethod.value = method
@@ -34,7 +50,7 @@ function handleSubmit() {
   if (!activeCollection.value) return
   const newRequest: RequestRef = {
     uid: nanoid(),
-    path: requestName.value,
+    path: '',
     method: requestMethod.value,
     description: requestName.value,
     operationId: requestName.value,
@@ -58,6 +74,19 @@ function handleSubmit() {
     activeCollection.value.requests.concat(newRequest.uid),
   )
 
+  const folderUid = selectedFolderId.value
+  if (folderUid) {
+    const folder = activeCollection.value.folders[folderUid]
+    folder.children.push(newRequest.uid)
+  } else {
+    // If no folder is selected, add to the root of the collection
+    collectionMutators.edit(
+      0,
+      'requests',
+      activeCollection.value.requests.concat(newRequest.uid),
+    )
+  }
+
   push(`/request/${newRequest.uid}`)
 
   emits('close')
@@ -69,31 +98,34 @@ function handleSubmit() {
   <form
     class="flex w-full flex-col gap-3"
     @submit.prevent="handleSubmit">
-    <input
-      v-model="requestName"
-      class="h-10 rounded border p-2"
-      label="Request Name"
-      placeholder="Request Name" />
-    <div
-      class="divide divide-b-3 flex flex-col divide-y overflow-hidden rounded border">
-      <button
-        v-for="method in Object.keys(REQUEST_METHODS)"
-        :key="method"
-        class="hover:bg-b-2 relative flex h-10 items-center justify-between p-2 after:block after:h-5 after:w-5 after:rounded-full after:border after:content-['']"
-        :class="{
-          'after:bg-b-btn': method === requestMethod,
-        }"
-        type="button"
-        @click="handleChangeMethod(method)">
-        <ScalarIcon
-          v-if="method === requestMethod"
-          class="text-b-1 absolute right-3"
-          icon="Checkmark"
-          size="xs" />
-        <span class="text-xxs capitalize">
-          {{ method }}
-        </span>
-      </button>
+    <div class="flex min-h-10 gap-3 rounded border p-1">
+      <HttpMethod
+        :isEditable="true"
+        isSquare
+        :method="requestMethod"
+        @change="handleChangeMethod" />
+      <input
+        v-model="requestName"
+        label="Request Name"
+        placeholder="Request Name" />
+    </div>
+    <div>
+      <span class="mb-1 block font-medium">Inside:</span>
+      <ScalarListbox
+        v-model="selectedFolder"
+        :options="folders"
+        resize>
+        <button
+          class="relative flex h-full min-h-10 w-full cursor-pointer items-center justify-between rounded border p-2"
+          type="button">
+          <span :class="selectedFolder ? 'text-c-1' : 'text-c-3'">{{
+            selectedFolder ? selectedFolder.label : 'Select Folder'
+          }}</span>
+          <ScalarIcon
+            icon="ChevronDown"
+            size="xs" />
+        </button>
+      </ScalarListbox>
     </div>
     <ScalarButton type="submit"> Create </ScalarButton>
   </form>
