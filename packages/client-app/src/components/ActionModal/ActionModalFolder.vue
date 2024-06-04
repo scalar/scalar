52653a0cd3a74cbaab277c23d238feedcc01cba2
anthony lio <lionnet.ant@gmail.com>
2024-06-04 10:04:33 +0300

    feat: add collection selector in action modal folder

--- packages/client-app/src/components/ActionModal/ActionModalFolder.vue
+++ packages/client-app/src/components/ActionModal/ActionModalFolder.vue
@@ -2,8 +2,8 @@
 import Folder from '@/assets/ascii/folder.ascii?raw'
 import ScalarAsciiArt from '@/components/ScalarAsciiArt.vue'
 import { useWorkspace } from '@/store/workspace'
-import { ScalarButton } from '@scalar/components'
-import { ref } from 'vue'
+import { ScalarButton, ScalarIcon, ScalarListbox } from '@scalar/components'
+import { computed, ref } from 'vue'
 
 defineProps<{
   title: string
@@ -13,14 +13,38 @@ const emits = defineEmits<{
   (event: 'close'): void
 }>()
 
-const { collectionMutators } = useWorkspace()
+const { collectionMutators, workspace } = useWorkspace()
 const folderName = ref('')
+const selectedCollectionId = ref('')
 
-function handleSubmit() {
-  if (folderName.value) {
-    /** TODO: set parent collection */
-    collectionMutators.addFolder(0, null, { name: folderName.value })
-    emits('close')
+const availableCollections = computed(() => {
+  return workspace.collections.map((collection) => ({
+    id: collection.uid,
+    label: collection.spec?.info?.title ?? '',
+  }))
+})
+
+const selectedCollection = computed({
+  get: () =>
+    availableCollections.value.find(
+      ({ id }) => id === selectedCollectionId.value,
+    ),
+  set: (opt) => {
+    if (opt?.id) selectedCollectionId.value = opt.id
+  },
+})
+
+const handleSubmit = () => {
+  if (folderName.value && selectedCollection.value) {
+    const collectionIdx = workspace.collections.findIndex(
+      (collection) => collection.uid === selectedCollectionId.value,
+    )
+    if (collectionIdx !== -1) {
+      collectionMutators.addFolder(collectionIdx, null, {
+        name: folderName.value,
+      })
+      emits('close')
+    }
   }
 }
 </script>
@@ -35,6 +59,24 @@ function handleSubmit() {
       class="h-10 rounded border p-2"
       label="Folder Name"
       placeholder="Folder Name" />
+    <div>
+      <span class="mb-1 block font-medium">Inside:</span>
+      <ScalarListbox
+        v-model="selectedCollection"
+        :options="availableCollections"
+        resize>
+        <button
+          class="relative flex h-full min-h-10 w-full cursor-pointer items-center justify-between rounded border p-2"
+          type="button">
+          <span :class="selectedCollection ? 'text-c-1' : 'text-c-3'">{{
+            selectedCollection ? selectedCollection.label : 'Select Collection'
+          }}</span>
+          <ScalarIcon
+            icon="ChevronDown"
+            size="xs" />
+        </button>
+      </ScalarListbox>
+    </div>
     <ScalarButton type="submit"> Create Folder </ScalarButton>
   </form>
 </template>
