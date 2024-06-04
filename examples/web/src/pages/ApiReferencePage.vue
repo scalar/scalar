46e42d338dc943bd8c9ecaf6a8348add73632daa
Geoff Scott <geoffgscott@gmail.com>
2024-06-04 13:36:31 -0700

    fix: Undo .value

--- examples/web/src/pages/ApiReferencePage.vue
+++ examples/web/src/pages/ApiReferencePage.vue
@@ -77,7 +77,7 @@ const parsedSpec = asyncComputed(
 <template>
   <ApiReferenceLayout
     :configuration="configuration"
-    :parsedSpec="parsedSpec.value"
+    :parsedSpec="parsedSpec"
     :rawSpec="content"
     @changeTheme="configuration.theme = $event.id"
     @updateContent="(v) => (content = v)">
