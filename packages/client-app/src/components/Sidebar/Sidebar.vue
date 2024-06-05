9744eab92bf9a564b0ef607515bd639095e39117
zameron <cameron.rohani@gmail.com>
2024-06-05 11:46:26 -0700

    fix: client scrolling in sidebar + dont remove request type on hover

--- packages/client-app/src/components/Sidebar/Sidebar.vue
+++ packages/client-app/src/components/Sidebar/Sidebar.vue
@@ -8,7 +8,7 @@ import { themeClasses } from '@/constants'
     <div class="min-h-header h-header flex items-center border-b px-4 text-sm">
       <h2 class="font-medium"><slot name="title" /></h2>
     </div>
-    <div class="custom-scroll sidebar-height overflow-visible">
+    <div class="custom-scroll sidebar-height">
       <slot name="content" />
     </div>
     <slot name="button" />
