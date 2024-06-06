9b67135534990550f1f83c99a8afbd0c27428c24
Amrit Kahlon <amrit@scalar.com>
2024-06-06 11:02:51 -0700

    Revert "fix: build breaking (#1973)" (#1975)
    This reverts commit fcdf7d6f296691848005f84639d0e5179c7da2b1.

--- packages/object-utils/vite.config.ts
+++ packages/object-utils/vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
     port: 9000,
   },
   build: createViteBuildOptions({
-    entry: await findEntryPoints({ allowCss: false }),
+    entry: ['src/index.ts'],
   }),
 })
