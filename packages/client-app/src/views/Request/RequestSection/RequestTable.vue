a8d429938438d5d8d8d08f397c3c407e1074f28c
anthony lio <lionnet.ant@gmail.com>
2024-06-06 09:11:40 +0300

    feat: handle file uploads table ui

--- packages/client-app/src/views/Request/RequestSection/RequestTable.vue
+++ packages/client-app/src/views/Request/RequestSection/RequestTable.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import DataTable from '@/components/DataTable/DataTable.vue'
+import DataTableCell from '@/components/DataTable/DataTableCell.vue'
 import DataTableCheckbox from '@/components/DataTable/DataTableCheckbox.vue'
 import DataTableInput from '@/components/DataTable/DataTableInput.vue'
 import DataTableRow from '@/components/DataTable/DataTableRow.vue'
@@ -69,15 +70,16 @@ const handleFileUpload = (idx: number) => {
         @input="items && idx === items.length - 1 && emit('addRow')"
         @selectVariable="(v) => handleSelectVariable(idx, 'value', v)"
         @update:modelValue="(v) => emit('updateRow', idx, 'value', v)" />
-      <div
+      <DataTableCell
         v-if="showUploadButton"
-        class="p-1">
+        class="group/upload p-1 overflow-hidden relative text-ellipsis whitespace-nowrap">
         <template v-if="item.binary">
           <span class="text-c-2">{{ item.binary?.name }}</span>
           <button
+            class="absolute backdrop-blur-sm border centered-x centered-y hidden rounded text-center p-0.5 w-[calc(100%_-_8px)] group-hover/upload:block"
             type="button"
             @click="emit('removeFile', idx)">
-            remove
+            Remove
           </button>
         </template>
         <template v-else>
@@ -93,7 +95,7 @@ const handleFileUpload = (idx: number) => {
               size="xs" />
           </ScalarButton>
         </template>
-      </div>
+      </DataTableCell>
     </DataTableRow>
   </DataTable>
 </template>
