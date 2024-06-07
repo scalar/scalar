eafc0645b35ff8e7057a431a5f032c1ea73be2a3
Marc Laventure <marc@scalar.com>
2024-06-07 12:02:10 -0700

    fix: revert revert build breaking (#1992)

--- packages/object-utils/vite.config.ts
+++ packages/object-utils/vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
     port: 9000,
   },
   build: createViteBuildOptions({
-    entry: ['src/index.ts'],
+    entry: await findEntryPoints({ allowCss: false }),
   }),
 })
