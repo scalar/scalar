fc419529d6d4ec8b37aec9251d849ba26113ee8b
Brynn Hawker <brynn@hawker.me>
2024-06-10 13:30:35 -0600

    fix: ensure there's no padding on the response tabs

--- packages/api-reference/src/components/Card/CardTabHeader.vue
+++ packages/api-reference/src/components/Card/CardTabHeader.vue
@@ -12,7 +12,7 @@ const changeTab = (index: number) => {
 }
 </script>
 <template>
-  <CardHeader class="scalar-card-header-tabs">
+  <CardHeader class="scalar-card-header scalar-card-header-tabs">
     <TabGroup @change="changeTab">
       <TabList class="tab-list custom-scroll">
         <slot />
@@ -32,7 +32,7 @@ const changeTab = (index: number) => {
   padding: 9px 12px;
   overflow: auto;
 }
-.scalar-card-header-tabs {
+.scalar-card-header.scalar-card-header-tabs {
   padding: 0;
 }
 </style>
