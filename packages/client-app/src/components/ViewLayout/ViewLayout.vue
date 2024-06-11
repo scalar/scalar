2ce812d79905b980e854de9cb8343e6794f13749
anthony lio <lionnet.ant@gmail.com>
2024-06-11 15:18:27 +0300

    fix: remove view layout border radius

--- packages/client-app/src/components/ViewLayout/ViewLayout.vue
+++ packages/client-app/src/components/ViewLayout/ViewLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="divide divide-x flex xl:flex-row flex-col xl:gap-0 gap-1 custom-scroll rounded">
+    class="divide divide-y xl:divide-y-0 xl:divide-x flex xl:flex-row flex-col custom-scroll rounded">
     <slot />
   </div>
 </template>
