85e6a6e2e0e0aae5fe0515375250051864e0f894
zameron <cameron.rohani@gmail.com>
2024-06-12 23:13:58 -0700

    fix: checkbox hover style

--- packages/client-app/src/components/DataTable/DataTableCheckbox.vue
+++ packages/client-app/src/components/DataTable/DataTableCheckbox.vue
@@ -21,9 +21,11 @@ defineEmits<{
       tabindex="-1"
       type="checkbox"
       @change="(e: any) => $emit('update:modelValue', e.target.checked)" />
-    <ScalarIcon
-      class="centered text-border peer-checked:text-c-2 group-hover/cell:text-c-1 pointer-events-none absolute stroke-[1.2]"
-      icon="Checkmark"
-      size="xs" />
+    <div
+      class="w-full h-full flex items-center justify-center centered text-border peer-checked:text-c-2 pointer-events-none absolute stroke-[1.2] before:absolute before:opacity-0 group-hover/cell:before:opacity-100 before:border before:rounded before:w-9/12 before:aspect-square before:top-1/2 before:left-1/2 before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:border-[1px]">
+      <ScalarIcon
+        icon="Checkmark"
+        size="xs" />
+    </div>
   </DataTableCell>
 </template>
