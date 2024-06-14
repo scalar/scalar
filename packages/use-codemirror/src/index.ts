62ec0d2b9706f38678a643f89b215cd39f044260
Hans Pagel <mail@hanspagel.com>
2024-06-14 10:43:34 +0200

    fix: TS issue with y-codemirror reappeared

--- packages/use-codemirror/src/index.ts
+++ packages/use-codemirror/src/index.ts
@@ -1,4 +1,5 @@
 export { type Extension } from '@codemirror/state'
+// @ts-expect-error y-codemirror is not exporting types correctly
 export { yCollab } from 'y-codemirror.next'
 export { colorPicker } from '@replit/codemirror-css-color-picker'
 
