19f67b8fea70456c3189ce259070f8ba6bf279f0
Hans Pagel <mail@hanspagel.com>
2024-06-14 13:04:55 +0200

    fix: response body is not pretty printed

--- packages/components/src/components/ScalarCodeBlock/ScalarCodeBlock.vue
+++ packages/components/src/components/ScalarCodeBlock/ScalarCodeBlock.vue
@@ -1,9 +1,8 @@
 <script lang="ts" setup>
 import { standardLanguages, syntaxHighlight } from '@scalar/code-highlight'
 import '@scalar/code-highlight/css/code.css'
+import { prettyPrintJson } from '@scalar/oas-utils/helpers'
 import { computed } from 'vue'
-
-import { prettyPrintString } from './utils/prettyPrintString'
 
 /**
  * Uses highlight.js for syntax highlighting
@@ -11,7 +10,7 @@ import { prettyPrintString } from './utils/prettyPrintString'
 const props = withDefaults(
   defineProps<{
     content: string | object
-    lang: string
+    lang?: string
     lineNumbers?: boolean
     hideCredentials?: string | string[]
   }>(),
@@ -22,14 +21,18 @@ const props = withDefaults(
 )
 
 const highlightedCode = computed(() => {
-  const html = syntaxHighlight(prettyPrintString(props.content), {
+  const html = syntaxHighlight(prettyPrintJson(props.content), {
     lang: props.lang.trim(),
     languages: standardLanguages,
     lineNumbers: props.lineNumbers,
     maskCredentials: props.hideCredentials,
   })
+
   // Need to remove the wrapping <pre> element so we can use v-html without another wrapper
-  return html.slice(5, -6)
+  // return html.slice(5, -6)
+
+  console.log(props.content, prettyPrintJson(props.content), html)
+  return html
 })
 </script>
 <template>
