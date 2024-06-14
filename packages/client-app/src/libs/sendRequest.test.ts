70236b94682bce32d132210849fa7dcaac4c64ec
Marc Laventure <marc@scalar.com>
2024-06-13 19:12:06 -0700

    feat: port tests to sendRequest in new client app (#2065)
    * feat: port tests to sendRequest in new client app
    
    * chore: add zode file schema
    
    * chore: revert zod olympics
    
    * chore: add dom to ts
    
    * chore: use jsdom for testing
    
    * chore: comment out cookie tests
    
    ---------
    
    Co-authored-by: tmastrom <thomas.mastromonaco@gmail.com>

---
+++ packages/client-app/src/libs/sendRequest.test.ts
@@ -0,0 +1,332 @@
+/**
+ * @vitest-environment jsdom
+ */
+import {
+  type ServerPayload,
+  createServer,
+} from '@scalar/oas-utils/entities/workspace/server'
+import {
+  type RequestExamplePayload,
+  type RequestPayload,
+  createRequest,
+  createRequestExample,
+  createRequestExampleParameter,
+} from '@scalar/oas-utils/entities/workspace/spec'
+import { describe, expect, it } from 'vitest'
+
+import { sendRequest } from './sendRequest'
+
+const PROXY_PORT = 5051
+const ECHO_PORT = 5052
+
+type MetaRequestPayload = {
+  serverPayload?: ServerPayload
+  requestPayload?: RequestPayload
+  requestExamplePayload?: Omit<RequestExamplePayload, 'requestUid'>
+}
+function createRequestExampleServer(metaRequestPayload: MetaRequestPayload) {
+  const request = createRequest(metaRequestPayload.requestPayload ?? {})
+
+  const example = createRequestExample({
+    requestUid: request.uid,
+    ...(metaRequestPayload.requestExamplePayload || {}),
+  })
+  const server = createServer(metaRequestPayload.serverPayload ?? {})
+
+  return {
+    request,
+    example,
+    server,
+  }
+}
+
+describe('sendRequest', () => {
+  it('shows a warning when scalar_url is missing', async () => {
+    const { request, example, server } = createRequestExampleServer({
+      serverPayload: { url: `http://127.0.0.1:${PROXY_PORT}` },
+    })
+
+    const result = await sendRequest(
+      request,
+      example,
+      server?.url + request.path,
+    )
+
+    expect(result?.response?.data).toContain(
+      'The `scalar_url` query parameter is required.',
+    )
+  })
+
+  it('reaches the echo server *without* the proxy', async () => {
+    const { request, example, server } = createRequestExampleServer({
+      serverPayload: { url: `http://127.0.0.1:${ECHO_PORT}` },
+    })
+
+    const result = await sendRequest(
+      request,
+      example,
+      server?.url + request.path,
+    )
+
+    expect(result?.response?.data).not.toContain('ECONNREFUSED')
+    expect(result?.response?.data).toMatchObject({
+      method: 'GET',
+      path: '/',
+    })
+  })
+
+  it('reaches the echo server *with* the proxy', async () => {
+    const { request, example, server } = createRequestExampleServer({
+      serverPayload: { url: `http://localhost:${ECHO_PORT}` },
+    })
+
+    const result = await sendRequest(
+      request,
+      example,
+      server?.url + request.path,
+    )
+
+    expect(result?.response?.data).toMatchObject({
+      method: 'GET',
+      path: '/',
+    })
+  })
+
+  // it('replaces variables', async () => {
+  //   const { request, example, server } = createRequestExampleServer({
+  //     serverPayload: { url: `http://localhost:${ECHO_PORT}` },
+  //     requestPayload: {
+  //       path: ':path',
+  //     },
+  //     requestExamplePayload: {
+  //       parameters: {
+  //         path: [
+  //           createRequestExampleParameter({
+  //             key: 'path',
+  //             value: 'example',
+  //             enabled: true,
+  //           }),
+  //         ],
+  //       },
+  //     },
+  //   })
+
+  //   const result = await sendRequest(
+  //     request,
+  //     example,
+  //     server?.url + request.path,
+  //   )
+
+  //   expect(result?.response?.data).toMatchObject({
+  //     method: 'GET',
+  //     path: '/example',
+  //   })
+  // })
+
+  // it('replaces variables in urls', async () => {
+  //   const request = {
+  //     url: `http://127.0.0.1:${ECHO_PORT}/{path}`,
+  //     variables: [
+  //       {
+  //         name: 'path',
+  //         value: 'example',
+  //         enabled: true,
+  //       },
+  //     ],
+  //   }
+
+  //   const result = await sendRequest(request)
+
+  //   expect(result?.response.data).toMatchObject({
+  //     method: 'GET',
+  //     path: '/example',
+  //   })
+  // })
+
+  it('sends query parameters', async () => {
+    const { request, example, server } = createRequestExampleServer({
+      serverPayload: { url: `http://127.0.0.1:${ECHO_PORT}` },
+      requestExamplePayload: {
+        parameters: {
+          query: [
+            createRequestExampleParameter({
+              key: 'foo',
+              value: 'bar',
+              enabled: true,
+            }),
+          ],
+        },
+      },
+    })
+
+    const result = await sendRequest(
+      request,
+      example,
+      server?.url + request.path,
+    )
+
+    expect((result?.response?.data as any).query).toMatchObject({
+      foo: 'bar',
+    })
+  })
+
+  it('merges query parameters', async () => {
+    const { request, example, server } = createRequestExampleServer({
+      serverPayload: { url: `http://127.0.0.1:${ECHO_PORT}?example=parameter` },
+      requestPayload: { path: '' },
+      requestExamplePayload: {
+        parameters: {
+          query: [
+            createRequestExampleParameter({
+              key: 'foo',
+              value: 'bar',
+              enabled: true,
+            }),
+          ],
+        },
+      },
+    })
+
+    const result = await sendRequest(
+      request,
+      example,
+      server?.url + request.path,
+    )
+
+    expect(result?.response?.data).toMatchObject({
+      query: {
+        example: 'parameter',
+        foo: 'bar',
+      },
+    })
+  })
+
+  // it('adds cookies as headers', async () => {
+  //   const { request, example, server } = createRequestExampleServer({
+  //     serverPayload: { url: `http://127.0.0.1:${ECHO_PORT}` },
+  //     requestExamplePayload: {
+  //       parameters: {
+  //         cookies: [
+  //           createRequestExampleParameter({
+  //             key: 'foo',
+  //             value: 'bar',
+  //             enabled: true,
+  //           }),
+  //         ],
+  //       },
+  //     },
+  //   })
+
+  //   const result = await sendRequest(
+  //     request,
+  //     example,
+  //     server?.url + request.path,
+  //   )
+
+  //   expect(result?.response?.data).toMatchObject({
+  //     cookies: {
+  //       foo: 'bar',
+  //     },
+  //   })
+  // })
+
+  // it('merges cookies', async () => {
+  //   const { request, example, server } = createRequestExampleServer({
+  //     serverPayload: { url: `http://127.0.0.1:${ECHO_PORT}` },
+  //     requestExamplePayload: {
+  //       parameters: {
+  //         cookies: [
+  //           createRequestExampleParameter({
+  //             key: 'foo',
+  //             value: 'bar',
+  //             enabled: true,
+  //           }),
+  //           createRequestExampleParameter({
+  //             key: 'another',
+  //             value: 'cookie',
+  //             enabled: true,
+  //           }),
+  //         ],
+  //       },
+  //     },
+  //   })
+
+  //   const result = await sendRequest(
+  //     request,
+  //     example,
+  //     server?.url + request.path,
+  //   )
+
+  //   expect(result?.response?.data).toMatchObject({
+  //     cookies: {
+  //       foo: 'bar',
+  //       another: 'cookie',
+  //     },
+  //   })
+  // })
+
+  // it('sends requests through a proxy', async () => {
+  //   const request = {
+  //     url: `http://127.0.0.1:${ECHO_PORT}/v1`,
+  //   }
+
+  //   const result = await sendRequest(request, `http://127.0.0.1:${PROXY_PORT}`)
+
+  //   expect(result?.response.data).toMatchObject({
+  //     method: 'GET',
+  //     path: '/v1',
+  //   })
+  // })
+
+  it('skips the proxy for requests to localhost', async () => {
+    const { request, example, server } = createRequestExampleServer({
+      serverPayload: { url: `http://127.0.0.1:${ECHO_PORT}/v1` },
+      requestPayload: { path: '' },
+    })
+
+    const result = await sendRequest(
+      request,
+      example,
+      server?.url + request.path,
+    )
+
+    expect(result?.response?.data).toMatchObject({
+      method: 'GET',
+      path: '/v1',
+    })
+  })
+
+  it('returns error for invalid domain', async () => {
+    const { request, example, server } = createRequestExampleServer({
+      serverPayload: { url: `http://DOES_NOT_EXIST` },
+    })
+
+    const result = await sendRequest(
+      request,
+      example,
+      server?.url + request.path,
+    )
+
+    expect(result?.response?.data?.trim().toLowerCase()).toContain(
+      'dial tcp: lookup does_not_exist',
+    )
+  })
+
+  it('keeps the trailing slash', async () => {
+    const { request, example, server } = createRequestExampleServer({
+      serverPayload: { url: `http://127.0.0.1:${ECHO_PORT}/v1/` },
+      requestPayload: { path: '' },
+    })
+
+    const result = await sendRequest(
+      request,
+      example,
+      server?.url + request.path,
+    )
+
+    expect(result?.response?.data).toMatchObject({
+      method: 'GET',
+      path: '/v1/',
+    })
+  })
+})
