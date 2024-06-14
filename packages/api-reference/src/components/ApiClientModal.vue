f39e9de5252b32db07f390ada135f7d648ec18e1
Amrit <amrit@scalar.com>
2024-06-14 09:57:55 -0700

    fix: remove client modal for now

--- packages/api-reference/src/components/ApiClientModal.vue
+++ packages/api-reference/src/components/ApiClientModal.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
-import type { SpecConfiguration } from '@scalar/oas-utils'
+// import type { SpecConfiguration } from '@scalar/oas-utils'
 import { onMounted, ref } from 'vue'
 
-import { apiClientBus } from './api-client-bus'
+// import { apiClientBus } from './api-client-bus'
 
-const props = defineProps<{
-  proxyUrl?: string
-  spec?: SpecConfiguration
-}>()
+// const props = defineProps<{
+//   proxyUrl?: string
+//   spec?: SpecConfiguration
+// }>()
 
 const el = ref<HTMLDivElement | null>(null)
 
 onMounted(async () => {
   if (!el.value) return
 
-  const { createScalarApiClient } = await import('@scalar/api-client-modal')
+  // const { createScalarApiClient } = await import('@scalar/api-client-modal')
 
-  const { open } = await createScalarApiClient(el.value, {
-    spec: props.spec ?? {},
-    proxyUrl: props.proxyUrl,
-  })
-
+  // const { open } = await createScalarApiClient(el.value, {
+  //   spec: props.spec ?? {},
+  //   proxyUrl: props.proxyUrl,
+  // })
+  //
   // Event bus to listen to apiClient events
-  apiClientBus.on(open)
+  // apiClientBus.on(open)
 })
 </script>
 <template>
--- packages/api-reference/src/components/api-client-bus.ts
+++ packages/api-reference/src/components/api-client-bus.ts
@@ -1,7 +1,7 @@
-import type { OpenClientPayload } from '@scalar/api-client-modal'
+// import type { OpenClientPayload } from '@scalar/api-client-modal'
 import { type EventBusKey, useEventBus } from '@vueuse/core'
 
-const apiClientBusKey: EventBusKey<OpenClientPayload> = Symbol()
+const apiClientBusKey: EventBusKey<any> = Symbol()
 
 /** Event bus to open the API Client */
 export const apiClientBus = useEventBus(apiClientBusKey)
