e49cca78826f2b1bf90b0bd6f5103f31f5630809
Hans Pagel <mail@hanspagel.com>
2024-06-17 11:12:37 +0200

    fix: The "data" argument must be of type string or an instance of Buffer, TypedArray, or DataView.

--- packages/galaxy/scripts/format.ts
+++ packages/galaxy/scripts/format.ts
@@ -4,8 +4,8 @@ import fs from 'node:fs'
 const base = fs.readFileSync('dist/3.1.yaml', 'utf-8')
 const latest = fs.readFileSync('dist/latest.yaml', 'utf-8')
 
-const baseOut = openapi().load(base).toJson()
-const latestOut = openapi().load(latest).toJson()
+const baseOut = await openapi().load(base).toJson()
+const latestOut = await openapi().load(latest).toJson()
 
 fs.writeFileSync('./dist/3.1.json', baseOut)
 fs.writeFileSync('./dist/latest.json', latestOut)
