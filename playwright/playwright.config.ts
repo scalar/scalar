20e57f8252c2d954231990add3cc596dd2e7f92b
Hans Pagel <mail@hanspagel.com>
2024-06-18 15:18:17 +0200

    ci: better way to get the playwright version

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -69,9 +69,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'pnpm --filter @scalar-examples/cdn-api-reference dev',
+    url: 'http://127.0.0.1:3173',
+    reuseExistingServer: !process.env.CI,
+  },
 })
