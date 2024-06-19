6e34c5940784d931de8a303c3e92d6f51e50cdb5
Hans Pagel <mail@hanspagel.com>
2024-06-19 17:07:44 +0200

    chore: rebase

--- examples/cdn-api-reference/src/index.ts
+++ examples/cdn-api-reference/src/index.ts
@@ -36,10 +36,6 @@ app.get('/play-button/standalone.js', (_request, reply) => {
     .send(playButtonBundle)
 })
 
-app.get('/with-header', (_request, reply) => {
-  reply.sendFile('api-reference-cdn-with-header.html', { cacheControl: false }) // overriding the options disabling cache-control headers) // serving path.join(__dirname, 'public', 'myHtml.html') directly
-})
-
 // Run the server!
 try {
   app.listen({ port: 3173 }, () => {
