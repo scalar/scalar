ad79893a2fa2c536bf62602db270daaad5812ece
Anthony Lio <anthony@scalar.com>
2024-06-21 18:37:23 +0300

    feat(client-app): hide header in read only mode (#2206)

--- packages/client-app/src/components/Sidebar/Sidebar.vue
+++ packages/client-app/src/components/Sidebar/Sidebar.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { themeClasses } from '@/constants'
+import { useWorkspace } from '@/store/workspace'
+
+const { workspace } = useWorkspace()
 </script>
 <template>
   <aside
     class="w-sidebar relative flex flex-col border-r bg-b-1"
     :class="[themeClasses.sidebar]">
-    <div class="xl:min-h-header py-2.5 flex items-center border-b px-4 text-sm">
+    <div
+      v-if="!workspace.isReadOnly"
+      class="xl:min-h-header py-2.5 flex items-center border-b px-4 text-sm">
       <h2 class="font-medium m-0 text-sm"><slot name="title" /></h2>
     </div>
     <div class="custom-scroll sidebar-height">
