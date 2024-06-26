af63d866a6e3ef7b8bc93f911651d937bc700c33
Hans Pagel <mail@hanspagel.com>
2024-06-26 12:38:48 +0200

    fix: tests

--- packages/api-client/src/helpers/redirectToProxy.test.ts
+++ packages/api-client/src/helpers/redirectToProxy.test.ts
@@ -6,7 +6,9 @@ describe('redirectToProxy', () => {
   it('rewrites URLs', async () => {
     expect(
       redirectToProxy('https://proxy.scalar.com', 'https://example.com'),
-    ).toBe('https://proxy.scalar.com/?scalar_url=https%3A%2F%2Fexample.com')
+    ).toBe(
+      'https://proxy.scalar.com/?scalar_url=https%253A%252F%252Fexample.com',
+    )
   })
 
   it('keeps query parameters', async () => {
@@ -16,7 +18,7 @@ describe('redirectToProxy', () => {
         'https://example.com',
       ),
     ).toBe(
-      'https://proxy.scalar.com/?foo=bar&scalar_url=https%3A%2F%2Fexample.com',
+      'https://proxy.scalar.com/?foo=bar&scalar_url=https%253A%252F%252Fexample.com',
     )
   })
 })
