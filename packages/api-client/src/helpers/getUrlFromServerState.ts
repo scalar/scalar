d9e1e9b4ff3e76f598e8012b8ddb70ad44d371b0
Hans Pagel <mail@hanspagel.com>
2024-06-26 11:06:42 +0200

    fix: getUrlFromServerState doesn’t work with key value objects

--- packages/api-client/src/helpers/getUrlFromServerState.ts
+++ packages/api-client/src/helpers/getUrlFromServerState.ts
@@ -9,19 +9,10 @@ export function getUrlFromServerState(state: ServerState) {
   // Get the selected server
   const server = state?.servers?.[state.selectedServer ?? 0]
 
-  // Make variables an key value object
-  const variables = state.variables.reduce(
-    (acc, variable) => {
-      acc[variable.name] = variable.value
-      return acc
-    },
-    {} as Record<string, string | number>,
-  )
-
   // Replace variables: {protocol}://{host}:{port}/{basePath}
   const url =
     typeof server?.url === 'string'
-      ? replaceVariables(server?.url, variables)
+      ? replaceVariables(server?.url, state.variables)
       : server?.url
 
   return url
