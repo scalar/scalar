5828e995d7a1519790c0381e1487987df606d618
Hans Pagel <mail@hanspagel.com>
2024-06-28 17:02:32 +0200

    chore: remove env defintion file

--- packages/components/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.module.css?inline'
-declare module 'vite-plugin-scope-tailwind'
-declare module 'prismjs/plugins/autoloader/prism-autoloader.js'
-declare module 'prismjs/components/prism-bash'
-declare module 'prismjs/components/prism-json'
-declare module 'prismjs/plugins/line-numbers/prism-line-numbers.js'
