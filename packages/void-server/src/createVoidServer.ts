bfa0e488843d2dcd0b7d6dd8d975bade9c948530
Hans Pagel <mail@hanspagel.com>
2024-07-01 12:37:10 +0200

    refactor: extract list of http error codes

--- packages/void-server/src/createVoidServer.ts
+++ packages/void-server/src/createVoidServer.ts
@@ -2,7 +2,6 @@ import { type Context, Hono } from 'hono'
 import { accepts } from 'hono/accepts'
 import { cors } from 'hono/cors'
 import type { StatusCode } from 'hono/utils/http-status'
-import { parse } from 'path'
 
 import {
   createHtmlResponse,
@@ -11,6 +10,7 @@ import {
   createZipFileResponse,
   getRequestData,
 } from './utils'
+import { errors } from './utils/constants'
 
 /**
  * Create a mock server instance
@@ -29,48 +29,6 @@ export async function createVoidServer() {
     const status = parseInt(originalStatusCode, 10) as StatusCode
 
     c.status(status)
-
-    // HTTP Error list
-    const errors: Partial<Record<StatusCode, string>> = {
-      '400': 'Bad Request',
-      '401': 'Unauthorized',
-      '402': 'Payment Required',
-      '403': 'Forbidden',
-      '404': 'Not Found',
-      '406': 'Not Acceptable',
-      '407': 'Proxy Authentication Required',
-      '408': 'Request Time-out',
-      '409': 'Conflict',
-      '410': 'Gone',
-      '411': 'Length Required',
-      '412': 'Precondition Failed',
-      '413': 'Request Entity Too Large',
-      '414': 'Request-URI Too Long',
-      '415': 'Unsupported Media Type',
-      '416': 'Requested Range Not Satisfiable',
-      '417': 'Expectation Failed',
-      '418': 'I’m a teapot',
-      '421': 'Unprocessable Entity',
-      '422': 'Misdirected Request',
-      '423': 'Locked',
-      '424': 'Failed Dependency',
-      '426': 'Upgrade Required',
-      '428': 'Precondition Required',
-      '429': 'Too Many Requests',
-      '431': 'Request Header Fileds Too Large',
-      '451': 'Unavailable For Legal Reasons',
-      '500': 'Internal Server Error',
-      '501': 'Not Implemented',
-      '502': 'Bad Gateway',
-      '503': 'Service Unavailable',
-      '504': 'Gateway Timeout',
-      '505': 'HTTP Version Not Supported',
-      '506': 'Variant Also Negotiates',
-      '507': 'Insufficient Storage',
-      '508': 'Loop Detected',
-      '510': 'Not Extended',
-      '511': 'Network Authentication Required',
-    }
 
     return c.text(errors?.[status] ?? 'Unknown Error')
   })
