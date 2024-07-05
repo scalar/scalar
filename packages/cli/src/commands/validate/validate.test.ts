067837e2c1ea473cca705d6d58cbcfabb444e1f7
Hans Pagel <mail@hanspagel.com>
2024-07-05 13:40:10 +0200

    fix: wrong error message in test file

--- packages/cli/src/commands/validate/validate.test.ts
+++ packages/cli/src/commands/validate/validate.test.ts
@@ -28,7 +28,7 @@ describe('validate', () => {
       .invoke(['validate', './packages/cli/src/commands/validate/invalid.json'])
 
     logs.should.contain(
-      'Cannot find supported Swagger/OpenAPI version in specification, version must be a string.',
+      'Can’t find supported Swagger/OpenAPI version in specification, version must be a string.',
     )
     logs.should.not.contain('OpenAPI 3.1')
     expect(exitCode).toBe(1)
