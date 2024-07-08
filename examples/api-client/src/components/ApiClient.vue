2b0f43e698c57f565bf99bc6e2d08b7aa9a5bac6
Hans Pagel <mail@hanspagel.com>
2024-07-08 16:42:53 +0200

    feat: add api-client example

---
+++ examples/api-client/src/components/ApiClient.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { createScalarApiClient } from '@scalar/api-client'
+import { onMounted, ref } from 'vue'
+
+const client = ref<HTMLElement | null>(null)
+
+onMounted(async () => {
+  // Initialize
+  const { open } = await createScalarApiClient(client.value, {
+    spec: {
+      url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
+    },
+    proxyUrl: 'https://proxy.scalar.com',
+  })
+
+  // Open the API client right-away
+  open()
+})
+</script>
+
+<template>
+  <div ref="client"></div>
+</template>
+
+<style>
+@import '@scalar/api-client/style.css';
+</style>
