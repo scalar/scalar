cf1a0c4a559c5c9b11c02a51f42194207ace003b
Hans Pagel <mail@hanspagel.com>
2024-07-09 13:04:52 +0200

    chore: update app title

--- packages/api-client-app/src/main/index.ts
+++ packages/api-client-app/src/main/index.ts
@@ -10,6 +10,7 @@ function createWindow(): void {
     width: 1280,
     height: 760,
     show: false,
+    title: 'Scalar API Client',
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
