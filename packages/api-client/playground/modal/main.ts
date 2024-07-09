362cb07142e62c1d13de92d36f61d57f42584499
Hans Pagel <mail@hanspagel.com>
2024-07-09 11:33:55 +0200

    chore: add two different playgrounds for the modal and the embed

---
+++ packages/api-client/playground/modal/main.ts
@@ -0,0 +1,18 @@
+import { createScalarApiClient } from '@/Modal'
+
+// Initialize
+const { open } = await createScalarApiClient(document.getElementById('app'), {
+  spec: {
+    url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
+  },
+  proxyUrl: 'https://proxy.scalar.com',
+})
+
+// Open the API client right-away
+open()
+
+// Or: Open a specific operation
+// open({
+//   method: 'GET',
+//   path: '/me',
+// })
