22ab1f3a2705f8d755b379cf6536fc941e5b9ed6
Hans Pagel <mail@hanspagel.com>
2024-07-09 11:13:17 +0200

    feat: add new createApiClientEmbed method (wip)

--- packages/api-client/playground/modal/main.ts
+++ packages/api-client/playground/modal/main.ts
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { createApiClientModal } from '@/Modal'
 
 // Initialize
@@ -45,17 +46,45 @@ import { createScalarApiClient } from '@/Modal'
 const { open } = await createScalarApiClient(document.getElementById('app'), {
 >>>>>>> 362cb0714 (chore: add two different playgrounds for the modal and the embed)
 =======
+=======
+<<<<<<< HEAD:packages/api-client/playground/modal/main.ts
+>>>>>>> fdb828894 (feat: add new createApiClientEmbed method (wip))
 import { createApiClientModal } from '@/Modal'
 
 // Initialize
 const { open } = await createApiClientModal(document.getElementById('app'), {
+<<<<<<< HEAD
 >>>>>>> d86314823 (fix: playground broken)
+=======
+=======
+// import { createScalarApiClient } from '@/Modal'
+// // Initialize
+// const { open } = await createScalarApiClient(document.getElementById('root'), {
+//   spec: {
+//     url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
+//   },
+//   proxyUrl: 'https://proxy.scalar.com',
+// })
+// // Open the API client right-away
+// open()
+// // Or: Open a specific operation
+// // open({
+// //   method: 'GET',
+// //   path: '/me',
+// // })
+import { createApiClientEmbed } from '@/Embed'
+
+// Initialize
+await createApiClientEmbed(document.getElementById('app'), {
+>>>>>>> 89318fb7d (feat: add new createApiClientEmbed method (wip)):packages/api-client/playground/main.ts
+>>>>>>> fdb828894 (feat: add new createApiClientEmbed method (wip))
   spec: {
     url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
   },
   proxyUrl: 'https://proxy.scalar.com',
 })
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 // // Open the API client right-away
 // open()
@@ -75,3 +104,13 @@ open()
 //   path: '/me',
 // })
 >>>>>>> 362cb0714 (chore: add two different playgrounds for the modal and the embed)
+=======
+// // Open the API client right-away
+// open()
+
+// // Or: Open a specific operation
+// // open({
+// //   method: 'GET',
+// //   path: '/me',
+// // })
+>>>>>>> fdb828894 (feat: add new createApiClientEmbed method (wip))
