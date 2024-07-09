6f4d20dbd50a651b19a5bcfa01fff9e39e364c3b
Cameron Rohani <cameronrohani@users.noreply.github.com>
2024-07-08 22:34:09 -0700

    fix: make client icons match text font weights (#2433)

--- packages/api-client/src/components/DarkModeToggle/DarkModeIconToggle.vue
+++ packages/api-client/src/components/DarkModeToggle/DarkModeIconToggle.vue
@@ -7,11 +7,11 @@ const { toggleDarkMode, isDark } = useDarkModeState()
 </script>
 <template>
   <button
-    class="w-[37px] hover:text-c-1 flex items-center justify-center rounded-lg p-[7px]"
+    class="w-[37px] hover:bg-b-2 flex items-center justify-center rounded-lg p-[7px]"
     type="button"
     @click="toggleDarkMode">
     <ScalarIcon
-      class="block h-6 w-6 stroke-[0.75]"
+      class="block h-6 w-6 stroke-[1.5]"
       icon="LightDarkModeToggle" />
     <ScreenReader>
       Switch to {{ isDark ? 'Light' : 'Dark' }} Mode
