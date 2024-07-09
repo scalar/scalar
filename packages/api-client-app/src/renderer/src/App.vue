390bc3b88f5fe7ba7fdda7e7c01c8123af1e5de1
Hans Pagel <mail@hanspagel.com>
2024-07-09 12:09:55 +0200

    feat: new electron app

---
+++ packages/api-client-app/src/renderer/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import ApiClient from './components/ApiClient.vue'
+</script>
+
+<template>
+  <ApiClient />
+</template>
---
+++ packages/api-client-app/src/renderer/src/components/ApiClient.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { createApiClientApp } from '@scalar/api-client'
+import { onMounted, ref } from 'vue'
+
+const client = ref<HTMLElement | null>(null)
+
+onMounted(async () => {
+  // Initialize
+  await createApiClientApp(client.value, {
+    spec: {
+      url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
+    },
+    proxyUrl: 'https://proxy.scalar.com',
+  })
+})
+</script>
+
+<template>
+  <div ref="client"></div>
+</template>
+
+<style>
+@import '@scalar/api-client/style.css';
+</style>
