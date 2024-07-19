5985a63e54530d56ee8fa20818ff207c31f0ec77
Amrit <amrit@scalar.com>
2024-07-18 23:27:14 -0700

    fix: localStorage works

--- examples/api-client/vite.config.ts
+++ examples/api-client/vite.config.ts
@@ -5,9 +5,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    'process.env.ENABLE_LOCAL_STORAGE': 'true',
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
