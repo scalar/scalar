1120150d0e3eba31f981761d42e2dab1f7db5ae7
Brynn Hawker <brynn@hawker.me>
2024-07-19 13:04:40 -0700

    feat: extract side toggle component

---
+++ packages/api-client/src/components/Sidebar/SidebarToggle.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ScalarIcon } from '@scalar/components'
+
+defineProps<{
+  modelValue: boolean
+}>()
+
+defineEmits<{
+  (e: 'update:modelValue', v: boolean): void
+}>()
+</script>
+<template>
+  <button
+    class="text-c-3 hover:bg-b-3 active:text-c-1 p-2 rounded"
+    type="button"
+    @click="$emit('update:modelValue', !modelValue)">
+    <ScalarIcon
+      :icon="modelValue ? 'SideBarOpen' : 'SideBarClosed'"
+      size="sm" />
+  </button>
+</template>
