cb8fa61a9c4f9f2e5a6bd24cda97ccc5967f084b
Amrit <amrit@scalar.com>
2024-07-23 09:24:09 -0700

    feat(api-client-react): keep use-client directive after build

--- packages/api-client-react/vite.config.ts
+++ packages/api-client-react/vite.config.ts
@@ -1,39 +1,16 @@
+import { createViteBuildOptions } from '@scalar/build-tooling'
 import react from '@vitejs/plugin-react'
-import * as path from 'path'
+import { preserveDirective } from 'rollup-preserve-directives'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
-import pkg from './package.json'
-
 export default defineConfig({
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: './src/index.ts',
-      name: '@scalar/api-client-react',
-      formats: ['es', 'cjs'],
-      fileName: 'index',
-    },
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'src/index.ts'),
-      },
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: [
-        ...Object.keys(pkg.dependencies),
-        ...Object.keys(pkg.peerDependencies),
-      ],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        exports: 'named',
-        globals: {
-          'react': 'React',
-          'react-dom': 'react-dom',
-        },
-      },
-    },
-  },
-  plugins: [react(), dts({ insertTypesEntry: true, rollupTypes: true })],
+  build: createViteBuildOptions({
+    entry: ['src/index.ts'],
+  }),
+  plugins: [
+    react(),
+    dts({ insertTypesEntry: true, rollupTypes: true }),
+    preserveDirective(),
+  ],
 })
