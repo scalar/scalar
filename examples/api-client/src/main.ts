561058c854c9c0bf6febd89eee93f775ccf9a90d
Amrit <amrit@scalar.com>
2024-07-24 22:09:48 -0700

    fix: client scalar com and add command

--- examples/api-client/src/main.ts
+++ examples/api-client/src/main.ts
@@ -1,9 +1,7 @@
-import { router } from '@scalar/api-client'
-import { ApiClientApp } from '@scalar/api-client/layouts/App'
+import { createApiClientApp } from '@scalar/api-client/layouts/App'
 import '@scalar/api-client/style.css'
-import { createApp } from 'vue'
 
-const app = createApp(ApiClientApp)
-
-app.use(router)
-app.mount('#scalar-client')
+// Initialize
+await createApiClientApp(document.getElementById('scalar-client'), {
+  proxyUrl: 'https://proxy.scalar.com',
+})
