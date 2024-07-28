89998edb100b2eacb9a3d49bcefe3175180e7fa6
marclave <marc@scalar.com>
2024-07-27 23:54:51 -0700

    fix: hide servers page

--- packages/api-client/src/constants.ts
+++ packages/api-client/src/constants.ts
@@ -2,6 +2,6 @@ export const ROUTES = [
   { prettyName: 'Request', name: 'request', icon: 'ExternalLink' },
   { prettyName: 'Cookies', name: 'cookies', icon: 'Cookie' },
   { prettyName: 'Env Vars', name: 'environment', icon: 'Brackets' },
-  { prettyName: 'Servers', name: 'servers', icon: 'Server' },
+  // { prettyName: 'Servers', name: 'servers', icon: 'Server' },
   // { label: 'Git Sync', icon: 'Branch', path: '/git-sync' },
 ] as const
