08502bcd28db36b44f4ddacb42b7e72e550bf643
Marc Laventure <marc@scalar.com>
2024-07-29 10:13:51 -0700

    feat: add help icon cta in app nav bar (#2668)
    * feat: add help icon cta in app nav bar
    
    * fix: add some icons + style
    
    * fix: clean up help icon
    
    * fix: clean up other icons
    
    * chore: use proper roadmap link
    
    ---------
    
    Co-authored-by: zameron <cameron.rohani@gmail.com>
    Co-authored-by: Brynn Hawker <brynn@hawker.me>

---
+++ packages/api-client/src/components/SideNav/SideHelp.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+import {
+  ScalarButton,
+  ScalarDropdown,
+  ScalarDropdownDivider,
+  ScalarDropdownItem,
+  ScalarIcon,
+} from '@scalar/components'
+</script>
+<template>
+  <ScalarDropdown :placement="'top-end'">
+    <ScalarButton
+      class="min-w-[37px] max-w-[42px] hover:bg-b-2 flex items-center justify-center rounded-lg p-[7px]"
+      fullWidth
+      variant="ghost">
+      <ScalarIcon
+        icon="Help"
+        thickness="1.5" />
+    </ScalarButton>
+
+    <!-- Workspace list -->
+    <template #items>
+      <span class="px-2.5 py-1.5 text-sm text-c-2 font-medium">Support</span>
+      <a
+        class="no-underline block"
+        href="https://discord.gg/scalar"
+        target="_blank">
+        <ScalarDropdownItem class="flex items-center gap-1.5 w-full">
+          <div class="flex items-center justify-center h-4 w-4">
+            <ScalarIcon
+              class="h-2.5"
+              icon="Discord" />
+          </div>
+          <span>Discord</span>
+        </ScalarDropdownItem>
+      </a>
+      <a
+        class="no-underline block"
+        href="https://github.com/scalar/scalar"
+        target="_blank">
+        <ScalarDropdownItem class="flex items-center gap-1.5 w-full">
+          <div class="flex items-center justify-center h-4 w-4">
+            <ScalarIcon
+              class="h-2.5"
+              icon="GitHub" />
+          </div>
+          <span>Github</span>
+        </ScalarDropdownItem>
+      </a>
+      <a
+        class="no-underline block"
+        href="mailto:marc@scalar.com"
+        target="_blank">
+        <ScalarDropdownItem class="flex items-center gap-1.5 w-full">
+          <div class="flex items-center justify-center h-4 w-4">
+            <ScalarIcon
+              class="h-2.5"
+              icon="Email" />
+          </div>
+          <span>Email</span>
+        </ScalarDropdownItem>
+      </a>
+      <ScalarDropdownDivider />
+      <span class="px-2.5 py-1.5 text-sm text-c-2 font-medium">Product</span>
+      <a
+        class="no-underline block"
+        href="https://scalar.com/changelog"
+        target="_blank">
+        <ScalarDropdownItem class="flex items-center gap-1.5 w-full">
+          <div class="flex items-center justify-center h-4 w-4">
+            <ScalarIcon
+              class="h-2.5"
+              icon="Changelog"
+              thickness="1.5" />
+          </div>
+          <span>Changelog</span>
+        </ScalarDropdownItem>
+      </a>
+      <a
+        class="no-underline block"
+        href="https://github.com/scalar/scalar/issues/2669"
+        target="_blank">
+        <ScalarDropdownItem class="flex items-center gap-1.5 w-full">
+          <div class="flex items-center justify-center h-4 w-4">
+            <ScalarIcon
+              class="h-2.5"
+              icon="Roadmap"
+              thickness="1.5" />
+          </div>
+          <span>Roadmap</span>
+        </ScalarDropdownItem>
+      </a>
+    </template>
+  </ScalarDropdown>
+</template>
