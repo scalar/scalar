475edf0d11a88ed0cd517a262efb9fc3426f34d2
Amrit <amrit@scalar.com>
2024-07-31 14:21:32 -0700

    feat: prevent dnd on read only mode

--- packages/api-client/src/views/Request/RequestSidebarItem.vue
+++ packages/api-client/src/views/Request/RequestSidebarItem.vue
@@ -160,6 +160,8 @@ const getDraggableOffsets = computed(() => {
 
 /** Guard to check if an element should be droppable */
 const _isDroppable = (draggingItem: DraggingItem, hoveredItem: HoveredItem) => {
+  // Cannot drop in read only mode
+  if (activeWorkspace.value.isReadOnly) return false
   // RequestExamples cannot be dropped on
   if (requestExamples[hoveredItem.id]) return false
   // Collection cannot be dropped into another collection
