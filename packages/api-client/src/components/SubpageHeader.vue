6a4b9d80877af377401fbe5da889148c2621620a
anthony lio <anthony@scalar.com>
2024-07-31 17:18:19 +0200

    fix(client-app): back to requests link

--- packages/api-client/src/components/SubpageHeader.vue
+++ packages/api-client/src/components/SubpageHeader.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { ScalarIcon } from '@scalar/components'
+import { useRoute } from 'vue-router'
+
+const currentRoute = useRoute()
 </script>
 <template>
   <div
@@ -8,7 +11,7 @@ import { ScalarIcon } from '@scalar/components'
       class="lg:min-h-header items-center w-full p-1 t-app__top-container flex items-center">
       <router-link
         class="text-c-2 text-sm font-medium gitbook-show ml-1 flex items-center p-1.5 hover:bg-b-3 rounded cursor-pointer gap-1 active:text-c-1 no-underline"
-        to="/workspace/default/request/default">
+        :to="`/workspace/${currentRoute.params.workspace}/request/default`">
         <ScalarIcon
           icon="ChevronLeft"
           size="sm" />
