52ccb1e384acafdf9be8c57393a7bb59f99bc682
Brynn Hawker <brynn@hawker.me>
2024-07-31 18:27:04 -0600

    feat: align body toggle

--- packages/api-client/src/views/Request/ResponseSection/ResponseBodyToggle.vue
+++ packages/api-client/src/views/Request/ResponseSection/ResponseBodyToggle.vue
@@ -7,17 +7,18 @@ defineEmits<{
 }>()
 </script>
 <template>
-  <div class="text-c-3 flex justify-center rounded text-xxs p-0.5 gap-0.5">
+  <div
+    class="text-c-3 flex -my-1 justify-center rounded text-xxs p-0.5 gap-0.5">
     <button
-      class="hover:bg-b-3 rounded px-1"
-      :class="{ 'bg-b-3 text-c-1 pointer-events-none': modelValue }"
+      class="hover:bg-b-3 rounded px-1 leading-none"
+      :class="{ 'bg-b-3 text-c-1 cursor-default': modelValue }"
       type="button"
       @click.stop="$emit('update:modelValue', true)">
       Preview
     </button>
     <button
       class="hover:bg-b-3 rounded px-1"
-      :class="{ 'bg-b-3 text-c-1 pointer-events-none': !modelValue }"
+      :class="{ 'bg-b-3 text-c-1 cursor-default': !modelValue }"
       type="button"
       @click.stop="$emit('update:modelValue', false)">
       Raw
