5beb6638b9412709b2105f01b4dcf188c28b9327
Hans Pagel <hanspagel@users.noreply.github.com>
2024-08-02 20:16:27 +0200

    feat(api-client-app): integrate fathom (#2754)
    * feat: integrate fathom
    
    * refactor: only import what we need
    
    * chore: add custom id
    
    * feat: get platform
    
    * chore: add fathom to the CSP
    
    * docs(changeset): feat: integrate fathom

---
+++ .changeset/blue-snails-add.md
@@ -0,0 +1,5 @@
+---
+'scalar-api-client': patch
+---
+
+feat: integrate fathom
--- packages/api-client-app/src/renderer/src/main.ts
+++ packages/api-client-app/src/renderer/src/main.ts
@@ -1,9 +1,10 @@
 import { webHashRouter } from '@scalar/api-client'
 import { createApiClientApp } from '@scalar/api-client/layouts/App'
 import '@scalar/api-client/style.css'
+import { load, trackEvent } from 'fathom-client'
 
 // Initialize
-createApiClientApp(
+await createApiClientApp(
   document.getElementById('scalar-client'),
   {
     proxyUrl: 'https://proxy.scalar.com',
@@ -11,3 +12,26 @@ createApiClientApp(
   true,
   webHashRouter,
 )
+
+/**
+ * Fathom Analytics offers simple & privacy-first tracking
+ * @see https://usefathom.com/
+ */
+load('EUNBEXQC', {
+  // Skips automatically tracking page views
+  auto: false,
+})
+
+// Track the launch event
+const { platform } = window.electron.process
+
+const os =
+  platform === 'darwin'
+    ? 'mac'
+    : platform === 'win32'
+      ? 'windows'
+      : platform === 'linux'
+        ? 'linux'
+        : 'unknown'
+
+trackEvent(`launch: ${os}`)
