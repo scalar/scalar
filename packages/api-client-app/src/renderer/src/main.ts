ff1addea16644230289b65e26788dcba9f6acbd0
Hans Pagel <mail@hanspagel.com>
2024-08-02 16:22:23 +0200

    feat: integrate fathom

--- packages/api-client-app/src/renderer/src/main.ts
+++ packages/api-client-app/src/renderer/src/main.ts
@@ -1,9 +1,10 @@
 import { webHashRouter } from '@scalar/api-client'
 import { createApiClientApp } from '@scalar/api-client/layouts/App'
 import '@scalar/api-client/style.css'
+import * as Fathom from 'fathom-client'
 
 // Initialize
-createApiClientApp(
+await createApiClientApp(
   document.getElementById('scalar-client'),
   {
     proxyUrl: 'https://proxy.scalar.com',
@@ -11,3 +12,14 @@ createApiClientApp(
   true,
   webHashRouter,
 )
+
+/**
+ * Fathom Analytics offers simple & privacy-first tracking
+ * @see https://usefathom.com/
+ */
+Fathom.load('MY_FATHOM_ID', {
+  // Skips automatically tracking page views
+  auto: false,
+})
+
+Fathom.trackEvent('launch')
