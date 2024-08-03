42a3e79a5cb72442d4db2d2d09e89d6b57bdaefd
tmastrom <thomas.mastromonaco@gmail.com>
2024-08-02 17:20:23 -0700

    feat: cli lint scalar config json

--- packages/cli/src/commands/lint/LintCommand.ts
+++ packages/cli/src/commands/lint/LintCommand.ts
@@ -1,10 +1,63 @@
 import { Command } from 'commander'
+import kleur from 'kleur'
+import prettyjson from 'prettyjson'
+
+import { useGivenFileOrConfiguration } from '../../utils'
+import { lint } from './lint'
 
 /**
  * Lint users scalar configs (scalar.config.json files)
  */
 export function LintCommand() {
   const cmd = new Command('lint')
+
+  cmd.description('Lint users scalar configs')
+  cmd.argument('[file]', 'File to lint')
+  cmd.action(async (inputArgument: string) => {
+    const startTime = performance.now()
+
+    // Read file
+    const input = useGivenFileOrConfiguration(inputArgument)
+
+    // Validate
+    const result = lint(input)
+
+    if (result.valid) {
+      console.log(kleur.green(`Matches the Scalar config specifications`))
+
+      const endTime = performance.now()
+
+      console.log()
+      console.log(
+        kleur.green('File validated'),
+        kleur.grey(
+          `in ${kleur.white(
+            `${kleur.bold(`${Math.round(endTime - startTime)}`)} ms`,
+          )}`,
+        ),
+      )
+      console.log()
+    } else {
+      console.log(prettyjson.render(result.data))
+      console.log()
+      console.error(
+        kleur.red('File doesn’t match the Scalar config specification.'),
+      )
+      console.log()
+      console.error(
+        kleur.red(
+          `${kleur.bold(
+            `${result.data?.length} error${
+              result.data && result.data.length > 1 ? 's' : ''
+            }`,
+          )} found.`,
+        ),
+      )
+      console.log()
+
+      process.exit(1)
+    }
+  })
 
   return cmd
 }
---
+++ packages/cli/src/commands/lint/types.ts
@@ -0,0 +1,77 @@
+type ConfigSidebarPage = {
+  path: string
+  name?: string
+  description?: string
+  backgroundImage?: string
+  icon?: string
+  type: 'page'
+  children?: ConfigSidebarItem[]
+}
+
+type ConfigSidebarFolder = {
+  name: string
+  type: 'folder'
+  children: ConfigSidebarItem[]
+  icon?: string
+}
+
+type ConfigSidebarLink = {
+  name: string
+  type: 'link'
+  url: string
+  icon?: string
+}
+
+export type ConfigSidebarItem =
+  | ConfigSidebarPage
+  | ConfigSidebarFolder
+  | ConfigSidebarLink
+
+export type ConfigGuide =
+  | {
+      name: string
+      description?: string
+      folder: string
+    }
+  | {
+      name: string
+      description?: string
+      sidebar: ConfigSidebarItem[]
+    }
+
+export type ConfigReference = {
+  name: string
+  path: string
+  description?: string
+}
+
+/** Top Level Scalar Config Object */
+export type ScalarConfig = {
+  publishOnMerge?: boolean
+  subdomain: string
+  customDomain?: string
+  siteMeta?: {
+    favicon?: string
+    ogImage?: string
+    title?: string
+    description?: string
+  }
+  siteConfig?: {
+    footer?: string
+    footerCss?: string
+    footerBelowSidebar?: boolean
+    headScript?: string
+    bodyScript?: string
+    theme?: string
+    logo?:
+      | string
+      | {
+          darkMode?: string
+          lightMode?: string
+        }
+  }
+  guides: ConfigGuide[]
+  references: ConfigReference[]
+  // TODO: add theme field
+  // TODO: add footer field
+}
