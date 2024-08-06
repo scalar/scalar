abdd3b8a5612476e6363f94f42cb81dd14e8b88e
tmastrom <thomas.mastromonaco@gmail.com>
2024-08-06 16:44:42 -0700

    chore: readme and test organization

--- packages/config/test/check.test.ts
+++ packages/config/test/check.test.ts
@@ -1,16 +1,16 @@
 import path from 'node:path'
 import { describe, expect, it } from 'vitest'
 
-import { check } from './check'
+import { check } from '../src/check'
 
 describe('check', () => {
   it('checks the valid json file', () => {
-    const result = check('/src/valid.json')
+    const result = check('/test/valid.json')
     console.log(JSON.stringify(result, null, 3))
     expect(result.valid).toBe(true)
   })
   it('checks the invalid json file', () => {
-    const result = check('/src/invalid.json')
+    const result = check('/test/invalid.json')
     console.log(JSON.stringify(result, null, 3))
     expect(result.valid).toBe(true)
   })
