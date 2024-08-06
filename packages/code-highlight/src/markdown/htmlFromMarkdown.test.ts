ec36670a8870f1c5e677308e7d829f9cfdc5e2a2
Hans Pagel <mail@hanspagel.com>
2024-08-06 12:20:35 +0200

    test: more htmlFromMarkdown tests

--- packages/code-highlight/src/markdown/htmlFromMarkdown.test.ts
+++ packages/code-highlight/src/markdown/htmlFromMarkdown.test.ts
@@ -9,6 +9,35 @@ describe('htmlFromMarkdown', () => {
     expect(html.trim()).toEqual('<h1>Example Heading</h1>')
   })
 
+  it('removes tags', () => {
+    const html = htmlFromMarkdown(`# <i>Example</i> <em>Heading</em>`, {
+      removeTags: ['i'],
+    })
+
+    expect(html.trim()).toEqual('<h1>Example <em>Heading</em></h1>')
+  })
+
+  it('removes script tags', () => {
+    const html = htmlFromMarkdown(
+      `# Example Heading<script>alert('foobar');</script>`,
+    )
+
+    expect(html.trim()).toEqual('<h1>Example Heading</h1>')
+  })
+
+  it('allows given tags', () => {
+    const html = htmlFromMarkdown(
+      `# Example Heading<script>alert('foobar');</script>`,
+      {
+        allowTags: ['script'],
+      },
+    )
+
+    expect(html.trim()).toEqual(
+      `<h1>Example Heading\n  <script>alert('foobar');</script>\n</h1>`,
+    )
+  })
+
   it('allows to add ids', () => {
     const html = htmlFromMarkdown(`# Example Heading`, {
       transformType: 'heading',
