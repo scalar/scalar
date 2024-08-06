1819ca47849c890c9e4406e45d641a71ad36cbf6
tmastrom <thomas.mastromonaco@gmail.com>
2024-08-06 16:30:57 -0700

    chore: setup build and test scripts for @scalar/config package

--- packages/config/src/check.test.ts
+++ packages/config/src/check.test.ts
@@ -5,12 +5,12 @@ import { check } from './check'
 
 describe('check', () => {
   it('checks the valid json file', () => {
-    const result = check('/packages/config/src/valid.json')
+    const result = check('/src/valid.json')
     console.log(JSON.stringify(result, null, 3))
     expect(result.valid).toBe(true)
   })
   it('checks the invalid json file', () => {
-    const result = check('/packages/config/src/invalid.json')
+    const result = check('/src/invalid.json')
     console.log(JSON.stringify(result, null, 3))
     expect(result.valid).toBe(true)
   })
