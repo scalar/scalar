2fbd7d5fb03a193512fd4ebd1db1bc9038979455
Hans Pagel <mail@hanspagel.com>
2024-08-09 14:26:02 +0200

    feat: new OpenApiDocument component to provide data

---
+++ packages/api-reference/playground/embed/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.mount('#app')
---
+++ packages/api-reference/src/embeds/OpenApiDocument/index.ts
@@ -0,0 +1,1 @@
+export { default as OpenApiDocument } from './OpenApiDocument.vue'
