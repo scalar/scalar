1d8da9ad029ad5a415dc262e9b7f2e166bdad435
Jan van Heesch <TheVaan@gmail.com>
2024-08-10 05:01:34 +0200

    feat(scalar_fastapi): extend configuration for FastAPI (#2631)
    * feat: add hide_models, hidden_clients and servers config
    
    * chore(docs): add FastAPI Scalar configuration
    
    * feat: add hide_download_button and default_open_all_tags to config
    
    * chore(docs): add hide_download_button and default_open_all_tags to FastAPI Scalar docs
    
    * feat: add layout, show_sidebar, dark_mode and search_hot_key config to Scalar for FastAPI
    
    * chore(docs): add layout, show_sidebar, dark_mode and search_hot_key to docs
    
    * chore: format
    
    ---------
    
    Co-authored-by: Hans Pagel <mail@hanspagel.com>

--- packages/scalar_fastapi/scalar_fastapi/scalar_fastapi.py
+++ packages/scalar_fastapi/scalar_fastapi/scalar_fastapi.py
@@ -1,5 +1,41 @@
+import json
+from enum import Enum
 from typing_extensions import Annotated, Doc
 from fastapi.responses import HTMLResponse
+
+
+class Layout(Enum):
+    MODERN = "modern"
+    CLASSIC = "classic"
+
+
+class SearchHotKey(Enum):
+    A = "a"
+    B = "b"
+    C = "c"
+    D = "d"
+    E = "e"
+    F = "f"
+    G = "g"
+    H = "h"
+    I = "i"
+    J = "j"
+    K = "k"
+    L = "l"
+    M = "m"
+    N = "n"
+    O = "o"
+    P = "p"
+    Q = "q"
+    R = "r"
+    S = "s"
+    T = "t"
+    U = "u"
+    V = "v"
+    W = "w"
+    X = "x"
+    Y = "y"
+    Z = "z"
 
 
 scalar_theme = """
@@ -153,6 +189,89 @@ def get_scalar_api_reference(
             """
         ),
     ] = scalar_theme,
+    layout: Annotated[
+        Layout,
+        Doc(
+            """
+            The layout to use for Scalar.
+            Default is "modern".
+            """
+        ),
+    ] = Layout.MODERN,
+    show_sidebar: Annotated[
+        bool,
+        Doc(
+            """
+            A boolean to show the sidebar.
+            Default is True which means the sidebar is shown.
+            """
+        ),
+    ] = True,
+    hide_download_button: Annotated[
+        bool,
+        Doc(
+            """
+            A boolean to hide the download button.
+            Default is False which means the download button is shown.
+            """
+        ),
+    ] = False,
+    hide_models: Annotated[
+        bool,
+        Doc(
+            """
+            A boolean to hide all models.
+            Default is False which means all models are shown.
+            """
+        ),
+    ] = False,
+    dark_mode: Annotated[
+        bool,
+        Doc(
+            """
+            Whether dark mode is on or off initially (light mode).
+            Default is True which means dark mode is used.
+            """
+        ),
+    ] = True,
+    search_hot_key: Annotated[
+        SearchHotKey,
+        Doc(
+            """
+            The hotkey to use for search.
+            Default is "k" (e.g. CMD+k).
+            """
+        ),
+    ] = SearchHotKey.K,
+    hidden_clients: Annotated[
+        bool | dict[str, bool | list[str]] | list[str],
+        Doc(
+            """
+            A dictionary with the keys being the target names and the values being a boolean to hide all clients of the target or a list clients.
+            If a boolean is provided, it will hide all the clients with that name.
+            Backwards compatibility: If a list of strings is provided, it will hide the clients with the name and the list of strings.
+            Default is [] which means no clients are hidden.
+            """
+        ),
+    ] = [],
+    servers: Annotated[
+        list[dict[str, str]],
+        Doc(
+            """
+            A list of dictionaries with the keys being the server name and the value being the server URL.
+            Default is [] which means no servers are provided.
+            """
+        ),
+    ] = [],
+    default_open_all_tags: Annotated[
+        bool,
+        Doc(
+            """
+            A boolean to open all tags by default.
+            Default is False which means all tags are closed by default.
+            """
+        ),
+    ] = False,
 ) -> HTMLResponse:
     html = f"""
     <!DOCTYPE html>
@@ -181,6 +300,22 @@ def get_scalar_api_reference(
       id="api-reference"
       data-url="{openapi_url}"
       data-proxy-url="{scalar_proxy_url}"></script>
+    <script>
+      var configuration = {{
+        layout: "{layout.value}",
+        showSidebar: {json.dumps(show_sidebar)},
+        hideDownloadButton: {json.dumps(hide_download_button)},
+        hideModels: {json.dumps(hide_models)},
+        darkMode: {json.dumps(dark_mode)},
+        searchHotKey: "{search_hot_key.value}",
+        hiddenClients: {json.dumps(hidden_clients)},
+        servers: {json.dumps(servers)},
+        defaultOpenAllTags: {json.dumps(default_open_all_tags)},
+      }}
+
+      document.getElementById('api-reference').dataset.configuration =
+        JSON.stringify(configuration)
+    </script>
     <script src="{scalar_js_url}"></script>
     </body>
     </html>
