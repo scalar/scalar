0d8212acdd64f89260a24a20414dcf3813e570b1
tmastrom <thomas.mastromonaco@gmail.com>
2024-08-14 09:34:42 -0700

    chore: copy yaml files for publish script

--- packages/galaxy/rollup.config.ts
+++ packages/galaxy/rollup.config.ts
@@ -2,6 +2,14 @@ import { createRollupConfig, findEntryPoints } from '@scalar/build-tooling'
 
 export default createRollupConfig({
   typescript: true,
+  copy: [
+    { src: 'src/specifications/3.1.yaml', dest: 'dist' },
+    {
+      src: 'src/specifications/3.1.yaml',
+      dest: 'dist',
+      rename: 'latest.yaml',
+    },
+  ],
   options: {
     input: await findEntryPoints(),
   },
