d782ba87ac490938564d629c2a9658e3b3fcd6d1
tmastrom <thomas.mastromonaco@gmail.com>
2024-08-15 13:14:51 -0700

    chore: galaxy dependencies

--- packages/galaxy/playground/index.ts
+++ packages/galaxy/playground/index.ts
@@ -2,8 +2,6 @@ import { serve } from '@hono/node-server'
 import { createMockServer } from '@scalar/mock-server'
 import fs from 'node:fs'
 
-// import specification from '../src/specification/3.1.yaml'
-
 const specification = fs.readFileSync('../src/specifications/3.1.yaml', 'utf-8')
 console.log(typeof specification)
 const port = process.env.PORT || 5052
@@ -22,7 +20,7 @@ serve(
     fetch: app.fetch,
     port: Number(port),
   },
-  (info) => {
+  (info: { port: number }) => {
     console.log()
     console.log(`🚧 Mock Server listening on http://localhost:${info.port}`)
     console.log()
