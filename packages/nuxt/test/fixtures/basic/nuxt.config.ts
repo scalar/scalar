59beee90f78aa1758f685c0527325122276b7f62
Amrit <amrit@scalar.com>
2024-08-23 10:52:12 -0700

    chore: remove tests

--- packages/nuxt/test/fixtures/basic/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtConfig({
-  modules: ['../src/module'],
-  scalar: {
-    spec: {
-      url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.yaml',
-    },
-  },
-})
