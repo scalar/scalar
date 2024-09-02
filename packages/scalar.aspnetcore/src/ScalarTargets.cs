fb51ed874c443d690121735b83f4001fc27dc2d3
Justin Lampe <xC0dex@users.noreply.github.com>
2024-09-02 20:43:14 +0200

    refactor: AspNetCore solution

--- packages/scalar.aspnetcore/src/ScalarTargets.cs
+++ packages/scalar.aspnetcore/src/ScalarTargets.cs
@@ -58,22 +58,3 @@ public enum ScalarTargets
     [Description("swift")]
     Swift
 }
-
-internal static class Extensions
-{
-    public static string GetDescription(this Enum enumValue)
-    {
-        var field = enumValue.GetType().GetField(enumValue.ToString());
-        if (field == null)
-        {
-            return enumValue.ToString();
-        }
-
-        if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
-        {
-            return attribute.Description;
-        }
-
-        return enumValue.ToString();
-    }
-}
