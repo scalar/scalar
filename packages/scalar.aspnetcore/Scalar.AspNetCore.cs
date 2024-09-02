883cd8129e59747ec8d14afcf3fbdebe07578d02
Facundo Juarez <facundoalejojuarez@gmail.com>
2024-09-02 01:50:13 -0300

    feat: Add new configuration options and docs (#3007)
    * Add new options, enum for Scalar Themes and strings const for Scalar Clinets
    
    * add new enums for ScalarClients and ScalarTargets
    add new option to filter by tarjets

--- packages/scalar.aspnetcore/Scalar.AspNetCore.cs
+++ packages/scalar.aspnetcore/Scalar.AspNetCore.cs
@@ -3,16 +3,12 @@ using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Scalar.AspNetCore
 {
     public static class OpenApiEndpointRouteBuilderExtensions
     {
-        public static IEndpointConventionBuilder MapScalarApiReference(this IEndpointRouteBuilder endpoints)
-        {
-            return endpoints.MapScalarApiReference(_ => { });
-        }
+        private const string DOCUMENT_NAME = "{documentName}";
 
         private static readonly JsonSerializerOptions JsonSerializerOptions = new()
         {
@@ -21,41 +17,46 @@ namespace Scalar.AspNetCore
             DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
         };
 
-        public static IEndpointConventionBuilder MapScalarApiReference(this IEndpointRouteBuilder endpoints,
-            Action<ScalarOptions> configureOptions)
+        public static IEndpointConventionBuilder MapScalarApiReference(this IEndpointRouteBuilder endpoints)
+        {
+            return endpoints.MapScalarApiReference(_ => { });
+        }
+        public static IEndpointConventionBuilder MapScalarApiReference(this IEndpointRouteBuilder endpoints, Action<ScalarOptions> configureOptions)
         {
             var options = new ScalarOptions();
             configureOptions(options);
 
-            var configurationJson = JsonSerializer.Serialize(options, JsonSerializerOptions);
+            if (!options.EndpointPathPrefix.Contains(DOCUMENT_NAME))
+                throw new ArgumentException($"`EndpointPathPrefix` must define `{DOCUMENT_NAME}`.");
 
-            return endpoints.MapGet(options.EndpointPathPrefix + "/{documentName}", (string documentName) =>
-                {
-                    var title = options.Title ?? $"Scalar API Reference -- {documentName}";
-                    var dataUrl = options.DataUrlFunc is not null ? options.DataUrlFunc(documentName) : $"/openapi/{documentName}.json";
-                    return Results.Content(
-                        $$"""
-                          <!doctype html>
-                          <html>
-                          <head>
-                              <title>{{title}}</title>
-                              <meta charset="utf-8" />
-                              <meta name="viewport" content="width=device-width, initial-scale=1" />
-                          </head>
-                          <body>
-                              <script id="api-reference" data-url="{{dataUrl}}"></script>
-                              <script>
-                              var configuration = {{configurationJson}}
+            return endpoints.MapGet(options.EndpointPathPrefix, (string documentName) =>
+            {
+                var title = options.Title.Replace(DOCUMENT_NAME, documentName);
+                var dataUrl = options.SetDataUrl?.Invoke(documentName) ?? $"/openapi/{documentName}.json";
+                var configuration = JsonSerializer.Serialize(new ScalaConfiguration(options), JsonSerializerOptions);
 
-                              document.getElementById('api-reference').dataset.configuration =
-                                  JSON.stringify(configuration)
-                              </script>
-                              <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
-                          </body>
-                          </html>
-                          """, "text/html");
-                })
-                .ExcludeFromDescription();
+                return Results.Content(
+                    $$"""
+                        <!doctype html>
+                        <html>
+                        <head>
+                            <title>{{title}}</title>
+                            <meta charset="utf-8" />
+                            <meta name="viewport" content="width=device-width, initial-scale=1" />
+                        </head>
+                        <body>
+                            <script id="api-reference" data-url="{{dataUrl}}"></script>
+                            <script>
+                            var configuration = {{configuration}}
+
+                            document.getElementById('api-reference').dataset.configuration = JSON.stringify(configuration)
+                            </script>
+                            <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+                        </body>
+                        </html>
+                        """, "text/html");
+            })
+            .ExcludeFromDescription();
         }
     }
 }
