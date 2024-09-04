41e08f53ed5d64e5d9d6f18d163a1cb68494f0eb
geoffgscott <59206100+geoffgscott@users.noreply.github.com>
2024-09-04 13:53:12 -0700

    Geoff/relax imports (#3042)
    * fix: Ignore invalid security schemes
    
    * fix: formatting issues

--- packages/oas-utils/src/transforms/import-spec.test.ts
+++ packages/oas-utils/src/transforms/import-spec.test.ts
@@ -1,126 +1,9 @@
 import { importSpecToWorkspace } from '@/transforms/import-spec'
+import circular from '@test/fixtures/basic-circular-spec.json'
+import petstoreMod from '@test/fixtures/petstore-tls.json'
 import { describe, expect, test } from 'vitest'
 
-const circular = {
-  openapi: '3.0.1',
-  info: {
-    title: 'Test OpenAPI definition',
-    version: '1.0.0',
-  },
-  paths: {
-    '/api/v1/updateEmployee': {
-      put: {
-        tags: ['employees'],
-        requestBody: {
-          content: {
-            'application/json': {
-              schema: {
-                type: 'array',
-                items: {
-                  $ref: '#/components/schemas/Employee',
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-    '/api/v1/updateManager': {
-      put: {
-        tags: ['managers'],
-        requestBody: {
-          content: {
-            'application/json': {
-              schema: {
-                type: 'array',
-                items: {
-                  $ref: '#/components/schemas/Employee',
-                },
-              },
-            },
-          },
-        },
-      },
-      delete: {
-        // tags: ['managers'],
-        requestBody: {
-          content: {
-            'application/json': {
-              schema: {
-                type: 'array',
-                items: {
-                  $ref: '#/components/schemas/Employee',
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  tags: [
-    { 'name': 'employees', 'x-scalar-children': [{ tagName: 'managers' }] },
-    { name: 'managers' },
-  ],
-  components: {
-    schemas: {
-      Employee: {
-        type: 'object',
-        properties: {
-          id: {
-            type: 'integer',
-          },
-          manager: {
-            $ref: '#/components/schemas/Employee',
-          },
-          team: {
-            type: 'array',
-            items: {
-              $ref: '#/components/schemas/Employee',
-            },
-          },
-        },
-      },
-    },
-    securitySchemes: {
-      bearerAuth: {
-        type: 'http',
-        scheme: 'bearer',
-      },
-      basicAuth: {
-        type: 'http',
-        scheme: 'basic',
-      },
-      apiKeyHeader: {
-        type: 'apiKey',
-        in: 'header',
-        name: 'X-API-Key',
-      },
-      apiKeyQuery: {
-        type: 'apiKey',
-        in: 'query',
-        name: 'api_key',
-      },
-      apiKeyCookie: {
-        type: 'apiKey',
-        in: 'cookie',
-        name: 'api_key',
-      },
-      oauth2: {
-        type: 'oauth2',
-        flows: {
-          implicit: {
-            authorizationUrl: 'https://galaxy.scalar.com/oauth/authorize',
-            scopes: {
-              'write:planets': 'modify planets in your account',
-              'read:planets': 'read your planets',
-            },
-          },
-        },
-      },
-    },
-  },
-}
+import galaxy from '../../../galaxy/dist/latest.json'
 
 describe('Import OAS Specs', () => {
   test('Handles circular', async () => {
@@ -136,11 +19,16 @@ describe('Import OAS Specs', () => {
     ).toEqual(true)
   })
 
+  test('Handles weird petstore', async () => {
+    const res = await importSpecToWorkspace(petstoreMod)
+
+    expect(res.error).toBe(false)
+  })
+
   // Causes cyclic dependency
-  // There are cyclic workspace dependencies: /home/amritk/apps/scalar/scalar/packages/galaxy, /home/amritk/apps/scalar/scalar/packages/mock-server, /home/amritk/apps/scalar/scalar/packages/oas-utils
-  // test('Loads galaxy spec', async () => {
-  //   const res = await importSpecToWorkspace(galaxySpec)
-  //
-  //   expect(res.error).toEqual(false)
-  // })
+  test('Loads galaxy spec', async () => {
+    const res = await importSpecToWorkspace(galaxy)
+
+    expect(res.error).toEqual(false)
+  })
 })
