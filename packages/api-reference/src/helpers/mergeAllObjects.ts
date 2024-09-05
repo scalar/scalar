b88d79d0dafe01eb6555667001e4a815d2ab41df
Hans Pagel <mail@hanspagel.com>
2024-09-05 10:49:22 +0200

    chore: comment mergeAllObjects

--- packages/api-reference/src/helpers/mergeAllObjects.ts
+++ packages/api-reference/src/helpers/mergeAllObjects.ts
@@ -1,3 +1,6 @@
+/**
+ * Merges an array of objects into a single object.
+ */
 export const mergeAllObjects = (items: Record<any, any>[]): any => {
   return items.reduce((acc, object) => {
     return {
