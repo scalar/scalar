c1d39c5ca5da330c26f8a984535a115ddc4b9279
Marc Laventure <marc@scalar.com>
2024-09-20 16:17:09 -0700

    fix: remove trailing slash in examples and in client for server (#3176)
    * fix: remove trailing slash from server on ui rendering
    
    * fix: remove trailing slash in examples and in client for server
    
    * docs(changeset): fix: remove trailing slash in examples and in client for server

--- packages/api-reference/src/legacy/helpers/getUrlFromServerState.ts
+++ packages/api-reference/src/legacy/helpers/getUrlFromServerState.ts
@@ -5,8 +5,12 @@ import { replaceVariables } from '@scalar/oas-utils/helpers'
  * Get the URL from the server state.
  */
 export function getUrlFromServerState(state: ServerState) {
-  // Get the selected server
+  // Get the selected server and remove trailing slash
   const server = state?.servers?.[state.selectedServer ?? 0]
+
+  if (server?.url?.endsWith('/')) {
+    server.url = server.url.slice(0, -1)
+  }
 
   // Replace variables: {protocol}://{host}:{port}/{basePath}
   const url =
