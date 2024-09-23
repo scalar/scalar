ab80776fe46af5f2df0b9e25af040687635c0c71
Hans Pagel <mail@hanspagel.com>
2024-09-23 11:54:15 +0200

    chore: code style

--- packages/config/test/check.test.ts
+++ packages/config/test/check.test.ts
@@ -3,15 +3,20 @@ import { describe, expect, it } from 'vitest'
 import { check } from '../src/index'
 
 describe('check', () => {
-  it.only('checks the valid json file', () => {
+  it('checks the valid json file', () => {
     const result = check('/test/valid.json')
+
     console.log(JSON.stringify(result, null, 3))
+
     expect(result.valid).toBe(true)
     expect(result.errors).toBe(null)
   })
+
   it('checks the invalid json file', () => {
     const result = check('/test/invalid.json')
+
     console.log(JSON.stringify(result, null, 3))
+
     expect(result.valid).toBe(false)
     expect(result.errors).toBeDefined()
   })
