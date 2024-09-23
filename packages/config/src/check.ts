1ff8f63dfe01d046f96e1322f1f0806f466c2d28
Hans Pagel <mail@hanspagel.com>
2024-09-23 12:22:26 +0200

    refactor: don’t mess with the given file path, improve test

--- packages/config/src/check.ts
+++ packages/config/src/check.ts
@@ -1,16 +1,28 @@
-import { Value } from '@sinclair/typebox/value'
+import { Value, type ValueError } from '@sinclair/typebox/value'
 import fs from 'node:fs'
 
 import { ScalarConfigType } from './configTypes'
 
 /** check scalar config file using the generated schema */
-export function check(filePath: string) {
-  const scalarConfigFile = fs.readFileSync(process.cwd() + filePath, 'utf8')
-  const scalarConfigJson = JSON.parse(scalarConfigFile)
+export function check(file: string) {
+  try {
+    const scalarConfigFile = fs.readFileSync(file, 'utf8')
+    const scalarConfiguration = JSON.parse(scalarConfigFile)
 
-  const result = Value.Check(ScalarConfigType, scalarConfigJson)
-  return {
-    valid: result,
-    errors: [...Value.Errors(ScalarConfigType, scalarConfigJson)],
+    const result = Value.Check(ScalarConfigType, scalarConfiguration)
+
+    return {
+      valid: result,
+      errors: [...Value.Errors(ScalarConfigType, scalarConfiguration)],
+    }
+  } catch (error: any) {
+    return {
+      valid: false,
+      errors: [
+        {
+          message: error.message,
+        } as ValueError,
+      ],
+    }
   }
 }
--- packages/config/test/check.test.ts
+++ packages/config/test/check.test.ts
@@ -1,23 +1,20 @@
+import path from 'node:path'
 import { describe, expect, it } from 'vitest'
 
 import { check } from '../src/index'
 
 describe('check', () => {
   it('checks the valid json file', () => {
-    const result = check('/test/valid.json')
+    const result = check(path.resolve('./test/valid.json'))
 
-    console.log(JSON.stringify(result, null, 3))
-
-    expect(result.valid).toBe(true)
     expect(result.errors).toBe(null)
+    expect(result.valid).toBe(true)
   })
 
   it('checks the invalid json file', () => {
-    const result = check('/test/invalid.json')
+    const result = check(path.resolve('./test/invalid.json'))
 
-    console.log(JSON.stringify(result, null, 3))
-
-    expect(result.valid).toBe(false)
     expect(result.errors).toBeDefined()
+    expect(result.valid).toBe(false)
   })
 })
