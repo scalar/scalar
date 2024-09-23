0aeb49b7bc239728ae8cdcdf47b40d68679d11cb
Hans Pagel <mail@hanspagel.com>
2024-09-23 13:04:49 +0200

    refactor: move findOpenApiDocumentUrl helper to @scalar/import

--- packages/import/src/resolve.test.ts
+++ packages/import/src/resolve.test.ts
@@ -1,24 +1,24 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 
-import { findOpenApiDocumentUrl } from './find-openapi-document-url'
+import { resolve } from './resolve'
 
 global.fetch = vi.fn()
 
 function createFetchResponse(data: string) {
   return {
     ok: true,
-    text: () => new Promise((resolve) => resolve(data)),
+    text: () => new Promise((r) => r(data)),
   }
 }
 
-describe('findOpenApiDocumentUrl', () => {
+describe('resolve', () => {
   beforeEach(() => {
     // @ts-expect-error
     global.fetch.mockReset()
   })
 
   it('returns JSON urls', async () => {
-    const result = await findOpenApiDocumentUrl(
+    const result = await resolve(
       'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
     )
 
@@ -28,7 +28,7 @@ describe('findOpenApiDocumentUrl', () => {
   })
 
   it('returns YAML urls', async () => {
-    const result = await findOpenApiDocumentUrl(
+    const result = await resolve(
       'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.yaml',
     )
 
@@ -36,7 +36,7 @@ describe('findOpenApiDocumentUrl', () => {
       'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.yaml',
     )
 
-    const otherResult = await findOpenApiDocumentUrl(
+    const otherResult = await resolve(
       'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.yml',
     )
 
@@ -46,15 +46,11 @@ describe('findOpenApiDocumentUrl', () => {
   })
 
   it('finds URLs from sandbox URL', async () => {
-    const result = await findOpenApiDocumentUrl(
-      'https://sandbox.scalar.com/p/dlw8v',
-    )
+    const result = await resolve('https://sandbox.scalar.com/p/dlw8v')
 
     expect(result).toBe('https://sandbox.scalar.com/files/dlw8v/openapi.yaml')
 
-    const otherResult = await findOpenApiDocumentUrl(
-      'https://sandbox.scalar.com/e/dlw8v',
-    )
+    const otherResult = await resolve('https://sandbox.scalar.com/e/dlw8v')
 
     expect(otherResult).toBe(
       'https://sandbox.scalar.com/files/dlw8v/openapi.yaml',
@@ -82,7 +78,7 @@ describe('findOpenApiDocumentUrl', () => {
     // @ts-expect-error Mocking types are missing
     fetch.mockResolvedValue(createFetchResponse(html))
 
-    const result = await findOpenApiDocumentUrl('https://example.com/reference')
+    const result = await resolve('https://example.com/reference')
 
     expect(result).toBe(
       'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.yaml',
@@ -110,7 +106,7 @@ describe('findOpenApiDocumentUrl', () => {
     // @ts-expect-error Mocking types are missing
     fetch.mockResolvedValue(createFetchResponse(html))
 
-    const result = await findOpenApiDocumentUrl('https://example.com/reference')
+    const result = await resolve('https://example.com/reference')
 
     expect(result).toBe(
       'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.yaml',
@@ -138,7 +134,7 @@ describe('findOpenApiDocumentUrl', () => {
     // @ts-expect-error Mocking types are missing
     fetch.mockResolvedValue(createFetchResponse(html))
 
-    const result = await findOpenApiDocumentUrl('https://example.com/reference')
+    const result = await resolve('https://example.com/reference')
 
     expect(result).toBe('https://example.com/openapi.yaml')
   })
@@ -169,7 +165,7 @@ describe('findOpenApiDocumentUrl', () => {
     // @ts-expect-error Mocking types are missing
     fetch.mockResolvedValue(createFetchResponse(html))
 
-    const result = await findOpenApiDocumentUrl('https://example.com/reference')
+    const result = await resolve('https://example.com/reference')
 
     expect(result).toBe('https://example.com/openapi.yaml')
   })
@@ -192,7 +188,7 @@ describe('findOpenApiDocumentUrl', () => {
     // @ts-expect-error Mocking types are missing
     fetch.mockResolvedValue(createFetchResponse(html))
 
-    const result = await findOpenApiDocumentUrl('https://example.com/reference')
+    const result = await resolve('https://example.com/reference')
 
     expect(result).toBe(
       'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.yaml',
@@ -219,7 +215,7 @@ describe('findOpenApiDocumentUrl', () => {
     // @ts-expect-error Mocking types are missing
     fetch.mockResolvedValue(createFetchResponse(html))
 
-    const result = await findOpenApiDocumentUrl('https://example.com/reference')
+    const result = await resolve('https://example.com/reference')
 
     expect(result).toBe(
       'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.yaml',
--- packages/import/src/resolve.ts
+++ packages/import/src/resolve.ts
@@ -1,4 +1,113 @@
-// TODO
-export async function resolve() {
-  return 'foobar'
+/**
+ * Find an OpenAPI document URL in the HTML of @scalar/api-reference and other places.
+ * This is useful to open the OpenAPI document from basically any source.
+ */
+export async function resolve(value?: string) {
+  // URLs
+  if (value?.startsWith('http://') || value?.startsWith('https://')) {
+    // https://*.json
+    if (value?.toLowerCase().endsWith('.json')) {
+      return value
+    }
+
+    // https://*.yaml
+    if (
+      value?.toLowerCase().endsWith('.yaml') ||
+      value?.toLowerCase().endsWith('.yml')
+    ) {
+      return value
+    }
+
+    // https://sandbox.scalar.com
+    const sandboxUrl = value.match(
+      /https:\/\/sandbox\.scalar\.com\/(p|e)\/([a-z0-9]+)/,
+    )
+
+    if (sandboxUrl?.[2]) {
+      return `https://sandbox.scalar.com/files/${sandboxUrl[2]}/openapi.yaml`
+    }
+
+    // Fetch URL
+    try {
+      const result = await fetch(value)
+
+      if (result.ok) {
+        const urlOrPath = parseHtml(await result.text())
+
+        if (urlOrPath) {
+          return makeRelativeUrlsAbsolute(value, urlOrPath)
+        }
+      } else {
+        console.warn(`[@scalar/import] Failed to fetch ${value}`)
+      }
+    } catch (error) {
+      console.error(`[@scalar/import] Failed to fetch ${value}`, error)
+    }
+  }
+
+  return undefined
+}
+
+/**
+ * Go through the HTML and try to find the OpenAPI document URL
+ */
+function parseHtml(html?: string) {
+  // Check whether it could be HTML
+  if (!html?.includes('<')) {
+    return undefined
+  }
+
+  // data-url="*"
+  const dataUrlMatch = html.match(/data-url=["']([^"']+)["']/)
+
+  if (dataUrlMatch?.[1]) {
+    return dataUrlMatch[1]
+  }
+
+  // spec-url="*"
+  const specUrlMatch = html.match(/spec-url=["']([^"']+)["']/)
+
+  if (specUrlMatch?.[1]) {
+    return specUrlMatch[1]
+  }
+
+  // Redoc.init('*')
+  const redocInit = html.match(/Redoc.init\(["']([^"']+)["']/)
+
+  if (redocInit?.[1]) {
+    return redocInit[1]
+  }
+
+  // &amp;quot;url&amp;quot;:&amp;quot;MY_CUSTOM_URL&amp;quot;
+  const configurationUrl = html.match(
+    /&amp;quot;url&amp;quot;:&amp;quot;([^;]+)&amp;quot;/,
+  )
+
+  if (configurationUrl?.[1]) {
+    return configurationUrl[1]
+  }
+
+  return undefined
+}
+
+/**
+ * URLs can be relative, but we need absolute URLs eventually.
+ */
+function makeRelativeUrlsAbsolute(baseUrl: string, path: string) {
+  // Check whether the path is already absolute
+  if (path.startsWith('http://') || path.startsWith('https://')) {
+    return path
+  }
+
+  // Combine the URL and the relative path
+  try {
+    const { href } = new URL(path, baseUrl)
+
+    return href
+  } catch (error) {
+    // Return original path if URL creation fails
+    console.error('[makeRelativeUrlsAbsolute] Error combining URLs:', error)
+
+    return path
+  }
 }
