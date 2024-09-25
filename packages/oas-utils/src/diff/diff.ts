9d5e8d570872db510155d00fc87a1d22f16e9d04
Amrit <amrit@scalar.com>
2024-09-25 12:41:13 -0700

    feat: switchin

--- packages/oas-utils/src/diff/diff.ts
+++ packages/oas-utils/src/diff/diff.ts
@@ -2,23 +2,15 @@ import microdiff from 'microdiff'
 
 const types = {
   CREATE: 'add',
-  REMOVE: 'delete',
-  CHANGE: 'edit',
+  REMOVE: 'remove',
+  CHANGE: 'modify',
 } as const
-
-/** TODO: add a ref dictionary to the parser to handle components */
-const allowedProperties: Record<string, boolean> = {
-  info: true,
-  servers: true,
-  security: true,
-  tags: true,
-  paths: true,
-}
 
 export type DiffChangeType = (typeof types)[keyof typeof types]
 
 /**
  * Compare two specs and return the changes for each request entry
+ *
  */
 export function diffSpec(a: object, b: object) {
   const diff = microdiff(a, b)
@@ -27,28 +19,34 @@ export function diffSpec(a: object, b: object) {
     string,
     {
       type: DiffChangeType
-      path: (string | number)[]
+      mutations: object[]
       value?: object
-    }[]
+    }
   > = {}
 
-  console.log({ diff })
-
   diff
-    .filter((d) => allowedProperties[d.path[0]])
+    .filter((d) => d.path[0] === 'paths')
     .forEach((d) => {
       const key = d.path
         .slice(0, 3)
         .map((p) => String(p).replaceAll('/', '~1'))
         .join('/')
 
-      requestChanges[key] ||= []
-      requestChanges[key].push(
-        d.type === 'REMOVE'
-          ? { type: types[d.type], path: d.path }
-          : { type: types[d.type], path: d.path, value: d.value },
-      )
+      if (!requestChanges[key])
+        requestChanges[key] = {
+          type: types[d.type],
+          mutations: [],
+        }
+
+      if (d.type === 'CHANGE') {
+        requestChanges[key].mutations.push({
+          ...d,
+          path: d.path.slice(3),
+        })
+      } else if (d.type === 'CREATE') {
+        requestChanges[key].value = d.value
+      }
     })
 
-  return requestChanges
+  return JSON.stringify(requestChanges, null, 2)
 }
