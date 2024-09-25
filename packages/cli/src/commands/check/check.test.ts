fb643894c7e7042b6f2e0ff8d89006532fe618ea
tmastrom <thomas.mastromonaco@gmail.com>
2024-09-25 14:34:42 +0300

    chore: fix tests

--- packages/cli/src/commands/check/check.test.ts
+++ packages/cli/src/commands/check/check.test.ts
@@ -1,21 +1,24 @@
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { describe, expect, it } from 'vitest'
 
 import { ScalarCli } from '../../../tests/invoke-cli'
 
 describe('check', () => {
   it('checks the given json file', () => {
+    const file = fileURLToPath(new URL('./valid.json', import.meta.url))
     const [exitCode, logs] = ScalarCli()
       .setCwd(path.resolve('./'))
-      .invoke(['check', '/src/commands/check/valid.json'])
-    logs.should.contain('Success')
+      .invoke(['check', file])
+    logs.should.contain('SUCCESS')
     expect(exitCode).toBe(0)
   })
   it('checks the invalid json file', () => {
+    const file = fileURLToPath(new URL('./invalid.json', import.meta.url))
     const [exitCode, logs] = ScalarCli()
       .setCwd(path.resolve('./'))
-      .invoke(['check', '/src/commands/check/invalid.json'])
-    logs.should.contain('false')
+      .invoke(['check', file])
+    logs.should.not.contain('SUCCESS')
     expect(exitCode).toBe(1)
   })
 })
