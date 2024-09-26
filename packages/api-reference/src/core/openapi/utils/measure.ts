4df75a634818b17b10aae1f9d9a0148db6f04be6
Hans Pagel <mail@hanspagel.com>
2024-09-26 22:49:19 +0200

    chore: return result of measured callback

--- packages/api-reference/src/core/openapi/utils/measure.ts
+++ packages/api-reference/src/core/openapi/utils/measure.ts
@@ -1,15 +1,17 @@
 /**
  * Measure the time it takes to execute a given function
  */
-export async function measure(
+export async function measure<T = any>(
   key: string,
-  callback: () => void | Promise<void>,
+  callback: () => T | Promise<T>,
 ) {
   const start = performance.now()
 
-  await callback()
+  const result = await callback()
 
   const end = performance.now()
 
   console.info(`${key}: ${Math.round(end - start)} ms`)
+
+  return result
 }
