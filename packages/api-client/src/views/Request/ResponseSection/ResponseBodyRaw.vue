a329fadfee7ec37852dab78622fc60c760a06b5a
Hans Pagel <mail@hanspagel.com>
2024-09-26 17:24:11 +0200

    chore: remove unused code

--- packages/api-client/src/views/Request/ResponseSection/ResponseBodyRaw.vue
+++ packages/api-client/src/views/Request/ResponseSection/ResponseBodyRaw.vue
@@ -1,8 +1,7 @@
 <script lang="ts" setup>
 import { ScalarCodeBlock } from '@scalar/components'
-import { isJsonString } from '@scalar/oas-utils/helpers'
 import { type CodeMirrorLanguage, useCodeMirror } from '@scalar/use-codemirror'
-import { computed, ref, toRaw } from 'vue'
+import { ref } from 'vue'
 
 const props = defineProps<{
   data: any
@@ -11,30 +10,17 @@ const props = defineProps<{
 
 const codeMirrorRef = ref<HTMLDivElement | null>(null)
 
-// Pretty print JSON
-const content = computed<string>(() => {
-  // Format JSON
-  const value = props.data
-  // Format JSON
-  if (value && isJsonString(value)) {
-    return JSON.stringify(JSON.parse(value as string), null, 2)
-  } else if (value && typeof toRaw(value) === 'object') {
-    return JSON.stringify(value, null, 2)
-  }
-  return value
-})
-
 useCodeMirror({
   codeMirrorRef,
   readOnly: true,
   lineNumbers: true,
-  content,
+  content: props.data,
   language: props.language,
 })
 </script>
 <template>
   <ScalarCodeBlock
-    :content="content"
+    :content="data"
     :lang="language" />
 </template>
 <style scoped>
