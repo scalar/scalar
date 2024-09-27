eba229a6f850a787378f8fc179091ff52b4f324f
anthony lio <anthony@scalar.com>
2024-09-27 16:07:38 +0300

    fix(api-reference): set block center to scroll search modal

--- packages/api-reference/src/features/Search/SearchModal.vue
+++ packages/api-reference/src/features/Search/SearchModal.vue
@@ -106,6 +106,7 @@ const navigateSearchResults = (direction: 'up' | 'down') => {
     if (element instanceof HTMLElement)
       element.scrollIntoView({
         behavior: 'smooth',
+        block: 'center',
       })
   })
 }
