c977d598aa6b56482daef7afbd1d1cb66e759c6f
Hans Pagel <mail@hanspagel.com>
2024-09-30 15:40:02 +0200

    chore: remove unused component

--- packages/api-client/src/views/Components/Schemas/Schemas.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import Sidebar from '@/components/Sidebar/Sidebar.vue'
-</script>
-<template>
-  <Sidebar title="Workspace Name" />
-  <div class="flex-1">SCHEMA!!</div>
-</template>
-<style scoped></style>
