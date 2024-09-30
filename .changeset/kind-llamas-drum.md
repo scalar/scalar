d8baf4ee84cca9d4514c1fb17d9a022187fadf41
Anthony Lio <anthony@scalar.com>
2024-09-30 23:06:57 +0300

    feat(api-client): squiggly icon for drafts (#3308)
    * feat(api-client): sets draft collection icon
    
    * docs(changeset): feat: sets draft collection icon
    
    * docs(changeset): feat: adds scribble icon

---
+++ .changeset/kind-llamas-drum.md
@@ -0,0 +1,5 @@
+---
+'@scalar/components': patch
+---
+
+feat: adds scribble icon
--- packages/components/src/components/ScalarIcon/icons/icons.ts
+++ packages/components/src/components/ScalarIcon/icons/icons.ts
@@ -63,6 +63,7 @@ export const ICONS = [
   'Play',
   'Refresh',
   'Roadmap',
+  'Scribble',
   'Search',
   'Server',
   'Show',
