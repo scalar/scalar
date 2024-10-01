d801fb9477a3bb90ba6689efb0a5e2a8d3bb2e96
tmastrom <thomas.mastromonaco@gmail.com>
2024-10-01 14:51:17 +0300

    chore: playground build wip

--- packages/snippetz/rollup.config.ts
+++ packages/snippetz/rollup.config.ts
@@ -6,11 +6,6 @@ import {
 
 const entries = await findEntryPoints()
 
-// Build the playground for docker deployments
-if (!process.env.BUILD_PLAYGROUND) {
-  entries.push('playground/index.ts')
-}
-
 export default createRollupConfig({
   typescript: true,
   options: {
