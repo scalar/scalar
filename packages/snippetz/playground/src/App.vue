c4d5b6e9223f60bf3c5b657ff5dd0dd9eb2fa0d4
tmastrom <thomas.mastromonaco@gmail.com>
2024-10-01 12:47:54 +0300

    feat: client target and url select input

--- packages/snippetz/playground/src/App.vue
+++ packages/snippetz/playground/src/App.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
+import { ScalarCodeBlock } from '@scalar/components'
 import { computed, ref } from 'vue'
 
+import type { ClientId, TargetId } from '../../src/core'
 import { snippetz } from '../../src/snippetz'
 
-const targets = ['node', 'js']
-const clients = ['undici', 'fetch', 'ofetch']
+const targets: TargetId[] = ['node', 'js']
+const clients: ClientId[] = ['undici', 'fetch', 'ofetch']
 
-const targetId = ref('node')
-const clientId = ref('undici')
+const targetId = ref(targets[0])
+const clientId = ref(clients[0])
 const url = ref('https://example.com')
 
 const snippet = computed(() =>
-  snippetz().print('node', 'undici', {
+  snippetz().print(targetId.value, clientId.value, {
     url: 'https://example.com',
   }),
 )
@@ -19,11 +21,35 @@ const snippet = computed(() =>
 
 <template>
   <div>@scalar/snippetz</div>
-  <div>{{ snippet }}</div>
+  <div>
+    request url:
+    <input
+      v-model="url"
+      type="text" />
+  </div>
+  <div>
+    Target:
+    <select v-model="targetId">
+      <option
+        v-for="target in targets"
+        :key="target"
+        :value="target">
+        {{ target }}
+      </option>
+    </select>
+  </div>
+  <div>
+    Client:
+    <select v-model="targetId">
+      <option
+        v-for="client in clients"
+        :key="client"
+        :value="client">
+        {{ client }}
+      </option>
+    </select>
+  </div>
+  <ScalarCodeBlock v-bind="{ content: snippet }"></ScalarCodeBlock>
 </template>
 
-<style scoped>
-button {
-  font-weight: bold;
-}
-</style>
+<style scoped></style>
