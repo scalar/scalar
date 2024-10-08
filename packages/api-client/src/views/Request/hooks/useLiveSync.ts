9d34977624ccf9a075d5764d9f674c0baf6cf0a2
Amrit <amrit@scalar.com>
2024-10-07 18:08:56 -0700

    feat: just added tests for request

--- packages/api-client/src/views/Request/hooks/useLiveSync.ts
+++ packages/api-client/src/views/Request/hooks/useLiveSync.ts
@@ -4,6 +4,7 @@ import { specDictionary } from '@/store/import-spec'
 import {
   combineRenameDiffs,
   diffToCollectionPayload,
+  diffToRequestPayload,
   diffToSecuritySchemePayload,
   diffToServerPayload,
   diffToTagPayload,
@@ -13,10 +14,8 @@ import {
   type Request,
   type RequestParameterPayload,
   type RequestPayload,
-  type Tag,
   requestSchema,
   serverSchema,
-  tagSchema,
 } from '@scalar/oas-utils/entities/spec'
 import {
   createHash,
@@ -28,6 +27,9 @@ import type { OpenAPIV3_1 } from '@scalar/openapi-types'
 import { useTimeoutPoll } from '@vueuse/core'
 import microdiff from 'microdiff'
 import { watch } from 'vue'
+
+/** Live Sync polling timeout */
+const FIVE_SECONDS = 5 * 1000
 
 /**
  * Hook which handles polling the documentUrl for changes then attempts to merge what is new
@@ -51,9 +53,6 @@ export const useLiveSync = () => {
     tags,
     tagMutators,
   } = useWorkspace()
-
-  /** Live Sync polling timeout */
-  const FIVE_SECONDS = 5 * 1000
 
   const { pause, resume } = useTimeoutPoll(async () => {
     const url = activeCollection.value?.documentUrl
@@ -91,16 +90,18 @@ export const useLiveSync = () => {
       combined.forEach((d) => {
         console.log('=========')
         console.log(d)
-        const { path, type } = d
-        if (!path.length || !activeCollection.value?.uid) return
+        if (!d.path.length || !activeCollection.value?.uid) return
 
         // Info/Security
-        if (path[0] === 'info' || path[0] === 'security') {
+        if (d.path[0] === 'info' || d.path[0] === 'security') {
           const payload = diffToCollectionPayload(d, activeCollection.value)
           if (payload) collectionMutators.edit(...payload)
         }
         // Components.securitySchemes
-        else if (path[0] === 'components' && path[1] === 'securitySchemes') {
+        else if (
+          d.path[0] === 'components' &&
+          d.path[1] === 'securitySchemes'
+        ) {
           const securitySchemePayload = diffToSecuritySchemePayload(
             d,
             activeCollection.value,
@@ -112,7 +113,7 @@ export const useLiveSync = () => {
           }
         }
         // Servers
-        else if (path[0] === 'servers') {
+        else if (d.path[0] === 'servers') {
           const serverPayload = diffToServerPayload(
             d,
             activeCollection.value,
@@ -124,7 +125,7 @@ export const useLiveSync = () => {
           }
         }
         // Tags
-        else if (path[0] === 'tags') {
+        else if (d.path[0] === 'tags') {
           const tagPayload = diffToTagPayload(d, tags, activeCollection.value)
           if (tagPayload) {
             const [method, ...payload] = tagPayload
@@ -132,116 +133,16 @@ export const useLiveSync = () => {
           }
         }
         // Paths
-        else if (path[0] === 'paths') {
-          console.log('=========')
-          console.log(d)
-          const [, _path, method, ...properties] = path as [
-            'paths',
-            Request['path'],
-            Request['method'] | 'method' | undefined,
-            keyof Request,
-          ]
-
-          // Path has changed
-          if (_path === 'path' && type === 'CHANGE') {
-            activeCollection.value.requests.forEach(
-              (uid) =>
-                requests[uid].path === d.oldValue &&
-                requestMutators.edit(uid, 'path', d.value),
-            )
-          }
-          // Method has changed
-          else if (method === 'method') {
-            activeCollection.value.requests.forEach(
-              (uid) =>
-                type === 'CHANGE' &&
-                requests[uid].method === d.oldValue &&
-                requestMutators.edit(uid, 'method', d.value),
-            )
-          }
-          // Add
-          else if (type === 'CREATE') {
-            // In some cases value goes { method: operation }
-            const [[_method, _operation]] = Object.entries(d.value)
-
-            const operation: OpenAPIV3_1.OperationObject<{
-              tags?: string[]
-              security?: OpenAPIV3_1.SecurityRequirementObject[]
-            }> = method ? d.value : _operation
-            const newMethod = method || _method
-
-            // TODO: match servers up and add if we don't have
-            const operationServers = serverSchema
-              .array()
-              .parse(operation.servers ?? [])
-
-            // Remove security here and add it correctly below
-            const { security: operationSecurity, ...operationWithoutSecurity } =
-              operation
-
-            const requestPayload: RequestPayload = {
-              ...operationWithoutSecurity,
-              method: isHTTPMethod(newMethod) ? newMethod : 'get',
-              path: _path,
-              parameters: (operation.parameters ??
-                []) as RequestParameterPayload[],
-              servers: operationServers.map((s) => s.uid),
-            }
-
-            // Add list of UIDs to associate security schemes
-            // As per the spec if there is operation level security we ignore the top level requirements
-            if (operationSecurity?.length)
-              requestPayload.security = operationSecurity.map((s) => {
-                const keys = Object.keys(s)
-
-                // Handle the case of {} for optional
-                if (keys.length) {
-                  const [key] = Object.keys(s)
-                  return {
-                    [key]: s[key],
-                  }
-                } else return s
-              })
-
-            // Save parse the request
-            const request = schemaModel(requestPayload, requestSchema, false)
-            if (request)
-              requestMutators.add(request, activeCollection.value.uid)
-            else
-              console.warn(
-                'Live Sync: was unable to add the new reqeust, please refresh to try again.',
-              )
-          }
-          // Delete
-          else if (type === 'REMOVE') {
-            const request = findResource<Request>(
-              activeCollection.value.requests,
-              requests,
-              (_request) =>
-                _request.path === _path && _request.method === method,
-            )
-            if (request)
-              requestMutators.delete(request, activeCollection.value.uid)
-          }
-          // Edit
-          else if (type === 'CHANGE') {
-            const request = findResource<Request>(
-              activeCollection.value.requests,
-              requests,
-              (r) => r.path === _path && r.method === method,
-            )
-
-            if (request) {
-              requestMutators.edit(
-                request.uid,
-                properties.join('.') as keyof Request,
-                d.value,
-              )
-
-              // TODO: Override the example bodies
-            } else
-              console.warn('Live Sync: request not found, was unable to update')
-          }
+        else if (d.path[0] === 'paths') {
+          const requestPayloads = diffToRequestPayload(
+            d,
+            activeCollection.value,
+            requests,
+          )
+          requestPayloads.forEach((rp) => {
+            const [method, ...payload] = rp
+            requestMutators[method](...payload)
+          })
         }
       })
 
