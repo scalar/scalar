11ab685360d454a8dac31e5e35b71566c353f777
tmastrom <thomas.mastromonaco@gmail.com>
2024-10-09 17:07:58 +0300

    feat: debounce function without dependencies

---
+++ packages/object-utils/src/mutator-record/debounce.test.ts
@@ -0,0 +1,48 @@
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
+
+import { debounce } from './debounce'
+
+describe('Functions are debounced', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  test('Debounce function', () => {
+    const func = vi.fn()
+    const debouncedFunc = debounce(func, 1000)
+
+    // Call it immediately
+    debouncedFunc()
+    expect(func).toHaveBeenCalledTimes(0) // func not called
+
+    // Call it several times with 500ms between each call
+    for (let i = 0; i < 10; i++) {
+      vi.advanceTimersByTime(500)
+      debouncedFunc()
+    }
+    expect(func).toHaveBeenCalledTimes(0) // func not called
+
+    // wait 1000ms
+    vi.advanceTimersByTime(1000)
+    expect(func).toHaveBeenCalledTimes(1) // func called
+  })
+
+  test('Debounce function with max wait', () => {
+    const func = vi.fn()
+    const debouncedFunc = debounce(func, 1000, { maxWait: 2000 })
+
+    // Call it immediately
+    debouncedFunc()
+    expect(func).toHaveBeenCalledTimes(0) // func not called
+
+    // Call it several times with 500ms between each call
+    for (let i = 0; i < 10; i++) {
+      vi.advanceTimersByTime(500)
+      debouncedFunc()
+    }
+    expect(func).toHaveBeenCalledTimes(2) // func is called twice because max wait is reached every 4 x 500ms
+  })
+})
