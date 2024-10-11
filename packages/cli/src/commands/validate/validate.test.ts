f9f55cf7423d1a4bdbd53de526c6c80135953fd0
Hans Pagel <mail@hanspagel.com>
2024-10-11 17:12:31 +0200

    test: validate file with external reference

--- packages/cli/src/commands/validate/validate.test.ts
+++ packages/cli/src/commands/validate/validate.test.ts
@@ -6,7 +6,9 @@ import { ScalarCli } from '../../../tests/invoke-cli'
 
 describe('validate', () => {
   it('validates the given json file', () => {
-    const file = fileURLToPath(new URL('./valid.json', import.meta.url))
+    const file = fileURLToPath(
+      new URL('./fixtures/valid.json', import.meta.url),
+    )
 
     const [exitCode, logs] = ScalarCli()
       .setCwd(path.resolve('./'))
@@ -17,7 +19,9 @@ describe('validate', () => {
   })
 
   it('validates the given yaml file', () => {
-    const file = fileURLToPath(new URL('./valid.yaml', import.meta.url))
+    const file = fileURLToPath(
+      new URL('./fixtures/valid.yaml', import.meta.url),
+    )
 
     const [exitCode, logs] = ScalarCli()
       .setCwd(path.resolve('./'))
@@ -28,7 +32,9 @@ describe('validate', () => {
   })
 
   it('shows errors for invalid file', () => {
-    const file = fileURLToPath(new URL('./invalid.json', import.meta.url))
+    const file = fileURLToPath(
+      new URL('./fixtures/invalid.json', import.meta.url),
+    )
 
     const [exitCode, logs] = ScalarCli()
       .setCwd(path.resolve('./'))
@@ -48,6 +54,19 @@ describe('validate', () => {
         'validate',
         'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
       ])
+
+    logs.should.contain('OpenAPI 3.1')
+    expect(exitCode).toBe(0)
+  })
+
+  it.only('validates external files', () => {
+    const file = fileURLToPath(
+      new URL('./fixtures/file-reference.yaml', import.meta.url),
+    )
+
+    const [exitCode, logs] = ScalarCli()
+      .setCwd(path.resolve('./'))
+      .invoke(['validate', file])
 
     logs.should.contain('OpenAPI 3.1')
     expect(exitCode).toBe(0)
