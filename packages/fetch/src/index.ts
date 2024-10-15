5ec9d67a1319450b410ab9e6234b2c339f65b4e7
tmastrom <thomas.mastromonaco@gmail.com>
2024-10-15 16:12:21 +0300

    chore: no export alias

--- packages/fetch/src/index.ts
+++ packages/fetch/src/index.ts
@@ -1,8 +1,4 @@
 export { formatApiError, formatApiResponse } from './format'
 export { schemaModel } from './schemaModel'
 export { request } from './request'
-export type {
-  ApiError as APIError,
-  ApiResponse as APIResponse,
-  RequestConfig,
-} from './types'
+export type { ApiError, ApiResponse, RequestConfig } from './types'
