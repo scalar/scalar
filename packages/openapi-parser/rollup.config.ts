4f2cc80feed88b58b80866f3cf20b6868e19d3c1
Hans Pagel <mail@hanspagel.com>
2024-10-16 16:45:46 +0200

    chore: use standard build tooling

--- packages/openapi-parser/rollup.config.ts
+++ packages/openapi-parser/rollup.config.ts
@@ -1,11 +1,7 @@
-import json from '@rollup/plugin-json'
-import terser from '@rollup/plugin-terser'
-import typescript from '@rollup/plugin-typescript'
-import { addPackageFileExports } from '@scalar/build-tooling'
-import { rm } from 'node:fs/promises'
-import { builtinModules } from 'node:module'
-import type { Plugin, RollupOptions } from 'rollup'
-import outputSize from 'rollup-plugin-output-size'
+import {
+  addPackageFileExports,
+  createRollupConfig,
+} from '@scalar/build-tooling'
 
 const entries = [
   './src/index.ts',
@@ -13,69 +9,11 @@ const entries = [
   './src/plugins/read-files/index.ts',
 ]
 
-await addPackageFileExports({ entries })
-
-const dir = 'dist'
-
-/**
- * Remove the output directory.
- */
-function cleanBeforeWrite(directory: string): Plugin {
-  let removePromise: Promise<void>
-
-  return {
-    generateBundle(_options, _bundle, isWrite) {
-      if (isWrite) {
-        // Only remove before first write, but make all writes wait on the removal
-        removePromise ??= rm(directory, {
-          force: true,
-          recursive: true,
-        })
-
-        return removePromise
-      }
-    },
-    name: 'clean-before-write',
-  }
-}
-
-/**
- * Rollup configuration
- */
-const config: RollupOptions[] = [
-  // Code
-  {
+export default createRollupConfig({
+  typescript: true,
+  options: {
     input: entries,
-    output: [
-      // ESM
-      {
-        format: 'esm',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-        dir,
-      },
-    ],
-    plugins: [
-      cleanBeforeWrite(dir),
-      typescript(),
-      json(),
-      terser(),
-      outputSize(),
-    ],
-    external: [
-      ...builtinModules,
-      ...builtinModules.map((m) => `node:${m}`),
-      'ajv/dist/2020',
-      'ajv-draft-04',
-      'ajv-formats',
-      'yaml',
-      '@scalar/openapi-types',
-    ],
-    treeshake: {
-      annotations: true,
-      preset: 'recommended',
-    },
   },
-]
+})
 
-export default config
+await addPackageFileExports({ entries })
