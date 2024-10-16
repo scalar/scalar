1b9bc4f8d08e66ecf00120267c0d15045a6f4a4f
Hans Pagel <mail@hanspagel.com>
2024-10-16 14:58:40 +0200

    feat: add react identifier

--- packages/api-reference-react/src/ApiReferenceReact.tsx
+++ packages/api-reference-react/src/ApiReferenceReact.tsx
@@ -13,7 +13,7 @@ globalThis.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = true
 globalThis.__VUE_PROD_DEVTOOLS__ = false
 
 /**
- * React wrapper around Api Reference
+ * React wrapper around the Scalar API Reference
  */
 export const ApiReferenceReact = (props: ReferenceProps) => {
   const el = useRef<HTMLDivElement | null>(null)
@@ -25,10 +25,13 @@ export const ApiReferenceReact = (props: ReferenceProps) => {
   useEffect(() => {
     if (!el.current) return reference?.unmount
 
-    const instance = createScalarReferences(
-      el.current,
-      props.configuration ?? {},
-    )
+    const defaultConfig = {
+      _integration: 'react',
+    } as const
+
+    const mergedConfig = { ...defaultConfig, ...props.configuration }
+
+    const instance = createScalarReferences(el.current, mergedConfig)
     setReference(instance)
 
     // Unmount on cleanup
@@ -36,8 +39,11 @@ export const ApiReferenceReact = (props: ReferenceProps) => {
   }, [el])
 
   useEffect(() => {
+    const defaultConfig = { _integration: 'react' } as const
+    const mergedConfig = { ...defaultConfig, ...props.configuration }
+
     reference?.updateConfig(
-      props.configuration ?? {},
+      mergedConfig,
       /** For React we will just replace the config if it changes */
       false,
     )
