0de4b2b613dbd48d73312a1f485a5098eeff7fa2
Hans Pagel <mail@hanspagel.com>
2024-10-16 17:46:44 +0200

    chore: remove tsx example

--- examples/tsx/index.ts
+++
@@ -1,9 +0,0 @@
-import { toJson } from '@scalar/openapi-parser'
-import { readFiles } from '@scalar/openapi-parser/plugins/read-files'
-
-console.log(
-  toJson({
-    foo: 'bar',
-  }),
-  readFiles,
-)
