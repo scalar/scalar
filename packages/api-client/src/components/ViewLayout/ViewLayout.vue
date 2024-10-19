62ae12f394556e3e961080b2073ddc3604dd6ea8
Ryan Poole <rmp135@gmail.com>
2024-10-19 10:38:03 +0100

    perf(api-client): improve large response performance in Chromium

--- packages/api-client/src/components/ViewLayout/ViewLayout.vue
+++ packages/api-client/src/components/ViewLayout/ViewLayout.vue
@@ -1,6 +1,25 @@
+<style scoped>
+.view-layout > :not([hidden]):not(:first-child) {
+  border-top-width: calc(calc(var(--scalar-border-width) / 2));
+  border-left-width: 0px;
+}
+.view-layout > :not([hidden]):first-child {
+  border-bottom-width: 0px;
+}
+
+@screen xl {
+  .view-layout > :not([hidden]):first-child {
+    border-right-width: 0px;
+  }
+  .view-layout > :not([hidden]):not(:first-child) {
+    border-top-width: 0px;
+    border-left-width: calc(calc(var(--scalar-border-width) / 2));
+  }
+}
+</style>
 <template>
   <div
-    class="divide-y md:divide-y-0 divide-1 flex flex-col min-h-0 flex-1 xl:overflow-hidden md:flex-row leading-3">
+    class="view-layout flex flex-col min-h-0 flex-1 xl:overflow-hidden md:flex-row leading-3">
     <slot />
   </div>
 </template>
--- packages/api-client/src/components/ViewLayout/ViewLayoutContent.vue
+++ packages/api-client/src/components/ViewLayout/ViewLayoutContent.vue
@@ -1,6 +1,25 @@
+<style scoped>
+.view-layout-content > :not([hidden]):not(:first-child) {
+  border-top-width: calc(calc(var(--scalar-border-width) / 2));
+  border-left-width: 0px;
+}
+.view-layout-content > :not([hidden]):first-child {
+  border-bottom-width: 0px;
+}
+
+@screen xl {
+  .view-layout-content > :not([hidden]):first-child {
+    border-right-width: 0px;
+  }
+  .view-layout-content > :not([hidden]):not(:first-child) {
+    border-top-width: 0px;
+    border-left-width: calc(calc(var(--scalar-border-width) / 2));
+  }
+}
+</style>
 <template>
   <div
-    class="border-t-1/2 md:border-t-0 divide divide-y xl:divide-y-0 xl:divide-x-1/2 flex xl:flex-row flex-col custom-scroll pr-0">
+    class="view-layout-content border-t-1/2 md:border-t-0 flex xl:flex-row flex-col custom-scroll pr-0">
     <slot />
   </div>
 </template>
