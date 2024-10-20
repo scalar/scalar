46b4e3b679912475af241ff51cb839af75575cfa
Brynn <brynnhawker@gmail.com>
2024-10-20 12:02:08 -0700

    Update ViewLayoutContent.vue

--- packages/api-client/src/components/ViewLayout/ViewLayoutContent.vue
+++ packages/api-client/src/components/ViewLayout/ViewLayoutContent.vue
@@ -1,22 +1,3 @@
-<style scoped>
-.view-layout-content > :not([hidden]):not(:first-child) {
-  border-top-width: calc(calc(var(--scalar-border-width) / 2));
-  border-left-width: 0px;
-}
-.view-layout-content > :not([hidden]):first-child {
-  border-bottom-width: 0px;
-}
-
-@screen xl {
-  .view-layout-content > :not([hidden]):first-child {
-    border-right-width: 0px;
-  }
-  .view-layout-content > :not([hidden]):not(:first-child) {
-    border-top-width: 0px;
-    border-left-width: calc(calc(var(--scalar-border-width) / 2));
-  }
-}
-</style>
 <template>
   <div
     class="*:border-t-1/2 first:*:border-t-0 xl:*:border-t-0 xl:*:border-l-1/2 xl:first:*:border-l-0 flex xl:flex-row flex-col custom-scroll pr-0">
