9fa43503017c3702281b234c986fe3829fc99090
Brynn <brynnhawker@gmail.com>
2024-10-20 12:02:47 -0700

    Update ViewLayoutContent.vue

--- packages/api-client/src/components/ViewLayout/ViewLayoutContent.vue
+++ packages/api-client/src/components/ViewLayout/ViewLayoutContent.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- Using *:border-* to avoid chrome performance issue with adjacent sibling selector (~) -->
   <div
     class="*:border-t-1/2 first:*:border-t-0 xl:*:border-t-0 xl:*:border-l-1/2 xl:first:*:border-l-0 flex xl:flex-row flex-col custom-scroll pr-0">
     <slot />
