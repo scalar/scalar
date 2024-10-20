3d2d49cfa6c6dc2ceb3af777c2c252d95b40b26b
Brynn <brynnhawker@gmail.com>
2024-10-20 12:01:48 -0700

    Update packages/api-client/src/components/ViewLayout/ViewLayoutContent.vue

--- packages/api-client/src/components/ViewLayout/ViewLayoutContent.vue
+++ packages/api-client/src/components/ViewLayout/ViewLayoutContent.vue
@@ -19,7 +19,7 @@
 </style>
 <template>
   <div
-    class="view-layout-content border-t-1/2 md:border-t-0 flex xl:flex-row flex-col custom-scroll pr-0">
+    class="*:border-t-1/2 first:*:border-t-0 xl:*:border-t-0 xl:*:border-l-1/2 xl:first:*:border-l-0 flex xl:flex-row flex-col custom-scroll pr-0">
     <slot />
   </div>
 </template>
