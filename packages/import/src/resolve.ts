4ff7a60514f8b6e0f3fa9de541f4dabc0b69bddc
Hans Pagel <mail@hanspagel.com>
2024-10-21 11:14:25 +0200

    feat: allow to pass null

--- packages/import/src/resolve.ts
+++ packages/import/src/resolve.ts
@@ -3,7 +3,7 @@
  * This is useful to open the OpenAPI document from basically any source.
  */
 export async function resolve(
-  value?: string,
+  value?: string | null,
 ): Promise<string | Record<string, any> | undefined> {
   // URLs
   if (value?.startsWith('http://') || value?.startsWith('https://')) {
