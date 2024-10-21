17d27ec8a7ab1965bea181333c0af4d0c10b42ff
Hans Pagel <mail@hanspagel.com>
2024-10-21 11:14:55 +0200

    fix: @scalar/import does not work with sandbox links with uppercase letters

--- packages/import/src/resolve.ts
+++ packages/import/src/resolve.ts
@@ -29,7 +29,7 @@ export async function resolve(
 
     // https://sandbox.scalar.com
     const sandboxUrl = value.match(
-      /https:\/\/sandbox\.scalar\.com\/(p|e)\/([a-z0-9]+)/,
+      /https:\/\/sandbox\.scalar\.com\/(p|e)\/([a-zA-Z0-9]+)/,
     )
 
     if (sandboxUrl?.[2]) {
