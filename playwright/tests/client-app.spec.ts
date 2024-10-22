e5973c76e0262ff043fec63d5b9b80e24f1e7cd4
tmastrom <thomas.mastromonaco@gmail.com>
2024-10-22 16:06:25 +0300

    test: playwright launches electron app

---
+++ playwright/tests/client-app.spec.ts
@@ -0,0 +1,28 @@
+import { _electron as electron, test } from '@playwright/test'
+
+test('launch app', async () => {
+  // Launch Electron app.
+  const electronApp = await electron.launch({
+    args: ['../packages/api-client-app/out/main/index.js'],
+  })
+
+  // Evaluation expression in the Electron context.
+  const appPath = await electronApp.evaluate(async ({ app }) => {
+    // This runs in the main Electron process, parameter here is always
+    // the result of the require('electron') in the main app script.
+    return app.getAppPath()
+  })
+  console.log(appPath)
+
+  // Get the first window that the app opens, wait if necessary.
+  const window = await electronApp.firstWindow()
+  // Print the title.
+  console.log(await window.title())
+  // Capture a screenshot.
+  //   await window.screenshot({ path: 'intro.png' })
+  // Direct Electron console to Node terminal.
+  window.on('console', console.log)
+  // Click button.
+  await window.click('text=Click me')
+  // Exit app.
+})
