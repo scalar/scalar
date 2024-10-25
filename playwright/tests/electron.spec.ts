c4d9ccb2e741ab5fce5af598d55027422a351439
tmastrom <thomas.mastromonaco@gmail.com>
2024-10-25 16:43:10 +0300

    test: launch electron app and verify window url

--- playwright/tests/electron.spec.ts
+++ playwright/tests/electron.spec.ts
@@ -1,32 +1,40 @@
 import { _electron as electron, expect, test } from '@playwright/test'
 
-test('launch app', async ({ browserName, isMobile }) => {
+test('launch app', async ({ page, browserName, isMobile }) => {
   if (browserName !== 'chromium' || isMobile) {
     test.skip()
   }
+
   // Launch Electron app.
   const electronApp = await electron.launch({
     args: ['../packages/api-client-app/out/main/index.js'],
   })
 
-  // Evaluation expression in the Electron context.
-  const appPath = await electronApp.evaluate(async ({ app }) => {
-    // This runs in the main Electron process, parameter here is always
-    // the result of the require('electron') in the main app script.
-    return app.getAppPath()
-  })
-  console.log(appPath)
+  // Wait for the main window to be created
+  await page.waitForTimeout(500)
 
-  // Get the first window that the app opens, wait if necessary.
-  const window = await electronApp.firstWindow()
-  // Print the title.
-  console.log(await window.title())
-  // Direct Electron console to Node terminal.
-  window.on('console', console.log)
+  const windows = electronApp.windows()
+  const window = windows.find((win) => win.url().includes('index.html'))
+  if (!window) {
+    throw new Error('Window not found')
+  }
+
+  // console.log('Window title:', await window.title()) // this app has no titile right now
+  // console.log('Window URL:', window.url())
+  expect(window.url()).toContain(
+    'packages/api-client-app/out/renderer/index.html#/workspace/default/request/default',
+  )
+
+  // TODO: Click an element
+  // await window.click('button:text("Workspace")')
+  // Or click by CSS selector
+  // await mainWindow.click('#some-button-id');
+  // Or click by XPath
+  // await window.click('//button[contains(text(), "Workspace")]')
+
   // Capture a screenshot.
-  expect(await window.screenshot({ path: 'homepage.png' })).not.toThrowError()
-  // Click button.
-  await window.click('text=Workspace')
+  // await window.screenshot({ path: 'electron.png' })
+
   // Exit app.
   await electronApp.close()
 })
