d5cdad42d3eafe9926574b098353f13968dc9025
Justin Lampe <xC0dex@users.noreply.github.com>
2024-10-26 20:17:56 +0200

    chore: Update playground

--- packages/scalar.aspnetcore/playground/Scalar.AspNetCore.Playground/Books/BookEndpoints.cs
+++ packages/scalar.aspnetcore/playground/Scalar.AspNetCore.Playground/Books/BookEndpoints.cs
@@ -20,7 +20,8 @@ internal static class BookEndpoints
                 var book = bookStore.GetById(bookId);
                 return book is null ? Results.NotFound() : Results.Ok(book);
             })
-            .Produces<Book>();
+            .Produces<Book>()
+            .Produces(StatusCodes.Status404NotFound);
 
         books
             .MapPost("/", ([FromServices] BookStore bookStore, Book myCustomBook) =>
@@ -28,7 +29,8 @@ internal static class BookEndpoints
                 var createdBook = bookStore.Add(myCustomBook);
                 return createdBook is null ? Results.Conflict() : Results.Created($"/books/{createdBook.BookId}", createdBook);
             })
-            .Produces<Book>();
+            .Produces<Book>(StatusCodes.Status201Created)
+            .Produces(StatusCodes.Status409Conflict);
 
         books
             .MapPut("/{bookId:guid}", ([FromServices] BookStore bookStore, Guid bookId, Book book) =>
@@ -36,12 +38,15 @@ internal static class BookEndpoints
                 var updatedBook = bookStore.UpdateById(bookId, book);
                 return updatedBook is null ? Results.NotFound() : Results.Ok(updatedBook);
             })
-            .Produces<Book>();
+            .Produces<Book>()
+            .Produces(StatusCodes.Status404NotFound);
 
         books.MapDelete("/{bookId:guid}", ([FromServices] BookStore bookStore, Guid bookId) =>
-        {
-            var deleted = bookStore.DeleteById(bookId);
-            return deleted ? Results.NoContent() : Results.NotFound();
-        });
+            {
+                var deleted = bookStore.DeleteById(bookId);
+                return deleted ? Results.NoContent() : Results.NotFound();
+            })
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status404NotFound);
     }
 }
