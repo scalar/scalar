de502f061f0793d2e44c15f7453d53b4510c56f1
Thomas Mastromonaco <36525329+tmastrom@users.noreply.github.com>
2024-10-28 14:54:49 +0200

    test(api-reference): make the standalone test stable (#3622)
    * chore: specify jsdom for test environment explicitly
    
    * test: combine test because execution order matters
    
    * fix: pnpm lock file
    
    * chore: allow Node 18 in playwright package
    
    * refector: use new waitFor helper instead of sleep for 50ms
    
    * refactor: split up the standalone test again
    
    * chore: run the tests after eachother
    
    ---------
    
    Co-authored-by: Hans Pagel <mail@hanspagel.com>

--- packages/api-reference/src/standalone.spec.ts
+++ packages/api-reference/src/standalone.spec.ts
@@ -1,23 +1,16 @@
 import galaxyContent from '@scalar/galaxy/latest.yaml?raw'
-import { afterAll, describe, expect, it } from 'vitest'
-import MatchMediaMock from 'vitest-matchmedia-mock'
+import { describe, expect, it } from 'vitest'
 
-import { sleep } from './helpers'
+import { waitFor } from './helpers/waitFor'
 import type { ReferenceProps } from './types'
 
-/**
- * Tests for standalone
- * A bit hacky as we are working with the dom without jsdom, but works for these tests
- * as each test builds off the last
- */
-describe('Standalone API References', () => {
-  const matchMediaMock = new MatchMediaMock()
-
-  afterAll(() => {
-    matchMediaMock.clear()
-  })
-
-  it('renders very basic references with standalone', async () => {
+describe.sequential('standalone', { retry: 3, timeout: 10000 }, () => {
+  // Generates the required script tag on the fly:
+  //
+  // <script id="api-reference" type="application/json">
+  //   { "openapi": "3.1.0", "info": { "title": "Example" }, "paths": {} }
+  // </script>
+  it('renders a basic reference with the HTML API', async () => {
     const script = document.createElement('script')
     script.id = 'api-reference'
     script.type = 'application/json'
@@ -29,39 +22,32 @@ describe('Standalone API References', () => {
     document.body.appendChild(script)
 
     await import('./standalone')
-    await sleep(50)
 
-    const rootElement = document.querySelector('[data-v-app]')
-    const reference = rootElement?.querySelector('.scalar-api-reference')
-    const h1 = reference?.querySelector('h1')
-
-    expect(rootElement?.contains(reference!)).toBeTruthy()
-    expect(h1?.innerHTML).toEqual('Example')
+    await waitFor(() => {
+      const reference = document.querySelector('.scalar-api-reference')
+      const header = reference?.querySelector('h1')
+      return header?.innerHTML === 'Example'
+    })
   })
 
-  it('removes the app element and reloads', async () => {
-    // Remove the main element
-    let rootElement = document.querySelector('[data-v-app]')
+  it('reloads the reference after removing the app element', async () => {
+    const rootElement = document.querySelector('[data-v-app]')
     rootElement?.parentNode?.removeChild(rootElement)
 
-    expect(document.body.contains(rootElement)).toBeFalsy()
+    expect(document.body.contains(rootElement!)).toBeFalsy()
     expect(document.querySelector('[data-v-app]')).toBeNull()
 
-    // Now we use the event to re-load the app
     document.dispatchEvent(new Event('scalar:reload-references'))
-    await sleep(50)
 
-    rootElement = document.querySelector('[data-v-app]')
-    const reference = rootElement?.querySelector('.scalar-api-reference')
-    const h1 = reference?.querySelector('h1')
-
-    expect(rootElement?.contains(reference!)).toBeTruthy()
-    expect(h1?.innerHTML).toEqual('Example')
+    await waitFor(() => {
+      const reference = document.querySelector('.scalar-api-reference')
+      const header = reference?.querySelector('h1')
+      return header?.innerHTML === 'Example'
+    })
   })
 
-  it('updates the spec url, detect for changes', async () => {
-    // Update the config with the galaxy spec
-    const ev = new CustomEvent('scalar:update-references-config', {
+  it('updates when the configuration changes', async () => {
+    const event = new CustomEvent('scalar:update-references-config', {
       detail: {
         configuration: {
           spec: {
@@ -70,12 +56,12 @@ describe('Standalone API References', () => {
         },
       } satisfies ReferenceProps,
     })
-    document.dispatchEvent(ev)
-    await sleep(50)
 
-    const rootElement = document.querySelector('[data-v-app]')
-    const h1 = rootElement?.querySelector('h1')
+    document.dispatchEvent(event)
 
-    expect(h1?.innerHTML).toEqual('Scalar Galaxy')
+    await waitFor(() => {
+      const header = document.querySelector('[data-v-app] h1')
+      return header?.innerHTML === 'Scalar Galaxy'
+    })
   })
 })
