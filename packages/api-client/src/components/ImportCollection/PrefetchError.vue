65b282f10caa2a2558cf486629d553066ada370c
Hans Pagel <mail@hanspagel.com>
2024-10-30 14:30:10 +0100

    chore: clean up

--- packages/api-client/src/components/ImportCollection/PrefetchError.vue
+++ packages/api-client/src/components/ImportCollection/PrefetchError.vue
@@ -2,40 +2,24 @@
 import type { PrefetchResult } from '@/components/ImportCollection/hooks/useUrlPrefetcher'
 import { isUrl } from '@/libs'
 import { ScalarIcon } from '@scalar/components'
-import { computed } from 'vue'
 
-const props = defineProps<{
+defineProps<{
   result?: PrefetchResult
 }>()
-
-const domain = computed(() => {
-  if (!props.result?.input || !isUrl(props.result.input)) {
-    return undefined
-  }
-
-  const hostname = new URL(props.result?.input)?.hostname
-
-  const CHARACTER_LIMIT = 24
-
-  return hostname?.length > CHARACTER_LIMIT
-    ? `${hostname.slice(0, CHARACTER_LIMIT)}...`
-    : hostname
-})
 </script>
 
 <template>
   <template v-if="result?.error">
     <div
-      v-if="result?.input && domain"
+      v-if="result?.input && isUrl(result.input)"
       class="text-sm break-words text-center w-full font-medium">
-      We couldn't find an OpenAPI document at this URL. You can try visiting
+      We couldn’t find an OpenAPI document at the provided URL. Please download
+      and import the
       <a
-        class="max-w-[50ch] truncate"
         :href="result.input"
         rel="noopener nofollow"
         target="_blank"
-        v-text="domain" />
-      to download the OpenAPI file manually.
+        v-text="'OpenAPI document manually'" />.
     </div>
 
     <div
