417e0c7b6f9045fbdb4c3371e1d15feb5f60ad1c
Cameron Rohani <cameronrohani@users.noreply.github.com>
2024-10-29 23:22:04 -0700

    feat(api-client): add download button to scalar web (#3680)
    * feat(api-client): add download button to scalar web
    
    * docs(changeset): feat(api-client): add download button to scalar web

---
+++ .changeset/rude-icons-hope.md
@@ -0,0 +1,6 @@
+---
+'scalar-api-client': patch
+'@scalar/api-client': patch
+---
+
+feat(api-client): add download button to scalar web
--- packages/api-client/src/components/SideNav/SideNav.vue
+++ packages/api-client/src/components/SideNav/SideNav.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ROUTES } from '@/constants'
+import { ScalarIcon } from '@scalar/components'
 import { useRouter } from 'vue-router'
 
 import SideHelp from './SideHelp.vue'
@@ -10,7 +11,7 @@ const { currentRoute } = useRouter()
 <template>
   <nav
     aria-label="App Navigation"
-    class="text-c-2 sm:w-13 flex sm:flex-col justify-center items-center px-2 py-2 scalar-sidenav relative drag-region bg-b-1"
+    class="text-c-2 sm:w-13 flex sm:flex-col justify-center items-center px-2 py-2 scalar-sidenav relative drag-region bg-b-1 group-sidenav"
     role="navigation">
     <ul class="flex sm:flex-col gap-1.5">
       <li
@@ -41,6 +42,16 @@ const { currentRoute } = useRouter()
       <li class="flex items-center no-drag-region">
         <SideHelp />
       </li>
+      <li class="hide-in-scalar-app">
+        <a
+          class="min-w-[37px] max-w-[37px] hover:bg-b-2 flex items-center justify-center rounded-lg p-[8px] text-c-3 focus:text-c-1 scalar-app-nav-padding group-hover/sidenav:border-1/2 group-sidenav-hover cursor-pointer"
+          href="https://scalar.com/download"
+          target="_blank">
+          <ScalarIcon
+            icon="Download"
+            thickness="1.5" />
+        </a>
+      </li>
     </ul>
   </nav>
 </template>
@@ -53,5 +64,19 @@ const { currentRoute } = useRouter()
 /** Clickable items must not be draggable, though */
 .no-drag-region {
   -webkit-app-region: no-drag;
+}
+
+.group-sidenav:hover .group-sidenav-hover {
+  box-shadow: 0 0 0 0.5px var(--scalar-border-color);
+  background: linear-gradient(rgba(255, 255, 255, 0.75), rgba(0, 0, 0, 0.035));
+}
+.dark-mode .group-sidenav:hover .group-sidenav-hover {
+  background: linear-gradient(rgba(255, 255, 255, 0.035), rgba(0, 0, 0, 0.15));
+}
+.group-sidenav .group-sidenav-hover:hover {
+  background: linear-gradient(rgba(0, 0, 0, 0.035), rgba(255, 255, 255, 0.75));
+}
+.dark-mode .group-sidenav .group-sidenav-hover:hover {
+  background: linear-gradient(rgba(0, 0, 0, 0.15), rgba(255, 255, 255, 0.035));
 }
 </style>
