3a0c367af4d9b76fa708e92f8922bc43d311d00f
_sss <inyourtimeguy@gmail.com>
2024-11-01 18:05:33 +0700

    fix(oas-utils): improve handling of non-string enums in parameter schema (#3718)
    * fix: improve handling of non-string enums in parameter schema
    
    * docs(changeset): fix: improve handling of non-string enums in parameter schema

---
+++ .changeset/old-yaks-exercise.md
@@ -0,0 +1,5 @@
+---
+'@scalar/oas-utils': patch
+---
+
+fix: improve handling of non-string enums in parameter schema
--- packages/oas-utils/src/entities/spec/request-example.test.ts
+++ packages/oas-utils/src/entities/spec/request-example.test.ts
@@ -3,32 +3,32 @@ import { describe, expect, it } from 'vitest'
 import { createParamInstance } from './request-examples'
 
 describe('createParamInstance', () => {
-  it('works with schema type number', () => {
+  it('works with schema enum type number', () => {
     const result = createParamInstance({
       in: 'path',
       name: 'foo',
       required: true,
       deprecated: false,
       schema: {
-        default: 1,
+        default: 1.2,
         type: 'number',
-        enum: [1, 2, 3],
+        enum: [1.2, 2.1, 3],
       },
     })
 
     expect(result).toEqual({
       key: 'foo',
-      value: '1',
+      value: '1.2',
       enabled: true,
       description: undefined,
       required: true,
-      enum: ['1', '2', '3'],
+      enum: ['1.2', '2.1', '3'],
       type: 'number',
-      default: 1,
+      default: 1.2,
     })
   })
 
-  it('works with schema type string', () => {
+  it('works with schema enum type string', () => {
     const result = createParamInstance({
       in: 'path',
       name: 'foo',
@@ -50,6 +50,56 @@ describe('createParamInstance', () => {
       enum: ['foo', 'bar'],
       type: 'string',
       default: 'foo',
+    })
+  })
+
+  it('works with schema enum type integer', () => {
+    const result = createParamInstance({
+      in: 'query',
+      name: 'foo',
+      required: true,
+      deprecated: false,
+      schema: {
+        default: 1,
+        type: 'integer',
+        enum: [1, 2, 3],
+      },
+    })
+
+    expect(result).toEqual({
+      key: 'foo',
+      value: '1',
+      enabled: true,
+      description: undefined,
+      required: true,
+      enum: ['1', '2', '3'],
+      type: 'integer',
+      default: 1,
+    })
+  })
+
+  it('works with schema enum type boolean', () => {
+    const result = createParamInstance({
+      in: 'query',
+      name: 'foo',
+      required: true,
+      deprecated: false,
+      schema: {
+        default: false,
+        type: 'boolean',
+        enum: [true, false],
+      },
+    })
+
+    expect(result).toEqual({
+      key: 'foo',
+      value: 'false',
+      enabled: true,
+      description: undefined,
+      required: true,
+      enum: ['true', 'false'],
+      type: 'boolean',
+      default: false,
     })
   })
 })
