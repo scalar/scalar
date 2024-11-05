dd1499a91a158f3c87c9d96a21d8f857681d2902
Hans Pagel <mail@hanspagel.com>
2024-11-05 17:10:25 +0100

    fix: another test

--- packages/galaxy/tests/yaml.test.ts
+++ packages/galaxy/tests/yaml.test.ts
@@ -4,6 +4,6 @@ import galaxy from '../src/specifications/3.1.yaml?raw'
 
 describe('yaml', () => {
   it('has OpenAPI version', () => {
-    expect(galaxy).toContain('openapi: 3.1.0')
+    expect(galaxy).toContain('openapi: 3.1.1')
   })
 })
