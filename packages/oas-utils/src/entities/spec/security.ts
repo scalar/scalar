5d12c3511cf7fa301547d4c21642a924de88d734
Amrit <amrit@scalar.com>
2024-11-08 15:22:48 -0800

    feat: base changes

--- packages/oas-utils/src/entities/spec/security.ts
+++ packages/oas-utils/src/entities/spec/security.ts
@@ -1,121 +1,6 @@
-import { schemaModel } from '@/helpers/schema-model'
 import { z } from 'zod'
 
 import { nanoidSchema } from '../shared'
-
-// ---------------------------------------------------------------------------
-// Collection values for Security Schemes
-
-const apiKeyExampleSchema = z.object({
-  type: z.literal('apiKey').default('apiKey'),
-  name: z.string().default(''),
-  value: z.string().default(''),
-})
-
-const httpExampleSchema = z.object({
-  type: z.literal('http').default('http'),
-  username: z.string().default(''),
-  password: z.string().default(''),
-  token: z.string().default(''),
-})
-
-const oauthImplicitExampleSchema = z.object({
-  type: z.literal('oauth-implicit').default('oauth-implicit'),
-  token: z.string().default(''),
-})
-
-const oauthPasswordExampleSchema = z.object({
-  type: z.literal('oauth-password').default('oauth-password'),
-  token: z.string().default(''),
-  username: z.string().default(''),
-  password: z.string().default(''),
-  clientSecret: z.string().default(''),
-})
-
-const oauthClientCredentialsExampleSchema = z.object({
-  type: z.literal('oauth-clientCredentials').default('oauth-clientCredentials'),
-  token: z.string().default(''),
-  clientSecret: z.string().default(''),
-})
-
-const oauthAuthorizationCodeExampleSchema = z.object({
-  type: z.literal('oauth-authorizationCode').default('oauth-authorizationCode'),
-  token: z.string().default(''),
-  clientSecret: z.string().default(''),
-})
-
-/**
- * Value schema for each type of OAuth Flow
- * Request Examples can store populated values for a given security scheme
- */
-export const securitySchemeExampleValueSchema = z.union([
-  apiKeyExampleSchema,
-  httpExampleSchema,
-  oauthImplicitExampleSchema,
-  oauthPasswordExampleSchema,
-  oauthClientCredentialsExampleSchema,
-  oauthAuthorizationCodeExampleSchema,
-])
-
-export type SecuritySchemeExampleValue = z.infer<
-  typeof securitySchemeExampleValueSchema
->
-
-/** The example values for the oauth2 schemes */
-export type SecuritySchemeOauth2ExampleValue = Extract<
-  SecuritySchemeExampleValue,
-  { type: `oauth-${string}` }
->
-
-/**
- * Generates a base set of example data for a given securityScheme
- *
- * TODO: we can probably remove this unless we really want to keep it separate
- */
-export function authExampleFromSchema(
-  scheme: SecurityScheme,
-  baseValues: any = {},
-): SecuritySchemeExampleValue | null {
-  try {
-    if (scheme.type === 'apiKey') {
-      return schemaModel(
-        { name: scheme.name, ...baseValues },
-        apiKeyExampleSchema,
-        false,
-      )
-    }
-    if (scheme.type === 'http') {
-      return schemaModel(baseValues, httpExampleSchema, false)
-    }
-    if (scheme.type === 'oauth2') {
-      if (scheme.flow.type === 'authorizationCode')
-        return schemaModel(
-          baseValues,
-          oauthAuthorizationCodeExampleSchema,
-          false,
-        )
-      if (scheme.flow.type === 'clientCredentials')
-        return schemaModel(
-          baseValues,
-          oauthClientCredentialsExampleSchema,
-          false,
-        )
-      if (scheme.flow.type === 'implicit')
-        return schemaModel(baseValues, oauthImplicitExampleSchema, false)
-      if (scheme.flow.type === 'password')
-        return schemaModel(baseValues, oauthPasswordExampleSchema, false)
-    }
-  } catch (e) {
-    console.error(e)
-  }
-  console.warn(
-    '[@scalar/oas-utils:security] Invalid schema for oauth example',
-    scheme,
-    baseValues,
-  )
-
-  return null
-}
 
 // ---------------------------------------------------------------------------
 // COMMON PROPS FOR ALL SECURITY SCHEMES
@@ -145,9 +30,13 @@ const oasSecuritySchemeApiKey = commonProps.extend({
   in: z.enum(securitySchemeApiKeyIn).optional().default('header'),
 })
 
-export const securityApiKeySchema = oasSecuritySchemeApiKey.merge(
-  extendedSecuritySchema,
-)
+const apiKeyValueSchema = z.object({
+  value: z.string().default(''),
+})
+
+export const securityApiKeySchema = oasSecuritySchemeApiKey
+  .merge(extendedSecuritySchema)
+  .merge(apiKeyValueSchema)
 export type SecuritySchemeApiKey = z.infer<typeof securityApiKeySchema>
 
 // ---------------------------------------------------------------------------
@@ -176,9 +65,15 @@ const oasSecuritySchemeHttp = commonProps.extend({
     .default('JWT'),
 })
 
-export const securityHttpSchema = oasSecuritySchemeHttp.merge(
-  extendedSecuritySchema,
-)
+const httpValueSchema = z.object({
+  username: z.string().default(''),
+  password: z.string().default(''),
+  token: z.string().default(''),
+})
+
+export const securityHttpSchema = oasSecuritySchemeHttp
+  .merge(extendedSecuritySchema)
+  .merge(httpValueSchema)
 export type SecuritySchemaHttp = z.infer<typeof securityHttpSchema>
 
 // ---------------------------------------------------------------------------
@@ -217,12 +112,12 @@ const oauthCommon = z.object({
    * The URL to be used for obtaining refresh tokens. This MUST be in the form of a
    * URL. The OAuth2 standard requires the use of TLS.
    */
-  refreshUrl: z.string().optional().default(''),
+  'refreshUrl': z.string().optional().default(''),
   /**
    * REQUIRED. The available scopes for the OAuth2 security scheme. A map
    * between the scope name and a short description for it. The map MAY be empty.
    */
-  scopes: z
+  'scopes': z
     .union([
       z.map(z.string(), z.string().optional()),
       z.record(z.string(), z.string().optional()),
@@ -230,7 +125,11 @@ const oauthCommon = z.object({
     ])
     .optional()
     .default({}),
-  selectedScopes: z.array(z.string()).optional().default([]),
+  'selectedScopes': z.array(z.string()).optional().default([]),
+  /** Extension to save the client Id associated with an oauth flow */
+  'x-scalar-client-id': z.string().optional().default(''),
+  /** The auth token */
+  'token': z.string().default(''),
 })
 
 /** Setup a default redirect uri if we can */
@@ -257,11 +156,15 @@ export const oasOauthFlowSchema = z
     oauthCommon.extend({
       type: z.literal('password'),
       tokenUrl,
+      clientSecret: z.string().default(''),
+      username: z.string().default(''),
+      password: z.string().default(''),
     }),
     /** Configuration for the OAuth Client Credentials flow. Previously called application in OpenAPI 2.0. */
     oauthCommon.extend({
       type: z.literal('clientCredentials'),
       tokenUrl,
+      clientSecret: z.string().default(''),
     }),
     /** Configuration for the OAuth Authorization Code flow. Previously called accessCode in OpenAPI 2.0.*/
     oauthCommon.extend({
@@ -278,17 +181,19 @@ export const oasOauthFlowSchema = z
         .optional()
         .default(defaultRedirectUri),
       tokenUrl,
+      'clientSecret': z.string().default(''),
     }),
   ])
   .optional()
   .default({ type: 'implicit', authorizationUrl: 'http://localhost:8080' })
 
 const oasSecuritySchemeOauth2 = commonProps.extend({
-  'type': z.literal('oauth2'),
+  type: z.literal('oauth2'),
   /** REQUIRED. An object containing configuration information for the flow types supported. */
-  'flow': oasOauthFlowSchema,
-  /** Extension to save the client Id associated with an oauth flow */
-  'x-scalar-client-id': z.string().optional().default(''),
+  flows: z.record(
+    z.enum(['implicit', 'password', 'clientCredentials', 'authorizationCode']),
+    oasOauthFlowSchema,
+  ),
 })
 
 export const securityOauthSchema = oasSecuritySchemeOauth2.merge(
