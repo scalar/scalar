a2140a426928051df44db41abba42aa38b7bdeac
tmastrom <thomas.mastromonaco@gmail.com>
2024-11-12 15:33:00 +0200

    feat: cloudbuild codegen script using bun

---
+++ tooling/cloudbuild/index.ts
@@ -0,0 +1,96 @@
+import { stringify } from 'yaml'
+
+// TODO: types
+
+/**
+ * Generate the appropriate cloudbuild.yaml file
+ */
+export default async function generateCloudBuild(serviceMap: any) {
+  console.log('Generating Cloud Build')
+  const containerImage = {
+    name: 'gcr.io/cloud-builders/docker',
+    id: 'build-base',
+    args: [
+      'build',
+      '-t',
+      'gcr.io/${_PROJECT_ID}/${_BASE_BUILDER}',
+      '-f',
+      './tooling/base-docker-builder/Dockerfile',
+      '.',
+    ],
+  }
+  const pushContainerImage = {
+    name: 'gcr.io/cloud-builders/docker',
+    id: 'push-base',
+    waitFor: ['build-base'],
+    args: ['push', 'gcr.io/${_PROJECT_ID}/${_BASE_BUILDER}'],
+  }
+
+  const cloudbuildSteps = [containerImage, pushContainerImage]
+
+  serviceMap.forEach((service) => {
+    const buildStep = {
+      name: 'gcr.io/cloud-builders/docker',
+      id: `build-${service.name}`,
+      waitFor: ['push-base'],
+      args: [
+        'buildx',
+        'build',
+        '--build-arg',
+        'BASE_IMAGE=gcr.io/${_PROJECT_ID}/${_BASE_BUILDER}',
+        '-t',
+        'gcr.io/${_PROJECT_ID}/' + `${service.cloudBuildName}`,
+        '-f',
+        service.dockerPath,
+        '.',
+      ],
+    }
+    cloudbuildSteps.push(buildStep)
+
+    const pushStep = {
+      name: 'gcr.io/cloud-builders/docker',
+      id: `push-${service.name}`,
+      waitFor: [`build-${service.name}`],
+      args: ['push', 'gcr.io/${_PROJECT_ID}/' + `${service.cloudBuildName}`],
+    }
+    cloudbuildSteps.push(pushStep)
+
+    const deployStep = {
+      name: 'gcr.io/google.com/cloudsdktool/cloud-sdk',
+      entrypoint: 'gcloud',
+      waitFor: [`push-${service.name}`],
+      args: [
+        'run',
+        'deploy',
+        `${service.cloudBuildName}`,
+        '--image=gcr.io/${_PROJECT_ID}/' + `${service.cloudBuildName}`,
+        '--region=$_REGION',
+        '--platform=managed',
+        '--allow-unauthenticated',
+        '--execution-environment=gen2',
+        '--cpu=$_CPU',
+        '--memory=$_MEMORY',
+        '--service-account=$_SERVICE_ACCOUNT',
+      ],
+    }
+    cloudbuildSteps.push(deployStep)
+  })
+
+  const options = {
+    machineType: 'E2_HIGHCPU_8',
+    logging: 'CLOUD_LOGGING_ONLY',
+  }
+
+  const cloudbuild = {
+    steps: cloudbuildSteps,
+    options: options,
+  }
+
+  const containerYaml = stringify(cloudbuild)
+  console.log(containerYaml)
+
+  return containerYaml
+
+  // TODO: write to a file
+  // TODO: overwrite the old file
+}
