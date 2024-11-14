5273b497f709aa21c0be19d2239864ceb0d407ac
Amrit <amrit@scalar.com>
2024-11-14 15:49:31 -0800

    feat: working on migration

--- packages/oas-utils/src/migrations/v-2.2.0/migration.ts
+++ packages/oas-utils/src/migrations/v-2.2.0/migration.ts
@@ -6,9 +6,31 @@ import type { v_2_2_0 } from './types.generated'
 export const migrate_v_2_2_0 = (
   data: v_2_1_0.DataRecord,
 ): v_2_2_0.DataRecord => {
-  console.info('Performing data migration v-2.1.0 to v-2.2.0')
+  // console.info('Performing data migration v-2.1.0 to v-2.2.0')
 
-  console.log('TODO: this')
+  // const collections = data.collections
+  // const cookies = Object.values(data.cookies).reduce((prev, c) => {
+  //   prev[c.u] = c.
+  //   return prev
+  // }, {})
+  // const environments = data.environments
+  // const requestExamples = data.requestExamples
+  // const requests = data.requests
+  // const securitySchemes = data.securitySchemes
+  // const servers = data.servers
+  // const tags = data.tags
+  // const workspaces = data.workspaces
 
+  // return {
+  //   collections,
+  //   cookies,
+  //   environments,
+  //   requestExamples,
+  //   requests,
+  //   securitySchemes,
+  //   servers,
+  //   tags,
+  //   workspaces,
+  // } satisfies v_2_2_0.DataRecord
   return data as v_2_2_0.DataRecord
 }
