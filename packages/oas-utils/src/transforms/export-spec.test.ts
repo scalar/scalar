fea7f4248dabfc7d26ea787491ec9cce8b7c02f2
Geoff Scott <geoffgscott@gmail.com>
2024-11-15 16:19:45 +0100

    fix: Correct TS test config

--- packages/oas-utils/src/transforms/export-spec.test.ts
+++ packages/oas-utils/src/transforms/export-spec.test.ts
@@ -5,8 +5,7 @@ import type {
   Tag,
 } from '@/entities/spec'
 import { yaml } from '@/helpers'
-import { exportSpecFromWorkspace } from '@/transforms/export-spec'
-import { importSpecToWorkspace } from '@/transforms/import-spec'
+import { exportSpecFromWorkspace, importSpecToWorkspace } from '@/transforms'
 import { describe, expect, test } from 'vitest'
 
 /** Convert an array of objects into a map of objects by UID */
@@ -18,12 +17,12 @@ function arrayToUidMap<T extends { uid: string }>(array: T[]) {
 }
 
 const testSpec = {
-  'openapi': '3.1.0',
-  'info': {
+  openapi: '3.1.0',
+  info: {
     title: 'Swagger Petstore - OpenAPI 3.1',
     version: '1.0.0',
   },
-  'paths': {
+  paths: {
     '/pet': {
       put: {
         tags: ['pet'],
@@ -48,28 +47,25 @@ const testSpec = {
           },
         ],
       },
-    },
-  },
-  '/pet': {
-    delete: {
-      tags: ['pet'],
-      summary: 'Delete an existing pet',
-      description: 'Delete an existing pet by Id',
-      operationId: 'deletePet',
-      parameters: [
-        {
-          in: 'query',
-          name: 'id',
-          schema: {
-            type: 'string',
+      delete: {
+        tags: ['pet'],
+        summary: 'Delete an existing pet',
+        description: 'Delete an existing pet by Id',
+        operationId: 'deletePet',
+        parameters: [
+          {
+            in: 'query',
+            name: 'id',
+            schema: {
+              type: 'string',
+            },
           },
-        },
-      ],
+        ],
+      },
     },
   },
 }
 
-console.log(yaml.stringify(testSpec))
 describe('Converts a collection into a spec', () => {
   test('Handles basic spec', async () => {
     const workspace = await importSpecToWorkspace(testSpec)
@@ -83,6 +79,8 @@ describe('Converts a collection into a spec', () => {
       tags: arrayToUidMap<Tag>(workspace.tags),
     })
 
-    console.log(yaml.stringify(exported))
+    expect(exported.paths).toHaveProperty('/pet')
+    expect(exported.paths['/pet']).toHaveProperty('put')
+    expect(exported.paths['/pet']).toHaveProperty('delete')
   })
 })
