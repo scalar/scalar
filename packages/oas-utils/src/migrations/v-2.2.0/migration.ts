b5e7f89afeb2fdbf8f115fd50bb941236bde3ef7
Amrit <amrit@scalar.com>
2024-11-14 16:56:23 -0800

    feat: so close

--- packages/oas-utils/src/migrations/v-2.2.0/migration.ts
+++ packages/oas-utils/src/migrations/v-2.2.0/migration.ts
@@ -2,6 +2,34 @@ import type { v_2_1_0 } from '@/migrations/v-2.1.0/types.generated'
 
 import type { v_2_2_0 } from './types.generated'
 
+/** Migrate security scheme from v-2.1.0 to v-2.2.0 */
+const migrateSecurityScheme = (
+  scheme: v_2_1_0.SecurityScheme,
+  auth: v_2_1_0.Collection['auth'][string],
+): v_2_2_0.SecurityScheme | null => {
+  // API Key
+  if (scheme.type === 'apiKey' && auth.type === 'apiKey') {
+    return {
+      ...scheme,
+      value: auth.value,
+    } satisfies v_2_2_0.SecurityScheme
+  }
+
+  // HTTP
+  if (scheme.type === 'http' && auth.type === 'http') {
+    return {
+      ...scheme,
+      username: auth.username,
+      password: auth.password,
+      token: auth.token,
+    } satisfies v_2_2_0.SecurityScheme
+  }
+
+  // OAuth
+
+  return null
+}
+
 /** V-2.1.0 to V-2.2.0 migration */
 export const migrate_v_2_2_0 = (
   data: v_2_1_0.DataRecord,
@@ -14,15 +42,12 @@ export const migrate_v_2_2_0 = (
     const collection = Object.values(data.collections).find((c) =>
       c.securitySchemes.includes(s.uid),
     )
-    if (!collection) return prev
+    const auth = collection?.auth?.[s.uid]
+    if (!auth) return prev
 
-    console.log(collection)
-    // if (s.type === 'oauth') {
-    // } else {
-    //   prev[s.uid] = {
-    //     ...s,
-    //   } satisfies v_2_2_0.SecurityScheme
-    // }
+    const newScheme = migrateSecurityScheme(s, auth)
+    if (newScheme) prev[s.uid] = newScheme
+
     return prev
   }, {})
 
