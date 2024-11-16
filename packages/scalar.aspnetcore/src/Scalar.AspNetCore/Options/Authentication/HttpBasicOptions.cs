e0819a22ffa49b3774fcb68e4604cffe01b6e106
bsdayo <bsdayo@outlook.com>
2024-11-16 17:41:49 +0800

    feat(Scalar.AspNetCore): add missing options model definition for prefilling http basic/bearer tokens

---
+++ packages/scalar.aspnetcore/src/Scalar.AspNetCore/Options/Authentication/HttpBasicOptions.cs
@@ -0,0 +1,19 @@
+namespace Scalar.AspNetCore;
+
+/// <summary>
+/// Represents the options for HTTP basic authentication.
+/// </summary>
+public sealed class HttpBasicOptions
+{
+    /// <summary>
+    /// Gets or sets the username used for HTTP basic authentication.
+    /// </summary>
+    /// <value>The default value is <c>null</c>.</value>
+    public string? Username { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the password used for HTTP basic authentication.
+    /// </summary>
+    /// <value>The default value is <c>null</c>.</value>
+    public string? Password { get; set; }
+}
--- packages/scalar.aspnetcore/src/Scalar.AspNetCore/Options/Authentication/ScalarAuthenticationOptions.cs
+++ packages/scalar.aspnetcore/src/Scalar.AspNetCore/Options/Authentication/ScalarAuthenticationOptions.cs
@@ -24,4 +24,11 @@ public sealed class ScalarAuthenticationOptions
     /// </summary>
     /// <value>The default value is <c>null</c>.</value>
     public OAuth2Options? OAuth2 { get; set; }
+
+    /// <summary>
+    /// Gets or sets the HTTP options.
+    /// This can be used if the OpenApi document has a HTTP security scheme.
+    /// </summary>
+    /// <value>The default value is <c>null</c>.</value>
+    public HttpOptions? Http { get; set; }
 }
