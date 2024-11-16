11de176cc27546bca4e230855ba2472c92fa0355
anthony lio <anthony@scalar.com>
2024-11-16 10:53:57 +0200

    style(api-client): updates sidebar icon

--- packages/api-client/src/components/CommandPalette/CommandPaletteCollection.vue
+++ packages/api-client/src/components/CommandPalette/CommandPaletteCollection.vue
@@ -56,7 +56,7 @@ const handleSubmit = () => {
           class="aspect-square px-0 h-auto"
           variant="outlined">
           <LibraryIcon
-            class="size-4 text-c-2 stroke-2"
+            class="size-4 text-c-2 stroke-[1.75]"
             :src="collectionIcon" />
         </ScalarButton>
       </IconSelector>
