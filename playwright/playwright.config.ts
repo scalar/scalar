bdc40cf59ad08775a4a15d1c352d7bda06033ef0
tmastrom <thomas.mastromonaco@gmail.com>
2024-11-19 18:34:19 +0200

    chore: only comment on pull request

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -55,10 +55,10 @@ export default defineConfig({
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
     },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
 
     /* Test against branded browsers. */
     // {
