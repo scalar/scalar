f07d5fce181f993c5662123a941a4b778442cab4
Amrit <amrit@scalar.com>
2024-11-20 15:44:22 -0800

    feat: some more work, need router

--- packages/api-client/src/store/index.ts
+++ packages/api-client/src/store/index.ts
@@ -1,1 +1,3 @@
 export * from './store'
+export { ACTIVE_ENTITIES_SYMBOL, useActiveEntities } from './active-entities'
+export { defaultRouterParamsFactory } from './router-params'
--- packages/api-client/src/store/router-params.ts
+++ packages/api-client/src/store/router-params.ts
@@ -2,22 +2,22 @@ import { PathId } from '@/router'
 import type { Router } from 'vue-router'
 
 export type RouterPathParams = Record<PathId, string>
+export const defaultRouterParamsFactory = () => ({
+  [PathId.Collection]: 'default',
+  [PathId.Environment]: 'default',
+  [PathId.Request]: 'default',
+  [PathId.Examples]: 'default',
+  [PathId.Schema]: 'default',
+  [PathId.Cookies]: 'default',
+  [PathId.Servers]: 'default',
+  [PathId.Workspace]: 'default',
+  [PathId.Settings]: 'default',
+})
 
 /** Getter function for router parameters */
 export function getRouterParams(router: Router) {
   return () => {
-    const pathParams = {
-      [PathId.Collection]: 'default',
-      [PathId.Environment]: 'default',
-      [PathId.Request]: 'default',
-      [PathId.Examples]: 'default',
-      [PathId.Schema]: 'default',
-      [PathId.Cookies]: 'default',
-      [PathId.Servers]: 'default',
-      [PathId.Workspace]: 'default',
-      [PathId.Settings]: 'default',
-    }
-
+    const pathParams = defaultRouterParamsFactory()
     const currentRoute = router.currentRoute.value
 
     if (currentRoute) {
