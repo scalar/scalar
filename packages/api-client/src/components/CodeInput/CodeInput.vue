ad455fbb80799f1ec6b3c121d09c4ace17489dad
Amrit <amrit@scalar.com>
2024-11-20 15:44:22 -0800

    feat: some more work, need router

--- packages/api-client/src/components/CodeInput/CodeInput.vue
+++ packages/api-client/src/components/CodeInput/CodeInput.vue
@@ -125,9 +125,9 @@ const extensions: Extension[] = []
 if (props.colorPicker) extensions.push(colorPickerExtension)
 extensions.push(
   pillPlugin({
-    activeEnvironment,
-    activeEnvVariables,
-    activeWorkspace,
+    environment: activeEnvironment.value,
+    envVariables: activeEnvVariables.value,
+    workspace: activeWorkspace.value,
     isReadOnly,
   }),
   backspaceCommand,
@@ -267,9 +267,9 @@ export default {
   <EnvironmentVariableDropdown
     v-if="showDropdown && props.withVariables && !isReadOnly"
     ref="dropdownRef"
-    :activeEnvVariables="computed(() => activeEnvVariables)"
-    :activeEnvironment="computed(() => activeEnvironment)"
     :dropdownPosition="dropdownPosition"
+    :envVariables="activeEnvVariables"
+    :environment="activeEnvironment"
     :query="dropdownQuery"
     :router="router"
     @select="handleDropdownSelect" />
