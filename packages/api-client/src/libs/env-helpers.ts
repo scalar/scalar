bf6781d3ded68092dc3bf614d975af6476db75b0
Amrit <amrit@scalar.com>
2024-11-20 12:27:22 -0800

    feat: a little bit at a time

---
+++ packages/api-client/src/libs/env-helpers.ts
@@ -0,0 +1,7 @@
+import type { Environment } from '@scalar/oas-utils/entities/environment'
+
+export type EnvVariables = { key: string; value: string }[]
+
+/** Gets the color of an environment with default fallback */
+export const getEnvColor = (environment: Environment) =>
+  environment ? environment.color : '#8E8E8E'
