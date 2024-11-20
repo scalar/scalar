28a6b055deb0d7a0394de1009ecdb102dd0280b0
Cameron Rohani <cameronrohani@users.noreply.github.com>
2024-11-19 18:16:03 -0800

    refactor(components): scalar menu apply same styles as sidebar (#3947)
    * style(components): scalar menu apply same styles as sidebar
    
    * docs(changeset): style(components): scalar menu flex col for products

---
+++ .changeset/late-bikes-provide.md
@@ -0,0 +1,5 @@
+---
+'@scalar/components': patch
+---
+
+style(components): scalar menu flex col for products
--- packages/components/src/components/ScalarMenu/ScalarMenuSection.vue
+++ packages/components/src/components/ScalarMenu/ScalarMenuSection.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 <template>
   <div class="flex flex-col gap-1.5">
-    <div class="px-2.5 leading text-c-1">
+    <div class="px-2.5 font-medium leading text-c-3">
       <slot name="title" />
     </div>
     <div class="flex flex-col">
