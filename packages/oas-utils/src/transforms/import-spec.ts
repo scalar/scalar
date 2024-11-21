118c152c0d1c15ee5990574bda6485722989c70e
Hans Pagel <mail@hanspagel.com>
2024-11-21 11:49:57 +0100

    feat: return code and message to make it look like the other errors

--- packages/oas-utils/src/transforms/import-spec.ts
+++ packages/oas-utils/src/transforms/import-spec.ts
@@ -34,7 +34,15 @@ export const parseSchema = async (spec: string | UnknownObject) => {
   // @see packages/api-reference/src/helpers/parse.ts
 
   const { filesystem, errors: loadErrors = [] } = await load(spec).catch(
-    (e) => ({ errors: [e.message], filesystem: [] }),
+    (e) => ({
+      errors: [
+        {
+          code: e.code,
+          message: e.message,
+        },
+      ],
+      filesystem: [],
+    }),
   )
 
   const { specification } = upgrade(filesystem)
