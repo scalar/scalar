5da67b24bce1fdb288b903a709e2843404b28ce2
Anthony Lio <anthony@scalar.com>
2024-11-21 13:08:50 +0200

    style(api-client): mobile layout fixtures (#3951)
    * style(api-client): maintains view layout caret alignment on mobile
    
    * style(api-client): sets view layout section flex on mobile
    
    * style(api-client): fixes alignment of request section input responsiveness
    
    * docs(changeset): style: enhances responsive alignment layout

---
+++ .changeset/big-rats-own.md
@@ -0,0 +1,5 @@
+---
+'@scalar/api-client': patch
+---
+
+style: enhances responsive alignment layout
--- packages/api-client/src/components/ViewLayout/ViewLayoutSection.vue
+++ packages/api-client/src/components/ViewLayout/ViewLayoutSection.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="flex xl:h-full xl:min-w-0 xl:flex-1 flex-col xl:custom-scroll bg-b-1">
+    class="flex xl:h-full xl:min-w-0 flex-1 flex-col xl:custom-scroll bg-b-1">
     <div
       class="min-h-12 xl:min-h-header flex items-center border-b-1/2 px-3.5 py-1.5 md:px-4 md:py-2 xl:px-6 xl:pr-5 text-sm font-medium sticky top-0 bg-b-1 xl:rounded-none z-1">
       <slot name="title" />
