982033840e6975d374218d46b307f504304a25fd
Amrit <amrit@scalar.com>
2024-11-21 13:00:31 -0800

    feat: added har request conversion

--- packages/api-reference/src/helpers/getExampleCode.ts
+++ packages/api-reference/src/helpers/getExampleCode.ts
@@ -1,3 +1,4 @@
+import { convertRequestToHarRequest } from '@/helpers/convertRequestToHarRequest'
 import {
   type ClientId as SnippetzClientId,
   type TargetId as SnippetzTargetId,
@@ -5,7 +6,6 @@ import {
 } from '@scalar/snippetz'
 import {
   HTTPSnippet,
-  type HarRequest,
   type ClientId as HttpSnippetLiteClientId,
   type TargetId as HttpSnippetLiteTargetId,
 } from 'httpsnippet-lite'
@@ -14,21 +14,35 @@ export type TargetId = HttpSnippetLiteTargetId | SnippetzTargetId
 export type ClientId = HttpSnippetLiteClientId | SnippetzClientId
 
 /**
- * Returns a code example for given HAR request
+ * Returns a code example for given Request
  */
 export async function getExampleCode(
   request: Request,
   target: TargetId,
   client: ClientId,
 ) {
-  // @scalar/snippetz
-  const snippetzTargetKey = target
+  // Convert request to HarRequest
+  const harRequest = await convertRequestToHarRequest(request)
 
-  if (snippetz().hasPlugin(snippetzTargetKey, client)) {
+  if (snippetz().hasPlugin(target, client)) {
+    // Transform harRequest to match snippetz's expected format
+    const snippetzRequest = {
+      ...harRequest,
+      postData: harRequest.postData
+        ? {
+            text: harRequest.postData.text || '',
+            mimeType: harRequest.postData.mimeType,
+            ...(harRequest.postData.comment && {
+              comment: harRequest.postData.comment,
+            }),
+          }
+        : undefined,
+    }
+
     return snippetz().print(
       target as SnippetzTargetId,
       client as SnippetzClientId,
-      request,
+      snippetzRequest,
     )
   }
 
@@ -47,7 +61,7 @@ export async function getExampleCode(
 
     // HTTPSnippet will return type string[] if passed input type HarEntry
     // Since we are passing type HarRequest output is a string
-    const code = await new HTTPSnippet(request).convert(
+    const code = await new HTTPSnippet(harRequest).convert(
       httpSnippetLiteTargetKey,
       httpSnippetLiteClientKey,
     )
