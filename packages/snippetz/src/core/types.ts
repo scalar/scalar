d46b290ca007c9ffd99f278547c3c592da67a3d3
Hans Pagel <mail@hanspagel.com>
2024-11-22 15:30:26 +0100

    feat: dynamically extend the targetid and clientid types

--- packages/snippetz/src/core/types.ts
+++ packages/snippetz/src/core/types.ts
@@ -1,18 +1,34 @@
 export type { Request } from 'har-format'
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface Clients {}
+}
+
+export type AddClient<T extends string, C extends string> = {
+  [K in `${T}/${C}`]: C
+}
+
+export type TargetId = keyof Clients & string extends `${infer T}/${string}`
+  ? T
+  : never
+
+export type ClientId<T extends TargetId> = Clients[keyof Clients &
+  `${T}/${string}`]
+
+/** What any plugins needs to return */
 export type Source = {
   /** The language or environment. */
   target: TargetId
   /** The identifier of the client. */
-  client: ClientId
+  client: ClientId<TargetId>
   /** The actual source code. */
   code: string
 }
 
-export type TargetId = 'node' | 'js' | 'shell'
-
-export type ClientId = 'undici' | 'fetch' | 'ofetch' | 'curl'
-
+/**
+ * Optional configuration for any plugin
+ */
 export type PluginConfiguration = {
   /** Credentials to add HTTP Basic Authentication */
   auth?: { username: string; password: string }
