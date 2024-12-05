03a54a977842096a4f54cdc01c298ac9ba4d5518
Brynn Hawker <brynn@hawker.me>
2024-12-05 16:41:49 +0100

    fix(api-client): remove app header height

--- packages/api-client/src/components/ViewLayout/ViewLayoutSection.vue
+++ packages/api-client/src/components/ViewLayout/ViewLayoutSection.vue
@@ -2,7 +2,7 @@
   <section
     class="flex xl:h-full xl:min-w-0 flex-1 flex-col xl:custom-scroll bg-b-1">
     <div
-      class="min-h-12 xl:min-h-header flex items-center border-b-1/2 px-3.5 py-1.5 md:px-4 md:py-2 xl:px-6 xl:pr-5 text-sm font-medium sticky top-0 bg-b-1 xl:rounded-none z-1">
+      class="min-h-12 xl:min-h-client-header flex items-center border-b-1/2 px-3.5 py-1.5 md:px-4 md:py-2 xl:px-6 xl:pr-5 text-sm font-medium sticky top-0 bg-b-1 xl:rounded-none z-1">
       <slot name="title" />
     </div>
     <slot />
