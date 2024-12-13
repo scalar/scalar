6b8ab657dacba703c884232a21c3082f00373dac
Hans Pagel <mail@hanspagel.com>
2024-12-13 21:30:32 +0100

    chore: update comment

--- packages/types/src/utils/utility-types.ts
+++ packages/types/src/utils/utility-types.ts
@@ -1,4 +1,5 @@
 /**
- * When the object values are unknown
+ * Represents an object with string keys and unknown values.
+ * Useful for handling objects with dynamic or unknown value types.
  */
 export type UnknownObject = Record<string, unknown>
