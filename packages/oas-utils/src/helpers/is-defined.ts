1baa529e4d77ecda4b1f82ccfd7e0b88682bf19d
Amrit <amrit@scalar.com>
2024-12-16 14:17:50 -0800

    feat: add isDefined helper

---
+++ packages/oas-utils/src/helpers/is-defined.ts
@@ -0,0 +1,14 @@
+/**
+ * Type safe alternative to array.filter(Boolean)
+ *
+ * @example
+ *
+ * ```ts
+ * const dataArray = [1, null, 2, undefined, 3].filter(isDefined)
+ * ```
+ *
+ * @see https://jaketrent.com/post/typescript-type-safe-filter-boolean/
+ */
+export const isDefined = <T>(
+  value: T | null | undefined,
+): value is NonNullable<T> => value !== null && value !== undefined
