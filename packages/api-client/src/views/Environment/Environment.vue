3f5fc9e96f91b7394515e489fca44de26406d49d
anthony lio <anthony@scalar.com>
2024-12-16 18:59:21 +0200

    fix(api-client): favors active workspace id in environment path

--- packages/api-client/src/views/Environment/Environment.vue
+++ packages/api-client/src/views/Environment/Environment.vue
@@ -291,8 +291,8 @@ const handleNavigation = (
   collectionId?: string,
 ) => {
   const path = collectionId
-    ? `/workspace/default/environment/${collectionId}/${uid}`
-    : `/workspace/default/environment/${uid}`
+    ? `/workspace/${activeWorkspace.value.uid}/environment/${collectionId}/${uid}`
+    : `/workspace/${activeWorkspace.value.uid}/environment/${uid}`
   if (event.metaKey) {
     window.open(path, '_blank')
   } else {
