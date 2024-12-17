f0b76abb8f185767cb5bebcb58510fc35c1dfa3b
Brynn Hawker <brynn@hawker.me>
2024-12-16 18:17:12 -0800

    fix(components): don't add background color to products in dark mode

--- packages/components/src/components/ScalarMenu/ScalarMenuProduct.vue
+++ packages/components/src/components/ScalarMenu/ScalarMenuProduct.vue
@@ -15,8 +15,8 @@ const variants = cva({
   base: 'relative flex min-w-0 flex-1 items-center gap-2 overflow-hidden rounded px-2 py-1.5 leading no-underline flex-row',
   variants: {
     selected: {
-      true: 'pointer-events-none bg-b-2',
-      false: 'cursor-pointer bg-b-1 hover:bg-b-2',
+      true: 'pointer-events-none bg-b-2 dark:bg-b-3',
+      false: 'cursor-pointer hover:bg-b-2 dark:hover:bg-b-3',
     },
   },
 })
