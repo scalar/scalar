9961c3619dab7961d9cf49e310437b7e4886759f
Hans Pagel <mail@hanspagel.com>
2024-12-18 10:59:33 +0100

    feat: load OpenAPI document, mount Vue component

--- packages/api-reference/playground/blocks/vite.config.ts
+++ packages/api-reference/playground/blocks/vite.config.ts
@@ -1,3 +1,12 @@
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig({})
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('../../src', import.meta.url)),
+    },
+  },
+})
