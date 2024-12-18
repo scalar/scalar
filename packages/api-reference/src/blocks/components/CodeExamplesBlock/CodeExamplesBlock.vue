e0bd8d933af34d9ca4bb9a70c99018cc8a54fd47
Hans Pagel <mail@hanspagel.com>
2024-12-18 13:13:59 +0100

    feat: add more stuff to the code examples block

--- packages/api-reference/src/blocks/components/CodeExamplesBlock/CodeExamplesBlock.vue
+++ packages/api-reference/src/blocks/components/CodeExamplesBlock/CodeExamplesBlock.vue
@@ -6,17 +6,40 @@
  */
 import { useBlockProps } from '@/blocks/hooks/useBlockProps'
 import type { BlockProps } from '@/blocks/types'
+import OperationPath from '@/components/OperationPath.vue'
+import { ExampleRequest } from '@/features/ExampleRequest'
+import { TestRequestButton } from '@/features/TestRequestButton'
 
 const props = defineProps<BlockProps>()
 
 const { operation } = useBlockProps(props)
+
+const request = new Request(`https://api.example.com${operation.value?.path}`, {
+  method: operation.value?.method?.toUpperCase() ?? 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+})
 </script>
 
 <template>
   <div v-if="operation">
-    <h2 class="font-bold">Code Examples</h2>
-    {{ operation.method }}
-    {{ operation.path }}
+    <ExampleRequest
+      fallback
+      :operation="operation"
+      :request="request"
+      :secretCredentials="[]">
+      <template #header>
+        <OperationPath
+          class="example-path"
+          :deprecated="operation.deprecated"
+          :path="operation.path" />
+      </template>
+      <template #footer>
+        <TestRequestButton :operation="operation" />
+      </template>
+    </ExampleRequest>
   </div>
   <div v-else>
     <p>No operation found.</p>
