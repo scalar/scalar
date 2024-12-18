6950765edb72c780054146679ce5f8cc89feff03
Hans Pagel <mail@hanspagel.com>
2024-12-18 09:43:24 +0100

    feat: add a blocks playground

---
+++ packages/api-reference/playground/blocks/src/main.ts
@@ -0,0 +1,71 @@
+// import { getLocation, createStore, createOperationBlock } from '@scalar/api-reference/blocks'
+import {
+  createOperationBlock,
+  createStore,
+  getLocation,
+} from '../../../src/blocks'
+
+const { store } = createStore({
+  url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
+})
+
+// TODO: Support for multiple API definitions
+// const store = createStore([
+//     {
+//         name: 'scalar-galaxy',
+//         url: 'http://jsdevli.rcom/',
+//         location: getLocation('GET', '/planets/1')
+//     }
+// ])
+
+const operationBlock = createOperationBlock({
+  element: '#scalar-api-reference',
+  store,
+  location: getLocation('GET', '/planets/1'),
+})
+
+operationBlock.mount('#scalar-api-reference')
+
+// // OperationBlock.vue
+// // Doesn’t have markup
+
+// defineProps<{
+//     store: StoreContext
+//     location: string,
+// }>()
+
+// // * create examples for
+
+// <Section>
+//     <OperationDetails>
+//     <CodeExamples>
+//     <ExampleResponses>
+// </Section>
+
+// // OperationDetails.vue
+
+// defineProps<{
+//     operation: OperationEntity
+// }>()
+
+// <h1>{{ operation.summary }}</h1>
+
+// // Code examples
+
+// defineProps({
+//     store: StoreContext
+//     location: string,
+// })
+
+// // CodeExamplesServerSelect
+
+// defineProps({
+//     activeServer,
+//     servers,
+// })
+
+// defineEmits({
+//     updateSelectedServer: uid => void,
+// })
+
+// // Example responses
