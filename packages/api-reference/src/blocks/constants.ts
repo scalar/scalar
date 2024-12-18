5e73b02217439c0e1ffecde15b347b3732170ffb
Hans Pagel <mail@hanspagel.com>
2024-12-18 10:59:33 +0100

    feat: load OpenAPI document, mount Vue component

---
+++ packages/api-reference/src/blocks/constants.ts
@@ -0,0 +1,11 @@
+export const ERRORS = {
+  NO_ELEMENT_PROVIDED:
+    'Scalar Blocks: No HTML element provided to mount operation block. Please provide an HTML element to mount the operation block into.',
+  NO_URL_PROVIDED:
+    'Scalar Blocks: No URL provided to import an OpenAPI document from. Please provide a URL to import an OpenAPI document from.',
+}
+
+export const WARNINGS = {
+  ELEMENT_NOT_FOUND:
+    'Scalar Blocks: HTML element not found. We’ll just create one and append it to the body.',
+}
