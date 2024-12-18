5c27822f27a1680eb2576588c6afac932739001b
Hans Pagel <mail@hanspagel.com>
2024-12-18 12:09:25 +0100

    feat: add basic components, data wrangling and import the css

--- packages/api-reference/playground/blocks/src/main.ts
+++ packages/api-reference/playground/blocks/src/main.ts
@@ -1,11 +1,11 @@
-// import { getLocation, createStore, createOperationBlock } from '@scalar/api-reference/blocks'
-import { toRaw } from 'vue'
-
+// TODO: import { getLocation, createStore, createOperationBlock } from '@scalar/api-reference/blocks'
 import {
   createOperationBlock,
   createStore,
   getLocation,
 } from '../../../src/blocks'
+// TODO: import '@scalar/blocks/style.css'
+import '../../../src/blocks/assets/style.css'
 
 const { store } = createStore({
   url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
@@ -16,58 +16,17 @@ const { store } = createStore({
 //     {
 //         name: 'scalar-galaxy',
 //         url: 'http://jsdevli.rcom/',
-//         location: getLocation('GET', '/planets/1')
 //     }
 // ])
 
 const operationBlock = createOperationBlock({
-  element: '#scalar-api-reference',
+  // If passed, the block will mount to the element during initialization.
+  // element: '#scalar-api-reference',
   store,
-  location: getLocation('GET', '/planets/1'),
+  location: getLocation('GET', '/planets/{planetId}'),
 })
 
-// operationBlock.mount('#scalar-api-reference')
-
-// // OperationBlock.vue
-// // Doesn’t have markup
-
-// defineProps<{
-//     store: StoreContext
-//     location: string,
-// }>()
-
-// // * create examples for
-
-// <Section>
-//     <OperationDetails>
-//     <CodeExamples>
-//     <ExampleResponses>
-// </Section>
-
-// // OperationDetails.vue
-
-// defineProps<{
-//     operation: OperationEntity
-// }>()
-
-// <h1>{{ operation.summary }}</h1>
-
-// // Code examples
-
-// defineProps({
-//     store: StoreContext
-//     location: string,
-// })
-
-// // CodeExamplesServerSelect
-
-// defineProps({
-//     activeServer,
-//     servers,
-// })
-
-// defineEmits({
-//     updateSelectedServer: uid => void,
-// })
-
-// // Example responses
+// Mount it after initialization with just a selector string …
+operationBlock.mount('#scalar-api-reference')
+// or a DOM element:
+// operationBlock.mount(document.getElementById('scalar-api-reference'))
--- packages/api-reference/src/blocks/utils/getLocation.ts
+++ packages/api-reference/src/blocks/utils/getLocation.ts
@@ -6,8 +6,10 @@
  *
  * '#/paths/get/~1planets~1{foo}'
  */
-export function getLocation(method: string, path: string): `#/${string}` {
+export function getLocation(method: string, path: string): `#/paths/${string}` {
+  // TODO: We could use proper helpers from the parser here.
+
   const encodedPath = path.replace(/\//g, '~1')
 
-  return `#/${method}/${encodedPath}`
+  return `#/paths/${method.toLowerCase()}/${encodedPath}`
 }
