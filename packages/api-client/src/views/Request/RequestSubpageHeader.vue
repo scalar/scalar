701f429754e0a4737cb82920f732d3c6a144a978
anthony lio <anthony@scalar.com>
2024-12-18 17:46:15 +0200

    style(api-client): updated sub page headers width utility

--- packages/api-client/src/views/Request/RequestSubpageHeader.vue
+++ packages/api-client/src/views/Request/RequestSubpageHeader.vue
@@ -28,7 +28,7 @@ const { currentRoute } = useRouter()
   <div
     class="lg:min-h-client-header flex items-center w-full justify-center p-2 pt-2 lg:pt-1 lg:p-1 flex-wrap t-app__top-container border-b-1/2">
     <div
-      class="flex flex-row items-center gap-1 lg:px-1 lg:mb-0 mb-2 lg:flex-1 w-6/12">
+      class="flex flex-row items-center gap-1 lg:px-1 lg:mb-0 mb-2 lg:flex-1 w-1/2">
       <SidebarToggle
         v-if="showSidebar"
         class="ml-1"
@@ -43,7 +43,7 @@ const { currentRoute } = useRouter()
     </div>
     <AddressBar @importCurl="$emit('importCurl', $event)" />
     <div
-      class="flex flex-row items-center gap-1 lg:px-2.5 lg:mb-0 mb-2 lg:flex-1 justify-end w-6/12">
+      class="flex flex-row items-center gap-1 lg:px-2.5 lg:mb-0 mb-2 lg:flex-1 justify-end w-1/2">
       <OpenApiClientButton
         v-if="isReadOnly && activeCollection?.documentUrl && !hideClientButton"
         buttonSource="modal"
