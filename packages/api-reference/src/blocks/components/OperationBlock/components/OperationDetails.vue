4ab0764c873a28e3505e757d4da1062a88a50905
Hans Pagel <mail@hanspagel.com>
2024-12-18 12:09:25 +0100

    feat: add basic components, data wrangling and import the css

---
+++ packages/api-reference/src/blocks/components/OperationBlock/components/OperationDetails.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import type { Request } from '@scalar/oas-utils/entities/spec'
+
+defineProps<{
+  operation: Request
+}>()
+</script>
+
+<template>
+  <h1 class="font-bold">{{ operation.method }} {{ operation.path }}</h1>
+  <p>
+    {{ operation.description }}
+  </p>
+</template>
---
+++ packages/api-reference/src/blocks/types.ts
@@ -0,0 +1,6 @@
+import type { StoreContext } from '@/blocks/lib/createStore'
+
+export type BlockProps = {
+  store: StoreContext
+  location: string
+}
