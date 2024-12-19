aeb5fbeb16005023b12f908190699073251e5b04
Hans Pagel <mail@hanspagel.com>
2024-12-19 15:17:28 +0100

    feat: createStore takes content as input

--- packages/api-reference/src/blocks/types.ts
+++
@@ -1,8 +0,0 @@
-import type { StoreContext } from '@/blocks/lib/createStore'
-
-export type BlockProps = {
-  store: StoreContext
-  location: string
-  // TODO: Allow to pick a collection
-  // TODO: Add collection prop
-}
