c73d9a5c74dfa88657e6b3f40a4c8d6283051510
Hans Pagel <mail@hanspagel.com>
2024-12-19 15:41:12 +0100

    feat: make themes work

--- packages/api-reference/src/blocks/components/ExampleResponsesBlock/ExampleResponsesBlock.vue
+++ packages/api-reference/src/blocks/components/ExampleResponsesBlock/ExampleResponsesBlock.vue
@@ -4,15 +4,17 @@
  * It should do the data wrangling and compose other components.
  * It should not have markup or CSS.
  */
+import ThemeStyles from '@/blocks/components/ThemeStyles.vue'
 import { type BlockProps, useBlockProps } from '@/blocks/hooks/useBlockProps'
 import { ExampleResponses } from '@/features/ExampleResponses'
 
 const props = defineProps<BlockProps>()
 
-const { operation } = useBlockProps(props)
+const { operation, theme } = useBlockProps(props)
 </script>
 
 <template>
+  <ThemeStyles :theme="theme" />
   <ExampleResponses
     v-if="operation"
     :responses="operation.responses" />
