14f8c23a660d6c754c967c24b9aff6787bb443b7
Hans Pagel <mail@hanspagel.com>
2024-12-19 17:19:12 +0100

    fix: broken data mapping in api-reference

--- packages/api-reference/src/features/Operation/components/OperationParameters.vue
+++ packages/api-reference/src/features/Operation/components/OperationParameters.vue
@@ -4,40 +4,40 @@ import type { Request as RequestEntity } from '@scalar/oas-utils/entities/spec'
 import ParameterList from './ParameterList.vue'
 import RequestBody from './RequestBody.vue'
 
-defineProps<{
-  parameters: RequestEntity['parameters']
-  requestBody: RequestEntity['requestBody']
+const props = defineProps<{
+  operation?: RequestEntity
 }>()
+
+const filterParameters = (where: 'path' | 'query' | 'header' | 'cookie') =>
+  props.operation?.parameters?.filter((parameter) => parameter.in === where) ??
+  []
 </script>
 <template>
-  <!-- Path parameters-->
-  <ParameterList
-    :parameters="parameters?.filter((parameter) => parameter.in === 'path')">
+  <!-- Path operation-->
+  <ParameterList :operation="filterParameters('path')">
     <template #title>Path Parameters</template>
   </ParameterList>
 
-  <!-- Query parameters -->
-  <ParameterList
-    :parameters="parameters?.filter((parameter) => parameter.in === 'query')">
+  <!-- Query operation -->
+  <ParameterList :operation="filterParameters('query')">
     <template #title>Query Parameters</template>
   </ParameterList>
 
   <!-- Headers -->
-  <ParameterList
-    :parameters="parameters?.filter((parameter) => parameter.in === 'header')">
+  <ParameterList :operation="filterParameters('header')">
     <template #title>Headers</template>
   </ParameterList>
 
   <!-- Form data -->
   <!-- TODO: How does formdata work in OpenAPI 3.1 again? -->
-  <!-- <ParameterList :parameters="parameterMap.formData">
+  <!-- <ParameterList :operation="parameterMap.formData">
     <template #title>Form Data</template>
   </ParameterList> -->
 
   <!-- Request body -->
   <RequestBody
-    v-if="requestBody"
-    :requestBody="requestBody">
+    v-if="operation?.requestBody"
+    :requestBody="operation.requestBody">
     <template #title>Body</template>
   </RequestBody>
 </template>
