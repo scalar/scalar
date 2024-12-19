17f38110d66f491d13aef90234fa8b9aad94edd7
Hans Pagel <mail@hanspagel.com>
2024-12-19 16:51:01 +0100

    feat: map store parameters to rendering

--- packages/api-reference/src/features/Operation/components/OperationParameters.vue
+++ packages/api-reference/src/features/Operation/components/OperationParameters.vue
@@ -1,53 +1,43 @@
 <script setup lang="ts">
-import { useParameters } from '@/hooks'
 import type { Request as RequestEntity } from '@scalar/oas-utils/entities/spec'
 
 import ParameterList from './ParameterList.vue'
 import RequestBody from './RequestBody.vue'
 
-const props = defineProps<{
-  operation: RequestEntity
+defineProps<{
+  parameters: RequestEntity['parameters']
+  requestBody: RequestEntity['requestBody']
 }>()
-
-const { parameterMap } = useParameters(props.operation)
 </script>
 <template>
   <!-- Path parameters-->
-  <ParameterList :parameters="parameterMap.path">
+  <ParameterList
+    :parameters="parameters?.filter((parameter) => parameter.in === 'path')">
     <template #title>Path Parameters</template>
   </ParameterList>
 
   <!-- Query parameters -->
-  <ParameterList :parameters="parameterMap.query">
+  <ParameterList
+    :parameters="parameters?.filter((parameter) => parameter.in === 'query')">
     <template #title>Query Parameters</template>
   </ParameterList>
 
   <!-- Headers -->
-  <ParameterList :parameters="parameterMap.header">
+  <ParameterList
+    :parameters="parameters?.filter((parameter) => parameter.in === 'header')">
     <template #title>Headers</template>
   </ParameterList>
 
-  <!-- Cookies -->
-  <ParameterList :parameters="parameterMap.cookie">
-    <template #title>Cookies</template>
-  </ParameterList>
-
-  <!-- Body parameters -->
-  <ParameterList
-    :parameters="parameterMap.body"
-    showChildren>
-    <template #title>Body Parameters</template>
-  </ParameterList>
-
   <!-- Form data -->
-  <ParameterList :parameters="parameterMap.formData">
+  <!-- TODO: How does formdata work in OpenAPI 3.1 again? -->
+  <!-- <ParameterList :parameters="parameterMap.formData">
     <template #title>Form Data</template>
-  </ParameterList>
+  </ParameterList> -->
 
   <!-- Request body -->
   <RequestBody
-    v-if="operation.information?.requestBody"
-    :requestBody="operation.information?.requestBody">
+    v-if="requestBody"
+    :requestBody="requestBody">
     <template #title>Body</template>
   </RequestBody>
 </template>
