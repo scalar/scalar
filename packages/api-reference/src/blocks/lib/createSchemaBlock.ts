74ddc2e82acd410f77ef0890d92b236aac61efc8
Hans Pagel <mail@hanspagel.com>
2024-12-20 22:46:00 +0100

    feat: schema block

---
+++ packages/api-reference/src/blocks/lib/createSchemaBlock.ts
@@ -0,0 +1,16 @@
+import { SchemaBlock } from '@/blocks/components/SchemaBlock'
+import { type CreateBlockOptions, createBlock } from '@/blocks/lib/createBlock'
+
+/**
+ * Creates a operation embed
+ *
+ * @example
+ * createSchemaBlock({
+ *   element: document.getElementById('scalar-api-reference'),
+ *   store: createStore({ url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json' }),
+ *   location: getLocation(['components', 'schemas', 'User'])
+ * })
+ */
+export function createSchemaBlock(options: CreateBlockOptions) {
+  return createBlock(SchemaBlock, options)
+}
