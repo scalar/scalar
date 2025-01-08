f1f5a7541e9b3c8d5e2e5dcfe6923ea3ee5fedb2
Amrit <amrit@scalar.com>
2025-01-07 17:34:47 -0800

    chore: revert commented code

--- packages/api-client/src/libs/parse-curl.ts
+++ packages/api-client/src/libs/parse-curl.ts
@@ -17,7 +17,7 @@ export function parseCurlCommand(curlCommand: string) {
     method?: RequestMethod
     headers?: Record<string, string>
     body?: string
-    queryParameters?: { key: string; value: string }[]
+    queryParameters?: Record<string, string>
     servers?: string[]
   } = { url: '' }
 
@@ -26,27 +26,26 @@ export function parseCurlCommand(curlCommand: string) {
 
   while (arg) {
     if (typeof arg === 'object' && 'op' in arg) {
-      // Does this do anything?
-      // if (arg.op === '&') {
-      //   // Extract query parameters
-      //   const nextArg = iterator.next().value
-      //   if (typeof nextArg === 'string') {
-      //     const queryParametersArray = parseQueryParameters(`?${nextArg}`)
-      //     const queryParameters = queryParametersArray.reduce(
-      //       (acc, { key, value }) => {
-      //         acc[key] = value
-      //         return acc
-      //       },
-      //       {} as Record<string, string>,
-      //     )
-      //     result.queryParameters = {
-      //       ...result.queryParameters,
-      //       ...queryParameters,
-      //     }
+      if (arg.op === '&') {
+        // Extract query parameters
+        const nextArg = iterator.next().value
+        if (typeof nextArg === 'string') {
+          const queryParametersArray = parseQueryParameters(`?${nextArg}`)
+          const queryParameters = queryParametersArray.reduce(
+            (acc, { key, value }) => {
+              acc[key] = value
+              return acc
+            },
+            {} as Record<string, string>,
+          )
+          result.queryParameters = {
+            ...result.queryParameters,
+            ...queryParameters,
+          }
 
-      //     console.log('queryParameters', queryParameters)
-      //   }
-      // }
+          console.log('queryParameters', queryParameters)
+        }
+      }
       arg = iterator.next().value
       continue
     }
