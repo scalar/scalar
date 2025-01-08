b7873f8d394e3d0569b11c08b0b1fd99b1d0c533
Hans Pagel <mail@hanspagel.com>
2025-01-08 12:21:12 +0100

    fix: operation doesn’t show up in the reference

--- packages/api-reference/src/blocks/utils/getLocation.ts
+++ packages/api-reference/src/blocks/utils/getLocation.ts
@@ -1,15 +1,27 @@
 import { ERRORS } from '@/blocks/constants'
 import { escapeJsonPointer } from '@scalar/openapi-parser'
+import type { OpenAPI } from '@scalar/openapi-types'
+
+/**
+ * OpenAPI paths that we support.
+ *
+ * @example
+ * ['paths', '/planets/{foo}', 'get']
+ * ['components', 'schemas', 'Planet]
+ */
+type ValidOpenApiPaths =
+  | ['paths', string, OpenAPI.HttpMethods | Uppercase<OpenAPI.HttpMethods>]
+  | ['components', 'schemas', string]
 
 /**
  * Encodes a location string with paths
  *
  * @example
- * getLocation(['paths', 'get', '/planets/{foo}'])
+ * getLocation(['paths', '/planets/{foo}', 'get'])
  *
  * '#/paths/~1planets~1{foo}/get'
  */
-export function getLocation(path: string[]) {
+export function getLocation(path: ValidOpenApiPaths) {
   const pointer = [
     '#',
     ...path
