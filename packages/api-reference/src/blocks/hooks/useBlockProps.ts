26b57964ac61b4f98605e983545f03be93036bb5
Hans Pagel <mail@hanspagel.com>
2025-01-15 13:26:59 +0100

    refactor: code style

--- packages/api-reference/src/blocks/hooks/useBlockProps.ts
+++ packages/api-reference/src/blocks/hooks/useBlockProps.ts
@@ -6,7 +6,7 @@ import type {
 import { unescapeJsonPointer } from '@scalar/openapi-parser'
 import { type ComputedRef, computed } from 'vue'
 
-// TODO: Move this to a shared location
+// TODO: Move this type to a shared location
 export type StoreContext = ReturnType<typeof createWorkspaceStore>
 
 export type BlockProps = {
@@ -34,32 +34,33 @@ export type BlockProps = {
 /**
  * Provides computed properties for the block, based on the standardized interface of the `createStore` function.
  */
-export function useBlockProps(props: BlockProps): {
+export function useBlockProps({ store, location }: BlockProps): {
   operation: ComputedRef<RequestEntity | undefined>
 } {
   // Just pick first collection for now
   const collection = computed(() => {
-    return Object.values(props.store?.collections ?? {})[0]
+    return Object.values(store?.collections ?? {})[0]
   }) as ComputedRef<Collection | undefined>
 
   // TODO: Use the collection name from the props
   //   const collection = computed(() => {
-  //     return Object.values(props.store.collections).find(
-  //       ({ name }) => name === (props.collection ?? 'default'),
+  //     return Object.values(store.collections).find(
+  //       ({ name }) => name === (collection ?? 'default'),
   //     )
   //   })
 
+  /** Resolve the operation (request) from the store */
   const operation = computed<RequestEntity | undefined>(() => {
-    if (!props.store?.collections || !props.store.requests) {
+    if (!store?.collections || !store.requests) {
       return undefined
     }
 
-    const collectionRequests = Object.values(props.store.requests).filter(
-      (request) => collection.value?.requests.includes(request.uid),
+    const collectionRequests = Object.values(store.requests).filter((request) =>
+      collection.value?.requests.includes(request.uid),
     )
 
     // Check whether we’re using the correct location
-    if (!props.location.startsWith('#/paths/')) {
+    if (!location.startsWith('#/paths/')) {
       throw new Error(
         'Invalid location, try using #/paths/$YOUR_ENDPOINT/$HTTP_METHOD',
       )
@@ -67,8 +68,8 @@ export function useBlockProps(props: BlockProps): {
 
     // Resolve the matching operation from the collection
     const result = collectionRequests.find((request) => {
-      const specifiedPath = unescapeJsonPointer(props.location.split('/')[2])
-      const specifiedMethod = props.location.split('/')[3].toLocaleLowerCase()
+      const specifiedPath = unescapeJsonPointer(location.split('/')[2])
+      const specifiedMethod = location.split('/')[3].toLocaleLowerCase()
 
       return (
         request.method === specifiedMethod && request.path === specifiedPath
