80c3463d0cbde128c920a9567621b9ccdad789af
Amrit <amrit@scalar.com>
2025-01-16 14:19:57 -0800

    fix: remove incorrect default security of optional

--- packages/api-reference/playground/esm/vite.config.ts
+++ packages/api-reference/playground/esm/vite.config.ts
@@ -1,6 +1,12 @@
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('../../src', import.meta.url)),
+    },
+  },
 })
