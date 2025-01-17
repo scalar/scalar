5546a3ebbe117e55574552f9ef2dfcb6425891fa
anthony lio <anthony@scalar.com>
2025-01-17 11:42:25 +0200

    fix(api-client): handles path variable on request path change

--- packages/api-client/src/views/Request/RequestSection/RequestPathParams.vue
+++ packages/api-client/src/views/Request/RequestSection/RequestPathParams.vue
@@ -120,6 +120,15 @@ watch(
     }
   },
 )
+
+watch(
+  () => activeRequest.value?.path,
+  (newURL) => {
+    if (newURL) {
+      handlePathVariableUpdate(newURL)
+    }
+  },
+)
 </script>
 <template>
   <ViewLayoutCollapse :itemCount="params.length">
@@ -130,7 +139,6 @@ watch(
     <RequestTable
       v-if="params.length"
       class="flex-1"
-      isEnabledHidden
       :items="params"
       @updateRow="updateRow" />
   </ViewLayoutCollapse>
