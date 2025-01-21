e2efed51de27343c353827e490d596104a0cfb25
Brynn Hawker <brynn@hawker.me>
2025-01-20 22:06:36 -0800

    fix(components): hide team logo label letter from screen readers

--- packages/components/src/components/ScalarMenu/ScalarMenuTeamProfile.vue
+++ packages/components/src/components/ScalarMenu/ScalarMenuTeamProfile.vue
@@ -14,9 +14,11 @@ const { cx } = useBindCx()
     <img
       v-if="src"
       class="size-5 rounded"
+      role="presentation"
       :src="src" />
     <div
       v-else
+      aria-hidden="true"
       class="flex items-center justify-center text-3xs font-medium text-c-3 size-5 bg-b-3 rounded">
       <template v-if="label && label.length > 0">
         {{ label[0] }}
