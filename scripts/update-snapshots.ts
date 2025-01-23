f77339bc86dc98f1b04dee5cdd4128443f8b1e9c
tmastrom <thomas.mastromonaco@gmail.com>
2025-01-23 17:08:37 +0200

    chore: add comments to the update snapshots script

--- scripts/update-snapshots.ts
+++ scripts/update-snapshots.ts
@@ -1,21 +1,24 @@
-/**
- * This script updates the snapshots for the CDN API Reference test
- */
 import fs from 'node:fs/promises'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-// ---------------------------------------------------------------------------
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
+// List of browsers we test on
+const browsers = ['chromium', 'firefox', 'Mobile-Chrome', 'Mobile-Safari']
+
+/**
+ * This script checks the playwright test results
+ * for the CDN API Reference test
+ * and updates the snapshot files.
+ *
+ * The intended use is to be run in the test-cdn-jsdelvr.yml GitHub action workflow
+ */
 async function updateSnapshots() {
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = path.dirname(__filename)
-
   const testResultsFolders = await fs.readdir(
     path.join(__dirname, '../playwright/test-results'),
   )
-
-  const browsers = ['chromium', 'firefox', 'Mobile-Chrome', 'Mobile-Safari']
 
   // filter out retry reports
   const playwrightReports = testResultsFolders.filter(
@@ -33,6 +36,7 @@ async function updateSnapshots() {
       path.join(__dirname, '../playwright/test-results', report),
     )
 
+    // Find the "actual" snapshot file
     const actualSnapshot = snapshotReport.find((name) =>
       name.includes('actual'),
     )
@@ -40,6 +44,8 @@ async function updateSnapshots() {
       continue
     }
 
+    // Copy the actual snapshot to the snapshot folder
+    // and rename the file with the browser name and platform (linux or macos)
     await fs.copyFile(
       path.join(
         __dirname,
@@ -55,7 +61,5 @@ async function updateSnapshots() {
     )
   }
 }
-
-// ---------------------------------------------------------------------------
 
 updateSnapshots()
