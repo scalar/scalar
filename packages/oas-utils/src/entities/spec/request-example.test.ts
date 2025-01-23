c89b4b65fb52c58868fa1050235cae351ee3f323
anthony lio <anthony@scalar.com>
2025-01-23 11:59:03 +0200

    feat(api-client): adds parameter examples support

--- packages/oas-utils/src/entities/spec/request-example.test.ts
+++ packages/oas-utils/src/entities/spec/request-example.test.ts
@@ -102,4 +102,104 @@ describe('createParamInstance', () => {
       default: false,
     })
   })
+
+  it('works with schema examples type number', () => {
+    const result = createParamInstance({
+      in: 'path',
+      name: 'foo',
+      required: true,
+      deprecated: false,
+      schema: {
+        default: 1.2,
+        type: 'number',
+        examples: [1.2, 2.1, 3],
+      },
+    })
+
+    expect(result).toEqual({
+      key: 'foo',
+      value: '1.2',
+      enabled: true,
+      description: undefined,
+      required: true,
+      examples: ['1.2', '2.1', '3'],
+      type: 'number',
+      default: 1.2,
+    })
+  })
+
+  it('works with schema examples type string', () => {
+    const result = createParamInstance({
+      in: 'query',
+      name: 'foo',
+      required: true,
+      deprecated: false,
+      schema: {
+        default: false,
+        type: 'boolean',
+        examples: ['foo', 'bar'],
+      },
+    })
+
+    expect(result).toEqual({
+      key: 'foo',
+      value: 'false',
+      enabled: true,
+      description: undefined,
+      required: true,
+      type: 'boolean',
+      default: false,
+      examples: ['foo', 'bar'],
+    })
+  })
+
+  it('works with schema examples type integer', () => {
+    const result = createParamInstance({
+      in: 'query',
+      name: 'foo',
+      required: true,
+      deprecated: false,
+      schema: {
+        default: 1,
+        type: 'integer',
+        examples: [1, 2, 3],
+      },
+    })
+
+    expect(result).toEqual({
+      key: 'foo',
+      value: '1',
+      enabled: true,
+      description: undefined,
+      required: true,
+      examples: ['1', '2', '3'],
+      type: 'integer',
+      default: 1,
+    })
+  })
+
+  it('works with schema examples type boolean', () => {
+    const result = createParamInstance({
+      in: 'query',
+      name: 'foo',
+      required: true,
+      deprecated: false,
+      schema: {
+        default: false,
+        type: 'boolean',
+        examples: [true, false],
+      },
+    })
+
+    expect(result).toEqual({
+      key: 'foo',
+      value: 'false',
+      enabled: true,
+      description: undefined,
+      required: true,
+      examples: ['true', 'false'],
+      type: 'boolean',
+      default: false,
+    })
+  })
 })
