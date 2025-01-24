09dee7e4c4b5a2ecfd02672b12fa9be0f92d3073
Amrit Kahlon <amrit@scalar.com>
2025-01-23 23:55:44 -0800

    fix(api-reference): inconsistent scroll position on page load when navigating to (#4573)
    * fix: lazy load all endpoints until the open one
    
    * docs(changeset): fix: jumping on initial load for big specs

--- packages/api-reference/src/components/Content/Tag/TagList.vue
+++ packages/api-reference/src/components/Content/Tag/TagList.vue
@@ -4,13 +4,13 @@ import { Operation } from '@/features/Operation'
 import { useNavState, useSidebar } from '@/hooks'
 import { useWorkspace } from '@scalar/api-client/store'
 import { ScalarErrorBoundary } from '@scalar/components'
-import type { Spec, Tag as tagType } from '@scalar/types/legacy'
+import type { Spec, Tag as TagType } from '@scalar/types/legacy'
 import { computed } from 'vue'
 
 import { Tag, TagAccordion } from './'
 
 const props = defineProps<{
-  tags: tagType[]
+  tags: TagType[]
   spec: Spec
   layout?: 'modern' | 'classic'
 }>()
@@ -19,19 +19,31 @@ const { getOperationId, getTagId, hash } = useNavState()
 const { collapsedSidebarItems } = useSidebar()
 const { requests, requestExamples, securitySchemes } = useWorkspace()
 
-const tagLayout = computed<typeof Tag>(() =>
+const tagLayout = computed(() =>
   props.layout === 'classic' ? TagAccordion : Tag,
 )
 
-// If the first load is models, we do not lazy load tags/operations
-const isLazy = props.layout !== 'classic' && !hash.value.startsWith('model')
+/**
+ * All tags on the list UNTIL the one afterfirst open tag should not be lazy loaded. However their operations should be.
+ * This so so we can get to the first open tag + operation as quick as possible and avoid any jumps
+ */
+const lazyIndex = computed(
+  () =>
+    props.tags.findIndex((tag) => !collapsedSidebarItems[getTagId(tag)]) + 1,
+)
+
+/** If the first load is models, we do not lazy load tags/operations */
+const isLazy = (index: number) =>
+  props.layout !== 'classic' &&
+  !hash.value.startsWith('model') &&
+  index > lazyIndex.value
 </script>
 <template>
   <Lazy
-    v-for="tag in tags"
+    v-for="(tag, index) in tags"
     :id="getTagId(tag)"
     :key="getTagId(tag)"
-    :isLazy="isLazy && !collapsedSidebarItems[getTagId(tag)]">
+    :isLazy="isLazy(index)">
     <Component
       :is="tagLayout"
       :id="getTagId(tag)"
@@ -41,7 +53,10 @@ const isLazy = props.layout !== 'classic' && !hash.value.startsWith('model')
         v-for="(operation, operationIndex) in tag.operations"
         :id="getOperationId(operation, tag)"
         :key="`${operation.httpVerb}-${operation.operationId}`"
-        :isLazy="operationIndex > 0">
+        :isLazy="
+          isLazy(index) ||
+          (collapsedSidebarItems[getTagId(tag)] && operationIndex > 0)
+        ">
         <ScalarErrorBoundary>
           <Operation
             :id="getOperationId(operation, tag)"
