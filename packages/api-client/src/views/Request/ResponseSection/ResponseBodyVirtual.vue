a91db3e83c11fcf987bfd0a0e76a1683c5e43d6a
anthony lio <anthony@scalar.com>
2025-02-05 18:24:26 +0200

    feat(api-client): adds download button in virtual response

--- packages/api-client/src/views/Request/ResponseSection/ResponseBodyVirtual.vue
+++ packages/api-client/src/views/Request/ResponseSection/ResponseBodyVirtual.vue
@@ -1,19 +1,37 @@
 <script setup lang="ts">
 import ViewLayoutCollapse from '@/components/ViewLayout/ViewLayoutCollapse.vue'
+import { useResponseBody } from '@/hooks/useResponseBody'
 import { ScalarVirtualText } from '@scalar/components'
 import { formatJsonOrYamlString } from '@scalar/oas-utils/helpers'
 import { computed } from 'vue'
 
+import ResponseBodyDownload from './ResponseBodyDownload.vue'
+
 const props = defineProps<{
   content: string
+  data: unknown
+  headers: { name: string; value: string; required: boolean }[]
 }>()
 
 const textContent = computed(() => formatJsonOrYamlString(props.content))
+
+const { mimeType, attachmentFilename, dataUrl } = useResponseBody({
+  data: props.data,
+  headers: props.headers,
+})
 </script>
 
 <template>
   <ViewLayoutCollapse class="!max-h-100% overflow-x-auto response-body-virtual">
     <template #title>Body</template>
+    <template
+      v-if="dataUrl"
+      #actions>
+      <ResponseBodyDownload
+        :filename="attachmentFilename"
+        :href="dataUrl"
+        :type="mimeType.essence" />
+    </template>
     <div
       class="py-1.5 px-2.5 font-code text-xxs border-1/2 rounded-t border-b-0">
       This response body is massive! Syntax highlighting won’t work here.
