f7769c1fb6869e5b7e1939a0af4e3a5cde8cb9c7
anthony lio <anthony@scalar.com>
2025-02-05 18:20:43 +0200

    feat(api-client): adds useResponseBody hook

---
+++ packages/api-client/src/hooks/useResponseBody.ts
@@ -0,0 +1,43 @@
+import { extractFilename } from '@/libs/extractAttachmentFilename'
+import { computed } from 'vue'
+import MIMEType from 'whatwg-mimetype'
+
+export function useResponseBody(props: {
+  data: unknown
+  headers: { name: string; value: string; required: boolean }[]
+}) {
+  const isBlob = (b: any): b is Blob => b instanceof Blob
+
+  const mimeType = computed(() => {
+    const contentType =
+      props.headers.find(
+        (header) => header.name.toLowerCase() === 'content-type',
+      )?.value ?? ''
+    return new MIMEType(contentType)
+  })
+
+  const attachmentFilename = computed(() => {
+    const value =
+      props.headers.find(
+        (header) => header.name.toLowerCase() === 'content-disposition',
+      )?.value ?? ''
+    return extractFilename(value)
+  })
+
+  const dataUrl = computed<string>(() => {
+    if (isBlob(props.data)) return URL.createObjectURL(props.data)
+    if (typeof props.data === 'string')
+      return URL.createObjectURL(
+        new Blob([props.data], { type: mimeType.value.toString() }),
+      )
+    if (props.data instanceof Object && Object.keys(props.data).length)
+      return URL.createObjectURL(
+        new Blob([JSON.stringify(props.data)], {
+          type: mimeType.value.toString(),
+        }),
+      )
+    return ''
+  })
+
+  return { mimeType, attachmentFilename, dataUrl }
+}
