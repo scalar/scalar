09b988871bb7990dbabc6a86608133a4d06aaea2
Brynn Hawker <brynn@hawker.me>
2025-02-10 11:30:12 -0800

    fix: tweak border

--- packages/components/src/components/ScalarFileUpload/ScalarFileUpload.vue
+++ packages/components/src/components/ScalarFileUpload/ScalarFileUpload.vue
@@ -119,7 +119,7 @@ const { cx } = useBindCx()
   <div
     v-bind="
       cx(
-        'flex flex-col relative border border-dashed border-c-3 has-[input:focus-visible]:outline outline-offset-1 rounded',
+        'flex flex-col relative border-dashed border-2 has-[input:focus-visible]:outline outline-offset-1 rounded',
       )
     "
     @dragenter="dragover = true"
