f5c5b568997827a48671d02bb791bce100453743
Hans Pagel <mail@hanspagel.com>
2025-02-10 15:13:17 +0100

    fix: tests

--- packages/api-client/src/routes.test.ts
+++ packages/api-client/src/routes.test.ts
@@ -137,7 +137,7 @@ describe('Routes', () => {
       .find((route) => route.name === 'servers')
     expect(serversRoute).toBeDefined()
     expect(serversRoute?.path).toBe(
-      '/workspace/:workspace/servers/:collectionId/:servers',
+      '/workspace/:workspace/servers/:collection/:servers',
     )
   })
 
@@ -155,7 +155,7 @@ describe('Routes', () => {
     expect(redirectResult).toEqual(
       expect.objectContaining({
         name: 'servers',
-        params: { collectionId: 'default', servers: 'default' },
+        params: { collection: 'default', servers: 'default' },
       }),
     )
   })
