d6b432f3b8d64d13bad0da25db78ce01d7b7f878
Hans Pagel <mail@hanspagel.com>
2025-02-11 15:58:33 +0100

    feat: new collection sidebar

--- packages/api-client/src/views/Collection/CollectionSidebar.vue
+++ packages/api-client/src/views/Collection/CollectionSidebar.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import SideNavGroup from '@/components/SideNav/SideNavGroup.vue'
-import SideNavRouterLink from '@/components/SideNav/SideNavRouterLink.vue'
-import { useLayout } from '@/hooks'
 import { PathId } from '@/routes'
 import { useActiveEntities } from '@/store'
-import type { Icon } from '@scalar/components'
+import {
+  type Icon,
+  ScalarSidebar,
+  ScalarSidebarItem,
+  ScalarSidebarItems,
+} from '@scalar/components'
 import { computed } from 'vue'
-import { type RouteLocationNamedRaw, useRouter } from 'vue-router'
+import { type RouteLocationNamedRaw, RouterLink, useRouter } from 'vue-router'
 
 const { currentRoute } = useRouter()
-const { layout } = useLayout()
 const { activeCollection } = useActiveEntities()
 
 type CollectionSidebarEntry = {
@@ -42,28 +43,24 @@ const routes = computed<CollectionSidebarEntry[]>(() => [
 ])
 </script>
 <template>
-  <nav
-    aria-label="App Navigation"
-    class="flex items-center justify-center sm:justify-between gap-1.5 app-drag-region pt-2"
-    :class="
-      layout === 'web' ? 'border h-header !pt-0' : 'sm:flex-col px-2 pb-2'
-    "
-    role="navigation">
-    <SideNavGroup class="app-no-drag-region">
-      <li
-        v-for="({ icon, to, displayName }, i) in routes"
-        :key="i">
-        <SideNavRouterLink
-          :active="
+  <div class="flex-1">
+    <ScalarSidebar>
+      <ScalarSidebarItems>
+        <ScalarSidebarItem
+          :is="RouterLink"
+          v-for="({ icon, to, displayName }, i) in routes"
+          :key="i"
+          class="text-sm"
+          :icon="icon"
+          :selected="
             typeof to.name === 'string' &&
             typeof currentRoute.name === 'string' &&
             currentRoute.name?.startsWith(to.name)
           "
-          :icon="icon"
           :to="to">
           {{ displayName }}
-        </SideNavRouterLink>
-      </li>
-    </SideNavGroup>
-  </nav>
+        </ScalarSidebarItem>
+      </ScalarSidebarItems>
+    </ScalarSidebar>
+  </div>
 </template>
