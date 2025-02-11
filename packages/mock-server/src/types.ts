abb2cc914d43f8f597d1fce617f2554046ce6400
Hans Pagel <mail@hanspagel.com>
2025-02-11 11:09:45 +0100

    chore: apply fixes to mock server package

--- packages/mock-server/src/types.ts
+++ packages/mock-server/src/types.ts
@@ -1,5 +1,5 @@
 import type { OpenAPI } from '@scalar/openapi-types'
-import { type Context, Hono } from 'hono'
+import type { Context } from 'hono'
 
 /** Available HTTP methods for Hono routes */
 export const httpMethods = [
