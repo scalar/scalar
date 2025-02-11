651d996a07361b624dd14a79d4c2389c5a6eda93
Hans Pagel <mail@hanspagel.com>
2025-02-11 11:23:53 +0100

    chore: apply fixes to code-highlight

--- packages/code-highlight/src/markdown/splitContent.test.ts
+++ packages/code-highlight/src/markdown/splitContent.test.ts
@@ -4,20 +4,20 @@ import { splitContent } from './markdown'
 
 describe('splitContent', () => {
   it('returns a single document if there’s no heading', () => {
-    const sections = splitContent(`Foobar`)
+    const sections = splitContent('Foobar')
 
     expect(sections).toMatchObject(['Foobar'])
   })
 
   it('splits the markdown document', () => {
-    const sections = splitContent(`# Example Heading\n\nFoobar`)
+    const sections = splitContent('# Example Heading\n\nFoobar')
 
     expect(sections).toMatchObject(['# Example Heading', 'Foobar'])
   })
 
   it('splits multiple headings', () => {
     const sections = splitContent(
-      `# Example Heading\n\n## Other Heading\n\nFoobar`,
+      '# Example Heading\n\n## Other Heading\n\nFoobar',
     )
 
     expect(sections).toMatchObject([
