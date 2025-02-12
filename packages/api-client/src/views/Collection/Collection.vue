c1dc47063cba5091204543d00dc8feacca14d07a
Hans Pagel <mail@hanspagel.com>
2025-02-12 13:22:25 +0100

    chore: clean up

--- packages/api-client/src/views/Collection/Collection.vue
+++ packages/api-client/src/views/Collection/Collection.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import ViewLayout from '@/components/ViewLayout/ViewLayout.vue'
-import { useActiveEntities } from '@/store'
+import ViewLayoutContent from '@/components/ViewLayout/ViewLayoutContent.vue'
 import CollectionNavigation from '@/views/Collection/CollectionNavigation.vue'
 import { RouterView } from 'vue-router'
 </script>
+
 <template>
   <ViewLayout class="flex md:flex-col">
     <CollectionNavigation />
@@ -12,4 +13,3 @@ import { RouterView } from 'vue-router'
     </ViewLayoutContent>
   </ViewLayout>
 </template>
-<style scoped></style>
