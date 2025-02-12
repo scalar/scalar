b21474bb67461ab88892770754f93efb7354c8c2
anthony lio <anthony@scalar.com>
2025-02-12 19:19:23 +0200

    feat(api-client): updates collection overview form

--- packages/api-client/src/views/Collection/CollectionInfoForm.vue
+++ packages/api-client/src/views/Collection/CollectionInfoForm.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-import Form from '@/components/Form/Form.vue'
+import LabelInput from '@/components/Form/LabelInput.vue'
+import IconSelector from '@/components/IconSelector.vue'
 import { useWorkspace } from '@/store'
 import { useActiveEntities } from '@/store/active-entities'
-import { computed } from 'vue'
+import { ScalarButton, ScalarMarkdown } from '@scalar/components'
+import { LibraryIcon } from '@scalar/icons'
+import { computed, ref } from 'vue'
 
 const { activeCollection } = useActiveEntities()
 const { collectionMutators } = useWorkspace()
+
+const collectionIcon =
+  activeCollection?.value?.['x-scalar-icon'] || ref('interface-content-folder')
 
 const fields = [
   { label: 'Title', key: 'title', placeholder: 'My Collection' },
@@ -27,8 +33,23 @@ const updateInfo = (key: string, value: string) => {
   })
 }
 
+const updateCollectionIcon = (value: string) => {
+  if (!activeCollection?.value?.uid) {
+    return
+  }
+
+  collectionMutators.edit(activeCollection?.value?.uid, 'x-scalar-icon', value)
+}
+
+const updateCollectionName = (value: string) => {
+  if (!activeCollection.value) return
+
+  collectionMutators.edit(activeCollection.value.uid, 'info.title', value)
+}
+
 const data = computed(() => {
   return {
+    icon: activeCollection?.value?.['x-scalar-icon'],
     title: activeCollection?.value?.info?.title,
     description: activeCollection?.value?.info?.description,
     version: activeCollection?.value?.info?.version,
@@ -36,12 +57,53 @@ const data = computed(() => {
 })
 </script>
 <template>
-  <Form
-    :data="data"
-    :onUpdate="updateInfo"
-    :options="fields">
-    <template #title>
-      <span class="text-c-2">Info</span>
-    </template>
-  </Form>
+  <div
+    :aria-label="`Collection: ${data.title}`"
+    class="flex flex-col mx-auto justify-center gap-2 pt-6 w-full md:w-3/5">
+    <div class="relative">
+      <IconSelector
+        v-model="collectionIcon"
+        placement="bottom-start"
+        @update:modelValue="updateCollectionIcon(collectionIcon)">
+        <ScalarButton
+          class="absolute cursor-pointer -left-7 top-1/2 -translate-y-1/2 h-6 w-6 aspect-square p-0.25 rounded hover:bg-b-2"
+          variant="ghost">
+          <LibraryIcon
+            class="size-4 text-c-2 stroke-[1.75]"
+            :src="collectionIcon" />
+        </ScalarButton>
+      </IconSelector>
+      <div class="group">
+        <LabelInput
+          inputId="collectionName"
+          placeholder="Collection Name"
+          :value="data.title"
+          @updateValue="updateCollectionName" />
+      </div>
+    </div>
+    <p class="leading-normal mt-3 text-c-2">
+      <ScalarMarkdown :value="data.description" />
+    </p>
+  </div>
 </template>
+<style scoped>
+:deep(.markdown) h2 {
+  @apply text-lg;
+}
+.group-hover-input {
+  border-width: var(--scalar-border-width);
+  border-color: transparent;
+}
+.group:hover .group-hover-input {
+  background: color-mix(
+    in srgb,
+    var(--scalar-background-1),
+    var(--scalar-background-2)
+  );
+  border-color: var(--scalar-border-color);
+}
+.group-hover-input:focus {
+  background: transparent !important;
+  border-color: var(--scalar-border-color) !important;
+}
+</style>
