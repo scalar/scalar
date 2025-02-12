efc8ec26a331d7950d068edf8d180ee006150f1a
Hans Pagel <mail@hanspagel.com>
2025-02-12 13:19:12 +0100

    feat: horizontal tab navigation for the collection page

--- packages/api-client/src/views/Collection/Collection.vue
+++ packages/api-client/src/views/Collection/Collection.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
-import Sidebar from '@/components/Sidebar/Sidebar.vue'
 import ViewLayout from '@/components/ViewLayout/ViewLayout.vue'
-import ViewLayoutContent from '@/components/ViewLayout/ViewLayoutContent.vue'
 import { useActiveEntities } from '@/store'
-import CollectionSidebar from '@/views/Collection/CollectionSidebar.vue'
+import CollectionNavigation from '@/views/Collection/CollectionNavigation.vue'
 import { RouterView } from 'vue-router'
-
-const { activeCollection } = useActiveEntities()
 </script>
 <template>
-  <ViewLayout>
-    <Sidebar :title="activeCollection?.info?.title || 'Untitled Collection'">
-      <template #content>
-        <CollectionSidebar />
-      </template>
-    </Sidebar>
+  <ViewLayout class="flex md:flex-col">
+    <CollectionNavigation />
     <ViewLayoutContent class="flex-1">
       <RouterView />
     </ViewLayoutContent>
