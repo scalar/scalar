a58f2370fceaca202536f0afd07975062b693032
Hans Pagel <mail@hanspagel.com>
2025-02-12 11:32:47 +0100

    fix: can’t parse HTML attributes with HTML tags inside

---
+++ packages/import/src/get-content-of-script-tag.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, it } from 'vitest'
+
+import { getContentOfScriptTag } from './resolve'
+
+describe('getContentOfScriptTag', () => {
+  it('returns the content of the script tag', () => {
+    const html =
+      '<script id="api-reference">console.log("Hello, world!");</script>'
+    expect(getContentOfScriptTag(html)).toBe('console.log("Hello, world!");')
+  })
+
+  it('works with single quotes', () => {
+    const html = `<script id='api-reference'>console.log("Hello, world!");</script>`
+    expect(getContentOfScriptTag(html)).toBe('console.log("Hello, world!");')
+  })
+
+  it('ignores HTML characters inside the attributes', () => {
+    // HTML tags inside the attributes
+    const html = `<!DOCTYPE html>
+    <html>
+      <head>
+        <title>Scalar API Reference</title>
+        <meta charset="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1" />
+        <style>
+          null
+        </style>
+      </head>
+      <body>
+        <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+        <script id="api-reference" type="application/json" data-configuration="foo<br>bar">console.log("Hello, world!");</script>
+      </body>
+    </html>`
+
+    expect(getContentOfScriptTag(html)).toBe('console.log("Hello, world!");')
+  })
+})
