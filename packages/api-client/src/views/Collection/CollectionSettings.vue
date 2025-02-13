53e40a9ada0dd214e99b6daa27779e649ec9efae
Hans Pagel <mail@hanspagel.com>
2025-02-13 12:45:28 +0100

    feat: delete collection from collection settings

--- packages/api-client/src/views/Collection/CollectionSettings.vue
+++ packages/api-client/src/views/Collection/CollectionSettings.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import DeleteSidebarListElement from '@/components/Sidebar/Actions/DeleteSidebarListElement.vue'
 import ViewLayoutSection from '@/components/ViewLayout/ViewLayoutSection.vue'
+import { PathId } from '@/router'
 import { useActiveEntities, useWorkspace } from '@/store'
+import { ScalarModal, useModal } from '@scalar/components'
+import { useRouter } from 'vue-router'
 
-const { activeCollection } = useActiveEntities()
+const { activeCollection, activeWorkspace, activeWorkspaceCollections } =
+  useActiveEntities()
 const { collectionMutators } = useWorkspace()
+const { replace } = useRouter()
+const deleteModal = useModal()
 
 function handleToggleWatchMode() {
   if (!activeCollection.value) {
@@ -18,6 +25,26 @@ function handleToggleWatchMode() {
     'watchMode',
     !activeCollection.value?.watchMode,
   )
+}
+
+function handleDeleteCollection() {
+  if (!activeCollection.value) return
+
+  if (!activeWorkspace.value) return
+
+  collectionMutators.delete(activeCollection.value, activeWorkspace.value)
+
+  // Redirect to the first collection
+  if (activeWorkspaceCollections.value[0]) {
+    replace({
+      name: 'request.root',
+      params: {
+        [PathId.Workspace]: activeWorkspace.value.uid,
+      },
+    })
+  }
+
+  deleteModal.hide()
 }
 </script>
 
@@ -40,4 +67,27 @@ function handleToggleWatchMode() {
       {{ activeCollection?.documentUrl }}
     </div>
   </ViewLayoutSection>
+  <ViewLayoutSection>
+    <template #title>Danger Zone</template>
+    <div>
+      <button
+        class="border rounded-md p-2 bg-b-3 text-c-1 w-64 m-4"
+        type="button"
+        @click="deleteModal.show()">
+        delete collection
+      </button>
+    </div>
+  </ViewLayoutSection>
+
+  <!-- Delete Modal -->
+  <ScalarModal
+    :size="'xxs'"
+    :state="deleteModal"
+    :title="`Delete ${activeCollection?.title}`">
+    <DeleteSidebarListElement
+      :variableName="activeCollection?.title ?? ''"
+      :warningMessage="'This action cannot be undone.'"
+      @close="deleteModal.hide()"
+      @delete="handleDeleteCollection" />
+  </ScalarModal>
 </template>
