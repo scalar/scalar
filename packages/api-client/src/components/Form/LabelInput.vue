b09013359c34378d0d964cfa016f5bdff56c18c5
anthony lio <anthony@scalar.com>
2025-02-14 11:02:19 +0200

    feat(api-client): updates label input component

--- packages/api-client/src/components/Form/LabelInput.vue
+++ packages/api-client/src/components/Form/LabelInput.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { ClientLayout } from '@/hooks/useLayout'
+import { useBindCx } from '@scalar/components'
 
 defineProps<{
   inputId: string
@@ -12,28 +13,37 @@ const emit = defineEmits<{
   (e: 'updateValue', value: string): void
 }>()
 
+const { cx } = useBindCx()
+
 const onInput = (event: Event) => {
   const target = event.target as HTMLInputElement
   emit('updateValue', target.value)
 }
 </script>
 <template>
-  <div
-    class="flex-1 flex gap-1 items-center lg:pr-24 pointer-events-none group">
+  <div v-bind="cx('flex-1 flex gap-1 items-center pointer-events-none group')">
     <template v-if="layout !== 'modal'">
       <label
-        class="absolute w-full h-full top-0 left-0 pointer-events-auto opacity-0 cursor-text"
+        v-bind="
+          cx(
+            'absolute w-full h-full top-0 left-0 pointer-events-auto opacity-0 cursor-text',
+          )
+        "
         :for="inputId" />
       <input
+        v-bind="
+          cx(
+            'flex-1 text-c-1 rounded pointer-events-auto relative w-full pl-1.25 -ml-0.5 md:-ml-1.25 h-8 group-hover-input has-[:focus-visible]:outline z-10',
+          )
+        "
         :id="inputId"
-        class="text-c-1 rounded pointer-events-auto relative w-full pl-1.25 -ml-0.5 md:-ml-1.25 h-8 group-hover-input has-[:focus-visible]:outline z-10"
         :placeholder="placeholder"
         :value="value"
         @input="onInput" />
     </template>
     <span
       v-else
-      class="flex items-center text-c-1 h-8">
+      v-bind="cx('flex items-center text-c-1 h-8')">
       {{ value }}
     </span>
   </div>
