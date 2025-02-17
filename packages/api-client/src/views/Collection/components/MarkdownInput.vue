6682366d6622082eea228fc8fbe6e3013b963305
anthony lio <anthony@scalar.com>
2025-02-17 10:15:54 +0200

    feat(api-client): adds collection markdown input placeholder

--- packages/api-client/src/views/Collection/components/MarkdownInput.vue
+++ packages/api-client/src/views/Collection/components/MarkdownInput.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import CodeInput from '@/components/CodeInput/CodeInput.vue'
 import { ScalarMarkdown } from '@scalar/components'
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
+
+import CodeInput from '@/components/CodeInput/CodeInput.vue'
 
 import ModeToggleButton from './ModeToggleButton.vue'
 
@@ -12,39 +13,34 @@ const { modelValue } = defineProps<{
 const emit = defineEmits<(e: 'update:modelValue', value: string) => void>()
 
 const mode = ref<'edit' | 'preview'>('preview')
-const codeInputRef = ref<HTMLInputElement | null>()
-
-watch(mode, (newMode) => {
-  if (newMode === 'edit') {
-    codeInputRef.value?.focus()
-  }
-})
 </script>
 
 <template>
   <div
-    class="border border-transparent hover:border-b-3 has-[:focus-visible]:bg-b-1 group flex flex-col relative rounded-lg overflow-hidden z-1">
+    class="hover:border-b-3 has-[:focus-visible]:bg-b-1 z-1 group relative flex flex-col overflow-hidden rounded-lg border border-transparent">
     <!-- Tabs -->
     <div
-      class="absolute bg-b-1 bottom-4 gap-1 hidden group-hover:grid grid-cols-2 left-1/2 p-1 text-sm rounded-lg -translate-x-1/2 z-[1]">
+      class="bg-b-1 absolute bottom-4 left-1/2 z-[1] hidden -translate-x-1/2 grid-cols-2 gap-1 rounded-lg p-1 text-sm group-hover:grid">
       <ModeToggleButton v-model="mode" />
       <div
-        class="absolute inset-0 -z-1 rounded bg-b-1 shadow-lg brightness-lifted" />
+        class="-z-1 bg-b-1 brightness-lifted absolute inset-0 rounded shadow-lg" />
     </div>
     <div class="h-full min-h-48 overflow-y-auto">
       <!-- Preview -->
       <template v-if="mode === 'preview'">
         <ScalarMarkdown
-          class="p-1.5"
-          :value="modelValue"
+          class="h-full rounded-lg p-1.5"
+          :class="{
+            'text-c-3 rounded border border-dashed': modelValue === '',
+          }"
+          :value="modelValue || 'Add description...'"
           @dblclick="mode = 'edit'" />
       </template>
 
       <!-- Edit -->
       <template v-if="mode === 'edit'">
         <CodeInput
-          ref="codeInputRef"
-          class="h-full px-1 py-0 !rounded-lg"
+          class="h-full !rounded-lg px-1 py-0"
           :class="{ 'min-h-[calc(1em*4)]': mode === 'edit' }"
           :modelValue="modelValue"
           @blur="mode = 'preview'"
@@ -52,7 +48,7 @@ watch(mode, (newMode) => {
       </template>
     </div>
     <div
-      class="hidden group-hover:block brightness-lifted group-has-[:focus-visible]:hidden absolute inset-0 -z-1 rounded bg-b-1 shadow-lg" />
+      class="brightness-lifted -z-1 bg-b-1 absolute inset-0 hidden rounded shadow-lg group-hover:block group-has-[:focus-visible]:hidden" />
   </div>
 </template>
 <style scoped>
