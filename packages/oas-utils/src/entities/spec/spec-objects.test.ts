f378a14e4279d11e663d29a9b785311e5a46d37b
Hans Pagel <mail@hanspagel.com>
2025-02-18 15:00:37 +0100

    fix: better deal with invalid values

---
+++ packages/oas-utils/src/entities/spec/spec-objects.test.ts
@@ -0,0 +1,143 @@
+import { describe, expect, it } from 'vitest'
+import { oasInfoSchema } from './spec-objects'
+
+describe('spec-objects', () => {
+  describe('oasInfoSchema', () => {
+    describe('title', () => {
+      it('parses title', () => {
+        const title = oasInfoSchema.parse({
+          title: 'My API',
+        })
+
+        expect(title).toMatchObject({
+          title: 'My API',
+        })
+      })
+
+      it('defaults to API', () => {
+        const title = oasInfoSchema.parse({})
+
+        expect(title).toMatchObject({
+          title: 'API',
+        })
+      })
+
+      it('ignores invalid title', () => {
+        const title = oasInfoSchema.parse({
+          title: 123,
+        })
+
+        expect(title).toMatchObject({ title: 'API' })
+      })
+    })
+
+    describe('version', () => {
+      it('parses version', () => {
+        const version = oasInfoSchema.parse({
+          version: '1.0.0',
+        })
+
+        expect(version).toMatchObject({ version: '1.0.0' })
+      })
+
+      it('defaults to 1.0', () => {
+        const version = oasInfoSchema.parse({})
+
+        expect(version).toMatchObject({ version: '1.0' })
+      })
+
+      it('ignores invalid version', () => {
+        const version = oasInfoSchema.parse({
+          version: 123,
+        })
+
+        expect(version).toMatchObject({ version: '1.0' })
+      })
+    })
+
+    describe('contact', () => {
+      it('parses contact object', () => {
+        const info = oasInfoSchema.parse({
+          contact: {
+            name: 'John Doe',
+            url: 'https://example.com',
+            email: 'john.doe@example.com',
+          },
+        })
+
+        expect(info).toMatchObject({
+          contact: {
+            name: 'John Doe',
+            url: 'https://example.com',
+            email: 'john.doe@example.com',
+          },
+        })
+      })
+
+      it('ignores invalid contact', () => {
+        const info = oasInfoSchema.parse({
+          contact: 123,
+        })
+
+        expect(info.contact).toStrictEqual(undefined)
+      })
+
+      it('works with just the name', () => {
+        const info = oasInfoSchema.parse({
+          contact: {
+            name: 'John Doe',
+          },
+        })
+
+        expect(info).toMatchObject({
+          contact: {
+            name: 'John Doe',
+          },
+        })
+      })
+
+      it('works with just the url', () => {
+        const info = oasInfoSchema.parse({
+          contact: {
+            url: 'https://example.com',
+          },
+        })
+
+        expect(info).toMatchObject({
+          contact: {
+            url: 'https://example.com',
+          },
+        })
+      })
+
+      it('works with just the email', () => {
+        const info = oasInfoSchema.parse({
+          contact: {
+            email: 'john.doe@example.com',
+          },
+        })
+
+        expect(info).toMatchObject({
+          contact: {
+            email: 'john.doe@example.com',
+          },
+        })
+      })
+
+      it('ignores invalid attributes', () => {
+        const info = oasInfoSchema.parse({
+          contact: {
+            name: 'John Doe',
+            url: 'invalid',
+            email: 'invalid',
+            something: 'random',
+          },
+        })
+
+        expect(info.contact).toStrictEqual({
+          name: 'John Doe',
+        })
+      })
+    })
+  })
+})
