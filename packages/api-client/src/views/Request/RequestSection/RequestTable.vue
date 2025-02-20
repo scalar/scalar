f44aa6a5a3ab25dcc922d4280208e6b095377755
anthony lio <anthony@scalar.com>
2025-02-20 11:43:14 +0200

    feat(api-client): adds request parameter value validation alert

--- packages/api-client/src/views/Request/RequestSection/RequestTable.vue
+++ packages/api-client/src/views/Request/RequestSection/RequestTable.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
+import { ScalarButton, ScalarIcon, ScalarTooltip } from '@scalar/components'
+import type { RequestExampleParameter } from '@scalar/oas-utils/entities/spec'
+import type { RouteLocationRaw } from 'vue-router'
+
 import CodeInput from '@/components/CodeInput/CodeInput.vue'
 import DataTable from '@/components/DataTable/DataTable.vue'
 import DataTableCell from '@/components/DataTable/DataTableCell.vue'
 import DataTableCheckbox from '@/components/DataTable/DataTableCheckbox.vue'
 import DataTableRow from '@/components/DataTable/DataTableRow.vue'
-import { ScalarButton, ScalarIcon, ScalarTooltip } from '@scalar/components'
-import type { RequestExampleParameter } from '@scalar/oas-utils/entities/spec'
-import { computed } from 'vue'
-import type { RouteLocationRaw } from 'vue-router'
 
-import { hasItemProperties } from '../libs/request'
+import { hasItemProperties, parameterIsInvalid } from '../libs/request'
 import RequestTableTooltip from './RequestTableTooltip.vue'
 
 const props = withDefaults(
@@ -54,21 +54,6 @@ const handleFileUpload = (idx: number) => {
   emit('uploadFile', idx)
 }
 
-const valueOutOfRange = (item: RequestExampleParameter) => {
-  return computed(() => {
-    if (item.type === 'integer' && item.value !== undefined) {
-      const value = Number(item.value)
-      if (item.minimum !== undefined && value < item.minimum) {
-        return `Min is ${item.minimum}`
-      }
-      if (item.maximum !== undefined && value > item.maximum) {
-        return `Max is ${item.maximum}`
-      }
-    }
-    return false
-  })
-}
-
 const flattenValue = (item: RequestExampleParameter) => {
   return Array.isArray(item.default) && item.default.length === 1
     ? item.default[0]
@@ -77,15 +62,18 @@ const flattenValue = (item: RequestExampleParameter) => {
 </script>
 <template>
   <DataTable
-    class="flex-1"
+    class="group/table flex-1"
     :columns="columns">
     <DataTableRow
       v-for="(item, idx) in items"
-      :key="idx">
+      :key="idx"
+      :class="{
+        alert: parameterIsInvalid(item).value,
+      }">
       <label class="contents">
         <template v-if="isGlobal">
           <RouterLink
-            class="!border-r-1/2 border-t-1/2 text-c-2 flex justify-center items-center"
+            class="!border-r-1/2 border-t-1/2 text-c-2 flex items-center justify-center"
             :to="item.route ?? {}">
             <span class="sr-only">Global</span>
             <ScalarTooltip
@@ -99,8 +87,8 @@ const flattenValue = (item: RequestExampleParameter) => {
               </template>
               <template #content>
                 <div
-                  class="grid gap-1.5 pointer-events-none max-w-[320px] w-content shadow-lg rounded bg-b-1 z-100 p-2 text-xxs leading-5 z-10 text-c-1">
-                  <div class="flex items-center text-c-1">
+                  class="w-content bg-b-1 z-100 text-xxs text-c-1 pointer-events-none z-10 grid max-w-[320px] gap-1.5 rounded p-2 leading-5 shadow-lg">
+                  <div class="text-c-1 flex items-center">
                     <span class="text-pretty">
                       Global cookies are shared across the whole workspace.
                     </span>
@@ -161,11 +149,6 @@ const flattenValue = (item: RequestExampleParameter) => {
           @update:modelValue="
             (v: string) => emit('updateRow', idx, 'value', v)
           ">
-          <template
-            v-if="valueOutOfRange(item).value"
-            #warning>
-            {{ valueOutOfRange(item).value }}
-          </template>
           <template #icon>
             <RequestTableTooltip
               v-if="hasItemProperties(item)"
@@ -175,14 +158,14 @@ const flattenValue = (item: RequestExampleParameter) => {
       </DataTableCell>
       <DataTableCell
         v-if="showUploadButton"
-        class="group/upload p-1 overflow-hidden relative text-ellipsis whitespace-nowrap">
+        class="group/upload relative overflow-hidden text-ellipsis whitespace-nowrap p-1">
         <template v-if="item.file">
           <div
-            class="text-c-2 max-w-[100%] overflow-hidden filemask flex items-end justify-end">
+            class="text-c-2 filemask flex max-w-[100%] items-end justify-end overflow-hidden">
             <span>{{ item.file?.name }}</span>
           </div>
           <button
-            class="absolute bg-b-2 font-medium centered-x centered-y hidden rounded text-center p-0.5 w-[calc(100%_-_8px)] group-hover/upload:block text-xs"
+            class="bg-b-2 centered-x centered-y absolute hidden w-[calc(100%_-_8px)] rounded p-0.5 text-center text-xs font-medium group-hover/upload:block"
             type="button"
             @click="emit('removeFile', idx)">
             Delete
@@ -190,7 +173,7 @@ const flattenValue = (item: RequestExampleParameter) => {
         </template>
         <template v-else>
           <ScalarButton
-            class="bg-b-2 hover:bg-b-3 border-0 py-px text-c-2 shadow-none"
+            class="bg-b-2 hover:bg-b-3 text-c-2 border-0 py-px shadow-none"
             size="sm"
             variant="outlined"
             @click="handleFileUpload(idx)">
