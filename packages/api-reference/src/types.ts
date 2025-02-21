f0328c3b59ab8dcfae17a82458d669753abe7026
Hans Pagel <mail@hanspagel.com>
2025-02-21 16:42:12 +0100

    chore: still export old configuration type for backwards compatibility

--- packages/api-reference/src/types.ts
+++ packages/api-reference/src/types.ts
@@ -1,10 +1,12 @@
 import type { HarRequest } from '@scalar/snippetz/types'
 import type { ThemeId } from '@scalar/themes'
 import type { ApiReferenceConfiguration, ApiReferenceConfigurationSchema } from '@scalar/types/api-reference'
-import type { ContentType, Spec } from '@scalar/types/legacy'
+import type { ContentType, ReferenceConfiguration, Spec } from '@scalar/types/legacy'
 import type { Slot } from 'vue'
 
 export type { ApiReferenceConfiguration }
+// TODO: Just here for backwards compatibility (2025-02-21)
+export type { ReferenceConfiguration }
 
 export type ReferenceProps = {
   configuration?: ApiReferenceConfiguration
