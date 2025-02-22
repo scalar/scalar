46f706db302483fbf98f570e4337ee08cf982e3a
Amrit <amrit@scalar.com>
2025-02-21 18:09:56 -0800

    chore: add a test to catch warnings in the client

--- packages/api-client/src/layouts/Web/create-api-client-web.test.ts
+++ packages/api-client/src/layouts/Web/create-api-client-web.test.ts
@@ -1,10 +1,14 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 
 import { createApiClientWeb } from './create-api-client-web'
 
 describe('createApiClientWeb', () => {
-  it('renders something', async () => {
+  it('renders the client with no warnings or errors', async () => {
+    const consoleWarnSpy = vi.spyOn(console, 'warn')
+    const consoleErrorSpy = vi.spyOn(console, 'error')
+
     const element = document.createElement('div')
+    element.id = 'scalar-client'
     expect(element).not.toBeNull()
 
     expect(element.innerHTML).not.toContain('App Navigation')
@@ -14,5 +18,12 @@ describe('createApiClientWeb', () => {
     })
 
     expect(element.innerHTML).toContain('App Navigation')
+
+    // Make sure we wait for the client to be mounted
+    await vi.waitFor(() => expect(element.innerHTML).toContain('My First Request'))
+
+    // Make sure we didn't log any warnings or errors
+    expect(consoleWarnSpy).not.toHaveBeenCalled()
+    expect(consoleErrorSpy).not.toHaveBeenCalled()
   })
 })
