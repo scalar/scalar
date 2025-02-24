05261b7bc6ca91b8546c3fdf42005a8ddbfeaa6b
anthony lio <anthony@scalar.com>
2025-02-24 09:43:17 +0200

    feat(types): adds vendor specific content type

--- packages/types/src/legacy/reference-config.ts
+++ packages/types/src/legacy/reference-config.ts
@@ -356,6 +356,8 @@ export type ContentType =
   | `application/x-www-form-urlencoded${OptionalCharset}`
   | `multipart/form-data${OptionalCharset}`
   | `*/*${OptionalCharset}`
+  | `application/vnd.${string}+json${OptionalCharset}`
+
 export type Cookie = {
   name: string
   value: string
