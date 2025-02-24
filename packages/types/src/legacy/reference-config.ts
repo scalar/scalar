8b42884be7f680f17dde086a8355b8cd8cfae0c0
Hans Pagel <mail@hanspagel.com>
2025-02-24 12:20:23 +0100

    feat: API definitions can have a name

--- packages/types/src/legacy/reference-config.ts
+++ packages/types/src/legacy/reference-config.ts
@@ -508,6 +508,12 @@ export type SpecConfiguration = {
    * @remark It’s recommended to pass an `url` instead of `content`.
    */
   content?: string | Record<string, any> | (() => Record<string, any>) | null | undefined
+  /**
+   * The title of the API definition used as an slug in the URL
+   *
+   * @example 'scalar-galaxy'
+   */
+  name?: string
 }
 
 export type Schema = {
