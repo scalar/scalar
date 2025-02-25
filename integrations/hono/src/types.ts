c88985e2d2537292536baa4707117bf6f0c0f64c
Hans Pagel <mail@hanspagel.com>
2025-02-25 16:06:09 +0100

    feat: use html-rendering lib for hono

---
+++ integrations/hono/src/types.ts
@@ -0,0 +1,6 @@
+import type { HtmlRenderingConfiguration } from '@scalar/api-reference/lib/html-rendering'
+
+/**
+ * The configuration for the Scalar API Reference for Express
+ */
+export type ApiReferenceConfiguration = HtmlRenderingConfiguration
