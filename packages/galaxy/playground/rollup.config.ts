6fd39f4c05796f031a70b70980134408b284aeb3
Hans Pagel <mail@hanspagel.com>
2025-02-25 17:13:09 +0100

    fix: cyclic dependency between packages (galaxy uses hono-api-reference, which uses galaxy)

--- packages/galaxy/playground/rollup.config.ts
+++ packages/galaxy/playground/rollup.config.ts
@@ -1,6 +1,6 @@
 import { createRollupConfig } from '@scalar/build-tooling'
 
-const input = ['src/index.ts']
+const input = ['./src/index.ts']
 
 // Do not use the findEntryPoints helper - it will overwrite the static file outputs
 export default createRollupConfig({
