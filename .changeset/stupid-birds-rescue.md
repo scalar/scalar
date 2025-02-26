0d58572e9e97904f41b08e9967b30b2da6967444
Marc Laventure <marc@scalar.com>
2025-02-26 02:56:03 -0800

    fix(snippetz): doesn’t parse text for JSON requests (#4896)
    * fix: handle case where mimeType application/json but only text is provided to body
    
    * docs(changeset): fix: handle case where mimeType application/json but only text is provided to body
    
    * test: convertWithHttpSnippetLite
    
    * chore: comments
    
    ---------
    
    Co-authored-by: Hans Pagel <mail@hanspagel.com>

---
+++ .changeset/stupid-birds-rescue.md
@@ -0,0 +1,5 @@
+---
+'@scalar/snippetz': patch
+---
+
+fix: handle case where mimeType application/json but only text is provided to body
--- packages/snippetz/src/utils/convertWithHttpSnippetLite.ts
+++ packages/snippetz/src/utils/convertWithHttpSnippetLite.ts
@@ -3,6 +3,8 @@ import type { HarRequest } from '@/types'
 
 /**
  * Takes a httpsnippet-lite client and converts the given request to a code example with it.
+ *
+ * @deprecated This a temporary wrapper around httpsnippet-lite. Let’s write all the generators ourselves instead.
  */
 export function convertWithHttpSnippetLite(
   // Couldn’t find the proper type, there was always a mismatch.
@@ -113,6 +115,15 @@ export function convertWithHttpSnippetLite(
     queryObj: queryObj ?? {},
     cookiesObj: cookiesObj ?? {},
   } as Request
+
+  // If the request is a JSON request, parse the text as JSON
+  if (convertRequest.postData?.mimeType === 'application/json' && convertRequest.postData?.text) {
+    try {
+      convertRequest.postData.jsonObj = JSON.parse(convertRequest.postData.text)
+    } catch (error) {
+      console.error('Error parsing JSON:', error)
+    }
+  }
 
   if (typeof client.convert === 'function') {
     return client.convert(convertRequest)
