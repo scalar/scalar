2c6ae93d3c47578b1ab32be54c8383dc26a882da
Amrit Kahlon <amrit@scalar.com>
2025-02-27 12:22:01 -0800

    fix(fastify):  location of standalone file (#4919)
    * fix: fastify and hono
    
    * docs(changeset): fix: add reference as dependency with correct pathing

---
+++ .changeset/clean-birds-peel.md
@@ -0,0 +1,6 @@
+---
+'@scalar/fastify-api-reference': patch
+'@scalar/hono-api-reference': patch
+---
+
+fix: add reference as dependency with correct pathing
--- integrations/fastify/vite.config.ts
+++ integrations/fastify/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: '../api-reference/dist/browser/standalone.js',
+          src: '../../packages/api-reference/dist/browser/standalone.js',
           dest: './js',
         },
       ],
