e94dfd3f5148f3a1a82886056a18678ddc3ff88c
Brynn Hawker <brynn@hawker.me>
2025-02-28 23:31:24 -0800

    fix: clean up content type selector

--- packages/api-reference/src/features/Operation/components/ParameterListItem.vue
+++ packages/api-reference/src/features/Operation/components/ParameterListItem.vue
@@ -70,9 +70,6 @@ const shouldShowParameter = computed(() => {
           class="parameter-item-icon"
           :icon="open ? 'ChevronDown' : 'ChevronRight'"
           thickness="1.5" />
-        <ScreenReader>
-          {{ open ? 'Collapse' : 'Expand' }}
-        </ScreenReader>
         <span class="parameter-item-name">
           {{ parameter.name }}
         </span>
