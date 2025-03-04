dbdbeaea16eb15f0c7caa6ea163f805173303acf
Hans Pagel <mail@hanspagel.com>
2025-03-04 15:46:38 +0100

    feat: add new post response scripts section

--- packages/api-client/src/views/Request/RequestSection/RequestSection.vue
+++ packages/api-client/src/views/Request/RequestSection/RequestSection.vue
@@ -18,6 +18,7 @@ import { useLayout } from '@/hooks'
 import { matchesDomain } from '@/libs/send-request/set-request-cookies'
 import { useWorkspace } from '@/store'
 import type { EnvVariable } from '@/store/active-entities'
+import PostResponseScripts from '@/views/Request/RequestSection/PostResponseScripts.vue'
 import RequestBody from '@/views/Request/RequestSection/RequestBody.vue'
 import RequestParams from '@/views/Request/RequestSection/RequestParams.vue'
 import RequestPathParams from '@/views/Request/RequestSection/RequestPathParams.vue'
@@ -173,11 +174,11 @@ const activeWorkspaceCookies = computed(() =>
         :envVariables="envVariables"
         :environment="environment"
         :example="example"
+        :invalidParams="invalidParams"
         :operation="operation"
         paramKey="path"
         title="Variables"
-        :workspace="workspace"
-        :invalidParams="invalidParams" />
+        :workspace="workspace" />
       <RequestParams
         v-show="activeSection === 'All' || activeSection === 'Cookies'"
         :envVariables="envVariables"
@@ -226,7 +227,6 @@ const activeWorkspaceCookies = computed(() =>
       <!-- Spacer -->
       <div class="-my-0.25 flex flex-grow" />
 
-      <!-- Code Snippet -->
       <ScalarErrorBoundary>
         <RequestCodeExample
           :collection="collection"
@@ -234,6 +234,9 @@ const activeWorkspaceCookies = computed(() =>
           :operation="operation"
           :server="server"
           :workspace="workspace" />
+      </ScalarErrorBoundary>
+      <ScalarErrorBoundary>
+        <PostResponseScripts :operation="operation" />
       </ScalarErrorBoundary>
     </div>
   </ViewLayoutSection>
