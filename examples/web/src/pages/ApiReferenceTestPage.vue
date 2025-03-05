cc2803b03e278e42440b4f6885dd93f519f72566
Amrit <amrit@scalar.com>
2025-03-05 15:58:50 -0800

    fix: examples

--- examples/web/src/pages/ApiReferenceTestPage.vue
+++ examples/web/src/pages/ApiReferenceTestPage.vue
@@ -1,23 +1,22 @@
 <script setup lang="ts">
-import {
-  ApiReference,
-  type ReferenceConfiguration,
-  type ReferenceLayoutType,
-} from '@scalar/api-reference'
+import { ApiReference, type ReferenceLayoutType } from '@scalar/api-reference'
 import content from '@scalar/galaxy/latest.yaml?raw'
 import type { ThemeId } from '@scalar/themes'
+import { apiReferenceConfigurationSchema } from '@scalar/types/api-reference'
 import { reactive } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
 
-const configuration = reactive<ReferenceConfiguration>({
-  theme: (`${route.params['theme']}` as ThemeId) || 'default',
-  isEditable: false,
-  layout: `${route.params['layout']}` as ReferenceLayoutType,
-  darkMode: !!route.query['darkMode'],
-  spec: { content },
-})
+const configuration = reactive(
+  apiReferenceConfigurationSchema.parse({
+    theme: `${route.params['theme']}`,
+    isEditable: false,
+    layout: `${route.params['layout']}`,
+    darkMode: !!route.query['darkMode'],
+    spec: { content },
+  }),
+)
 </script>
 <template>
   <ApiReference :configuration="configuration" />
