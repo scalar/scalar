f9df22c8811ce67ab6c545ae2f8216a413cb95ae
Hans Pagel <mail@hanspagel.com>
2025-03-05 11:40:46 +0100

    fix: types package not available in the Docker images

--- packages/core/test/few-dependencies.test.ts
+++ packages/core/test/few-dependencies.test.ts
@@ -2,11 +2,12 @@ import { readFileSync } from 'node:fs'
 import path from 'node:path'
 import { describe, expect, it } from 'vitest'
 
-describe('no-dependencies', () => {
+describe('few-dependencies', () => {
   it('doesn’t have any production dependencies', () => {
     const packageJson = readFileSync(path.join(__dirname, '..', 'package.json'), 'utf-8')
     const dependencies = JSON.parse(packageJson).dependencies
 
-    expect(dependencies).toBeUndefined()
+    expect(dependencies).toBeDefined()
+    expect(Object.keys(dependencies)).toStrictEqual(['@scalar/types'])
   })
 })
