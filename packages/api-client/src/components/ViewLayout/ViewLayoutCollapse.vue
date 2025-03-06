ffb8a38a36fcd13ef144c93e2536e1ab3a515c55
anthony lio <anthony@scalar.com>
2025-03-06 18:19:04 +0200

    feat(api-client): adds suffix slot to layout collapse

--- packages/api-client/src/components/ViewLayout/ViewLayoutCollapse.vue
+++ packages/api-client/src/components/ViewLayout/ViewLayoutCollapse.vue
@@ -19,13 +19,13 @@ const id = useId()
   <Disclosure
     v-slot="{ open }"
     as="div"
-    class="group/collapse focus-within:text-c-1 text-c-2 request-item"
+    class="group/collapse focus-within:text-c-1 text-c-2 request-item border-b"
     :defaultOpen="defaultOpen"
     :static="layout === 'reference' ? true : undefined">
     <section
       :aria-labelledby="id"
       class="contents">
-      <div class="bg-b-2 flex items-center">
+      <div class="bg-b-2 flex max-h-8 items-center">
         <DisclosureButton
           :class="[
             'hover:text-c-1 group box-content flex max-h-8 flex-1 items-center gap-2.5 overflow-hidden px-1 py-1.5 text-sm font-medium outline-none md:px-1.5 xl:pl-2 xl:pr-0.5',
