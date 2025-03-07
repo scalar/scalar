46ea0362a671cd1184156564adcf91eda55ca19d
Amrit Kahlon <amrit@scalar.com>
2025-03-06 19:19:08 -0800

    feat(api-reference): add version logged to the console (#5006)
    * chore: add version log to console
    
    * docs(changeset): chore: added standalone reference version log to the console
    
    * chore: cleanup space

--- packages/api-reference/src/standalone.ts
+++ packages/api-reference/src/standalone.ts
@@ -5,4 +5,9 @@
  */
 import { getConfigurationFromDataAttributes, mountScalarApiReference } from '@/standalone/lib/html-api'
 
+// Log the version of the API Reference
+if (process.env.SCALAR_API_REFERENCE_VERSION) {
+  console.info('Scalar API Reference version:', process.env.SCALAR_API_REFERENCE_VERSION)
+}
+
 mountScalarApiReference(document, getConfigurationFromDataAttributes(document))
