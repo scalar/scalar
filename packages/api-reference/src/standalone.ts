082faafc418a2b67f330925b78e2d58b60dbc2d5
Hans Pagel <mail@hanspagel.com>
2025-03-07 11:29:21 +0100

    feat: ditch data-attributes, offer a new API

--- packages/api-reference/src/standalone.ts
+++ packages/api-reference/src/standalone.ts
@@ -1,13 +1,26 @@
 /**
  * This file is the entry point for the CDN version of the API Reference.
  *
- * It’s responsible for finding the spec and configuration in the HTML, and mounting the Vue app.
+ * It’s responsible for finding the configuration and the OpenAPI document in the HTML, and mounting the Vue app.
  */
-import { getConfigurationFromDataAttributes, mountScalarApiReference } from '@/standalone/lib/html-api'
+import { registerScalar } from '@/standalone/lib/global-scalar'
+import { createApiReference, findDataAttributes, getConfigurationFromDataAttributes } from '@/standalone/lib/html-api'
 
 // Log the version of the API Reference
 if (process.env.SCALAR_API_REFERENCE_VERSION) {
-  console.info('Scalar API Reference version:', process.env.SCALAR_API_REFERENCE_VERSION)
+  console.info(`@scalar/api-reference@${process.env.SCALAR_API_REFERENCE_VERSION}`)
 }
 
-mountScalarApiReference(document, getConfigurationFromDataAttributes(document))
+// Register the createApiReference function in the global Scalar object (new)
+declare global {
+  interface Window {
+    Scalar: {
+      createApiReference: typeof createApiReference
+    }
+  }
+}
+
+registerScalar(createApiReference)
+
+// Look for data attributes in the HTML (legacy)
+findDataAttributes(document, getConfigurationFromDataAttributes(document))
