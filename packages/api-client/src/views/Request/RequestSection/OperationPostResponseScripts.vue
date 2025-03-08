b82b4d2afde1d9431e1506364efe04e36c93c6a4
anthony lio <anthony@scalar.com>
2025-03-08 11:57:00 +0200

    fix(api-client): removes unused operation post response scripts elements

--- packages/api-client/src/views/Request/RequestSection/OperationPostResponseScripts.vue
+++ packages/api-client/src/views/Request/RequestSection/OperationPostResponseScripts.vue
@@ -1,29 +1,13 @@
 <script setup lang="ts">
 import type { Operation } from '@scalar/oas-utils/entities/spec'
-import { computed } from 'vue'
 
 import ViewLayoutCollapse from '@/components/ViewLayout/ViewLayoutCollapse.vue'
-import { useWorkspace } from '@/store'
-import { useActiveEntities } from '@/store/active-entities'
 
-import PostResponseScripts from './components/PostResponseScripts.vue'
-import type { PostResponseScript } from './types/post-response'
+import PostResponseScripts from './PostResponseScripts.vue'
 
 const { operation } = defineProps<{
   operation: Operation
 }>()
-
-const { requestMutators } = useWorkspace()
-const { activeEnvironment, activeEnvVariables, activeWorkspace } =
-  useActiveEntities()
-
-const scripts = computed(
-  () => (operation['x-post-response'] || []) as PostResponseScript[],
-)
-
-const updateScripts = (newScripts: PostResponseScript[] | undefined) => {
-  requestMutators.edit(operation.uid, 'x-post-response', newScripts || [])
-}
 </script>
 
 <template>
@@ -31,12 +15,6 @@ const updateScripts = (newScripts: PostResponseScript[] | undefined) => {
     class="group/preview w-full border-b-0"
     :defaultOpen="false">
     <template #title>Post Response Scripts</template>
-    <PostResponseScripts
-      v-if="activeEnvironment && activeWorkspace"
-      :envVariables="activeEnvVariables"
-      :environment="activeEnvironment"
-      :onUpdate="updateScripts"
-      :scripts="scripts"
-      :workspace="activeWorkspace" />
+    <PostResponseScripts :operation="operation" />
   </ViewLayoutCollapse>
 </template>
