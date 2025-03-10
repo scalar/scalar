dd38a686cf67e96d6cc8a8477a04fe67f1288ca7
Hans Pagel <hanspagel@users.noreply.github.com>
2025-03-10 18:13:29 +0100

    feat(nextjs): use HTML rendering lib from `@scalar/core` (#5042)
    * feat: use html rendering lib for NextJS
    
    * docs(changeset): feat: use html rendering lib from @scalar/core
    
    * chore: make it a minor update
    
    * fix: expects full api reference configuration in nextjs integration
    
    * chore: clean up
    
    * fix: TS error
    
    * Update integrations/nextjs/src/types.ts
    
    ---------
    
    Co-authored-by: Amrit Kahlon <amrit@scalar.com>

---
+++ .changeset/young-ladybugs-taste.md
@@ -0,0 +1,5 @@
+---
+'@scalar/nextjs-api-reference': minor
+---
+
+feat: use html rendering lib from @scalar/core
--- integrations/nextjs/src/ApiReference.ts
+++ integrations/nextjs/src/ApiReference.ts
@@ -1,9 +1,13 @@
-import type { ReferenceConfiguration } from '@scalar/types/legacy'
+import { getHtmlDocument } from '@scalar/core/libs/html-rendering'
 
-import { nextjsThemeCss } from './theme'
+import { customTheme } from './custom-theme'
+import type { ApiReferenceConfiguration } from './types'
 
-export type ApiReferenceOptions = ReferenceConfiguration & {
-  cdn?: string
+/**
+ * The default configuration for the API Reference.
+ */
+const DEFAULT_CONFIGURATION: Partial<ApiReferenceConfiguration> = {
+  _integration: 'nextjs',
 }
 
 /**
@@ -14,79 +18,19 @@ export type ApiReferenceOptions = ReferenceConfiguration & {
  * @params config - the Api Reference config object
  * @params options - reserved for future use to add customization to the response
  */
-export const ApiReference = (config: ApiReferenceOptions) => {
-  // If no spec is passed, show a warning.
-  if (!config.spec?.content && !config.spec?.url) {
-    throw new Error(
-      '[@scalar/nextjs-api-reference] You didn’t provide a spec.content or spec.url. Please provide one of these options.',
-    )
-  }
-
-  // Execute content function if it exists
-  if (typeof config.spec?.content === 'function') {
-    config.spec.content = config.spec.content()
-  }
-
-  // Convert the document to a string
-  const documentString = config?.spec?.content
-    ? typeof config?.spec?.content === 'string'
-      ? config.spec.content
-      : JSON.stringify(config.spec.content)
-    : ''
-
-  // Delete content from configuration
-  if (config?.spec?.content) {
-    delete config.spec.content
-  }
-
-  // Add the default CSS
-  if (!config?.customCss && !config?.theme) {
-    config.customCss = nextjsThemeCss
-  }
-
-  // Check the config for a custom CDN string or set to default
-  const cdnString = config?.cdn ? config.cdn : 'https://cdn.jsdelivr.net/npm/@scalar/api-reference@latest'
-
-  // Add _integration: 'nextjs' to the configuration, but allow user to overwrite.
-  const defaultConfiguration = {
-    _integration: 'nextjs',
-  }
-
-  // Convert the configuration to a string
-  const configString = JSON.stringify({
-    ...defaultConfiguration,
-    ...(config ?? {}),
-  })
-    .split('"')
-    .join('&quot;')
+export const ApiReference = (givenConfiguration: Partial<ApiReferenceConfiguration>) => {
+  // Merge the defaults
+  const configuration = {
+    ...DEFAULT_CONFIGURATION,
+    ...givenConfiguration,
+  } satisfies Partial<ApiReferenceConfiguration>
 
   return async () => {
-    return new Response(
-      `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Scalar API Reference</title>
-  </head>
-  <body>
-    <script
-      id="api-reference"
-      type="application/json"
-      data-configuration="${configString}">
-      ${documentString}
-    </script>
-    <script src="${cdnString}"></script>
-  </body>
-</html>
-      `,
-      {
-        status: 200,
-        headers: {
-          'Content-Type': 'text/html',
-        },
+    return new Response(getHtmlDocument(configuration, customTheme), {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/html',
       },
-    )
+    })
   }
 }
