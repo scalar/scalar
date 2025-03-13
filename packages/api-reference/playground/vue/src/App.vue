ae413af0c77f2ac94b92a8abc7b7331d42a866e8
Hans Pagel <hanspagel@users.noreply.github.com>
2025-03-13 11:25:55 +0100

    feat(api-reference): debug bar (#5080)
    * feat: new Vue example
    
    * fix(api-reference): sets scroll to debug bar
    
    * feat: switch between layouts
    
    * chore: remove console.log
    
    ---------
    
    Co-authored-by: anthony lio <anthony@scalar.com>

---
+++ packages/api-reference/playground/vue/src/App.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { createApiReference } from '../../../src/index'
+
+import '@scalar/api-reference/style.css'
+
+import type { ApiReferenceConfigurationWithSources } from '@scalar/types/api-reference'
+import { onMounted, reactive, ref } from 'vue'
+
+import DebugBar from './components/DebugBar.vue'
+
+const containerRef = ref<HTMLDivElement>()
+
+const configuration = reactive({
+  layout: 'modern' as const,
+  theme: 'default' as const,
+  spec: {
+    sources: [
+      {
+        title: 'Scalar Galaxy',
+        url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
+      },
+    ],
+  },
+  showSidebar: true,
+  withDefaultFonts: true,
+})
+
+let app: ReturnType<typeof createApiReference> | null = null
+
+onMounted(async () => {
+  app = createApiReference(containerRef.value, configuration)
+})
+
+const updateConfiguration = (
+  newConfiguration: Partial<ApiReferenceConfigurationWithSources>,
+) => {
+  Object.assign(configuration, newConfiguration)
+  app?.updateConfiguration(newConfiguration)
+}
+</script>
+
+<template>
+  <div class="flex h-dvh min-h-dvh overflow-hidden">
+    <!-- Main content -->
+    <div class="flex-1 overflow-y-auto">
+      <div ref="containerRef"></div>
+    </div>
+    <!-- Sidebar -->
+    <div class="w-96 overflow-y-auto border-l border-stone-800 bg-stone-900">
+      <DebugBar
+        v-model="configuration"
+        @update:modelValue="updateConfiguration" />
+    </div>
+  </div>
+</template>
