812c1f1ed6c3254b8b345736ecc8c24bb8206b0d
Amrit <amrit@scalar.com>
2025-03-15 14:32:15 -0700

    chore: export types in react package

--- packages/api-reference-react/src/index.ts
+++ packages/api-reference-react/src/index.ts
@@ -1,2 +1,9 @@
 export * from './ApiReferenceReact'
+
+/** Re-export types for ease of use */
+export type {
+  ApiReferenceConfiguration,
+  ApiReferenceConfigurationWithSources,
+  MultiReferenceConfiguration,
+} from '@scalar/types/api-reference'
 export type { ReferenceProps } from '@scalar/api-reference'
