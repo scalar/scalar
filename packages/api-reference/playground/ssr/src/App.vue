bf6c04c71a1fed8f33de449f115df6852772ad92
Hans Pagel <mail@hanspagel.com>
2025-03-17 12:52:08 +0100

    feat: add new Vue SSR playground

---
+++ packages/api-reference/playground/ssr/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ApiReference } from '@scalar/api-reference'
+</script>
+
+<template>
+  <ApiReference
+    :configuration="{
+      url: 'https://petstore.swagger.io/v2/swagger.json',
+    }" />
+</template>
---
+++ packages/api-reference/playground/ssr/src/entry-client.ts
@@ -0,0 +1,6 @@
+import './style.css'
+import { createApp } from './main'
+
+const { app } = createApp()
+
+app.mount('#app')
