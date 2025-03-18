632b01fe7236ecb48474790f1d39216f3467c839
Hans Pagel <mail@hanspagel.com>
2025-03-18 17:17:59 +0100

    feat: add alternative App component that uses the HTML/JS API

---
+++ packages/api-reference/playground/ssr/src/AppWithJsApi.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+/**
+ * TODO: This works, but doesn’t render anything on the server. Sooo we have some work ahead of us.
+ */
+import { onMounted, ref } from 'vue'
+
+import { createApiReference } from '../../../src/standalone/lib/html-api'
+
+const containerRef = ref<HTMLDivElement>()
+
+onMounted(() => {
+  if (!containerRef.value) {
+    return
+  }
+
+  createApiReference(containerRef.value, {
+    url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
+  })
+})
+</script>
+
+<template>
+  <div ref="containerRef"></div>
+</template>
