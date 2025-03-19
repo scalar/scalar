427c32588f53b9b70bd2ead1f5d8c6b8b2675329
Brynn <brynn@hawker.me>
2025-03-18 23:56:38 -0700

    fix(components): add missing forms exports (#5179)
    * fix(components): add missing forms exports
    
    * docs(changeset): fix(components): add missing forms exports

--- packages/components/src/components/ScalarForm/index.ts
+++ packages/components/src/components/ScalarForm/index.ts
@@ -1,1 +1,3 @@
 export { default as ScalarForm } from './ScalarForm.vue'
+export { default as ScalarFormField } from './ScalarFormField.vue'
+export { default as ScalarFormSection } from './ScalarFormSection.vue'
