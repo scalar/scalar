adaa0957966968621b505aff8a1eeeb2d3004b16
Hans Pagel <mail@hanspagel.com>
2025-03-25 16:35:54 +0100

    feat: reference object and more tests

---
+++ packages/openapi-types/src/schemas/3.1/compliance/reference-object.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest'
+
+import { ReferenceObjectSchema } from '../reference-object'
+
+describe('reference-object', () => {
+  describe('ReferenceObjectSchema', () => {
+    // https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.1.md#reference-object-example
+    it('Reference Object Example', () => {
+      const result = ReferenceObjectSchema.parse({
+        $ref: '#/components/schemas/Pet',
+      })
+
+      expect(result).toEqual({
+        $ref: '#/components/schemas/Pet',
+      })
+    })
+  })
+})
