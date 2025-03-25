48bb207a93047c93522567a96d1af66d6e2690c2
anthony lio <anthony@scalar.com>
2025-03-25 13:22:50 +0200

    style(api-client): favors function export over export all in modal

--- packages/api-client/src/layouts/Modal/index.ts
+++ packages/api-client/src/layouts/Modal/index.ts
@@ -1,2 +1,2 @@
 export { default as ApiClientModal } from './ApiClientModal.vue'
-export * from './create-api-client-modal'
+export { createApiClientModal, createApiClientModalSync } from './create-api-client-modal'
