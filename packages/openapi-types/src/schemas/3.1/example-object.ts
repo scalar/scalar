8c3facca91ab7fcb84d7318c4c3770c43c85f35f
Hans Pagel <mail@hanspagel.com>
2025-03-26 12:36:12 +0100

    fix: value in example is required

--- packages/openapi-types/src/schemas/3.1/example-object.ts
+++ packages/openapi-types/src/schemas/3.1/example-object.ts
@@ -19,7 +19,7 @@ export const ExampleObjectSchema = z.object({
   /**
    * Embedded literal example. The value field and externalValue field are mutually exclusive. To represent examples of media types that cannot naturally represented in JSON or YAML, use a string value to contain the example, escaping where necessary.
    */
-  value: z.any(),
+  value: z.any().optional(),
   /**
    * A URI that identifies the literal example. This provides the capability to reference examples that cannot easily be
    * included in JSON or YAML documents. The value field and externalValue field are mutually exclusive. See the rules
