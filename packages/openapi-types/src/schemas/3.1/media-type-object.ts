0b44944b6b4e73649955ec5937a52b0da69a1d80
Hans Pagel <mail@hanspagel.com>
2025-03-26 12:36:28 +0100

    feat: add reference object to media type object

--- packages/openapi-types/src/schemas/3.1/media-type-object.ts
+++ packages/openapi-types/src/schemas/3.1/media-type-object.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 import { EncodingObjectSchema } from './encoding-object'
 import { ExampleObjectSchema } from './example-object'
+import { ReferenceObjectSchema } from './reference-object'
 import { SchemaObjectSchema } from './schema-object'
 
 /**
@@ -26,7 +27,7 @@ export const MediaTypeObjectSchema = z.object({
    * The examples field is mutually exclusive of the example field. Furthermore, if referencing a schema which contains
    * an example, the examples value SHALL override the example provided by the schema.
    */
-  examples: z.record(z.string(), ExampleObjectSchema).optional(),
+  examples: z.record(z.string(), z.union([ReferenceObjectSchema, ExampleObjectSchema])).optional(),
   /**
    * A map between a property name and its encoding information. The key, being the property name, MUST exist in the
    * schema as a property. The encoding object SHALL only apply to requestBody objects when the media type is
