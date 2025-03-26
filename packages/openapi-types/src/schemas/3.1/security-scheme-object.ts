15cc0114f2e675e0de932b492a9c4ad1216ba594
Hans Pagel <mail@hanspagel.com>
2025-03-26 12:17:41 +0100

    chore: add comment

--- packages/openapi-types/src/schemas/3.1/security-scheme-object.ts
+++ packages/openapi-types/src/schemas/3.1/security-scheme-object.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-// TODO: Implement
+// TODO: Implement after https://github.com/scalar/scalar/pull/5230 is merged
 // export const SecuritySchemeObjectSchema = z.object({
 //   type: z.string(),
 //   description: z.string().optional(),
