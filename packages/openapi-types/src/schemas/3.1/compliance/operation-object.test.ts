846376c1ff6fabe5d7e645bf1fff77f533ad16e2
Hans Pagel <mail@hanspagel.com>
2025-03-27 16:54:55 +0100

    test: more operation compliance tests

--- packages/openapi-types/src/schemas/3.1/compliance/operation-object.test.ts
+++ packages/openapi-types/src/schemas/3.1/compliance/operation-object.test.ts
@@ -1,12 +1,7 @@
 import { describe, expect, it } from 'vitest'
-
 import { OperationObjectSchema } from '../unprocessed/operation-object'
 
 describe('operation-object', () => {
-  // TODO: Add 'Considerations for File Uploads': https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.1.md#considerations-for-file-uploads
-  // TODO: Add this: https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.1.md#example-url-encoded-form-with-json-values
-  // TODO: Add this: https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.1.md#example-url-encoded-form-with-binary-values
-  // TODO: Add this: https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.1.md#example-basic-multipart-form
   // TODO: Add this: https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.1.md#example-multipart-form-with-encoding-objects
   // TODO: Add this: https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.1.md#example-multipart-form-with-multiple-files
   describe('OperationObjectSchema', () => {
@@ -125,6 +120,236 @@ describe('operation-object', () => {
             petstore_auth: ['write:pets', 'read:pets'],
           },
         ],
+      })
+    })
+
+    describe('Considerations for File Uploads', () => {
+      it('PNG image as a binary file', () => {
+        const result = OperationObjectSchema.parse({
+          requestBody: {
+            content: {
+              'image/png': {},
+            },
+          },
+        })
+
+        expect(result).toEqual({
+          requestBody: {
+            content: {
+              'image/png': {},
+            },
+          },
+        })
+      })
+
+      it('arbitrary binary file', () => {
+        const result = OperationObjectSchema.parse({
+          requestBody: {
+            content: {
+              'application/octet-stream': {},
+            },
+          },
+        })
+
+        expect(result).toEqual({
+          requestBody: {
+            content: {
+              'application/octet-stream': {},
+            },
+          },
+        })
+      })
+
+      it('arbitrary JSON without constraints beyond being syntactically valid', () => {
+        const result = OperationObjectSchema.parse({
+          requestBody: {
+            content: {
+              'application/json': {},
+            },
+          },
+        })
+
+        expect(result).toEqual({
+          requestBody: {
+            content: {
+              'application/json': {},
+            },
+          },
+        })
+      })
+    })
+
+    describe('Encoding the x-www-form-urlencoded Media Type', () => {
+      it(`When there is no encoding field, the serialization strategy is based on the Encoding Object's default values`, () => {
+        const result = OperationObjectSchema.parse({
+          requestBody: {
+            content: {
+              'application/x-www-form-urlencoded': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    id: {
+                      type: 'string',
+                      format: 'uuid',
+                    },
+                    address: {
+                      // complex types are stringified to support RFC 1866
+                      type: 'object',
+                      properties: {},
+                    },
+                  },
+                },
+              },
+            },
+          },
+        })
+
+        expect(result).toEqual({
+          requestBody: {
+            content: {
+              'application/x-www-form-urlencoded': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    id: {
+                      type: 'string',
+                      format: 'uuid',
+                    },
+                    address: {
+                      // complex types are stringified to support RFC 1866
+                      type: 'object',
+                      properties: {},
+                    },
+                  },
+                },
+              },
+            },
+          },
+        })
+      })
+
+      it('Note that application/x-www-form-urlencoded is a text format, which requires base64-encoding any binary data', () => {
+        const result = OperationObjectSchema.parse({
+          requestBody: {
+            content: {
+              'application/x-www-form-urlencoded': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    name: {
+                      type: 'string',
+                    },
+                    icon: {
+                      // The default with "contentEncoding" is application/octet-stream,
+                      // so we need to set image media type(s) in the Encoding Object.
+                      type: 'string',
+                      contentEncoding: 'base64url',
+                    },
+                  },
+                },
+              },
+            },
+            encoding: {
+              icon: {
+                contentType: 'image/png, image/jpeg',
+              },
+            },
+          },
+        })
+
+        expect(result).toEqual({
+          requestBody: {
+            content: {
+              'application/x-www-form-urlencoded': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    name: {
+                      type: 'string',
+                    },
+                    icon: {
+                      // The default with "contentEncoding" is application/octet-stream,
+                      // so we need to set image media type(s) in the Encoding Object.
+                      type: 'string',
+                      contentEncoding: 'base64url',
+                    },
+                  },
+                },
+              },
+            },
+            encoding: {
+              icon: {
+                contentType: 'image/png, image/jpeg',
+              },
+            },
+          },
+        })
+      })
+
+      it('Example: Basic Multipart Form', () => {
+        const result = OperationObjectSchema.parse({
+          requestBody: {
+            content: {
+              'multipart/form-data': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    id: {
+                      // default for primitives without a special format is text/plain
+                      type: 'string',
+                      format: 'uuid',
+                    },
+                    profileImage: {
+                      // default for string with binary format is `application/octet-stream`
+                      type: 'string',
+                      format: 'binary',
+                    },
+                    addresses: {
+                      // default for arrays is based on the type in the `items`
+                      // subschema, which is an object, so `application/json`
+                      type: 'array',
+                      items: {
+                        $ref: '#/components/schemas/Address',
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        })
+
+        expect(result).toEqual({
+          requestBody: {
+            content: {
+              'multipart/form-data': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    id: {
+                      // default for primitives without a special format is text/plain
+                      type: 'string',
+                      format: 'uuid',
+                    },
+                    profileImage: {
+                      // default for string with binary format is `application/octet-stream`
+                      type: 'string',
+                      format: 'binary',
+                    },
+                    addresses: {
+                      // default for arrays is based on the type in the `items`
+                      // subschema, which is an object, so `application/json`
+                      type: 'array',
+                      items: {
+                        $ref: '#/components/schemas/Address',
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        })
       })
     })
   })
--- packages/openapi-types/src/schemas/3.1/processed/request-body-object.ts
+++ packages/openapi-types/src/schemas/3.1/processed/request-body-object.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod'
+import { EncodingObjectSchema } from './encoding-object'
 import { MediaTypeObjectSchema } from './media-type-object'
 
 /**
@@ -21,4 +22,10 @@ export const RequestBodyObjectSchema = z.object({
    * Determines if the request body is required in the request. Defaults to false.
    */
   required: z.boolean().optional(),
+  /**
+   * Only mentioned in the example:
+   *
+   * @see https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.1.md#request-body-object
+   */
+  encoding: z.record(z.string(), EncodingObjectSchema).optional(),
 })
