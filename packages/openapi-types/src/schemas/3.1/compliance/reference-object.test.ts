07a62f5b8c9bd374d44138883209055bc23226c0
Hans Pagel <mail@hanspagel.com>
2025-03-27 14:44:36 +0100

    feat: add $refs to components

--- packages/openapi-types/src/schemas/3.1/compliance/reference-object.test.ts
+++ packages/openapi-types/src/schemas/3.1/compliance/reference-object.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest'
 
+import { ComponentsObjectSchema } from '../unprocessed/components-object'
 import { ReferenceObjectSchema } from '../unprocessed/reference-object'
 
 describe('reference-object', () => {
@@ -12,6 +13,96 @@ describe('reference-object', () => {
 
       expect(result).toEqual({
         $ref: '#/components/schemas/Pet',
+      })
+    })
+  })
+
+  describe('ComponentsObjectSchema', () => {
+    it('schemas with $ref', () => {
+      const result = ComponentsObjectSchema.parse({
+        responses: {
+          200: {
+            $ref: '#/components/responses/foobar',
+          },
+        },
+        parameters: {
+          id: {
+            $ref: '#/components/parameters/foobar',
+          },
+        },
+        examples: {
+          foobar: {
+            $ref: '#/components/examples/foobar',
+          },
+        },
+        requestBodies: {
+          foobar: {
+            $ref: '#/components/requestBodies/foobar',
+          },
+        },
+        headers: {
+          foobar: {
+            $ref: '#/components/headers/foobar',
+          },
+        },
+        securitySchemes: {
+          foobar: {
+            $ref: '#/components/securitySchemes/foobar',
+          },
+        },
+        links: {
+          foobar: {
+            $ref: '#/components/links/foobar',
+          },
+        },
+        callbacks: {
+          foobar: {
+            $ref: '#/components/callbacks/foobar',
+          },
+        },
+      })
+
+      expect(result).toEqual({
+        responses: {
+          200: {
+            $ref: '#/components/responses/foobar',
+          },
+        },
+        parameters: {
+          id: {
+            $ref: '#/components/parameters/foobar',
+          },
+        },
+        examples: {
+          foobar: {
+            $ref: '#/components/examples/foobar',
+          },
+        },
+        requestBodies: {
+          foobar: {
+            $ref: '#/components/requestBodies/foobar',
+          },
+        },
+        headers: {
+          foobar: {
+            $ref: '#/components/headers/foobar',
+          },
+        },
+        securitySchemes: {
+          foobar: {
+            $ref: '#/components/securitySchemes/foobar',
+          },
+        },
+        links: {
+          foobar: {
+            $ref: '#/components/links/foobar',
+          },
+        },
+        callbacks: {
+          foobar: {
+            $ref: '#/components/callbacks/foobar',
+          },
+        },
       })
     })
   })
--- packages/openapi-types/src/schemas/3.1/unprocessed/components-object.ts
+++ packages/openapi-types/src/schemas/3.1/unprocessed/components-object.ts
@@ -1,4 +1,16 @@
+import { z } from 'zod'
 import { ComponentsObjectSchema as OriginalComponentsObjectSchema } from '../processed/components-object'
+import { CallbackObjectSchema } from './callback-object'
+import { ExampleObjectSchema } from './example-object'
+import { HeaderObjectSchema } from './header-object'
+import { LinkObjectSchema } from './link-object'
+import { ParameterObjectSchema } from './parameter-object'
+import { PathItemObjectSchema } from './path-item-object'
+import { ReferenceObjectSchema } from './reference-object'
+import { RequestBodyObjectSchema } from './request-body-object'
+import { ResponseObjectSchema } from './response-object'
+import { SchemaObjectSchema } from './schema-object'
+import { SecuritySchemeObjectSchema } from './security-scheme-object'
 
 /**
  * Components Object
@@ -8,4 +20,45 @@ import { ComponentsObjectSchema as OriginalComponentsObjectSchema } from '../pro
  *
  * @see https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.1.md#components-object
  */
-export const ComponentsObjectSchema = OriginalComponentsObjectSchema
+export const ComponentsObjectSchema = OriginalComponentsObjectSchema.extend({
+  /**
+   * An object to hold reusable Schema Objects.
+   */
+  schemas: z.record(z.string(), SchemaObjectSchema).optional(),
+  /**
+   * An object to hold reusable Response Objects.
+   */
+  responses: z.record(z.string(), z.union([ReferenceObjectSchema, ResponseObjectSchema])).optional(),
+  /**
+   * An object to hold reusable Parameter Objects.
+   */
+  parameters: z.record(z.string(), z.union([ReferenceObjectSchema, ParameterObjectSchema])).optional(),
+  /**
+   * An object to hold reusable Example Objects.
+   */
+  examples: z.record(z.string(), z.union([ReferenceObjectSchema, ExampleObjectSchema])).optional(),
+  /**
+   * An object to hold reusable Request Body Objects.
+   */
+  requestBodies: z.record(z.string(), z.union([ReferenceObjectSchema, RequestBodyObjectSchema])).optional(),
+  /**
+   * An object to hold reusable Header Objects.
+   */
+  headers: z.record(z.string(), z.union([ReferenceObjectSchema, HeaderObjectSchema])).optional(),
+  /**
+   * An object to hold reusable Security Scheme Objects.
+   */
+  securitySchemes: z.record(z.string(), SecuritySchemeObjectSchema).optional(),
+  /**
+   * An object to hold reusable Link Objects.
+   */
+  links: z.record(z.string(), z.union([ReferenceObjectSchema, LinkObjectSchema])).optional(),
+  /**
+   * An object to hold reusable Callback Objects.
+   */
+  callbacks: z.record(z.string(), z.union([ReferenceObjectSchema, CallbackObjectSchema])).optional(),
+  /**
+   * An object to hold reusable Path Item Objects.
+   */
+  pathItems: z.record(z.string(), PathItemObjectSchema).optional(),
+})
