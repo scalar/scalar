0ee390d8a84eb1e8ff0b61c5a7e27272018ba7e8
Hans Pagel <mail@hanspagel.com>
2025-03-28 16:39:45 +0100

    fix: tests

--- packages/openapi-types/src/schemas/3.1/processed/security-scheme-object.ts
+++ packages/openapi-types/src/schemas/3.1/processed/security-scheme-object.ts
@@ -90,7 +90,7 @@ export const OAuthFlowObjectSchema = z.object({
    * REQUIRED. The available scopes for the OAuth2 security scheme. A map
    * between the scope name and a short description for it. The map MAY be empty.
    */
-  'scopes': z.record(z.string(), z.string().optional()).optional().default({}).catch({}),
+  scopes: z.record(z.string(), z.string().optional()).optional().default({}).catch({}),
 })
 
 export const ImplicitFlowSchema = OAuthFlowObjectSchema.extend({
