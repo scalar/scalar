27bbd4770406fbbd1cf67d66734795909614e971
Hans Pagel <mail@hanspagel.com>
2025-04-01 14:33:46 +0200

    feat: add aliases for x-codeSamples

--- packages/openapi-types/src/schemas/extensions/x-code-samples.ts
+++ packages/openapi-types/src/schemas/extensions/x-code-samples.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod'
 
+export const XCodeSampleSchema = z.object({
+  lang: z.string().optional().catch(undefined),
+  label: z.string().optional().catch(undefined),
+  source: z.string(),
+})
+
 export const XCodeSamplesSchema = z.object({
-  'x-codeSamples': z
-    .array(
-      z.object({
-        lang: z.string().optional().catch(undefined),
-        label: z.string().optional().catch(undefined),
-        source: z.string(),
-      }),
-    )
-    .optional(),
+  'x-codeSamples': XCodeSampleSchema.array().optional().catch(undefined),
+  'x-code-samples': XCodeSampleSchema.array().optional().catch(undefined),
+  'x-custom-examples': XCodeSampleSchema.array().optional().catch(undefined),
 })
