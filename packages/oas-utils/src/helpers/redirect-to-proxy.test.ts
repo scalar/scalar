2057e98bd72469a0bcfd22b732db52ad0aafae10
Hans Pagel <mail@hanspagel.com>
2025-04-02 10:30:23 +0200

    test: skips proxy when proxyUrl is relative

--- packages/oas-utils/src/helpers/redirect-to-proxy.test.ts
+++ packages/oas-utils/src/helpers/redirect-to-proxy.test.ts
@@ -3,32 +3,36 @@ import { describe, expect, it } from 'vitest'
 import { isRelativePath, redirectToProxy } from './redirect-to-proxy.ts'
 
 describe('redirectToProxy', () => {
-  it('rewrites URLs', async () => {
+  it('rewrites URLs', () => {
     expect(redirectToProxy('https://proxy.scalar.com', 'https://example.com')).toBe(
       'https://proxy.scalar.com/?scalar_url=https%3A%2F%2Fexample.com',
     )
   })
 
-  it('keeps query parameters', async () => {
+  it('keeps query parameters', () => {
     expect(redirectToProxy('https://proxy.scalar.com?foo=bar', 'https://example.com')).toBe(
       'https://proxy.scalar.com/?foo=bar&scalar_url=https%3A%2F%2Fexample.com',
     )
   })
 
-  it('skips the proxy if no proxy url is passed', async () => {
+  it('skips the proxy if no proxy url is passed', () => {
     expect(redirectToProxy('', 'https://example.com')).toBe('https://example.com')
   })
 
-  it('skips the proxy for relative URLs starting with a slash', async () => {
+  it('skips the proxy for relative URLs starting with a slash', () => {
     expect(redirectToProxy('https://proxy.scalar.com', '/api')).toBe('/api')
   })
 
-  it('skips the proxy for relative URLs not starting with a slash', async () => {
+  it('skips the proxy for relative URLs not starting with a slash', () => {
     expect(redirectToProxy('https://proxy.scalar.com', 'api')).toBe('api')
   })
 
-  it('skips the proxy for relative URLs not starting with a slash, but containing a dot', async () => {
+  it('skips the proxy for relative URLs not starting with a slash, but containing a dot', () => {
     expect(redirectToProxy('https://proxy.scalar.com', 'openapi.json')).toBe('openapi.json')
+  })
+
+  it('skips the proxy when the proxyUrl is relative', () => {
+    expect(redirectToProxy('/proxy', 'https://example.com')).toBe('https://example.com')
   })
 })
 
