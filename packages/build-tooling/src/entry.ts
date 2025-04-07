44c3cf09f44ee0ba1de151b14eaecb725184a14c
Justin Lampe <xC0dex@users.noreply.github.com>
2025-04-07 15:14:10 +0200

    normalize paths

--- packages/build-tooling/src/entry.ts
+++ packages/build-tooling/src/entry.ts
@@ -5,6 +5,7 @@
  *
  */
 import fs from 'node:fs/promises'
+import path from 'node:path'
 
 import { glob } from 'glob'
 
@@ -63,8 +64,11 @@ export async function addPackageFileExports({
   const paths = Array.isArray(entries) ? entries : [entries]
 
   paths.forEach((entry) => {
+    console.log(entry)
     // Get the nested path that will be transpiled to dist with preserved modules
-    const segments = entry.split('/').filter((s) => !['.', 'src'].includes(s))
+    // Always use forward slashes for paths in package.json regardless of OS
+    const normalizedEntry = entry.split(path.sep).join('/')
+    const segments = normalizedEntry.split('/').filter((s) => !['.', 'src'].includes(s))
 
     /** Nested folder the entry files lives in for a path scoped export */
     const namespace = segments.slice(0, -1)
