802dc443488f09c85f8a303b80c5aa107db84a5d
Hans Pagel <mail@hanspagel.com>
2025-04-10 23:43:53 +0200

    refactor: explicit exports in the api client store

--- packages/api-client/src/store/index.ts
+++ packages/api-client/src/store/index.ts
@@ -1,4 +1,12 @@
-export * from './store'
+export {
+  type CreateWorkspaceStoreOptions,
+  type UpdateScheme,
+  type WorkspaceStore,
+  createWorkspaceStore,
+  useWorkspace,
+  WORKSPACE_SYMBOL,
+} from './store'
+
 export {
   ACTIVE_ENTITIES_SYMBOL,
   createActiveEntitiesStore,
