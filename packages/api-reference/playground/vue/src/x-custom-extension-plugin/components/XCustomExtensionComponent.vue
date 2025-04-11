a34820d578fb89a20d5ef8b5e544fc2c5a5e6ed1
Hans Pagel <mail@hanspagel.com>
2025-04-11 11:30:31 +0200

    feat: improve styling

--- packages/api-reference/playground/vue/src/x-custom-extension-plugin/components/XCustomExtensionComponent.vue
+++ packages/api-reference/playground/vue/src/x-custom-extension-plugin/components/XCustomExtensionComponent.vue
@@ -18,22 +18,23 @@ defineProps<{
   display: flex;
   font-family: var(--scalar-font-code);
   margin-top: 1rem;
-  border: 1px solid var(--scalar-border-color);
-  border-radius: 0.5rem;
+  border: var(--scalar-border-width) solid var(--scalar-border-color);
+  border-radius: var(--scalar-radius);
   background: repeating-linear-gradient(
     -45deg,
     var(--scalar-background-2) 0,
-    var(--scalar-background-3) 1px,
-    var(--scalar-background-1) 1px,
-    var(--scalar-background-1) 4px
+    var(--scalar-background-2) 1px,
+    transparent 1px,
+    transparent 5px
   );
+  background-size: 100%;
   overflow: hidden;
 }
 
 .x-custom-extension .label {
   background: var(--scalar-background-2);
-  border-right: 1px solid var(--scalar-border-color);
-  color: var(--scalar-color-2);
+  border-right: var(--scalar-border-width) solid var(--scalar-border-color);
+  color: var(--scalar-color-1);
   padding: 0.5rem 1rem;
   overflow: hidden;
 }
