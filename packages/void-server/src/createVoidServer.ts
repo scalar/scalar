e519767e389ce8f8292056c944ec6339684e277c
Hans Pagel <mail@hanspagel.com>
2025-04-15 12:40:54 +0200

    feat: use hono logger

--- packages/void-server/src/createVoidServer.ts
+++ packages/void-server/src/createVoidServer.ts
@@ -1,11 +1,13 @@
 import { type Context, Hono } from 'hono'
 import { accepts } from 'hono/accepts'
 import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
 import type { StatusCode } from 'hono/utils/http-status'
 
 import { errors } from '@/utils/constants.ts'
 import { createHtmlResponse } from '@/utils/createHtmlResponse.ts'
 import { createJsonResponse } from '@/utils/createJsonResponse.ts'
+import { createStreamResponse } from '@/utils/createStreamResponse.ts'
 import { createXmlResponse } from '@/utils/createXmlResponse.ts'
 import { createZipFileResponse } from '@/utils/createZipFileResponse.ts'
 import { getRequestData } from '@/utils/getRequestData.ts'
@@ -16,13 +18,14 @@ import { getRequestData } from '@/utils/getRequestData.ts'
 export async function createVoidServer() {
   const app = new Hono()
 
+  // Logger
+  app.use(logger())
+
   // CORS headers
   app.use(cors())
 
   // HTTP errors
   app.all('/:status{[4-5][0-9][0-9]}', async (c: Context) => {
-    console.info(`${c.req.method} ${c.req.path}`)
-
     const { status: originalStatusCode } = c.req.param()
     const status = Number.parseInt(originalStatusCode ?? '0', 10) as StatusCode
 
@@ -33,15 +36,11 @@ export async function createVoidServer() {
 
   // No content
   app.all('/:status{204}', async (c: Context) => {
-    console.info(`${c.req.method} ${c.req.path}`)
-
     return c.body(null, 204)
   })
 
   // Return content based on the file extension
   app.all('/:filename{.+\\.(html|xml|json|zip)$}', async (c: Context) => {
-    console.info(`${c.req.method} ${c.req.path}`)
-
     const requestData = await getRequestData(c)
 
     const { filename } = c.req.param()
@@ -61,8 +60,6 @@ export async function createVoidServer() {
 
   // All other requests just respond with a JSON containing all the request data
   app.all('/*', async (c: Context) => {
-    console.info(`${c.req.method} ${c.req.path}`)
-
     const requestData = await getRequestData(c)
 
     const acceptedContentType = accepts(c, {
