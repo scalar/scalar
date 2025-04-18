fa633f16ce7eee17673eaf2c63ce2a7e6fed46a8
Brynn Hawker <brynn@hawker.me>
2025-04-18 15:42:07 -0700

    test(icons): add tests for useScalarIcon

---
+++ packages/icons/vitest.config.ts
@@ -0,0 +1,11 @@
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    environment: 'jsdom',
+    root: fileURLToPath(new URL('./', import.meta.url)),
+  },
+})
