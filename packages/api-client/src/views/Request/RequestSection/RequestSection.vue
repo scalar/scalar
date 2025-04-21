c4e268a14df9760fc712412425c167380f42cd0f
anthony lio <anthony@scalar.com>
2025-04-21 21:12:05 +0300

    fix(api-client): removes unused prop from request params request section

--- packages/api-client/src/views/Request/RequestSection/RequestSection.vue
+++ packages/api-client/src/views/Request/RequestSection/RequestSection.vue
@@ -227,8 +227,7 @@ const labelRequestNameId = useId()
         :readOnlyEntries="activeWorkspaceCookies"
         :role="selectedFilter === 'All' ? 'none' : 'tabpanel'"
         title="Cookies"
-        :workspace="workspace"
-        workspaceParamKey="cookies" />
+        :workspace="workspace" />
       <RequestParams
         v-show="selectedFilter === 'All' || selectedFilter === 'Headers'"
         :id="filterIds.Headers"
