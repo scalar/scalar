e8211f0d170813caa05783e3fb389a26da49d77a
anthony lio <anthony@scalar.com>
2025-04-21 21:11:24 +0300

    feat(api-client): updates environment default color to white for better accessibility

--- packages/api-client/src/libs/env-helpers.ts
+++ packages/api-client/src/libs/env-helpers.ts
@@ -1,6 +1,8 @@
 import type { Environment } from '@scalar/oas-utils/entities/environment'
 
-export type EnvVariables = { key: string; value: string; source: string }[]
+export type EnvVariables = { key: string; value: string; source: 'global' | 'collection' }[]
 
 /** Gets the color of an environment with default fallback */
-export const getEnvColor = (environment: Environment) => (environment ? environment.color : '#8E8E8E')
+export function getEnvColor(environment: Environment): string {
+  return environment?.color || '#FFFFFF'
+}
