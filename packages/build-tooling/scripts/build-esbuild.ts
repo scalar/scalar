a86a13ff433ed8957c4ca37d6eff9e17a931fa3c
Justin Lampe <xC0dex@users.noreply.github.com>
2025-04-29 20:05:13 +0200

    fix: Windows build

--- packages/build-tooling/scripts/build-esbuild.ts
+++ packages/build-tooling/scripts/build-esbuild.ts
@@ -1,4 +1,4 @@
 #!/usr/bin/env vite-node
 import { executeCommands } from './utils/utils'
 
-executeCommands(['vite-node esbuild.ts', 'pnpm types:build'], 'esbuild')
+executeCommands(['pnpm vite-node esbuild.ts', 'pnpm types:build'], 'esbuild')
