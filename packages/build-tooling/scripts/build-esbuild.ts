98e84c338f4cee1dbbd211394f6aac9f0f9af9df
Justin Lampe <xC0dex@users.noreply.github.com>
2025-05-12 20:43:50 +0200

    remove pnpm command

--- packages/build-tooling/scripts/build-esbuild.ts
+++ packages/build-tooling/scripts/build-esbuild.ts
@@ -1,4 +1,4 @@
 #!/usr/bin/env vite-node
 import { executeCommands } from './utils/utils'
 
-executeCommands(['pnpm vite-node esbuild.ts', 'pnpm types:build'], 'esbuild')
+executeCommands(['vite-node esbuild.ts', 'pnpm types:build'], 'esbuild')
