2b48efc31120a525e7bb4f4b2531ecdb8589e952
Hans Pagel <mail@hanspagel.com>
2025-05-15 14:39:14 +0200

    feat: actually speed up the upgrade

--- packages/openapi-parser/src/utils/upgrade.ts
+++ packages/openapi-parser/src/utils/upgrade.ts
@@ -1,11 +1,19 @@
 import type { OpenAPIV3_1 } from '@scalar/openapi-types'
 
+import type { UnknownObject } from '@scalar/types/utils'
 import type { AnyObject, Filesystem, UpgradeResult } from '../types/index'
 import { getEntrypoint } from './getEntrypoint'
 import { isFilesystem } from './isFilesystem'
 import { normalize } from './normalize'
 import { upgradeFromThreeToThreeOne } from './upgradeFromThreeToThreeOne'
 import { upgradeFromTwoToThree } from './upgradeFromTwoToThree'
+
+const upgraders = [
+  // Swagger 2.0 -> OpenAPI 3.0
+  upgradeFromTwoToThree,
+  // OpenAPI 3.0 -> OpenAPI 3.1
+  upgradeFromThreeToThreeOne,
+]
 
 /**
  * Upgrade specification to OpenAPI 3.1.0
@@ -17,11 +25,10 @@ export function upgrade(value: string | AnyObject | Filesystem): UpgradeResult<O
       version: '3.1',
     }
   }
-  const upgraders = [upgradeFromTwoToThree, upgradeFromThreeToThreeOne]
 
   // TODO: Run upgrade over the whole filesystem
   const result = upgraders.reduce(
-    (currentSpecification, upgrader) => upgrader(currentSpecification),
+    (currentSpecification, upgrader) => upgrader(currentSpecification as UnknownObject),
     isFilesystem(value) ? getEntrypoint(value as Filesystem).specification : normalize(value),
   ) as OpenAPIV3_1.Document
 
