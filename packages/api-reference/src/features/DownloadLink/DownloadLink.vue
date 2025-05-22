6934b72fa6f1cc8117fc4ee8642f5e423fba8c31
anthony lio <anthony@scalar.com>
2025-05-22 15:22:59 +0300

    feat(api-reference): updates download link component

--- packages/api-reference/src/features/DownloadLink/DownloadLink.vue
+++ packages/api-reference/src/features/DownloadLink/DownloadLink.vue
@@ -2,6 +2,7 @@
 import GitHubSlugger from 'github-slugger'
 import { computed } from 'vue'
 
+import Badge from '@/components/Badge/Badge.vue'
 import { useConfig } from '@/hooks/useConfig'
 import { downloadEventBus } from '@/libs/download'
 
@@ -16,53 +17,117 @@ const slugger = new GitHubSlugger()
 const filename = computed(() => slugger.slug(title ?? ''))
 
 // The id is retrieved at the layout level.
-const handleDownloadClick = () => {
-  downloadEventBus.emit({ id: '', filename: filename.value })
+const handleDownloadClick = (format: 'json' | 'yaml') => {
+  downloadEventBus.emit({ id: '', filename: filename.value, format })
 }
 </script>
 <template>
-  <div class="download">
-    <template v-if="!config?.hideDownloadButton">
-      <!-- Direct URL -->
-      <template v-if="config?.url">
-        <a
-          class="download-button"
-          download
-          :href="config?.url"
-          target="_blank">
-          Download OpenAPI Document
-        </a>
-      </template>
-      <!-- Generate download from document -->
-      <template v-else>
-        <button
-          class="download-button"
-          role="link"
-          type="button"
-          @click="handleDownloadClick">
-          Download OpenAPI Document
-        </button>
-      </template>
-    </template>
+  <div
+    v-if="!config?.hideDownloadButton"
+    class="download-container">
+    <a
+      class="download-button"
+      @click.prevent="handleDownloadClick('json')"
+      variant="ghost">
+      <span> Download OpenAPI Document </span>
+      <Badge class="extension">json</Badge>
+    </a>
+    <a
+      class="download-button"
+      @click.prevent="handleDownloadClick('yaml')"
+      variant="ghost">
+      <span> Download OpenAPI Document </span>
+      <Badge class="extension">yaml</Badge>
+    </a>
   </div>
 </template>
 
 <style scoped>
-.download {
-  margin-bottom: 24px;
+.download-container {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin: 4px 0 24px;
+  position: relative;
+  width: fit-content;
+}
+
+.download-container:hover::before {
+  content: '';
+  width: calc(100% + 76px);
+  height: 90px;
+  position: absolute;
+  top: -11px;
+  left: -12px;
+  border-radius: var(--scalar-radius-lg);
+  box-shadow: var(--scalar-shadow-2);
+  pointer-events: none;
+  background: var(--scalar-background-1);
 }
 
 .download-button {
-  color: var(--scalar-link-color, var(--scalar-color-accent));
-  font-weight: var(--scalar-link-font-weight, inherit);
-  text-decoration: var(--scalar-text-decoration) !important;
-  text-decoration-color: var(--scalar-text-decoration-color);
-  font-size: var(--scalar-paragraph);
+  color: var(--scalar-link-color);
   cursor: pointer;
+  display: flex;
+  gap: 4px;
+  height: fit-content;
+  padding: 0;
+  position: relative;
+  white-space: nowrap;
 }
-.download-button:hover {
-  color: var(--scalar-link-color-hover, var(--scalar-color-accent));
-  text-decoration: var(--scalar-text-decoration-hover) !important;
-  text-decoration-color: var(--scalar-text-decoration-color-hover);
+
+.download-button::before {
+  border-radius: var(--scalar-radius);
+  content: '';
+  height: calc(100% + 16px);
+  left: -9px;
+  position: absolute;
+  top: -8px;
+  width: calc(100% + 70px);
+}
+
+.download-button:hover::before {
+  background: var(--scalar-background-2);
+  border: var(--scalar-border-width) solid var(--scalar-border-color);
+}
+
+.download-button span {
+  align-items: center;
+  color: var(--scalar-link-color);
+  display: flex;
+  font-size: var(--scalar-font-size-2);
+  font-weight: var(--scalar-regular);
+  gap: 6px;
+  line-height: 1.625;
+  z-index: 1;
+}
+
+.download-button:hover span {
+  text-decoration: var(--scalar-text-decoration-hover);
+  text-underline-offset: 2px;
+}
+
+/* Second button displayed when hovering over the download container */
+.download-button:nth-of-type(2) {
+  display: none;
+  position: absolute;
+  top: 42px;
+}
+
+.download-container:hover .download-button:nth-of-type(2) {
+  display: flex;
+}
+
+.extension {
+  left: calc(100% + 8px);
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 1;
+}
+
+.download-container:hover .extension {
+  opacity: 1;
 }
 </style>
