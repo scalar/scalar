f13517a09fcadd2ddb89f0a604a0ba5e39130996
Brynn Hawker <brynn@hawker.me>
2025-05-30 16:47:06 -0700

    fix(api-client): move global styles out of client layout

--- projects/client-scalar-com/src/main.ts
+++ projects/client-scalar-com/src/main.ts
@@ -1,5 +1,6 @@
 import { createApiClientWeb } from '@scalar/api-client/layouts/Web'
 import '@scalar/api-client/style.css'
+import './style.css'
 
 createApiClientWeb(document.getElementById('scalar-client'), {
   proxyUrl: 'https://proxy.scalar.com',
