ba84a3c38d6a32b927d4f319c16ad4dc3d7639aa
Brynn <brynn@hawker.me>
2025-06-03 14:45:39 -0700

    feat(components): add ScalarFormError (#5835)
    * fix(themes): add alert color
    
    * feat(components): add ScalarFormError
    
    * docs(changeset): feat(components): add ScalarFormError
    
    * docs(changeset): fix(themes): add alert color
    
    * fix: add closing tag to story

--- packages/components/src/components/ScalarForm/index.ts
+++ packages/components/src/components/ScalarForm/index.ts
@@ -1,3 +1,4 @@
 export { default as ScalarForm } from './ScalarForm.vue'
 export { default as ScalarFormField } from './ScalarFormField.vue'
 export { default as ScalarFormSection } from './ScalarFormSection.vue'
+export { default as ScalarFormError } from './ScalarFormError.vue'
