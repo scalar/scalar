6577526e188ded05412101cf382ef43936ac1651
DemonHa <hh360284@gmail.com>
2025-06-09 00:07:43 -0400

    doc: update documentation

--- packages/openapi-parser/src/plugins-browser.ts
+++ packages/openapi-parser/src/plugins-browser.ts
@@ -1,1 +1,3 @@
 export { fetchUrls } from './utils/bundle/plugins/fetch-urls'
+export { parseJson } from './utils/bundle/plugins/parse-json'
+export { parseYaml } from './utils/bundle/plugins/parse-yaml'
