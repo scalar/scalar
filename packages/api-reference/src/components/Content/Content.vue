b12cd8ebce259c3b50ed25a949c62aae83729af5
Anthony Lio <anthony@scalar.com>
2025-06-13 19:55:45 +0300

    fix(api-reference): authentication cards style (#5925)
    * fix(api-reference): updates introduction auth table style
    
    * docs(changeset): fix: updates introduction auth table style

--- packages/api-reference/src/components/Content/Content.vue
+++ packages/api-reference/src/components/Content/Content.vue
@@ -204,7 +204,6 @@ const introCardsSlot = computed(() =>
 .introduction-card {
   display: flex;
   flex-direction: column;
-  background: var(--scalar-background-1);
 }
 .introduction-card-item {
   display: flex;
