fce72eb79d4e1a98b270752a619774bf90e4bcfe
Justin Lampe <xC0dex@users.noreply.github.com>
2025-06-15 18:39:46 +0200

    add xml docs

--- integrations/aspire/src/Scalar.Aspire/Extensions/DistributedApplicationBuilderExtensions.cs
+++ integrations/aspire/src/Scalar.Aspire/Extensions/DistributedApplicationBuilderExtensions.cs
@@ -5,16 +5,40 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Scalar.Aspire;
 
+/// <summary>
+/// Provides extension methods for configuring Scalar API reference in Aspire applications.
+/// </summary>
 public static class DistributedApplicationBuilderExtensions
 {
+    /// <summary>
+    /// Adds a Scalar API reference resource to the distributed application with default settings.
+    /// </summary>
+    /// <param name="builder">The distributed application builder.</param>
+    /// <param name="configureOptions">Optional action to configure <see cref="ScalarAspireOptions"/>.</param>
+    /// <returns>A resource builder for the <see cref="ScalarResource"/>.</returns>
     public static IResourceBuilder<ScalarResource> AddScalarApiReference(this IDistributedApplicationBuilder builder, Action<ScalarAspireOptions>? configureOptions = null)
         => builder.AddScalarApiReference(DefaultResourceName, null, configureOptions);
 
+    /// <summary>
+    /// Adds a Scalar API reference resource to the distributed application with a specified name.
+    /// </summary>
+    /// <param name="builder">The distributed application builder.</param>
+    /// <param name="name">The name of the Scalar resource.</param>
+    /// <param name="configureOptions">Optional action to configure <see cref="ScalarAspireOptions"/>.</param>
+    /// <returns>A resource builder for the <see cref="ScalarResource"/>.</returns>
     public static IResourceBuilder<ScalarResource> AddScalarApiReference(
         this IDistributedApplicationBuilder builder,
         [ResourceName] string name,
         Action<ScalarOptions> configureOptions) => builder.AddScalarApiReference(name, null, configureOptions);
 
+    /// <summary>
+    /// Adds a Scalar API reference resource to the distributed application with detailed configuration.
+    /// </summary>
+    /// <param name="builder">The distributed application builder.</param>
+    /// <param name="name">The name of the Scalar resource.</param>
+    /// <param name="port">Optional port number for the Scalar service.</param>
+    /// <param name="configureOptions">Optional action to configure <see cref="ScalarAspireOptions"/>.</param>
+    /// <returns>A resource builder for the <see cref="ScalarResource"/>.</returns>
     public static IResourceBuilder<ScalarResource> AddScalarApiReference(
         this IDistributedApplicationBuilder builder,
         [ResourceName] string name,
--- integrations/aspire/src/Scalar.Aspire/ScalarResource.cs
+++ integrations/aspire/src/Scalar.Aspire/ScalarResource.cs
@@ -2,4 +2,8 @@
 
 namespace Scalar.Aspire;
 
+/// <summary>
+/// Represents a Scalar container resource.
+/// </summary>
+/// <param name="name">The unique name identifier for the Scalar resource.</param>
 public sealed class ScalarResource(string name) : ContainerResource(name);
