ce3ab85740c989d5f53505712364eb87261b54b9
Justin Lampe <xC0dex@users.noreply.github.com>
2025-06-18 21:05:37 +0200

    update ci

--- integrations/aspire/src/Scalar.Aspire/Constants.cs
+++ integrations/aspire/src/Scalar.Aspire/Constants.cs
@@ -4,6 +4,9 @@ internal static class Constants
 {
     public const string Image = "scalarapi/aspire-api-reference";
 
+    /// <remarks>
+    /// This value is automatically updated by the pipeline.
+    /// </remarks>
     public const string ImageTag = "latest";
 
     public const int DefaultPort = 8080;
