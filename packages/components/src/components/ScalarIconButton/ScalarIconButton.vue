d751b6467dd07442112b1d794b4fba28310d22d1
Brynn Hawker <brynn@hawker.me>
2025-06-18 20:04:29 -0700

    refactor(components): clean up ScalarIconButton types

--- packages/components/src/components/ScalarIconButton/ScalarIconButton.vue
+++ packages/components/src/components/ScalarIconButton/ScalarIconButton.vue
@@ -1,50 +1,14 @@
 <script setup lang="ts">
-import type { ScalarIconComponent, ScalarIconWeight } from '@scalar/icons/types'
 import { useBindCx } from '@scalar/use-hooks/useBindCx'
-import { cva } from '@scalar/use-hooks/useBindCx'
-import type { VariantProps } from 'cva'
-import type { Component } from 'vue'
 
-import { styles } from '../ScalarButton'
 import { type Icon, ScalarIconLegacyAdapter } from '../ScalarIcon'
+import type { ScalarIconButtonProps } from './types'
+import { variants } from './variants'
 
-type Variants = VariantProps<typeof variants>
-
-withDefaults(
-  defineProps<{
-    label: string
-    icon: Icon | ScalarIconComponent
-    disabled?: boolean
-    variant?: Variants['variant']
-    size?: Variants['size']
-    thickness?: string
-    weight?: ScalarIconWeight
-  }>(),
-  {
-    variant: 'ghost',
-    size: 'md',
-  },
-)
+const { tooltip } = defineProps<ScalarIconButtonProps>()
 
 defineOptions({ inheritAttrs: false })
 const { cx } = useBindCx()
-
-const variants = cva({
-  base: 'scalar-icon-button grid aspect-square cursor-pointer rounded',
-  variants: {
-    size: {
-      xxs: 'size-3.5 p-0.5',
-      xs: 'size-5 p-1',
-      sm: 'size-6 p-1',
-      md: 'size-10 p-3',
-      full: 'h-full w-full',
-    },
-    disabled: {
-      true: 'cursor-not-allowed shadow-none',
-    },
-    variant: styles,
-  },
-})
 </script>
 <template>
   <button
