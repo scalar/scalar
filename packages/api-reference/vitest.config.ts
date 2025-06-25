66c5e2002e3367d5bf30e743c2ea48df88fe5563
Hans Pagel <mail@hanspagel.com>
2025-06-25 15:32:08 +0200

    ci: generate code coverage report and upload to codecov

--- packages/api-reference/vitest.config.ts
+++ packages/api-reference/vitest.config.ts
@@ -1,14 +1,15 @@
-import { fileURLToPath } from 'node:url'
-import { defineConfig, mergeConfig } from 'vitest/config'
+import { mergeConfig } from 'vitest/config'
 
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      root: fileURLToPath(new URL('./', import.meta.url)),
+export default mergeConfig(viteConfig, {
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      include: ['src/**'],
+      exclude: ['node_modules', 'dist', 'test', '**/*.preview.ts'],
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
     },
-  }),
-)
+  },
+})
