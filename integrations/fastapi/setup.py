d2c11c67382bc54fda36334c889c9cc3f379b5f1
Hans Pagel <mail@hanspagel.com>
2025-07-02 14:28:55 +0200

    feat: read fastapi package version from package.json

--- integrations/fastapi/setup.py
+++ integrations/fastapi/setup.py
@@ -1,8 +1,17 @@
+import json
+import os
 from setuptools import setup, find_packages
+
+# Read version from package.json
+def get_version():
+    package_json_path = os.path.join(os.path.dirname(__file__), 'package.json')
+    with open(package_json_path, 'r') as f:
+        package_data = json.load(f)
+    return package_data['version']
 
 setup(
     name='scalar_fastapi',
-    version='1.2.0',
+    version=get_version(),
     packages=find_packages(),
     install_requires=[],
     author='Marc Laventure',
