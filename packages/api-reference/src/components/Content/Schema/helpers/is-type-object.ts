9a206387d18afea3f0ef2cbd33d81ca3f95d6532
Hans Pagel <mail@hanspagel.com>
2025-07-03 13:35:03 +0200

    fix: non-object types

--- packages/api-reference/src/components/Content/Schema/helpers/is-type-object.ts
+++ packages/api-reference/src/components/Content/Schema/helpers/is-type-object.ts
@@ -4,14 +4,22 @@ import type { OpenAPIV3_1 } from '@scalar/openapi-types'
  * Checks whether a JSON schema is of type object
  */
 export const isTypeObject = (schema: unknown): schema is OpenAPIV3_1.SchemaObject => {
+  // null, primitive types, arrays
   if (schema === null || typeof schema !== 'object' || Array.isArray(schema)) {
     return false
   }
 
-  const hasType = 'type' in schema && schema.type === 'object'
+  const hasType = 'type' in schema
+  const hasTypeObject = hasType && schema.type === 'object'
 
-  if (hasType) {
+  // type: object
+  if (hasTypeObject) {
     return true
+  }
+
+  // type: string, number, boolean, array, etc.
+  if (hasType && !hasTypeObject) {
+    return false
   }
 
   const hasProperties = 'properties' in schema
