21154f66d8ef4080805fa89f78cd57542b1b173e
Hans Pagel <mail@hanspagel.com>
2025-07-04 13:20:37 +0200

    refactor: remove old loops, render operations in new loop

--- packages/api-reference/src/components/Content/Tags/TagSection.vue
+++ packages/api-reference/src/components/Content/Tags/TagSection.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
 import type { Collection } from '@scalar/oas-utils/entities/spec'
-import type { Spec, Tag as TagType } from '@scalar/types/legacy'
+import type { Spec } from '@scalar/types/legacy'
 import { computed, nextTick, ref, useId } from 'vue'
 
 import { SectionContainer } from '@/components/Section'
 import ShowMoreButton from '@/components/ShowMoreButton.vue'
 import { useSidebar } from '@/features/sidebar'
+import type { TraversedTag } from '@/features/traverse-schema'
 import { useNavState } from '@/hooks/useNavState'
 
 import Tag from './Tag.vue'
 
 const props = defineProps<{
   id?: string
-  tag: TagType
+  tag: TraversedTag
   collection: Collection
   spec: Spec
 }>()
@@ -27,14 +28,16 @@ const { getTagId } = useNavState()
 const tagId = computed(() => props.id || getTagId(props.tag) || '')
 
 const moreThanOneTag = computed(
-  () => props.spec.tags?.length && props.spec.tags?.length > 1,
+  () => true,
+  // TODO:
+  // () => props.spec.tags?.length && props.spec.tags?.length > 1,
 )
 
 const moreThanOneDefaultTag = computed(
   () =>
     moreThanOneTag.value ||
-    props.tag?.name !== 'default' ||
-    props.tag?.description !== '',
+    props.tag?.title !== 'default' ||
+    props.tag?.tag.description !== '',
 )
 
 async function focusContents() {
@@ -59,7 +62,7 @@ async function focusContents() {
     <ShowMoreButton
       v-if="!collapsedSidebarItems[tagId] && moreThanOneTag"
       :id="tagId"
-      :aria-label="`Show all ${tag['x-displayName'] ?? tag.name} endpoints`"
+      :aria-label="`Show all ${tag.title} endpoints`"
       @click="focusContents" />
     <div
       v-else
