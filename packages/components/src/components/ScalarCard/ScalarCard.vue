2ca6777f3130f2e8f1548155a30d16c5cbbbf354
Brynn Hawker <brynn@hawker.me>
2025-07-10 21:34:33 -0700

    docs(components): add doc blocks for scalar card components

--- packages/components/src/components/ScalarCard/ScalarCard.vue
+++ packages/components/src/components/ScalarCard/ScalarCard.vue
@@ -1,12 +1,26 @@
+<script lang="ts">
+/**
+ * Scalar card component
+ *
+ * Creates an aria region using the nearest ScalarCardHeader as the label
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Reference/Roles/region_role
+ *
+ * @example
+ * <ScalarCard>
+ *   <ScalarCardHeader>Header</ScalarCardHeader>
+ *   <ScalarCardSection>Content</ScalarCardSection>
+ *   <ScalarCardFooter>Footer</ScalarCardFooter>
+ * </ScalarCard>
+ */
+export default {}
+</script>
 <script setup lang="ts">
 import { useBindCx } from '@scalar/use-hooks/useBindCx'
 import { computed } from 'vue'
 
 import { useCardRegion } from './useCardHeading'
 
-/**
- * Scalar Card - A composable card component with region-based accessibility
- */
 const { label } = defineProps<{
   /** The a11y label for the card region */
   label?: string
