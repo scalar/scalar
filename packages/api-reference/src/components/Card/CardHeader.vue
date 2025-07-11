29aa24521daff9a6b5d09a1dc22389be504a366c
Brynn Hawker <brynn@hawker.me>
2025-07-10 20:29:20 -0700

    feat(api-reference): move request example tabs to request example component

--- packages/api-reference/src/components/Card/CardHeader.vue
+++ packages/api-reference/src/components/Card/CardHeader.vue
@@ -8,7 +8,9 @@ const { cx } = useBindCx()
 </script>
 <template>
   <CardContent
-    v-bind="cx('scalar-card-header leading-[1.35] font-medium py-2.25 px-3')">
+    v-bind="
+      cx('scalar-card-header leading-[1.35] font-medium py-2.25 px-3 shrink-0')
+    ">
     <div class="scalar-card-header-title min-w-0 flex-1 truncate">
       <slot />
     </div>
