1abdf81b6c929ba0c0013d77e11b7fe1f61e4184
Justin Lampe <xC0dex@users.noreply.github.com>
2025-07-16 10:05:33 +0200

    feat(api-client): add x-scalar-credentials-location

---
+++ integrations/aspire/tests/Scalar.Aspire.Tests/CredentialsLocationConverterTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace Scalar.Aspire.Tests;
+
+public class CredentialsLocationConverterTests
+{
+    [Fact]
+    public void Converter_ShouldSerializeToStringFast()
+    {
+        // Arrange
+        var authorizationCodeFlow = new AuthorizationCodeFlow
+        {
+            CredentialsLocation = CredentialsLocation.Header
+        };
+
+        // Act
+        var json = JsonSerializer.Serialize(authorizationCodeFlow);
+
+        // Assert
+        json.Should().Contain("\"header\"");
+    }
+}
