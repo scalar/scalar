8efc457e43554dc8a52ab1784e208ecff4455b7f
Amrit <amrit@scalar.com>
2025-07-17 09:59:41 -0700

    fix: value in request example + dropdown

--- packages/api-reference/src/v2/blocks/scalar-request-example-block/components/ExamplePicker.vue
+++ packages/api-reference/src/v2/blocks/scalar-request-example-block/components/ExamplePicker.vue
@@ -1,34 +1,17 @@
 <script setup lang="ts">
 import {
   ScalarButton,
-  ScalarCombobox,
-  type ScalarComboboxOption,
+  ScalarDropdown,
+  ScalarDropdownItem,
 } from '@scalar/components'
 import { ScalarIconCaretDown } from '@scalar/icons'
 import type { ExampleObject } from '@scalar/workspace-store/schemas/v3.1/strict/example'
-import { computed } from 'vue'
 
 const props = defineProps<{
   examples: Record<string, ExampleObject>
 }>()
 
 const selectedExampleKey = defineModel<string>({ required: true })
-
-/** Generate the options for the combobox */
-const exampleOptions = computed<ScalarComboboxOption[]>(() => {
-  return Object.keys(props.examples).map((key) => ({
-    id: key,
-    label: getLabel(key),
-  }))
-})
-
-/** Get the currently selected example */
-const selectedExample = computed<ScalarComboboxOption>(
-  () =>
-    exampleOptions.value.find(
-      (option) => option.id === selectedExampleKey.value,
-    ) ?? exampleOptions.value[0],
-)
 
 /** Generate label for an example */
 const getLabel = (key: string | null) => {
@@ -41,33 +24,36 @@ const getLabel = (key: string | null) => {
 }
 
 /** Handle example selection */
-const selectExample = (option: ScalarComboboxOption) => {
-  selectedExampleKey.value = option.id
+const selectExample = (key: string) => {
+  selectedExampleKey.value = key
 }
 
 // For testing
 defineExpose({
   getLabel,
-  selectedExample,
   selectedExampleKey,
 })
 </script>
 
 <template>
-  <ScalarCombobox
-    class="max-h-80"
-    :modelValue="selectedExample"
-    :options="exampleOptions"
-    teleport
-    placement="bottom-start"
-    @update:modelValue="selectExample">
+  <ScalarDropdown
+    placement="bottom"
+    resize>
     <ScalarButton
       data-testid="example-picker"
       class="text-c-1 hover:bg-b-2 flex h-full w-fit gap-1.5 px-1.5 py-0.75 font-normal"
       fullWidth
       variant="ghost">
-      <span>{{ selectedExample?.label || 'Select an example' }}</span>
+      <span>{{ getLabel(selectedExampleKey) }}</span>
       <ScalarIconCaretDown />
     </ScalarButton>
-  </ScalarCombobox>
+    <template #items>
+      <ScalarDropdownItem
+        v-for="(_, key) in examples"
+        :key="key"
+        @click="selectExample(key)">
+        {{ getLabel(key) }}
+      </ScalarDropdownItem>
+    </template>
+  </ScalarDropdown>
 </template>
