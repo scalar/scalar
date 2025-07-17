679a35d7d374496481787a15acd708ecc3d6f24a
anthony lio <anthony@scalar.com>
2025-07-17 07:52:31 +0300

    fix(api-reference): increases server variables label input font size

--- packages/api-reference/src/features/base-url/BaseUrl.vue
+++ packages/api-reference/src/features/base-url/BaseUrl.vue
@@ -37,7 +37,7 @@ const updateServer = (newServer: string) => {
 </script>
 <template>
   <label
-    class="bg-b-2 flex h-8 items-center rounded-t-lg border border-b-0 px-3 py-2.5 text-base font-medium">
+    class="bg-b-2 flex h-8 items-center rounded-t-lg border border-b-0 px-3 py-2.5 font-medium">
     Server
   </label>
   <div
