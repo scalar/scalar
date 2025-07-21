0dc2db25798ca64abe6ec899f099c0850ed27ef2
Hans Pagel <mail@hanspagel.com>
2025-07-21 15:41:31 +0200

    chore: oops, restore oas-utils/parse and delete api-reference/parse

---
+++ packages/oas-utils/src/helpers/parse.test.ts
@@ -0,0 +1,56 @@
+import { describe, expect, it } from 'vitest'
+
+import { isJsonString, json, parseJsonOrYaml, transformToJson, yaml } from './parse'
+
+describe('Handles yaml and json parsing', () => {
+  it('Parses basic yaml', () => {
+    expect(yaml.parse('openapi: 3.0.0')).toEqual({
+      openapi: '3.0.0',
+    })
+
+    expect(json.parse('{ "openapi": "3.0.0" }')).toEqual({
+      openapi: '3.0.0',
+    })
+  })
+
+  it('Fails if any type except an object is not returned', () => {
+    expect(() => yaml.parse('10')).toThrowError()
+    expect(() => json.parse('10')).toThrowError()
+  })
+
+  it('transforms Yaml to JSON', () => {
+    expect(transformToJson('openapi: 3.0.0')).toMatchObject(JSON.stringify({ openapi: '3.0.0' }))
+  })
+})
+
+describe('isJsonString', () => {
+  it('keeps a path as is', async () => {
+    expect(isJsonString('foobar')).toBe(false)
+  })
+
+  it('removes slash', async () => {
+    expect(isJsonString('{ "foo": "bar" }')).toBe(true)
+  })
+
+  it('trims whitespace', async () => {
+    expect(isJsonString({ foo: 'bar' })).toBe(false)
+  })
+})
+
+describe('parseJsonOrYaml', () => {
+  it('Handles json', () => {
+    expect(parseJsonOrYaml('{ "a": 10 }')).toEqual({ a: 10 })
+  })
+
+  it('Handles YAML', () => {
+    expect(parseJsonOrYaml('a: 10')).toEqual({ a: 10 })
+  })
+
+  it('Throws for invalid json or yaml', () => {
+    expect(() => parseJsonOrYaml('asdasdad: 0---  \n--')).toThrowError()
+  })
+
+  it('Throws if an object is not returned', () => {
+    expect(() => parseJsonOrYaml('asda')).toThrowError()
+  })
+})
