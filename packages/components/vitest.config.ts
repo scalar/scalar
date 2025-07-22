691e37c21bd89ef5469989d7532e61d96b8df24a
Hans Pagel <mail@hanspagel.com>
2025-07-22 13:55:54 +0200

    fix: vitest config still broken

--- packages/components/vitest.config.ts
+++ packages/components/vitest.config.ts
@@ -1,36 +1,15 @@
 import { fileURLToPath } from 'node:url'
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vitest/config'
-import { configDefaults } from 'vitest/config'
+import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
 
-import tailwindcss from '@tailwindcss/vite'
-import svgLoader from 'vite-svg-loader'
+import viteConfig from './vite.config'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-    // Ensure the viewBox is preserved
-    svgLoader({
-      svgoConfig: {
-        multipass: true,
-        plugins: [
-          {
-            name: 'preset-default',
-            params: {
-              overrides: {
-                // @see https://github.com/svg/svgo/issues/1128
-                removeViewBox: false,
-              },
-            },
-          },
-        ],
-      },
-    }),
-  ],
-  test: {
-    environment: 'jsdom',
-    exclude: [...configDefaults.exclude, 'e2e/*'],
-    root: fileURLToPath(new URL('./', import.meta.url)),
-  },
-})
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+  }),
+)
