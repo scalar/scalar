643cd3c6828a3b803a447a35064e12c7f8f0e459
Amrit <amrit@scalar.com>
2025-07-23 15:14:48 -0700

    fix: add custom-scrollbar styles

--- packages/api-reference/src/components/LinkList/LinkList.vue
+++ packages/api-reference/src/components/LinkList/LinkList.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="mb-3 flex h-auto min-h-8 max-w-full items-center gap-2 overflow-auto text-xs whitespace-nowrap xl:mb-1.5">
+    class="custom-scroll mb-3 flex h-auto min-h-8 max-w-full items-center gap-2 overflow-x-auto text-xs whitespace-nowrap xl:mb-1.5">
     <slot />
   </div>
 </template>
