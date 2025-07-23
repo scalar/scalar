b56e3f06531e6d01cc55b625c9275bd5dfbd56df
anthony lio <anthony@scalar.com>
2025-07-23 13:39:54 +0300

    fix(use-codemirror): increases line height according to latest font size update

--- packages/api-client/src/components/DataTable/DataTableInput.vue
+++ packages/api-client/src/components/DataTable/DataTableInput.vue
@@ -161,11 +161,11 @@ const handleLabelClick = () => {
   display: flex;
   font-family: var(--scalar-font);
   font-size: var(--scalar-small);
-  padding: 6px 8px;
+  padding: 5px 8px;
   width: 100%;
 }
 :deep(.cm-content):has(.cm-pill) {
-  padding: 6px 8px;
+  padding: 5px 8px;
 }
 :deep(.cm-content .cm-pill:not(:last-of-type)) {
   margin-right: 0.5px;
