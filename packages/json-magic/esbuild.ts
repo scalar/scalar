950f7e9798839bd77b424e293f224915275c7b95
DemonHa <hh360284@gmail.com>
2025-07-28 07:25:49 +0800

    feat: add derefernece function

--- packages/json-magic/esbuild.ts
+++ packages/json-magic/esbuild.ts
@@ -2,5 +2,11 @@ import { build } from '@scalar/build-tooling/esbuild'
 
 await build({
   platform: 'shared',
-  entries: ['src/bundle', 'src/magic-proxy', 'src/bundle/plugins/node', 'src/bundle/plugins/browser'],
+  entries: [
+    'src/bundle',
+    'src/magic-proxy',
+    'src/dereference',
+    'src/bundle/plugins/node',
+    'src/bundle/plugins/browser',
+  ],
 })
