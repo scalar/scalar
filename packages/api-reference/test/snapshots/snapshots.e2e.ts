ad901aa60613899c0f89176b1b73c1ad7af4dc0f
Hans Pagel <mail@hanspagel.com>
2025-08-04 14:30:19 +0200

    refactor: test snapshots for all given sources

--- packages/api-reference/test/snapshots/snapshots.e2e.ts
+++ packages/api-reference/test/snapshots/snapshots.e2e.ts
@@ -1,81 +1,100 @@
 import { expect, test } from '@playwright/test'
+import type { ApiReferenceConfigurationWithSources } from '@scalar/types'
 import { serveExample } from '@test/utils/serve-example'
 
-// TODO: We should make the documents an array that we loop over to check the snapshots.
+const SOURCES: ApiReferenceConfigurationWithSources['sources'] = [
+  {
+    // TODO: This should actually use @scalar/galaxy, ideally directly from the file system (so it’ll already fail before we publish an updated version.)
+    slug: 'test-api',
+    content: {
+      openapi: '3.1.1',
+      info: {
+        title: 'Test API',
+        version: '1.0.0',
+      },
+      paths: {
+        '/users': {
+          get: {
+            summary: 'Get all users',
+            tags: ['user-tag'],
+          },
+        },
+      },
+    },
+  },
+  {
+    title: 'Scalar Galaxy (Classic Layout)',
+    slug: 'scalar-galaxy-classic-layout',
+    url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
+    // @ts-expect-error The types are wrong.
+    layout: 'classic',
+  },
+  // {
+  //   title: 'Swagger Petstore 2.0',
+  //   slug: 'swagger-petstore-2.0',
+  //   url: 'https://petstore.swagger.io/v2/swagger.json',
+  // },
+  // {
+  //   title: 'Swagger Petstore 3.0',
+  //   slug: 'swagger-petstore-3.0',
+  //   url: 'https://petstore3.swagger.io/api/v3/openapi.json',
+  // },
+  {
+    title: 'Swagger Petstore 3.1',
+    slug: 'swagger-petstore-3.1',
+    url: 'https://petstore31.swagger.io/api/v31/openapi.json',
+  },
+  {
+    title: 'Valtown',
+    slug: 'valtown',
+    url: 'https://docs.val.town/openapi.documented.json',
+  },
+  // {
+  //   title: 'Zoom',
+  //   slug: 'zoom',
+  //   url: 'https://developers.zoom.us/api-hub/meetings/methods/endpoints.json',
+  // },
+  // {
+  //   title: 'Cloudinary',
+  //   slug: 'cloudinary',
+  //   url: 'https://cloudinary.com/documentation/schemas/analysis-api/public-schema.yml',
+  // },
+  // {
+  //   title: 'Tailscale',
+  //   slug: 'tailscale',
+  //   url: 'https://api.tailscale.com/api/v2?outputOpenapiSchema=true',
+  // },
+  // {
+  //   title: 'Maersk',
+  //   slug: 'maersk',
+  //   url: 'https://edpmediastorage.blob.core.windows.net/media/air_booking_v1-0_26092023_scalar_spec.yaml',
+  // },
+  // {
+  //   title: 'Bolt',
+  //   slug: 'bolt',
+  //   url: 'https://assets.bolt.com/external-api-references/bolt.yml',
+  // },
+  // {
+  //   title: 'OpenStatus',
+  //   slug: 'openstatus',
+  //   url: 'https://api.openstatus.dev/v1/openapi',
+  // },
+]
 
 test.describe('snapshots', () => {
   test('matches all existing snapshots', async ({ page }) => {
     const example = await serveExample({
       // TODO: We want to pull those documents from the CDN, so the tests don’t fail when they change their API.
-      sources: [
-        {
-          // TODO: This should actually use @scalar/galaxy, ideally directly from the file system (so it’ll already fail before we publish an updated version.)
-          content: {
-            openapi: '3.1.1',
-            info: {
-              title: 'Test API',
-              version: '1.0.0',
-            },
-            paths: {
-              '/users': {
-                get: {
-                  summary: 'Get all users',
-                  tags: ['user-tag'],
-                },
-              },
-            },
-          },
-        },
-        {
-          title: 'Scalar Galaxy (Classic Layout)',
-          slug: 'scalar-galaxy-classic-layout',
-          url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
-          // @ts-expect-error The types are wrong.
-          layout: 'classic',
-        },
-        {
-          title: 'Swagger Petstore 2.0',
-          url: 'https://petstore.swagger.io/v2/swagger.json',
-        },
-        {
-          title: 'Swagger Petstore 3.0',
-          url: 'https://petstore3.swagger.io/api/v3/openapi.json',
-        },
-        {
-          title: 'Swagger Petstore 3.1',
-          url: 'https://petstore31.swagger.io/api/v31/openapi.json',
-        },
-        {
-          title: 'Valtown',
-          url: 'https://docs.val.town/openapi.documented.json',
-        },
-        {
-          title: 'Zoom',
-          url: 'https://developers.zoom.us/api-hub/meetings/methods/endpoints.json',
-        },
-        {
-          title: 'Cloudinary',
-          url: 'https://cloudinary.com/documentation/schemas/analysis-api/public-schema.yml',
-        },
-        {
-          title: 'Tailscale',
-          url: 'https://api.tailscale.com/api/v2?outputOpenapiSchema=true',
-        },
-        {
-          title: 'Maersk',
-          url: 'https://edpmediastorage.blob.core.windows.net/media/air_booking_v1-0_26092023_scalar_spec.yaml',
-        },
-        {
-          title: 'Bolt',
-          url: 'https://assets.bolt.com/external-api-references/bolt.yml',
-        },
-        {
-          title: 'OpenStatus',
-          url: 'https://api.openstatus.dev/v1/openapi',
-        },
-      ],
+      sources: SOURCES,
     })
 
+    SOURCES.forEach(async (source) => {
+      await page.goto(`${example}?api=${source.slug}`)
+      await expect(page).toHaveScreenshot(`${source.slug}-snapshot.png`, {
+        fullPage: true,
+        maxDiffPixelRatio: 0.02,
+      })
+    })
     await page.goto(example)
     await expect(page).toHaveScreenshot('scalar-galaxy-snapshot.png', {
       fullPage: true,
