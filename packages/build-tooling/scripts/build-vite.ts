5269521b231ec279442404df7edee340352930d6
geoff <geoffgscott@gmail.com>
2025-08-05 10:28:40 -0700

    feat(build-tooling): Migrate bin scripts to .js. They should be dep free

--- packages/build-tooling/scripts/build-vite.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env vite-node
-import { executeCommands } from './utils/utils'
-
-executeCommands(['vite build', 'pnpm types:build'], 'Vite build')
--- packages/build-tooling/scripts/types-check-vue.js
+++ packages/build-tooling/scripts/types-check-vue.js
@@ -1,5 +1,5 @@
-#!/usr/bin/env vite-node
-import { executeCommand } from './utils/utils'
+#!/usr/bin/env node
+import { executeCommand } from './utils/utils.js'
 
 // Run Vue TypeScript type-checking without emitting files
 executeCommand('vue-tsc --noEmit', 'Error during Vue TypeScript type checking')
