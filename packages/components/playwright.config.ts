9819805644fa2b9c013db158d32b830da4dab450
Brynn Hawker <brynn@hawker.me>
2025-08-06 17:13:15 -0700

    test(components): update playwright tests to use docker

--- packages/components/playwright.config.ts
+++ packages/components/playwright.config.ts
@@ -14,14 +14,37 @@ export default defineConfig({
     },
   },
 
-  webServer: {
-    command: 'pnpm preview',
-    url: 'http://localhost:5100',
-    reuseExistingServer: !process.env.CI,
-  },
+  webServer: [
+    /**
+     * Playwright Test Server
+     *
+     * This runs the playwright test browser(s) in a docker container to make sure
+     * the tests are run in a consistent environment locally and in CI.
+     */
+    {
+      name: 'Playwright',
+      command: 'pnpm test:e2e:playwright',
+      url: 'http://localhost:5001',
+      timeout: 120 * 1000,
+      reuseExistingServer: !process.env.CI,
+    },
+    /**
+     * Storybook
+     *
+     * This runs the storybook built storybook files from `pnpm build:storybook`
+     * unless you're already running the storybook dev server.
+     */
+    {
+      name: 'Storybook',
+      command: 'pnpm preview',
+      url: 'http://localhost:5100',
+      reuseExistingServer: !process.env.CI,
+    },
+  ],
   use: {
-    baseURL: 'http://localhost:5100/',
-    // Use a smaller viewport for components
+    /** The base URL is on the docker host where we're running storybook */
+    baseURL: 'http://host.docker.internal:5100/',
+    /** Use a smaller viewport for components */
     viewport: { width: 640, height: 480 },
   },
 })
