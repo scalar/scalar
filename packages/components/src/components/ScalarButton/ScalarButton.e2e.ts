b688a0672cc87f234ddba3b83293d95fe735a53a
Brynn Hawker <brynn@hawker.me>
2025-08-09 00:08:26 -0700

    test(components): crop ScalarButton snapshots

--- packages/components/src/components/ScalarButton/ScalarButton.e2e.ts
+++ packages/components/src/components/ScalarButton/ScalarButton.e2e.ts
@@ -2,11 +2,13 @@ import { test } from '@test/helpers'
 
 const stories = ['Base', 'Ghost', 'Danger', 'Disabled', 'Loading']
 
-test.describe('ScalarButton', () =>
+test.describe('ScalarButton', () => {
+  test.use({ crop: true })
   stories.forEach((story) =>
     test(story, async ({ page, snapshot }) => {
       await snapshot()
       await page.getByRole('button').hover()
       await snapshot('hover')
     }),
-  ))
+  )
+})
