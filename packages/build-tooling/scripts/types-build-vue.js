186847f55ef852fd23300841e6b7f6ffab62a0f8
Geoff Scott <geoffgscott@gmail.com>
2025-08-11 16:05:07 -0700

    feat(build-tooling): Add TS build time to tooling

--- packages/build-tooling/scripts/types-build-vue.js
+++ packages/build-tooling/scripts/types-build-vue.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 import { executeCommands } from './utils/utils.js'
+import as from 'ansis'
 
+const start = performance.now()
 executeCommands(['vue-tsc -p tsconfig.build.json', 'tsc-alias -p tsconfig.build.json'], 'Vue types build')
+
+const duration = (performance.now() - start) / 1000
+console.log(as.green(`Vue types build completed in ${duration.toFixed(2)}s`))
