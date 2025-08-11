73aa4d2b4b3064fdc71093af6a87cbe7dbc3b70c
tree <sol.analyzoor@gmail.com>
2025-08-12 00:44:12 +0530

    Merge pull request #3 from DefiLlama/llm-txt
    rename file and update text for llms.txt

--- examples/web/vite.config.ts
+++ examples/web/vite.config.ts
@@ -1,5 +1,7 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import path from 'path'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
