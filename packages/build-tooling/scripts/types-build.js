1f52b45fee1981c67706af1721acbba81c787719
geoffgscott <59206100+geoffgscott@users.noreply.github.com>
2025-08-11 21:54:39 -0700

    feat(build-tooling): Add TS build time to tooling (#6519)
    * feat(build-tooling): Add TS build time to tooling
    
    * docs(changeset): Outputs the time it took to build types when running tsc/vue-tsc

--- packages/build-tooling/scripts/types-build.js
+++ packages/build-tooling/scripts/types-build.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 import { executeCommands } from './utils/utils.js'
+import as from 'ansis'
 
+const start = performance.now()
 executeCommands(['tsc -p tsconfig.build.json', 'tsc-alias -p tsconfig.build.json'], 'types build')
+
+const duration = (performance.now() - start) / 1000
+console.log(as.green(`Types build completed in ${duration.toFixed(2)}s`))
