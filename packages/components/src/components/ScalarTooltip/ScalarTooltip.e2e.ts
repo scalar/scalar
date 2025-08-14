a45d605fe5eb79cb4aced31726884af2c49e693e
Brynn Hawker <brynn@hawker.me>
2025-08-14 15:29:36 -0700

    feat(components): merge hotkey tests back together

--- packages/components/src/components/ScalarTooltip/ScalarTooltip.e2e.ts
+++ packages/components/src/components/ScalarTooltip/ScalarTooltip.e2e.ts
@@ -1,7 +1,26 @@
-import { takeTooltipSnapshot } from '@/components/ScalarTooltip/helpers'
 import { test } from '@test/helpers'
 
+import { ELEMENT_ID } from '@/components/ScalarTooltip/constants'
+import type { TestBody } from '@test/helpers'
+
+/** Opens the tooltip and takes a snapshot */
+const takeTooltipSnapshot: TestBody = async ({ page, snapshot }) => {
+  // Make the viewport smaller for the tooltip snapshots
+
+  await page.getByRole('button', { name: 'Hover Me' }).hover() // Open the tooltip
+  await page.waitForSelector(`#${ELEMENT_ID}`) // Wait for the tooltip to be visible
+  await snapshot() // Take a snapshot of the open tooltip
+}
+
 test.describe('ScalarTooltip', () => {
-  test.use({ background: true, colorModes: ['light', 'dark'] })
+  test.use({
+    viewport: { width: 240, height: 160 },
+    background: true,
+    colorModes: ['light', 'dark'],
+  })
+
   test('Base', takeTooltipSnapshot)
+
+  const stories = ['Hotkey', 'Label and Hotkey']
+  stories.forEach((story) => test(story, takeTooltipSnapshot))
 })
