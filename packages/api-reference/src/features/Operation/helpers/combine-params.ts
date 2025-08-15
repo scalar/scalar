e7e1f4868eb837b05eef6ee4c081c1f457d86d27
Amrit <amrit@scalar.com>
2025-08-15 01:35:54 -0700

    fix: tests

--- packages/api-reference/src/features/Operation/helpers/combine-params.ts
+++ packages/api-reference/src/features/Operation/helpers/combine-params.ts
@@ -1,3 +1,4 @@
+import { isDefined } from '@scalar/helpers/array/is-defined'
 import { getResolvedRef } from '@scalar/workspace-store/helpers/get-resolved-ref'
 import type { ParameterObject } from '@scalar/workspace-store/schemas/v3.1/strict/parameter'
 import type { OperationObject, PathItemObject } from '@scalar/workspace-store/schemas/v3.1/strict/path-operations'
@@ -7,7 +8,7 @@ export const combineParams = (
   pathParams: PathItemObject['parameters'] = [],
   operationParams: OperationObject['parameters'] = [],
 ): ParameterObject[] => {
-  const allParams = [...pathParams, ...operationParams].map((param) => getResolvedRef(param))
+  const allParams = [...pathParams, ...operationParams].map((param) => getResolvedRef(param)).filter(isDefined)
 
   // Use a Map to ensure unique in+name combinations
   // Operation parameters take precedence over path parameters
