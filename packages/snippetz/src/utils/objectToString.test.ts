db6c27b5b83ef3ff7f9039d73f0859a162ece923
Federico Bond <federicobond@gmail.com>
2025-08-25 12:57:43 +1000

    test(snippetz): add tests for objectToString function

---
+++ packages/snippetz/src/utils/objectToString.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest'
+
+import { objectToString } from './objectToString'
+
+describe('objectToString', () => {
+  it('formats a simple object', () => {
+    expect(objectToString({ foo: 'bar', baz: 'qux' })).toBe(`{
+  foo: 'bar',
+  baz: 'qux'
+}`)
+  })
+})
