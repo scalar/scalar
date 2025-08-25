82d7c3d2e7b0592af61e75dee05c142bdfd26361
Amrit <amrit@scalar.com>
2025-08-25 16:17:50 -0700

    chore: cleanup ternary

--- packages/api-reference/src/v2/blocks/scalar-info-block/components/InfoVersion.vue
+++ packages/api-reference/src/v2/blocks/scalar-info-block/components/InfoVersion.vue
@@ -10,15 +10,14 @@ const { version } = defineProps<{
 
 /** Format the version number to be displayed in the badge */
 const prefixedVersion = computed(() => {
-  // Prefix the version with “v” if the first character is a number, don't prefix if it's not.
-  // Don't output anything when version is not a string.
-  return typeof version === 'string'
-    ? version.toString().match(/^\d/)
-      ? `v${version}`
-      : version
-    : typeof version === 'number'
-      ? `v${version}`
-      : undefined
+  if (version == null) {
+    return version
+  }
+
+  const versionString = String(version)
+
+  // Prefix with "v" if the first character is a number
+  return /^\d/.test(versionString) ? `v${versionString}` : versionString
 })
 </script>
 <template>
