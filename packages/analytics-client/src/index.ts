4e42c9da71bc95aa1071d80925e6f623adfe82d4
Liam Debell <liam@scalar.com>
2025-08-27 13:57:48 -0700

    feat(analytics): Add new analytics-client package

---
+++ packages/analytics-client/src/index.ts
@@ -0,0 +1,50 @@
+import { analyticsEvents, type analyticsEventData, type Events, analyticsEventEnum } from '@/events'
+import type { z } from 'zod'
+
+export const analyticsFactory = (
+  /**
+   * Base URL for the API. For Scalar services all services live
+   * at a root path of the baseUrl
+   *
+   * ex. https://services.scalar.com/core/<endpointName>
+   *     or
+   *     http://localhost:9999/core/<endpointName>
+   */
+  baseUrl: string,
+  /**
+   * Getter function to retrieve the active auth token
+   * This will be run on each request
+   */
+  getAuthToken: () => string | null,
+) => {
+  async function capture<E extends Events>(
+    event: E,
+    ...[data]: z.input<(typeof analyticsEventData)[E]> extends undefined
+      ? []
+      : [z.input<(typeof analyticsEventData)[E]>]
+  ) {
+    if (!analyticsEvents.includes(event)) {
+      throw new Error('[Analytics]: Invalid event submission')
+    }
+
+    const authToken = getAuthToken()
+
+    await fetch(`${baseUrl}/analytics/send`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        ...(authToken ? { 'authorization': `Bearer ${authToken}` } : {}),
+      },
+      body: JSON.stringify({
+        event,
+        data,
+      }),
+    })
+  }
+
+  return {
+    capture,
+  }
+}
+
+export { analyticsEventEnum }
