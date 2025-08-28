dafec5df7623c9004cb3d5b15d4d5f9d5cf6999d
Geoff Scott <geoffgscott@gmail.com>
2025-08-28 16:42:34 -0700

    feat: only build required packages for tests

--- packages/api-reference/playwright.config.ts
+++ packages/api-reference/playwright.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from '@playwright/test'
 // https://playwright.dev/docs/test-configuration
 export default defineConfig({
   testMatch: 'test/**/*.e2e.ts',
+  workers: '100%',
 })
