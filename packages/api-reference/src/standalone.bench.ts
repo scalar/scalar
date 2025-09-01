45856f4000c4ad129975b49372333d1231dbe748
Amrit <amrit@scalar.com>
2025-09-01 13:15:38 -0700

    chore: added a bench

---
+++ packages/api-reference/src/standalone.bench.ts
@@ -0,0 +1,14 @@
+import { bench, describe, expect, vi } from 'vitest'
+
+describe('standalone', () => {
+  bench(
+    'createApiReference load time',
+    async () => {
+      // This needs to be here not in a beforeEach for some reason or it doesn't reset
+      vi.resetModules()
+      const { createApiReference } = await import('@/standalone/lib/html-api')
+      expect(createApiReference).toBeDefined()
+    },
+    { iterations: 1 },
+  )
+})
