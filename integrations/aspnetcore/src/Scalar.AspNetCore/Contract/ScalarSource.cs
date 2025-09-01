73d2c897bdf3bc7359a5b2a7811a9f4d3e8b1fc7
Justin Lampe <xC0dex@users.noreply.github.com>
2025-09-01 08:31:24 +0200

    support default document

--- integrations/aspnetcore/src/Scalar.AspNetCore/Contract/ScalarSource.cs
+++ integrations/aspnetcore/src/Scalar.AspNetCore/Contract/ScalarSource.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace Scalar.AspNetCore;
 
 internal sealed class ScalarSource
@@ -5,4 +7,7 @@ internal sealed class ScalarSource
     public required string Title { get; init; }
 
     public required string Url { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public required bool Default { get; init; }
 }
