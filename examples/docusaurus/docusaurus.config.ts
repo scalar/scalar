555a889175a71f7119ab93452574607910016616
Hans Pagel <mail@hanspagel.com>
2025-09-02 17:58:52 +0200

    chore: restore file

--- examples/docusaurus/docusaurus.config.ts
+++ examples/docusaurus/docusaurus.config.ts
@@ -57,132 +57,130 @@ const config: Config = {
       '@scalar/docusaurus',
       {
         id: 'json-url-cdn',
-        label: 'Docusaurus',
+        label: 'json-url-cdn',
         route: '/json-url-cdn',
         cdn: 'https://cdn.jsdelivr.net/npm/@scalar/api-reference@1.28.11',
         configuration: {
           url: 'https://registry.scalar.com/@scalar/apis/galaxy/latest?format=json',
-          forceDarkModeState: 'dark',
-          darkMode: true,
         },
       } as ScalarOptions,
     ],
-    //     [
-    //       '@scalar/docusaurus',
-    //       {
-    //         id: 'yaml-url',
-    //         label: 'yaml-url',
-    //         route: '/yaml-url',
-    //         configuration: {
-    //           url: 'https://registry.scalar.com/@scalar/apis/galaxy/latest?format=yaml',
-    //         },
-    //       } as ScalarOptions,
-    //     ],
-    //     [
-    //       '@scalar/docusaurus',
-    //       {
-    //         id: 'json-string',
-    //         label: 'json-string',
-    //         route: '/json-string',
-    //         configuration: {
-    //           content: `{
-    //   "openapi": "3.1.0",
-    //   "info": {
-    //     "title": "JSON Docs",
-    //     "version": "1.0.0"
-    //   },
-    //   "paths": {}
-    // }`,
-    //         },
-    //       } as ScalarOptions,
-    //     ],
-    //     [
-    //       '@scalar/docusaurus',
-    //       {
-    //         id: 'yaml-string',
-    //         label: 'yaml-string',
-    //         route: '/yaml-string',
-    //         configuration: {
-    //           content: `openapi: 3.1.0
-    // info:
-    //   title: YAML Docs
-    //   version: 1.0.0
-    // paths: {}
-    // `,
-    //         },
-    //       } as ScalarOptions,
-    //     ],
+    [
+      '@scalar/docusaurus',
+      {
+        id: 'yaml-url',
+        label: 'yaml-url',
+        route: '/yaml-url',
+        configuration: {
+          url: 'https://registry.scalar.com/@scalar/apis/galaxy/latest?format=yaml',
+        },
+      } as ScalarOptions,
+    ],
+    [
+      '@scalar/docusaurus',
+      {
+        id: 'json-string',
+        label: 'json-string',
+        route: '/json-string',
+        configuration: {
+          content: `{
+  "openapi": "3.1.0",
+  "info": {
+    "title": "JSON Docs",
+    "version": "1.0.0"
+  },
+  "paths": {}
+}`,
+        },
+      } as ScalarOptions,
+    ],
+    [
+      '@scalar/docusaurus',
+      {
+        id: 'yaml-string',
+        label: 'yaml-string',
+        route: '/yaml-string',
+        configuration: {
+          content: `openapi: 3.1.0
+info:
+  title: YAML Docs
+  version: 1.0.0
+paths: {}
+`,
+        },
+      } as ScalarOptions,
+    ],
   ],
 
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
-    // navbar: {
-    //   title: 'My Site',
-    //   logo: {
-    //     alt: 'My Site Logo',
-    //     src: 'img/logo.svg',
-    //   },
-    //   items: [
-    //     {
-    //       type: 'docSidebar',
-    //       sidebarId: 'tutorialSidebar',
-    //       position: 'left',
-    //       label: 'Tutorial',
-    //     },
-    //     { to: '/blog', label: 'Blog', position: 'left' },
-    //     {
-    //       href: 'https://github.com/facebook/docusaurus',
-    //       label: 'GitHub',
-    //       position: 'right',
-    //     },
-    //   ],
-    // },
-    // footer: {
-    //   style: 'dark',
-    //   links: [
-    //     {
-    //       title: 'Docs',
-    //       items: [
-    //         {
-    //           label: 'Tutorial',
-    //           to: '/docs/intro',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'Community',
-    //       items: [
-    //         {
-    //           label: 'Stack Overflow',
-    //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-    //         },
-    //         {
-    //           label: 'Discord',
-    //           href: 'https://discordapp.com/invite/docusaurus',
-    //         },
-    //         {
-    //           label: 'Twitter',
-    //           href: 'https://twitter.com/docusaurus',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'More',
-    //       items: [
-    //         {
-    //           label: 'Blog',
-    //           to: '/blog',
-    //         },
-    //         {
-    //           label: 'GitHub',
-    //           href: 'https://github.com/facebook/docusaurus',
-    //         },
-    //       ],
-    //     },
-    //   ],
-    //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    // },
+    navbar: {
+      title: 'My Site',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
   } satisfies Preset.ThemeConfig,
 }
 
