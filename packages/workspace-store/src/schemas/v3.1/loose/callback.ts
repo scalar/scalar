4236a37d71765958a701d9a283af77e7c8a03c55
DemonHa <hh360284@gmail.com>
2025-09-03 12:06:32 -0400

    chore: delete loose schemas

--- packages/workspace-store/src/schemas/v3.1/loose/callback.ts
+++
@@ -1,9 +0,0 @@
-import { Type, type TSchema } from '@scalar/typebox'
-import { ReferenceObjectSchema } from './reference'
-
-export const callbackObjectSchemaBuilder = <P extends TSchema>(pathItem: P) =>
-  Type.Record(
-    Type.String(),
-    /** A Path Item Object used to define a callback request and expected responses. A complete example is available. */
-    Type.Union([pathItem, ReferenceObjectSchema]),
-  )
