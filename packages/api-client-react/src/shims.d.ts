c50661d6ad85c89775c366e5f3cc8a57eac9d80d
DemonHa <hh360284@gmail.com>
2025-09-05 22:43:36 -0400

    chore: upgrade biomev1 -> v2

--- packages/api-client-react/src/shims.d.ts
+++ packages/api-client-react/src/shims.d.ts
@@ -1,9 +1,7 @@
-// biome-ignore lint/style/noNamespace: It's OK, Biome.
+/** biome-ignore-all lint/style/noNamespace: It's OK, Biome. */
+/** biome-ignore-all lint/suspicious/noVar: It's OK, Biome. */
 declare namespace globalThis {
-  // biome-ignore lint/style/noVar: It's OK, Biome.
   var __VUE_OPTIONS_API__: boolean
-  // biome-ignore lint/style/noVar: It's OK, Biome.
   var __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: boolean
-  // biome-ignore lint/style/noVar: It's OK, Biome.
   var __VUE_PROD_DEVTOOLS__: boolean
 }
