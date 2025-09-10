a5d352fdf2d4fd536dcd5b0f4a2787f8b8c08f28
Justin Lampe <xC0dex@users.noreply.github.com>
2025-09-10 11:55:07 +0200

    use options

--- integrations/aspnetcore/src/Scalar.AspNetCore.Microsoft/ScalarDocumentProvider.cs
+++ integrations/aspnetcore/src/Scalar.AspNetCore.Microsoft/ScalarDocumentProvider.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Scalar.AspNetCore.Microsoft;
 
@@ -8,13 +9,14 @@ internal sealed class ScalarDocumentProvider(IServiceProvider serviceProvider) :
     public async Task<string> GetDocumentContentAsync(string documentName, CancellationToken cancellationToken)
     {
         var documentProvider = serviceProvider.GetKeyedService<IOpenApiDocumentProvider>(documentName);
+        var openApiOptions = serviceProvider.GetRequiredService<IOptionsMonitor<OpenApiOptions>>().Get(documentName);
         if (documentProvider is null)
         {
             throw new InvalidOperationException($"No OpenAPI document provider found for document name '{documentName}'.");
         }
 
         var document = await documentProvider.GetOpenApiDocumentAsync(cancellationToken);
-        return await document.SerializeAsJsonAsync(OpenApiSpecVersion.OpenApi3_1, cancellationToken);
+        return await document.SerializeAsJsonAsync(openApiOptions.OpenApiVersion, cancellationToken);
     }
 }
 
