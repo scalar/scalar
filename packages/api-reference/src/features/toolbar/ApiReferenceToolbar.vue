2bfd5cbbacc94a7c324523235ff1807280851a50
Brynn Hawker <brynn@hawker.me>
2025-09-10 17:21:15 -0700

    feat(api-reference): only show toolbar on localhost

--- packages/api-reference/src/features/toolbar/ApiReferenceToolbar.vue
+++ packages/api-reference/src/features/toolbar/ApiReferenceToolbar.vue
@@ -1,10 +1,20 @@
 <script lang="ts" setup>
+import { computed } from 'vue'
+
 defineProps<{
   store?: any
 }>()
+
+const showToolbar = computed<boolean>(() => {
+  if (!window) {
+    return false
+  }
+  return window.location.hostname === 'localhost'
+})
 </script>
 <template>
   <header
+    v-if="showToolbar"
     class="api-reference-toolbar h-header bg-b-1 sticky top-0 z-10 flex items-center justify-center border-b">
     Dev Toolbar
   </header>
