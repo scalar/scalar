ce2d06f747717fb58e5f34a78eaed3e25aca3483
Brynn Hawker <brynn@hawker.me>
2025-09-11 21:57:30 -0700

    feat(components): add toggle group components

--- packages/components/src/components/ScalarToggle/ScalarToggleGroup.vue
+++ packages/components/src/components/ScalarToggle/ScalarToggleGroup.vue
@@ -1,32 +1,26 @@
 <script lang="ts">
 /**
- * Scalar Checkbox Group component
+ * Scalar Toggle Group component
  *
- * A group of checkbox inputs allowing for multiple selections.
+ * A group of toggle inputs allowing for multiple selections.
  */
 export default {}
 </script>
 <script setup lang="ts">
-import { useId } from 'vue'
-
+import type { ScalarCheckboxOption } from '../ScalarCheckboxInput'
 import { ScalarFormInputGroup } from '../ScalarForm'
-import ScalarCheckboxInput from './ScalarCheckboxInput.vue'
-import type { ScalarCheckboxOption } from './types'
+import ScalarToggleInput from './ScalarToggleInput.vue'
 
 const { options = [] } = defineProps<{
   options?: ScalarCheckboxOption[]
 }>()
 
 const model = defineModel<ScalarCheckboxOption[]>({ default: [] })
-
-const name = useId()
 </script>
 <template>
   <ScalarFormInputGroup>
-    <ScalarCheckboxInput
+    <ScalarToggleInput
       v-for="option in options"
-      :value="option.value"
-      :name
       :modelValue="model?.some(({ value }) => value === option.value)"
       @update:modelValue="
         (checked) =>
@@ -36,6 +30,6 @@ const name = useId()
       "
       :key="option.value">
       {{ option.label }}
-    </ScalarCheckboxInput>
+    </ScalarToggleInput>
   </ScalarFormInputGroup>
 </template>
