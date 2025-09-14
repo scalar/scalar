7ca48eeff87911c3fcacbf257abc42c96b2a662f
Brynn Hawker <brynn@hawker.me>
2025-09-14 11:48:45 -0700

    feat(api-reference): clean up temporary share link

---
+++ packages/api-reference/src/features/toolbar/ApiReferenceToolbarBlurb.vue
@@ -0,0 +1,5 @@
+<template>
+  <p class="text-c-3 [&:code]:font-code text-center leading-normal">
+    <slot />
+  </p>
+</template>
--- packages/api-reference/src/features/toolbar/ApiReferenceToolbarShare.vue
+++ packages/api-reference/src/features/toolbar/ApiReferenceToolbarShare.vue
@@ -1,13 +1,16 @@
 <script lang="ts" setup>
 import {
-  ScalarButton,
   ScalarFormSection,
+  ScalarIconButton,
   ScalarTextInput,
 } from '@scalar/components'
+import { ScalarIconCopy } from '@scalar/icons'
 import type { ApiReferenceConfiguration } from '@scalar/types'
+import { useClipboard } from '@scalar/use-hooks/useClipboard'
 import type { WorkspaceStore } from '@scalar/workspace-store/client'
-import { ref } from 'vue'
+import { computed } from 'vue'
 
+import ApiReferenceToolbarBlurb from '@/features/toolbar/ApiReferenceToolbarBlurb.vue'
 import ApiReferenceToolbarPopover from '@/features/toolbar/ApiReferenceToolbarPopover.vue'
 
 const { configuration } = defineProps<{
@@ -15,26 +18,35 @@ const { configuration } = defineProps<{
   configuration?: Partial<ApiReferenceConfiguration>
 }>()
 
-const url = ref('')
-
-async function handleGenerateLink() {
+const tempUrl = computed<string>(() => {
   const data = encodeURIComponent(JSON.stringify(configuration))
-  url.value = `https://editor.scalar.com/preview?config=${data}`
-}
+  return `https://editor.scalar.com/preview?config=${data}`
+})
+
+const { copyToClipboard } = useClipboard()
 </script>
 <template>
   <ApiReferenceToolbarPopover class="w-100">
     <template #label>Share</template>
     <ScalarFormSection>
       <template #label>Temporary Link</template>
-      <ScalarButton
-        variant="outlined"
-        @click="handleGenerateLink">
-        Generate
-      </ScalarButton>
       <ScalarTextInput
         readonly
-        :modelValue="url" />
+        :modelValue="tempUrl"
+        @click="copyToClipboard(tempUrl)">
+        <template #aside>
+          <ScalarIconButton
+            :icon="ScalarIconCopy"
+            label="Copy link to clipboard"
+            class="-m-1 -mr-1.5"
+            size="sm"
+            @click="copyToClipboard(tempUrl)" />
+        </template>
+      </ScalarTextInput>
+      <ApiReferenceToolbarBlurb class="-mt-1">
+        Currently sharing is only available for hosted specifications configured
+        with a <code>URL</code>.
+      </ApiReferenceToolbarBlurb>
     </ScalarFormSection>
   </ApiReferenceToolbarPopover>
 </template>
