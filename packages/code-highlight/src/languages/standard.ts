c43dbbf3bd81517a904678bd0af52c59829c27b4
Graeme Nathan <graeme@scalar.com>
2025-09-17 07:03:21 -0700

    feat(code-highlighting): extend available languages
    Adds three new exports: mediumLanguages (those less popular than
    standard, but moreso than specialized), specializedLanguages, and
    allLanguages.

--- packages/code-highlight/src/languages/standard.ts
+++ packages/code-highlight/src/languages/standard.ts
@@ -43,6 +43,39 @@ import lua from 'highlight.js/lib/languages/lua'
 import perl from 'highlight.js/lib/languages/perl'
 import properties from 'highlight.js/lib/languages/properties'
 import scala from 'highlight.js/lib/languages/scala'
+import cmake from 'highlight.js/lib/languages/cmake'
+import crystal from 'highlight.js/lib/languages/crystal'
+import elm from 'highlight.js/lib/languages/elm'
+import erlang from 'highlight.js/lib/languages/erlang'
+import fsharp from 'highlight.js/lib/languages/fsharp'
+import glsl from 'highlight.js/lib/languages/glsl'
+import groovy from 'highlight.js/lib/languages/groovy'
+import handlebars from 'highlight.js/lib/languages/handlebars'
+import julia from 'highlight.js/lib/languages/julia'
+import nim from 'highlight.js/lib/languages/nim'
+import nix from 'highlight.js/lib/languages/nix'
+import protobuf from 'highlight.js/lib/languages/protobuf'
+import stylus from 'highlight.js/lib/languages/stylus'
+import twig from 'highlight.js/lib/languages/twig'
+import verilog from 'highlight.js/lib/languages/verilog'
+import vhdl from 'highlight.js/lib/languages/vhdl'
+import ada from 'highlight.js/lib/languages/ada'
+import apache from 'highlight.js/lib/languages/apache'
+import applescript from 'highlight.js/lib/languages/applescript'
+import asciidoc from 'highlight.js/lib/languages/asciidoc'
+import awk from 'highlight.js/lib/languages/awk'
+import coffeescript from 'highlight.js/lib/languages/coffeescript'
+import coq from 'highlight.js/lib/languages/coq'
+import d from 'highlight.js/lib/languages/d'
+import haml from 'highlight.js/lib/languages/haml'
+import lisp from 'highlight.js/lib/languages/lisp'
+import delphi from 'highlight.js/lib/languages/delphi'
+import prolog from 'highlight.js/lib/languages/prolog'
+import scheme from 'highlight.js/lib/languages/scheme'
+import smalltalk from 'highlight.js/lib/languages/smalltalk'
+import tcl from 'highlight.js/lib/languages/tcl'
+import latex from 'highlight.js/lib/languages/latex'
+import vim from 'highlight.js/lib/languages/vim'
 // @ts-expect-error No types available
 import curl from 'highlightjs-curl'
 
@@ -96,3 +129,58 @@ const standardLanguages: Record<string, LanguageFn> = {
   xml,
   yaml,
 }
+const mediumLanguages: Record<string, LanguageFn> = {
+  cmake,
+  crystal,
+  elm,
+  erl: erlang,
+  erlang,
+  fsharp,
+  glsl,
+  groovy,
+  handlebars,
+  hbs: handlebars,
+  jl: julia,
+  julia,
+  nim,
+  nix,
+  proto: protobuf,
+  protobuf,
+  styl: stylus,
+  stylus,
+  twig,
+  verilog,
+  vhdl,
+}
+
+const specializedLanguages: Record<string, LanguageFn> = {
+  ada,
+  adoc: asciidoc,
+  apache,
+  applescript,
+  asciidoc,
+  awk,
+  coffee: coffeescript,
+  coffeescript,
+  coq,
+  d,
+  haml,
+  hs: haskell,
+  lisp,
+  latex,
+  pascal: delphi,
+  prolog,
+  scheme,
+  smalltalk,
+  tcl,
+  tex: latex,
+  vim,
+}
+
+const allLanguages: Record<string, LanguageFn> = {
+  ...standardLanguages,
+  ...mediumLanguages,
+  ...specializedLanguages,
+}
+
+export { standardLanguages, mediumLanguages, specializedLanguages, allLanguages }
