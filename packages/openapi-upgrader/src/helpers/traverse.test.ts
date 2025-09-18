6323b649c5b78108623ade4ba42b7eaee65d0d18
Matthias Keckl <53833818+matthyk@users.noreply.github.com>
2025-09-18 08:01:56 +0200

    fix(openapi-upgrader): traverse circular references

--- packages/openapi-upgrader/src/helpers/traverse.test.ts
+++ packages/openapi-upgrader/src/helpers/traverse.test.ts
@@ -80,4 +80,50 @@ describe('traverse', () => {
 
     expect(result).toEqual({ a: null, b: 2 })
   })
+
+  it('handles circular references', () => {
+    const definition: { a: number; b?: unknown; c: number } = { a: 2, c: 3 }
+    definition.b = definition
+
+    const transform = (schema: any) => {
+      return Object.fromEntries(
+        Object.entries(schema).filter(([, value]) => {
+          if (typeof value !== 'number') {
+            return true
+          }
+
+          return value % 2 === 0
+        }),
+      )
+    }
+
+    const result = traverse(definition, transform)
+
+    expect(result).toMatchObject({ a: 2, b: { a: 2, b: { a: 2 } } })
+  })
+
+  it('handles circular references with arrays', () => {
+    const definition: { a: number; b?: unknown; c: number[] } = { a: 8, c: [4, 1, 8] }
+    definition.b = definition
+
+    const transform = (schema: any) => {
+      return Object.fromEntries(
+        Object.entries(schema).map(([key, value]) => {
+          if (Array.isArray(value)) {
+            return [key, value.sort()]
+          }
+
+          if (typeof value !== 'number') {
+            return [key, value]
+          }
+
+          return [key, value ** 2]
+        }),
+      )
+    }
+
+    const result = traverse(definition, transform)
+
+    expect(result).toMatchObject({ a: 64, c: [1, 4, 8], b: { a: 64, c: [1, 4, 8] } })
+  })
 })
