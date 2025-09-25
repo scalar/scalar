9172dff8f94978f4766644c203baed6e81caba9e
Wesley Skeen <SkeeWe01@risk.regn.net>
2025-09-25 15:02:45 +0100

    feat(httpclient): improve F# HttpClient code generation with enhanced URL and cookie handling

--- packages/snippetz/src/plugins/fsharp/httpclient/httpclient.ts
+++ packages/snippetz/src/plugins/fsharp/httpclient/httpclient.ts
@@ -4,31 +4,25 @@ import { httpClientHelpers } from './httpclient.helpers'
 /**
  * fsharp/httpClient
  */
+
 export const fsharpHttpclient: Plugin = {
   target: 'fsharp',
   client: 'httpclient',
   title: 'HttpClient',
-  generate(request, configuration) {
+  generate: (request, configuration) => {
     if (!request) {
       return ''
     }
 
     const options: Record<string, any> = {}
-    const url = request.url || ''
 
-    // Handle headers
-    if (request.headers && Array.isArray(request.headers) && request.headers.length > 0) {
-      options.headers = httpClientHelpers.extractHeaders(request.headers)
-    }
-
-    // Handle query parameters
-    if (request.queryString && request.queryString.length > 0) {
-      options.query = httpClientHelpers.extractQueryString(request.queryString)
-    }
-
-    // Handle cookies
-    if (request.cookies && request.cookies.length > 0) {
-      options.cookies = httpClientHelpers.extractCookies(request.cookies)
+    let urlWithPotentialQueryString = request.url
+    // handle url
+    if (request.url) {
+      if (request.queryString && request.queryString.length > 0) {
+        const queryString = httpClientHelpers.extractQueryString(request.queryString)
+        urlWithPotentialQueryString = `${request.url}=${queryString}`
+      }
     }
 
     // Handle authentication
@@ -39,34 +33,32 @@ export const fsharpHttpclient: Plugin = {
     // Generate the fsharp code
     let code = ''
     console.log(options, configuration, request)
-    if (Object.keys(options).length > 0) {
-      // If we have cookies, we need to create a cookie container and a handler
-      // otherwise, we can just create the client
-      if (options.cookies && Object.keys(options.cookies).length > 0) {
-        code += httpClientHelpers.turnCookiesToCode(options.cookies, url)
-      } else {
-        code += 'let client = new HttpClient()\n'
-      }
 
-      // If we have a query string, we need to append it to the URL
-      // otherwise, we can just set the base address
-      if (options.query && Object.keys(options.query).length > 0) {
-        code += httpClientHelpers.turnQueryStringToCode(options.query, url)
-      } else {
-        code += `client.BaseAddress <- Uri("${url}")\n`
-      }
+    // Init the HttpRequestMessage
+    code += `let httpRequestMessage = new HttpRequestMessage( HttpMethod("${request.method}"), new Uri("${urlWithPotentialQueryString}"))\n\n`
 
-      if (options.headers && Object.keys(options.headers).length > 0) {
-        code += httpClientHelpers.turnHeadersToCode(options.headers)
-      }
-
-      if (request.postData) {
-        code += httpClientHelpers.turnPostDataToCode(request.postData)
-      }
-    } else {
-      code += 'let client = new HttpClient()\n'
-      code += `client.BaseAddress <- Uri("${url}")\n`
+    // Headers
+    if (request.headers) {
+      code += httpClientHelpers.turnHeadersToCode(request.headers)
     }
+
+    // PostData
+    if (request.postData) {
+      code += httpClientHelpers.turnPostDataToCode(request.postData)
+    }
+
+    // Cookies
+    if (request.cookies && request.cookies.length > 0 && request.url) {
+      code += httpClientHelpers.turnCookiesToCode(request.cookies, request.url)
+
+      code += '// Send the request\n'
+      code += 'let client = new HttpClient(handler)\n'
+    } else {
+      code += '// Send the request\n'
+      code += 'let client = new HttpClient()\n'
+    }
+
+    code += 'let! result = client.SendAsync(httpRequestMessage)\n'
 
     return code
   },
