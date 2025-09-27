2606c109b1b63086d7d7bc171e30970728cb59c8
Brynn Hawker <brynn@hawker.me>
2025-09-26 19:03:04 -0700

    feat(api-reference): update feature flag to be a url param

--- packages/api-reference/src/features/toolbar/ApiReferenceToolbar.vue
+++ packages/api-reference/src/features/toolbar/ApiReferenceToolbar.vue
@@ -8,9 +8,6 @@ import ApiReferenceToolbarConfig from '@/features/toolbar/ApiReferenceToolbarCon
 import ApiReferenceToolbarSdks from '@/features/toolbar/ApiReferenceToolbarSdks.vue'
 import ApiReferenceToolbarShare from '@/features/toolbar/ApiReferenceToolbarShare.vue'
 
-/** Turn this on to enabled the localhost toolbar */
-const FEATURE_FLAG = true as const
-
 defineProps<{
   workspace?: WorkspaceStore
   configuration?: Partial<ApiReferenceConfiguration>
@@ -19,9 +16,17 @@ defineProps<{
 const overrides = defineModel<Partial<ApiReferenceConfiguration>>('overrides')
 
 const showToolbar = computed<boolean>(() => {
-  if (!window || !FEATURE_FLAG) {
+  if (!window) {
     return false
   }
+
+  // Only show the toolbar if the "toolbar" URL parameter is set to a truthy value
+  const params = new URLSearchParams(window.location.search)
+  const featureParam = params.get('toolbar')
+  if (!featureParam || featureParam === '0' || featureParam === 'false') {
+    return false
+  }
+
   return isLocalUrl(window.location.href)
 })
 </script>
@@ -31,10 +36,10 @@ const showToolbar = computed<boolean>(() => {
     class="api-reference-toolbar h-header bg-b-1 sticky top-0 z-10 flex justify-center border-b px-15">
     <div
       class="flex max-w-(--refs-content-max-width) flex-1 items-center justify-end">
-      <ApiReferenceToolbarShare
-        :configuration
-        :workspace />
-      <ApiReferenceToolbarSdks :configuration />
+      <template v-if="workspace">
+        <ApiReferenceToolbarShare :workspace />
+        <ApiReferenceToolbarSdks :workspace />
+      </template>
       <ApiReferenceToolbarConfig
         :configuration
         v-model:overrides="overrides" />
