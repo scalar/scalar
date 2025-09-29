c8658758aeeb1fd5466e04618ec16ef9022f0b68
Hans Pagel <mail@hanspagel.com>
2025-09-29 11:53:06 +0200

    refactor(api-reference): update plugin extension structure to use 'component' instead of 'value'

--- packages/api-reference/src/plugins/plugin-manager.test.ts
+++ packages/api-reference/src/plugins/plugin-manager.test.ts
@@ -1,5 +1,6 @@
 import type { ApiReferencePlugin } from '@scalar/types/api-reference'
 import { describe, expect, it } from 'vitest'
+
 import { createPluginManager } from './plugin-manager'
 
 describe('plugin-manager', () => {
@@ -13,44 +14,44 @@ describe('plugin-manager', () => {
     it('registers and retrieves plugins correctly', () => {
       const mockPlugin: ApiReferencePlugin = () => ({
         name: 'testPlugin',
-        extensions: [{ name: 'x-test', value: 'testValue' }],
+        extensions: [{ name: 'x-test', component: 'testComponent' }],
       })
 
       const manager = createPluginManager({ plugins: [mockPlugin] })
       const extensions = manager.getSpecificationExtensions('x-test')
-      expect(extensions).toEqual([{ name: 'x-test', value: 'testValue' }])
+      expect(extensions).toEqual([{ name: 'x-test', component: 'testComponent' }])
     })
 
     it('filters extensions by name', () => {
       const mockPlugin: ApiReferencePlugin = () => ({
         name: 'testPlugin',
         extensions: [
-          { name: 'x-test', value: 'testValue' },
-          { name: 'x-other', value: 'otherValue' },
+          { name: 'x-test', component: 'testComponent' },
+          { name: 'x-other', component: 'otherComponent' },
         ],
       })
 
       const manager = createPluginManager({ plugins: [mockPlugin] })
       const extensions = manager.getSpecificationExtensions('x-test')
-      expect(extensions).toEqual([{ name: 'x-test', value: 'testValue' }])
+      expect(extensions).toEqual([{ name: 'x-test', component: 'testComponent' }])
     })
 
     it('registers multiple plugins correctly', () => {
       const mockPlugin1: ApiReferencePlugin = () => ({
         name: 'testPlugin1',
-        extensions: [{ name: 'x-test', value: 'testValue1' }],
+        extensions: [{ name: 'x-test', component: 'testComponent1' }],
       })
 
       const mockPlugin2: ApiReferencePlugin = () => ({
         name: 'testPlugin2',
-        extensions: [{ name: 'x-test', value: 'testValue2' }],
+        extensions: [{ name: 'x-test', component: 'testComponent2' }],
       })
 
       const manager = createPluginManager({ plugins: [mockPlugin1, mockPlugin2] })
       const extensions = manager.getSpecificationExtensions('x-test')
       expect(extensions).toEqual([
-        { name: 'x-test', value: 'testValue1' },
-        { name: 'x-test', value: 'testValue2' },
+        { name: 'x-test', component: 'testComponent1' },
+        { name: 'x-test', component: 'testComponent2' },
       ])
     })
   })
