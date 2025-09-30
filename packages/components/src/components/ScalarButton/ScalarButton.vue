ef7ffe765d769b5ccb2b48f2740e6f97f55a3d81
Brynn Hawker <brynn@hawker.me>
2025-09-29 20:14:11 -0700

    feat(components): update ScalarButton props and styling

--- packages/components/src/components/ScalarButton/ScalarButton.vue
+++ packages/components/src/components/ScalarButton/ScalarButton.vue
@@ -1,28 +1,39 @@
+<script lang="ts">
+/**
+ * Scalar Button Component
+ *
+ * Provides a styled button, if you are looking for
+ * an icon only button, use ScalarIconButton instead,
+ * it's a helpful wrapper around this component
+ *
+ * @example
+ *   <ScalarButton>
+ *     <template #icon>
+ *       <!-- Icon -->
+ *     </template>
+ *     <!-- Button label -->
+ *   </ScalarButton>
+ */
+export default {}
+</script>
 <script setup lang="ts">
 import { useBindCx } from '@scalar/use-hooks/useBindCx'
 
 import { type LoadingState, ScalarLoading } from '../ScalarLoading'
 import { type Variants, variants } from './variants'
 
-/**
- * Scalar Button
- */
-withDefaults(
-  defineProps<{
-    disabled?: boolean
-    fullWidth?: boolean
-    loading?: LoadingState | undefined
-    size?: Variants['size']
-    variant?: Variants['variant']
-    type?: 'button' | 'submit' | 'reset'
-  }>(),
-  {
-    fullWidth: false,
-    size: 'md',
-    variant: 'solid',
-    type: 'button',
-  },
-)
+const {
+  size = 'md',
+  variant = 'solid',
+  type = 'button',
+} = defineProps<{
+  disabled?: boolean
+  fullWidth?: boolean
+  loading?: LoadingState | undefined
+  size?: Variants['size']
+  variant?: Variants['variant']
+  type?: 'button' | 'submit' | 'reset'
+}>()
 
 defineOptions({ inheritAttrs: false })
 const { cx } = useBindCx()
@@ -38,7 +49,7 @@ const { cx } = useBindCx()
     ">
     <div
       v-if="$slots.icon"
-      class="mr-2 h-4 w-4"
+      class="mr-2 size-4"
       :class="{ invisible: loading?.isLoading }">
       <slot name="icon" />
     </div>
--- packages/components/src/components/ScalarButton/variants.ts
+++ packages/components/src/components/ScalarButton/variants.ts
@@ -1,15 +1,14 @@
-import type { VariantProps } from 'cva'
-
 import { cva } from '@scalar/use-hooks/useBindCx'
+import type { VariantProps } from 'cva'
 
 export const styles = {
   solid: [
     'scalar-button-solid',
-    'bg-b-btn text-c-btn shadow-sm focus-visible:border-c-btn active:bg-b-btn active:shadow-none hover:bg-h-btn outline-offset-1',
+    'bg-b-btn text-c-btn focus-visible:border-c-btn active:bg-b-btn hover:bg-h-btn outline-offset-1',
   ],
   outlined: [
     'scalar-button-outlined',
-    'active:bg-btn-1 border border-solid border-border bg-b-1 text-c-1 hover:bg-b-2 shadow',
+    'active:bg-btn-1 border border-solid border-border bg-b-1 text-c-1 hover:bg-b-2',
   ],
   ghost: ['scalar-button-ghost', 'bg-transparent text-c-3 active:text-c-1 hover:text-c-1'],
   danger: ['scalar-button-danger', 'bg-c-danger text-white active:brightness-90 hover:brightness-90'],
