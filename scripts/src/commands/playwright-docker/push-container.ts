5132f0665524e20858090ae906364e6add7bd134
Brynn <brynn@hawker.me>
2025-10-08 02:02:33 -0700

    chore: update playwright to 1.56.0 (#7025)

--- scripts/src/commands/playwright-docker/push-container.ts
+++ scripts/src/commands/playwright-docker/push-container.ts
@@ -1,11 +1,12 @@
+import { Command } from 'commander'
+
 import { runCommand } from '@/helpers'
 import { latestVersion } from '@/helpers/npm-version'
-import { Command } from 'commander'
 
 export const updatePlaywrightDocker = new Command('update-playwright-docker')
   .description('Update the docker image to the latest playwright')
   .action(async () => {
-    const { version } = await latestVersion('playwright', '1.55.0')
+    const { version } = await latestVersion('playwright', '1.56.0')
 
     await runCommand(
       `docker build -t "scalarapi/playwright:${version}" --build-arg PLAYWRIGHT_VERSION=${version} ${import.meta.dirname}`,
