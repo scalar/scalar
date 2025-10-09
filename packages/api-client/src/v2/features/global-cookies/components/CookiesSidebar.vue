8a5389f3d171e2d2807396c9c69381601049b703
DemonHa <hh360284@gmail.com>
2025-10-08 21:37:29 -0400

    feat: global cookies feature

---
+++ packages/api-client/src/v2/features/global-cookies/components/CookiesSidebar.vue
@@ -0,0 +1,152 @@
+<script setup lang="ts">
+import {
+  ScalarSidebar,
+  ScalarSidebarItem,
+  ScalarSidebarItems,
+  ScalarSidebarSection,
+} from '@scalar/components'
+import { ScalarIconFolder, ScalarIconGlobe } from '@scalar/icons'
+import { useBreakpoints } from '@scalar/use-hooks/useBreakpoints'
+import { ref } from 'vue'
+
+const { width = 300 } = defineProps<{
+  /** Sidebar title */
+  title?: string
+  /** Current document name */
+  documentName?: string | null
+  /** List of all document names */
+  documents: string[]
+  /** Provided sidebar width */
+  width?: number
+}>()
+
+const emits = defineEmits<{
+  (e: 'update:width', value: number): void
+  (e: 'update:selection', value: string | null): void
+}>()
+
+defineSlots<{
+  default?(): unknown
+}>()
+
+const isDragging = ref(false)
+
+const { breakpoints } = useBreakpoints()
+
+const startDrag = (event: MouseEvent) => {
+  event.preventDefault()
+
+  const startX = event.clientX
+
+  /** Current sidebar width when dragging starts */
+  const startWidth = width
+
+  const doDrag = (dragEvent: MouseEvent) => {
+    isDragging.value = true
+    document.body.classList.add('dragging')
+    let newWidth = startWidth + dragEvent.clientX - startX
+    if (newWidth > 420) {
+      /** Elastic effect */
+      newWidth = 420 + (newWidth - 420) * 0.2
+    }
+    if (newWidth < 240) {
+      newWidth = 240
+    }
+    emits('update:width', newWidth)
+  }
+
+  const stopDrag = () => {
+    isDragging.value = false
+    document.body.classList.remove('dragging')
+    document.documentElement.removeEventListener('mousemove', doDrag, false)
+    document.documentElement.removeEventListener('mouseup', stopDrag, false)
+    /** Reset to max width if exceeded */
+    if (width > 420) {
+      emits('update:width', 360)
+    } else if (width < 240) {
+      emits('update:width', 240)
+    }
+  }
+
+  document.documentElement.addEventListener('mousemove', doDrag, false)
+  document.documentElement.addEventListener('mouseup', stopDrag, false)
+}
+
+const sidebarRef = ref<HTMLElement | null>(null)
+</script>
+<template>
+  <div
+    ref="sidebarRef"
+    class="relative"
+    :class="{ dragging: isDragging }"
+    :style="{
+      width: `${width}px`,
+      /** Hide sidebar on smaller screens */
+      display: breakpoints.lg ? 'block' : 'none',
+    }">
+    <ScalarSidebar class="h-full w-full">
+      <div class="custom-scroll flex min-h-0 flex-1 flex-col overflow-x-clip">
+        <slot>
+          <ScalarSidebarItems>
+            <ScalarSidebarSection>
+              {{ title }}
+              <template #items>
+                <ScalarSidebarItem
+                  is="button"
+                  :active="!documentName"
+                  @click="() => emits('update:selection', null)">
+                  <template #icon>
+                    <ScalarIconGlobe />
+                  </template>
+                  Workspace cookies
+                </ScalarSidebarItem>
+                <ScalarSidebarItem
+                  is="button"
+                  v-for="doc in documents"
+                  :key="doc"
+                  :active="doc === documentName"
+                  @click="() => emits('update:selection', doc)">
+                  <template #icon>
+                    <ScalarIconFolder />
+                  </template>
+                  {{ doc }}
+                </ScalarSidebarItem>
+              </template>
+            </ScalarSidebarSection>
+          </ScalarSidebarItems>
+          <!-- Spacer -->
+          <div class="flex-1"></div>
+        </slot>
+      </div>
+    </ScalarSidebar>
+    <div
+      class="resizer"
+      @mousedown="startDrag" />
+  </div>
+</template>
+<style scoped>
+.resizer {
+  width: 5px;
+  cursor: col-resize;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  border-right: 2px solid transparent;
+  transition: border-right-color 0.3s;
+}
+.resizer:hover,
+.dragging .resizer {
+  border-right-color: var(--scalar-background-3);
+}
+.dragging {
+  cursor: col-resize;
+}
+.dragging::before {
+  content: '';
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- packages/api-client/src/v2/features/global-cookies/components/GlobalCookies.vue
+++ packages/api-client/src/v2/features/global-cookies/components/GlobalCookies.vue
@@ -2,7 +2,11 @@
 import { ScalarIconCookie } from '@scalar/icons'
 import type { Environment } from '@scalar/oas-utils/entities/environment'
 
+import ViewLayout from '@/components/ViewLayout/ViewLayout.vue'
+import ViewLayoutContent from '@/components/ViewLayout/ViewLayoutContent.vue'
+import ViewLayoutSection from '@/components/ViewLayout/ViewLayoutSection.vue'
 import type { EnvVariable } from '@/store'
+import CookiesSidebar from '@/v2/features/global-cookies/components/CookiesSidebar.vue'
 import CookiesTable from '@/v2/features/global-cookies/components/CookiesTable.vue'
 
 type Cookie = {
@@ -12,8 +16,15 @@ type Cookie = {
   isDisabled?: boolean
 }
 
-defineProps<{
+const { sidebarWidth = 300 } = defineProps<{
+  /** Current selected document name or when null it means workspace level cookies */
+  documentName: string | null
+  /** List of all document names */
+  documents: string[]
+  /** List of all cookies for the current document or workspace */
   cookies: Cookie[]
+  /** Sidebar width */
+  sidebarWidth?: number
 
   /** TODO: remove when we migrate to the new store */
   environment: Environment
@@ -21,33 +32,52 @@ defineProps<{
 }>()
 
 const emits = defineEmits<{
-  (e: 'addCookie', payload: Partial<Cookie>): void
-  (e: 'updateCookie', index: number, payload: Partial<Cookie>): void
-  (e: 'deleteCookie', index: number): void
+  (e: 'navigation:update:selection', value: string | null): void
+  (e: 'navigation:update:sidebarWidth', value: number): void
+  (e: 'cookie:add', payload: Partial<Cookie>): void
+  (e: 'cookie:update', index: number, payload: Partial<Cookie>): void
+  (e: 'cookie:delete', index: number): void
 }>()
 </script>
 
 <template>
-  <div class="bg-b-1 h-[100%] w-full! overflow-auto! px-5 py-15">
-    <div class="mx-auto flex w-full max-w-[720px] !flex-col gap-8">
-      <div class="flex flex-col gap-2">
-        <h2 class="flex items-center gap-2 text-xl font-bold">
-          <ScalarIconCookie />Global cookies
-        </h2>
-        <p class="text-c-2 mb-4 text-sm">Manage your global cookies here.</p>
-      </div>
-
-      <CookiesTable
-        :data="cookies"
-        :envVariables="envVariables"
-        :environment="environment"
-        @addRow="(payload) => emits('addCookie', payload)"
-        @deleteRow="(index) => emits('deleteCookie', index)"
-        @updateRow="
-          (index, payload) => emits('updateCookie', index, payload)
-        " />
-    </div>
-  </div>
+  <ViewLayout>
+    <CookiesSidebar
+      :documentName="documentName"
+      :documents="documents"
+      title="Global Cookies"
+      :width="sidebarWidth"
+      @update:selection="(value) => emits('navigation:update:selection', value)"
+      @update:width="
+        (value) => emits('navigation:update:sidebarWidth', value)
+      " />
+    <ViewLayoutContent class="flex-1">
+      <ViewLayoutSection>
+        <div
+          class="mx-auto flex max-h-full w-full max-w-[720px] !flex-col gap-8 overflow-auto px-5 py-15">
+          <div class="flex flex-col gap-2">
+            <h2 class="flex items-center gap-2 text-xl font-bold">
+              <ScalarIconCookie />
+              <template v-if="!documentName"> Global cookies </template>
+              <template v-else>{{ documentName }} cookies</template>
+            </h2>
+            <p class="text-c-2 mb-4 text-sm">
+              Manage your global cookies here.
+            </p>
+          </div>
+          <CookiesTable
+            :data="cookies"
+            :envVariables="envVariables"
+            :environment="environment"
+            @addRow="(payload) => emits('cookie:add', payload)"
+            @deleteRow="(index) => emits('cookie:delete', index)"
+            @updateRow="
+              (index, payload) => emits('cookie:update', index, payload)
+            " />
+        </div>
+      </ViewLayoutSection>
+    </ViewLayoutContent>
+  </ViewLayout>
 </template>
 
 <style scoped>
