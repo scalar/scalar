6fba9a39b0de4235c7e0f7a0a61a82f02f7ee54d
Hans Pagel <mail@hanspagel.com>
2025-10-14 13:45:48 +0200

    refactor(RenderPlugins): remove unused exports for ExampleFeedbackPlugin and ExampleFeedbackWidget

--- packages/api-reference/src/components/RenderPlugins/index.ts
+++ packages/api-reference/src/components/RenderPlugins/index.ts
@@ -1,3 +1,1 @@
-export { ExampleFeedbackPlugin } from './ExampleFeedbackPlugin'
-export { default as ExampleFeedbackWidget } from './ExampleFeedbackWidget.vue'
 export { default as RenderPlugins } from './RenderPlugins.vue'
