2827d92af7ce3243b2ec13304a03b52d70fa96e8
Vincent Biret <vincentbiret@hotmail.com>
2025-10-14 12:33:49 -0400

    feat: adds function overload to match the return type to the upgrade method
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

--- packages/openapi-upgrader/src/upgrade.ts
+++ packages/openapi-upgrader/src/upgrade.ts
@@ -1,5 +1,4 @@
-import type { OpenAPIV3, OpenAPIV3_1 } from '@scalar/openapi-types'
-import type { UnknownObject } from '@scalar/types/utils'
+import type { OpenAPI, OpenAPIV2, OpenAPIV3, OpenAPIV3_1, OpenAPIV3_2 } from '@scalar/openapi-types'
 
 import { upgradeFromThreeOneToThreeTwo } from '@/3.1-to-3.2'
 
@@ -7,9 +6,18 @@ import { upgradeFromTwoToThree } from './2.0-to-3.0'
 import { upgradeFromThreeToThreeOne } from './3.0-to-3.1'
 
 /**
- * Upgrade OpenAPI documents from Swagger 2.0 or OpenAPI 3.0 to OpenAPI 3.1.0
+ * Upgrade OpenAPI documents from Swagger 2.0 or OpenAPI 3.0 to the specified target version
  */
-export function upgrade(value: UnknownObject, targetVersion: '3.0' | '3.1' | '3.2') {
+export function upgrade(value: OpenAPIV2.Document, targetVersion: '3.0'): OpenAPIV3.Document
+export function upgrade(value: OpenAPIV2.Document | OpenAPIV3.Document, targetVersion: '3.1'): OpenAPIV3_1.Document
+export function upgrade(
+  value: OpenAPIV2.Document | OpenAPIV3.Document | OpenAPIV3_1.Document,
+  targetVersion: '3.2',
+): OpenAPIV3_2.Document
+export function upgrade(
+  value: OpenAPI.Document,
+  targetVersion: '3.0' | '3.1' | '3.2',
+): OpenAPIV3.Document | OpenAPIV3_1.Document | OpenAPIV3_2.Document {
   // Swagger 2.0 -> OpenAPI 3.0
   const openapi30 = upgradeFromTwoToThree(value)
   if (targetVersion === '3.0') {
@@ -25,8 +33,7 @@ export function upgrade(value: UnknownObject, targetVersion: '3.0' | '3.1' | '3.
   // OpenAPI 3.1 -> OpenAPI 3.2
   const openapi32 = upgradeFromThreeOneToThreeTwo(openapi31)
   if (targetVersion === '3.2') {
-    // TODO: We need OpenAPI 3.2 types
-    return openapi32 as OpenAPIV3_1.Document
+    return openapi32 as OpenAPIV3_2.Document
   }
 
   return openapi32
