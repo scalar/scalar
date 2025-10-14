f7e0c820de35b25c87863fb60df79ee59037a8cf
Vincent Biret <vincentbiret@hotmail.com>
2025-10-14 15:20:11 -0400

    chore: reverts some overloads specialization to maintain public api surface compatibility
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

--- packages/openapi-upgrader/src/upgrade.ts
+++ packages/openapi-upgrader/src/upgrade.ts
@@ -4,15 +4,16 @@ import { upgradeFromThreeOneToThreeTwo } from '@/3.1-to-3.2'
 
 import { upgradeFromTwoToThree } from './2.0-to-3.0'
 import { upgradeFromThreeToThreeOne } from './3.0-to-3.1'
+import type { UnknownObject } from '@scalar/types/utils'
 
 /**
  * Upgrade OpenAPI documents from Swagger 2.0 or OpenAPI 3.0 to the specified target version
  */
-export function upgrade(value: OpenAPIV2.Document, targetVersion: '3.0'): OpenAPIV3.Document
-export function upgrade(value: OpenAPIV2.Document | OpenAPIV3.Document, targetVersion: '3.1'): OpenAPIV3_1.Document
-export function upgrade(value: OpenAPI.Document, targetVersion: '3.2'): OpenAPIV3_2.Document
+export function upgrade(value: UnknownObject, targetVersion: '3.0'): OpenAPIV3.Document
+export function upgrade(value: UnknownObject, targetVersion: '3.1'): OpenAPIV3_1.Document
+export function upgrade(value: UnknownObject, targetVersion: '3.2'): OpenAPIV3_2.Document
 export function upgrade(
-  value: OpenAPI.Document,
+  value: UnknownObject,
   targetVersion: '3.0' | '3.1' | '3.2',
 ): OpenAPIV3.Document | OpenAPIV3_1.Document | OpenAPIV3_2.Document {
   // Swagger 2.0 -> OpenAPI 3.0
