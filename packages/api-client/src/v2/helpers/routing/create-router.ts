e8536b7277b27d98c707a621b9127d8f17ba15f5
Amrit <amrit@scalar.com>
2025-10-15 14:34:38 -0700

    chore: routing complete

--- packages/api-client/src/v2/helpers/routing/create-router.ts
+++ packages/api-client/src/v2/helpers/routing/create-router.ts
@@ -6,7 +6,8 @@ import {
 } from 'vue-router'
 
 import type { CreateApiClientOptions } from '@/v2/helpers/create-api-client'
-import { modalRoutes } from '@/v2/helpers/create-routes'
+
+import { appRoutes, modalRoutes } from './routes'
 
 /**
  * Creates the appropriate router with the appropriate routes based on the layout
@@ -16,11 +17,12 @@ export const createRouter = ({ layout = 'desktop', store }: Pick<CreateApiClient
     // Modal is missing the workspace routes
     case 'modal':
       return createVueRouter({ history: createMemoryHistory(), routes: modalRoutes })
-    // Deskop and web have the same routes
-    case 'desktop':
-      return createVueRouter({ history: createWebHashHistory(), routes: [] })
+    // Web uses the standard HTML5 history API
     case 'web':
+      return createVueRouter({ history: createWebHistory(), routes: appRoutes })
+    // Electron app has to use the webHashHistory due to file routing
+    case 'desktop':
     default:
-      return createVueRouter({ history: createWebHistory(), routes: [] })
+      return createVueRouter({ history: createWebHashHistory(), routes: appRoutes })
   }
 }
