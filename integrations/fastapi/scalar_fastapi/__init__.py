670ea99efd614a631d2fd3299ef800a8ea7bcc49
Hamza Plojovic <hamzaplojovic9@gmail.com>
2025-10-17 19:54:18 +0200

    Add OpenAPISource to __init__, so It can be imported
    I cannot believe this was pushed to pypi in this state

--- integrations/fastapi/scalar_fastapi/__init__.py
+++ integrations/fastapi/scalar_fastapi/__init__.py
@@ -1,3 +1,3 @@
-from .scalar_fastapi import get_scalar_api_reference, Layout, SearchHotKey, Theme
+from .scalar_fastapi import get_scalar_api_reference, Layout, SearchHotKey, Theme, OpenAPISource
 
-__all__ = ["get_scalar_api_reference", "Layout", "SearchHotKey", "Theme"]
+__all__ = ["get_scalar_api_reference", "Layout", "SearchHotKey", "Theme", "OpenAPISource"]
