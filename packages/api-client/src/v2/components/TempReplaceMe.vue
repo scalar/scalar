93d8924a7062e62eba964e8d5c6c620b02abf8df
Amrit <amrit@scalar.com>
2025-10-16 17:16:04 -0700

    feat: the layout is complete

--- packages/api-client/src/v2/components/TempReplaceMe.vue
+++ packages/api-client/src/v2/components/TempReplaceMe.vue
@@ -3,11 +3,5 @@
 <template>
   <div>
     <h1>This is just a dummy page</h1>
-
-    <button
-      type="button"
-      @click="$emit('newTab', 'stuff')">
-      New Tab
-    </button>
   </div>
 </template>
