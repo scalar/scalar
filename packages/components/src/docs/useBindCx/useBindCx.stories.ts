c40da9025dc94a2e2c02e0155dd4118d2937b2dd
Brynn Hawker <brynn@hawker.me>
2025-10-20 17:11:58 -0700

    docs(useBindCx): update examples to show splitting of style attrs and other attrs

--- packages/components/src/docs/useBindCx/useBindCx.stories.ts
+++ packages/components/src/docs/useBindCx/useBindCx.stories.ts
@@ -1,11 +1,59 @@
-import type { Meta, StoryObj } from '@storybook/vue3'
-import { computed, ref, watch } from 'vue'
-import { MockComponent } from './MockComponent'
+import type { Args, Meta, StoryObj } from '@storybook/vue3'
+import { type Component, computed, ref, watch } from 'vue'
+
+import { ComplexComponent, SimpleComponent } from './MockComponents'
 
 const attrsToList = (attrs?: Record<string, any>) =>
   Object.entries(attrs || {})
     .map(([key, value]) => (typeof value === 'string' ? `${key}="${value}"` : `:${key}="${value}"`))
     .join(' ')
+
+const renderStory =
+  ({ component }: { component: Component }) =>
+  (args: Args) => ({
+    components: { MockComponent: component },
+    setup() {
+      const mock = ref<InstanceType<typeof SimpleComponent>>()
+
+      const rendered = ref('')
+      const observer = new MutationObserver(() => {
+        rendered.value = mock.value?.$el.outerHTML ?? ''
+      })
+      watch(
+        mock,
+        () => {
+          observer.disconnect()
+          if (!mock.value) {
+            return
+          }
+          rendered.value = mock.value.$el.outerHTML
+          observer.observe(mock.value.$el, { attributes: true, subtree: true })
+        },
+        { immediate: true },
+      )
+
+      const passedIn = computed(() => {
+        return `<MockComponent ${args.class ? `class="${args.class}"` : ''} ${args.active ? 'active' : ''} ${attrsToList(args.attrs)}/>`
+      })
+
+      const bind = computed(() => ({
+        ...args,
+        attrs: undefined,
+        ...args.attrs,
+      }))
+      return { bind, passedIn, rendered, mock }
+    },
+    template: `
+<div class="grid grid-cols-[auto_1fr] items-center gap-y-8 gap-x-4">
+  <span class="font-medium">Template</span>
+  <code class="font-code">{{ passedIn }}</code>
+  <span class="font-medium">HTML</span>
+  <code class="font-code">{{ rendered }}</code>
+  <span class="font-medium">Rendered</span>
+  <MockComponent ref="mock" v-bind="bind" />
+</div>
+`,
+  })
 
 const meta = {
   title: 'Playgrounds / useBindCx',
@@ -29,6 +77,15 @@ const meta = {
         defaultValue: { summary: '' },
       },
     },
+    style: {
+      control: 'text',
+      description: 'Styles to apply to the component',
+      table: {
+        subcategory: 'Attributes',
+        type: { summary: 'object' },
+        defaultValue: { summary: '' },
+      },
+    },
     attrs: {
       control: 'object',
       description: 'Additional attributes to apply to the component',
@@ -39,53 +96,15 @@ const meta = {
       },
     },
   },
-  render: (args) => ({
-    components: { MockComponent },
-    setup() {
-      const mock = ref<InstanceType<typeof MockComponent>>()
-
-      const rendered = ref('')
-      const observer = new MutationObserver((mutations) => {
-        rendered.value = (mutations[0]?.target as HTMLElement)?.outerHTML ?? ''
-      })
-      watch(
-        mock,
-        () => {
-          observer.disconnect()
-
-          if (!mock.value) {
-            return
-          }
-          rendered.value = mock.value.$el.outerHTML
-          observer.observe(mock.value.$el, { attributes: true })
-        },
-        { immediate: true },
-      )
-
-      const passedIn = computed(() => {
-        return `<MockComponent ${args.class ? `class="${args.class}"` : ''} ${args.active ? 'active' : ''} ${attrsToList(args.attrs)}/>`
-      })
-      const bind = computed(() => ({
-        ...args,
-        attrs: undefined,
-        ...args.attrs,
-      }))
-      return { bind, passedIn, rendered, mock }
-    },
-    template: `
-<table class="grid grid-cols-[auto_1fr] items-center gap-y-8 gap-x-4">
-  <span class="font-medium">Template</span>
-  <code class="font-code">{{ passedIn }}</code>
-  <span class="font-medium">HTML</span>
-  <code class="font-code">{{ rendered }}</code>
-  <span class="font-medium">Rendered</span>
-  <MockComponent ref="mock" v-bind="bind" />
-</table>
-`,
-  }),
 } satisfies Meta
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Playground: Story = {}
+export const Simple: Story = {
+  render: renderStory({ component: SimpleComponent }),
+}
+
+export const Complex: Story = {
+  render: renderStory({ component: ComplexComponent }),
+}
