beed040b3398a77e63d064953db597a9181caa21
Amrit <amrit@scalar.com>
2025-10-23 11:23:24 -0700

    fix: auth issue

--- packages/api-reference/src/v2/helpers/map-config-to-client-store.ts
+++ packages/api-reference/src/v2/helpers/map-config-to-client-store.ts
@@ -187,6 +187,7 @@ export function mapConfigToClientStore({
         shouldLoad: true,
         documentUrl: undefined,
         useCollectionSecurity: true,
+        ...toValue(config),
       })
     },
   )
