03f9b8c41dd0dc4b9d30fc760ac40e878604d75d
Jan van Heesch <jan.vanheesch@milhouse.solutions>
2025-10-24 23:24:22 +0200

    chore(django-ninja): update setup.py and package.json; add run_tests.py

--- integrations/django-ninja/setup.py
+++ integrations/django-ninja/setup.py
@@ -1,18 +1,25 @@
+import json
+import os
 from setuptools import setup, find_packages
+
+
+# Read version from package.json
+def get_version():
+    package_json_path = os.path.join(os.path.dirname(__file__), "package.json")
+    with open(package_json_path, "r") as f:
+        package_data = json.load(f)
+    return package_data["version"]
+
 
 setup(
     name="scalar_django_ninja",
-    version="1.0.0",
-    packages=find_packages(),
-    install_requires=[
-        # Add your package dependencies here
-    ],
+    version=get_version(),
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[],
     author="Marc Laventure",
     author_email="marc@scalar.com",
     description="This plugin provides an easy way to render a beautiful API reference based on a OpenAPI/Swagger file with Django Ninja.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/scalar/scalar",
-    # classifiers=[
-    # ],
 )
