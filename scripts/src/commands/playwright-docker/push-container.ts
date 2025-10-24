aab4f996d12414dea174e59d2ae8ff76e23bfc7b
Geoff Scott <geoffgscott@gmail.com>
2025-10-24 15:54:16 -0700

    feat: revert playwright version

--- scripts/src/commands/playwright-docker/push-container.ts
+++ scripts/src/commands/playwright-docker/push-container.ts
@@ -1,12 +1,11 @@
 import { Command } from 'commander'
 
 import { runCommand } from '@/helpers'
-import { latestVersion } from '@/helpers/npm-version'
 
 export const updatePlaywrightDocker = new Command('update-playwright-docker')
   .description('Update the docker image to the latest playwright')
   .action(async () => {
-    const { version } = await latestVersion('playwright', '1.56.1')
+    const version = '1.56.0'
 
     await runCommand(
       `docker build -t "scalarapi/playwright:${version}" --build-arg PLAYWRIGHT_VERSION=${version} ${import.meta.dirname}`,
