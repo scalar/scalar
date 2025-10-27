b64265b3e8b447a4d1c6dafaca8135ef69545d98
Amrit Kahlon <amrit@scalar.com>
2025-10-27 12:58:37 -0700

    chore(workspace-store): type updates to the workspace bus (#7181)
    * fix: workspace bus types
    
    * docs(changeset): chore: type updates to workspace bus

--- packages/workspace-store/src/events/definitions/meta.ts
+++ packages/workspace-store/src/events/definitions/meta.ts
@@ -1,6 +1,6 @@
 import type { AvailableClients } from '@scalar/types/snippetz'
 
-/** Event definitions for the workspace meta */
+/** Event definitions for the workspace/document meta */
 export type MetaEvents = {
   /** Update the dark mode theme setting */
   'update:dark-mode': boolean
@@ -8,4 +8,9 @@ export type MetaEvents = {
   'update:active-document': string
   /** Update the selected client on the workspace */
   'update:selected-client': AvailableClients[number]
+  /** Update the icon of the active document/workspace*/
+  'update:icon': {
+    icon: string
+    type: 'document' | 'workspace'
+  }
 }
