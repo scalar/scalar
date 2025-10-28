399ea98b0b5da2ded86f5219ba87c2cf68338df6
DemonHa <hh360284@gmail.com>
2025-10-28 19:48:20 -0400

    chore: resolve threads

--- packages/workspace-store/src/helpers/unpack-proxy.ts
+++ packages/workspace-store/src/helpers/unpack-proxy.ts
@@ -8,6 +8,5 @@ import { unpackOverridesProxy } from '@/helpers/overrides-proxy'
  * Unpacks special vue reactivity & override & detect-changes & magic proxy from an input object or array,
  * returning the "raw" plain object or array.
  */
-export const unpackProxyObject = <T extends object | Array<unknown>>(input: T): T => {
-  return unpackDetectChangesProxy(toRaw(getRaw(unpackOverridesProxy(input))))
-}
+export const unpackProxyObject = <T extends object | Array<unknown>>(input: T): T =>
+  unpackDetectChangesProxy(toRaw(getRaw(unpackOverridesProxy(input))))
