7a22de509a2fb0a45c9afc147af24884dd99e124
Amrit <amrit@scalar.com>
2025-10-31 14:28:00 -0700

    fix: whoops

--- packages/api-client/src/vitest.setup.ts
+++ packages/api-client/src/vitest.setup.ts
@@ -56,6 +56,18 @@ beforeEach(() => {
     toggleSidebarOpen: vi.fn(),
     setSidebarOpen: vi.fn(),
   })
+
+  /**
+   * Mock ResizeObserver which is used by @headlessui/vue Dialog component
+   * but is not available in the test environment. If you need to test it you can set your own mock in the test file.
+   *
+   * @see https://github.com/jsdom/jsdom/issues/3368
+   */
+  global.ResizeObserver = vi.fn().mockImplementation(() => ({
+    disconnect: vi.fn(),
+    observe: vi.fn(),
+    unobserve: vi.fn(),
+  }))
 })
 
 afterEach(() => {
