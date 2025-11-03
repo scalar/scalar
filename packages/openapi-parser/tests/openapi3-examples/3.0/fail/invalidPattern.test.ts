1fb945b538a537c0ea32e52d0b8ebaf6565514cd
Luke Hagar <luke.hagar@sailpoint.com>
2025-11-03 09:21:25 -0600

    Update packages/openapi-parser/tests/openapi3-examples/3.0/fail/invalidPattern.test.ts
    Co-authored-by: Hans Pagel <hanspagel@users.noreply.github.com>

--- packages/openapi-parser/tests/openapi3-examples/3.0/fail/invalidPattern.test.ts
+++ packages/openapi-parser/tests/openapi3-examples/3.0/fail/invalidPattern.test.ts
@@ -7,8 +7,6 @@ describe('invalidPattern', () => {
   it('returns an error', async () => {
     const result = await validate(invalidPattern)
 
-    // Now gives a better error than the generic $ref error.
-    // The pattern format validation is stricter and catches the invalid regex.
     expect(result.errors?.[0]?.message).toBe('format must match format "regex"')
     expect(result.errors?.length).toBe(1)
     expect(result.valid).toBe(false)
