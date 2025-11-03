94bad575694c48b90ef8a2c8d9cb0d9c86a578bc
Luke Hagar <luke.hagar@sailpoint.com>
2025-11-03 09:21:09 -0600

    Update packages/openapi-parser/tests/openapi3-examples/3.0/fail/invalidSchemaName.test.ts
    Co-authored-by: Hans Pagel <hanspagel@users.noreply.github.com>

--- packages/openapi-parser/tests/openapi3-examples/3.0/fail/invalidSchemaName.test.ts
+++ packages/openapi-parser/tests/openapi3-examples/3.0/fail/invalidSchemaName.test.ts
@@ -7,8 +7,6 @@ describe('invalidSchemaName', () => {
   it('returns an error', async () => {
     const result = await validate(invalidSchemaName)
 
-    // This test used to fail due to an error that has been corrected,
-    // I fixed the other OpenAPI syntax issues in the test file and now it passes as expected.
     expect(result.errors?.length).toBe(0)
     expect(result.valid).toBe(true)
   })
