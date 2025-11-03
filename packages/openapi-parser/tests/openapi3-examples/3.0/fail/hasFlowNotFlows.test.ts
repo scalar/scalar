530d77cf63865baef93851b041ac91ac7ff71e50
Luke Hagar <luke.hagar@sailpoint.com>
2025-11-03 09:21:37 -0600

    Update packages/openapi-parser/tests/openapi3-examples/3.0/fail/hasFlowNotFlows.test.ts
    Co-authored-by: Hans Pagel <hanspagel@users.noreply.github.com>

--- packages/openapi-parser/tests/openapi3-examples/3.0/fail/hasFlowNotFlows.test.ts
+++ packages/openapi-parser/tests/openapi3-examples/3.0/fail/hasFlowNotFlows.test.ts
@@ -7,7 +7,6 @@ describe('hasFlowNotFlows', () => {
   it('returns an error', async () => {
     const result = await validate(hasFlowNotFlows)
 
-    // The security scheme has "flow" instead of "flows" (typo).
     // Thanks to the discriminator on the SecurityScheme oneOf (using the "type" property),
     // AJV knows to validate against OAuth2SecurityScheme and gives us a specific error.
     expect(result.errors?.[0]?.message).toBe('Property flow is not expected to be here')
