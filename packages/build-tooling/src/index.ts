065a8e0ed773490368a448836fc5eb6eecf88ef1
Marco Pasqualetti <marco.pasqualetti@live.com>
2025-11-12 18:45:38 +0100

    feat(build-tooling): remove deprecated exports

--- packages/build-tooling/src/index.ts
+++ packages/build-tooling/src/index.ts
@@ -1,16 +1,2 @@
 /** biome-ignore-all lint/performance/noBarrelFile: entrypoint */
 export { addPackageFileExports, findEntryPoints } from './helpers'
-
-import { createRollupConfig } from './rollup'
-import { ViteWatchWorkspace, alias, createViteBuildOptions } from './vite'
-
-export {
-  /** @deprecated import from '@scalar/build-tooling/vite' */
-  alias,
-  /** @deprecated import from '@scalar/build-tooling/vite' */
-  createViteBuildOptions,
-  /** @deprecated import from '@scalar/build-tooling/vite' */
-  ViteWatchWorkspace,
-  /** @deprecated import from '@scalar/build-tooling/rollup' */
-  createRollupConfig,
-}
