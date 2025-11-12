e8a40aa2a73339b7b1963a4c4e75e099174a5857
DemonHa <hh360284@gmail.com>
2025-11-12 17:45:12 -0500

    chore: add galaxy and petstore as default documents for testing

--- packages/api-client/playground/v2/web/main.ts
+++ packages/api-client/playground/v2/web/main.ts
@@ -1,57 +1,6 @@
 import '@/style.css'
 
-import { createWorkspaceStore } from '@scalar/workspace-store/client'
-
 import { createApiClientApp } from '@/v2/features/app'
-
-const workspaceStore = createWorkspaceStore()
-await workspaceStore.addDocument({
-  name: 'scalar-galaxy',
-  url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.json',
-  config: {
-    'x-scalar-reference-config': {
-      settings: {
-        proxyUrl: 'https://proxy.scalar.com',
-      },
-    },
-  },
-})
-await workspaceStore.addDocument({
-  name: 'swagger-petstore-3-0',
-  url: 'https://petstore3.swagger.io/api/v3/openapi.json',
-  config: {
-    'x-scalar-reference-config': {
-      settings: {
-        proxyUrl: 'https://proxy.scalar.com',
-      },
-    },
-  },
-})
-
-await workspaceStore.addDocument({
-  name: 'drafts',
-  document: {
-    openapi: '3.2.0',
-    info: {
-      title: 'Drafts',
-      version: '1.0.0',
-    },
-    paths: {
-      '/': {
-        get: {
-          summary: 'My First Request',
-        },
-      },
-    },
-  },
-  config: {
-    'x-scalar-reference-config': {
-      settings: {
-        proxyUrl: 'https://proxy.scalar.com',
-      },
-    },
-  },
-})
 
 const el = document.getElementById('scalar-client')
 
--- packages/api-client/src/v2/hooks/use-workspace-selector.ts
+++ packages/api-client/src/v2/hooks/use-workspace-selector.ts
@@ -97,6 +97,18 @@ export const useWorkspaceSelector = ({
       document: defaultDocument,
     })
 
+    // For the dev mode we might want couple more documents for testing purpose
+    // TODO: remove as soon as the command palette is implemented
+    await draftStore.addDocument({
+      name: 'scalar-galaxy',
+      url: 'https://galaxy.scalar.com/openapi.yaml',
+    })
+
+    await draftStore.addDocument({
+      name: 'swagger-petstore-3-0',
+      url: 'https://petstore3.swagger.io/api/v3/openapi.json',
+    })
+
     const persistence = await persistencePromise
     await persistence.workspace.setItem(id, {
       name: name,
