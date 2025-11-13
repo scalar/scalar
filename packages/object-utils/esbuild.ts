a68b1afc54b2aa95a224fee89c3266bffed57215
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2025-11-13 12:09:21 +0100

    fix(object-utils): enable `knip` (#7274)
    * fix(object-utils): enable knip
    
    * refactor(object-utils): add esbuild explicit exports
    
    * refactor(object-utils): remove unused internal types

--- packages/object-utils/esbuild.ts
+++ packages/object-utils/esbuild.ts
@@ -1,6 +1,14 @@
 import { build } from '@scalar/build-tooling/esbuild'
 
 build({
-  entries: 'auto',
+  entries: [
+    './src/arrays/index.ts',
+    './src/clone/index.ts',
+    './src/merge/index.ts',
+    './src/mutator-record/index.ts',
+    './src/nested/index.ts',
+    './src/parse/index.ts',
+    './src/transforms/index.ts',
+  ],
   platform: 'shared',
 })
