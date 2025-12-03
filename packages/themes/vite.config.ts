35af6bfce4bc4ae3d0fc5783ea36e0165e964361
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2025-12-03 11:28:35 +0100

    fix(themes): enable `knip` (#7458)

--- packages/themes/vite.config.ts
+++ packages/themes/vite.config.ts
@@ -1,9 +1,7 @@
 import { createViteBuildOptions } from '@scalar/build-tooling/vite'
-import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue()],
   build: {
     ...createViteBuildOptions({
       entry: ['src/index.ts', 'src/tailwind.css', 'src/style.css', 'src/fonts/fonts.css'],
