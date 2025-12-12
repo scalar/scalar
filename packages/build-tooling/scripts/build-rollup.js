f5b0ed42906801362464d23a8dd16c9634be2060
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2025-12-12 12:40:47 +0100

    feat(build-tooling): remove `rollup` (#7516)

--- packages/build-tooling/scripts/build-rollup.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import { executeCommands } from './utils/utils.js'
-
-executeCommands(['rollup -c rollup.config.ts --configPlugin typescript', 'pnpm types:build'], 'Rollup build')
