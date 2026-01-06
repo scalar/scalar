d43565c8616d0f95ed21f881b936e69a6d59a5c9
Amrit <amrit@scalar.com>
2026-01-06 15:17:03 -0800

    fix: default security

--- packages/api-reference/src/components/Content/Auth/Auth.vue
+++ packages/api-reference/src/components/Content/Auth/Auth.vue
@@ -9,20 +9,25 @@ import {
 } from '@scalar/api-client/v2/features/operation'
 import type { ApiReferenceConfigurationRaw } from '@scalar/types/api-reference'
 import type { WorkspaceEventBus } from '@scalar/workspace-store/events'
-import { unpackProxyObject } from '@scalar/workspace-store/helpers/unpack-proxy'
 import type { XScalarEnvironment } from '@scalar/workspace-store/schemas/extensions/document/x-scalar-environments'
 import type { ServerObject } from '@scalar/workspace-store/schemas/v3.1/strict/openapi-document'
 import type { WorkspaceDocument } from '@scalar/workspace-store/schemas/workspace'
 import { computed, watch } from 'vue'
 
-const { document, environment, eventBus, options } = defineProps<{
-  options: Pick<ApiReferenceConfigurationRaw, 'persistAuth' | 'proxyUrl'>
-  document: WorkspaceDocument | undefined
-  eventBus: WorkspaceEventBus
-  securitySchemes: MergedSecuritySchemes
-  selectedServer: ServerObject | null
-  environment: XScalarEnvironment
-}>()
+import { getDefaultSecurity } from '@/components/Content/Auth/helpers/get-default-security'
+
+const { document, environment, eventBus, options, securitySchemes } =
+  defineProps<{
+    options: Pick<
+      ApiReferenceConfigurationRaw,
+      'authentication' | 'persistAuth' | 'proxyUrl'
+    >
+    document: WorkspaceDocument | undefined
+    eventBus: WorkspaceEventBus
+    securitySchemes: MergedSecuritySchemes
+    selectedServer: ServerObject | null
+    environment: XScalarEnvironment
+  }>()
 
 /** Compute what the security requirements should be for the document */
 const securityRequirements = computed(() =>
@@ -42,18 +47,21 @@ const selectedSecurity = computed(() =>
 watch(
   () => document?.['x-scalar-selected-security'],
   (documentSelectedSecurity) => {
-    if (
-      typeof documentSelectedSecurity !== 'undefined' ||
-      !securityRequirements.value.length
-    ) {
+    if (typeof documentSelectedSecurity !== 'undefined') {
+      return
+    }
+
+    const selectedRequirements = getDefaultSecurity(
+      securityRequirements.value,
+      options.authentication?.preferredSecurityScheme,
+      securitySchemes,
+    )
+    if (!selectedRequirements) {
       return
     }
 
     eventBus.emit('auth:update:selected-security-schemes', {
-      selectedRequirements: unpackProxyObject(
-        securityRequirements.value.slice(0, 1),
-        { depth: 1 },
-      ),
+      selectedRequirements: [selectedRequirements],
       newSchemes: [],
       meta: { type: 'document' },
     })
