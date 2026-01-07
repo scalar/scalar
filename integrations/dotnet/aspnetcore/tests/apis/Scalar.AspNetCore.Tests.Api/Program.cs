28e43127fe2267e6c846f92c49cc0c13f84cfabc
Justin Lampe <xC0dex@users.noreply.github.com>
2026-01-07 10:47:22 +0100

    remove support for obsolete EndpointPrefix property

--- integrations/dotnet/aspnetcore/tests/apis/Scalar.AspNetCore.Tests.Api/Program.cs
+++ integrations/dotnet/aspnetcore/tests/apis/Scalar.AspNetCore.Tests.Api/Program.cs
@@ -22,10 +22,6 @@ app.MapScalarApiReference("/external/document/scalar", options =>
     options.OpenApiRoutePattern = "https://example.com/openapi.json";
 }).AllowAnonymous();
 
-#pragma warning disable CS0618 // Type or member is obsolete
-app.MapScalarApiReference(options => options.WithEndpointPrefix("/legacy/{documentName}")).AllowAnonymous();
-#pragma warning restore CS0618 // Type or member is obsolete
-
 app.Run();
 
 public partial class Program;
