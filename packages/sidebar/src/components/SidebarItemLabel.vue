bf1b41f21a0e9717881812fb210c75f4c93048a9
Brynn Hawker <brynn@hawker.me>
2026-01-09 13:52:30 -0800

    refactor(sidebar): only add a span if the item is actually deprecated

---
+++ packages/sidebar/src/components/SidebarItemLabel.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { ScalarWrappingText } from '@scalar/components'
+
+import type { Item } from '@/types'
+
+const { item } = defineProps<{
+  /**
+   * The sidebar item to render.
+   */
+  item: Item
+  /**
+   * The source of the operation title.
+   */
+  operationTitleSource?: 'path' | 'summary'
+}>()
+</script>
+<template>
+  <template v-if="item.type === 'model'">
+    <ScalarWrappingText
+      preset="property"
+      :text="item.title" />
+  </template>
+  <template v-else>
+    <ScalarWrappingText
+      :text="
+        operationTitleSource === 'path' && 'path' in item
+          ? (item.path as string)
+          : (item.title as string)
+      " />
+  </template>
+</template>
