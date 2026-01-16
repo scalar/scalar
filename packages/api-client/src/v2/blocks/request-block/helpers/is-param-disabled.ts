bfc13a625aa3159d7b1098603f657fdb4d8ac67a
Amrit <amrit@scalar.com>
2026-01-16 12:14:26 -0800

    chore: tests updated

--- packages/api-client/src/v2/blocks/request-block/helpers/is-param-disabled.ts
+++ packages/api-client/src/v2/blocks/request-block/helpers/is-param-disabled.ts
@@ -10,5 +10,14 @@ import type { ExampleObject, ParameterObject } from '@scalar/workspace-store/sch
  * @param example - The example to check.
  * @returns true if the parameter is disabled, false otherwise.
  */
-export const isParamDisabled = (param: ParameterObject, example: ExampleObject | undefined) =>
-  example?.['x-disabled'] ?? (!param.required && param.in !== 'path')
+export const isParamDisabled = (param: ParameterObject, example: ExampleObject | undefined) => {
+  const xDisabled = example?.['x-disabled']
+
+  // If x-disabled is explicitly set (true or false), use that value
+  if (typeof xDisabled === 'boolean') {
+    return xDisabled
+  }
+
+  // Otherwise, disable optional parameters (except path parameters which are always required)
+  return !param.required && param.in !== 'path'
+}
