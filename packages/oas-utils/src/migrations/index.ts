59028ace457d5c3dd45806c8cec89f92a4cc0941
Amrit <amrit@scalar.com>
2026-01-20 14:19:57 -0800

    feat: got initial migration strat up

--- packages/oas-utils/src/migrations/index.ts
+++ packages/oas-utils/src/migrations/index.ts
@@ -1,3 +1,10 @@
 export { DATA_VERSION, DATA_VERSION_LS_LEY } from './data-version'
+export {
+  clearLegacyLocalStorage,
+  markMigrationComplete,
+  migrateLocalStorageToIndexDb,
+  shouldMigrateToIndexDb,
+  transformLegacyDataToWorkspace,
+} from './migrate-to-indexdb'
 export { migrator } from './migrator'
 export { semverLessThan } from './semver'
