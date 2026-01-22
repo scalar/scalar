bd78e21512d42c68dbcef768ff42a686c0205bf4
Amrit <amrit@scalar.com>
2026-01-22 10:43:09 -0800

    chore: ignore instead

--- packages/workspace-store/esbuild.ts
+++ packages/workspace-store/esbuild.ts
@@ -8,7 +8,6 @@ const entries = [
   './src/schemas/*.ts',
   './src/schemas/extensions/operation/index.ts',
   './src/schemas/extensions/workspace/index.ts',
-  './src/schemas/reference-config/index.ts',
   './src/schemas/v3.1/strict/openapi-document.ts',
   './src/schemas/v3.1/strict/type-guards.ts',
   './src/events/index.ts',
