3b117d89ae39174092a2592b84d9766a2a5b6f99
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2026-01-27 10:41:10 +0100

    fix(nextjs): enable `knip` (#7908)

---
+++ .changeset/fresh-heads-say.md
@@ -0,0 +1,5 @@
+---
+'@scalar/nextjs-api-reference': patch
+---
+
+fix: use named instead of star exports
--- integrations/nextjs/vite.config.ts
+++ integrations/nextjs/vite.config.ts
@@ -1,5 +1,6 @@
-import react from '@vitejs/plugin-react'
 import * as path from 'node:path'
+
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
