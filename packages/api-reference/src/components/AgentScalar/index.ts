b01002887e8661b4e605439291fe4920577b130a
marclave <marc@scalar.com>
2026-02-01 12:53:08 -0800

    fix: remove unused export

--- packages/api-reference/src/components/AgentScalar/index.ts
+++ packages/api-reference/src/components/AgentScalar/index.ts
@@ -1,3 +1,2 @@
 export { default as AgentScalarButton } from './AgentScalarButton.vue'
 export { default as AgentScalarDrawer } from './AgentScalarDrawer.vue'
-export { default as AgentScalarTooltip } from './AgentScalarTooltip.vue'
