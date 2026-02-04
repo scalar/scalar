bcf69831bd4146f697e8fa27f5a7e0845e43aff5
Amrit <amrit@scalar.com>
2026-02-04 12:22:36 -0800

    feat: added posthog from npm package

--- projects/client-scalar-com/src/main.ts
+++ projects/client-scalar-com/src/main.ts
@@ -2,6 +2,13 @@ import { createApiClientWeb } from '@scalar/api-client/layouts/Web'
 import '@scalar/api-client/style.css'
 import './style.css'
 
+import posthog from 'posthog-js'
+
+posthog.init('phc_3elIjSOvGOo5aEwg6krzIY9IcQiRubsBtglOXsQ4Uu4', {
+  api_host: 'https://us.i.posthog.com',
+  defaults: '2025-11-30',
+})
+
 void createApiClientWeb(document.getElementById('scalar-client'), {
   proxyUrl: 'https://proxy.scalar.com',
 })
