d9a538cf53b35200dacd2577808ac47a6a2f4192
Hans Pagel <mail@hanspagel.com>
2026-02-12 16:57:29 +0100

    feat: nest description headings under introduction

--- packages/workspace-store/src/navigation/helpers/traverse-description.test.ts
+++ packages/workspace-store/src/navigation/helpers/traverse-description.test.ts
@@ -48,7 +48,66 @@ describe('traverseDescription', () => {
       },
       parentId: 'parent-1',
     })
-    expect(result).toEqual([{ id: 'heading-introduction', title: 'Introduction', type: 'text' }])
+    expect(result).toEqual([{ id: 'heading-introduction', title: 'Introduction', type: 'text', children: [] }])
+  })
+
+  it('should nest all headings under introduction when description starts with text', () => {
+    const description = `
+This is the introduction paragraph.
+
+# Main Section
+Some content
+## Subsection 1
+Details for subsection
+# Another Section
+More content
+## Another Subsection
+Final details
+    `
+    const result = traverseDescription({
+      info: { title: 'Openapi Document', version: '1.0.0', description },
+      generateId: (props) => {
+        if (props.type === 'text') {
+          return getHeadingId({ value: props.value })
+        }
+
+        return 'unknown-id'
+      },
+      parentId: 'parent-1',
+    })
+
+    expect(result).toHaveLength(1)
+    expect(result[0]).toEqual({
+      id: 'heading-introduction',
+      title: 'Introduction',
+      type: 'text',
+      children: [
+        {
+          id: 'heading-main-section',
+          title: 'Main Section',
+          type: 'text',
+          children: [
+            {
+              id: 'heading-subsection-1',
+              title: 'Subsection 1',
+              type: 'text',
+            },
+          ],
+        },
+        {
+          id: 'heading-another-section',
+          title: 'Another Section',
+          type: 'text',
+          children: [
+            {
+              id: 'heading-another-subsection',
+              title: 'Another Subsection',
+              type: 'text',
+            },
+          ],
+        },
+      ],
+    })
   })
 
   it('should create single level entries for h1 headings', () => {
