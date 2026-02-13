f63999148b5b26866e06c4bb12e573a4b4d167db
Amrit <amrit@scalar.com>
2026-02-13 14:50:53 -0800

    chore: remove red outline

--- packages/sidebar/src/components/SidebarItemDecorator.vue
+++ packages/sidebar/src/components/SidebarItemDecorator.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-b-2 border-red absolute top-[1lh] right-0.75 flex -translate-y-1/2 rounded border p-0.25 opacity-0 peer-hover/button:opacity-100 peer-focus-visible/button:opacity-100 focus-within:opacity-100 hover:opacity-100 has-[&[aria-expanded=true]]:opacity-100">
+    class="bg-b-2 absolute top-[1lh] right-0.75 flex -translate-y-1/2 rounded border p-px opacity-0 peer-hover/button:opacity-100 peer-focus-visible/button:opacity-100 focus-within:opacity-100 hover:opacity-100 has-aria-expanded:opacity-100">
     <slot />
   </div>
 </template>
