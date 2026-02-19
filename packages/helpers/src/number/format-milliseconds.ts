4655ff1bd496243ade15e9296ebdefc9c9c9e1c3
Amrit <amrit@scalar.com>
2026-02-19 15:42:47 -0800

    chore: fix

--- packages/helpers/src/number/format-milliseconds.ts
+++ packages/helpers/src/number/format-milliseconds.ts
@@ -3,7 +3,7 @@
  * else returns ms appended to the number
  */
 export const formatMilliseconds = (ms: number, decimals = 2): string => {
-  if (ms > 1000) {
+  if (Math.abs(ms) > 1000) {
     return (ms / 1000).toFixed(decimals) + 's'
   }
   return ms + 'ms'
