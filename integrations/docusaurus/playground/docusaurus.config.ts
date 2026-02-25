276bf2ec06fda7040a2a49e7c79deb69a127ccfb
Hans Pagel <mail@hanspagel.com>
2026-02-25 12:40:27 +0100

    fix: docusaurus blog breaks playground

--- integrations/docusaurus/playground/docusaurus.config.ts
+++ integrations/docusaurus/playground/docusaurus.config.ts
@@ -39,12 +39,7 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -128,7 +123,6 @@ paths: {}
           position: 'left',
           label: 'Tutorial',
         },
-        { to: '/blog', label: 'Blog', position: 'left' },
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
@@ -168,10 +162,6 @@ paths: {}
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
             {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
