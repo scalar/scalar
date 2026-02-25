525da9ea7fed7a32464a5b56745c2f6cdf18d0aa
Hans Pagel <mail@hanspagel.com>
2026-02-25 12:42:11 +0100

    fix: docusaurus warnings

--- integrations/docusaurus/playground/docusaurus.config.ts
+++ integrations/docusaurus/playground/docusaurus.config.ts
@@ -19,7 +19,11 @@ const config: Config = {
   projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
