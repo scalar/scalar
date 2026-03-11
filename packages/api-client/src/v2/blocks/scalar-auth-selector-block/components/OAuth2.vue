e293722d52ecb11a10f8c93b9a3e43a159d387a1
Liam Debell <liam@scalar.com>
2026-03-11 11:33:20 +0000

    feat(docs): Persist auth and token url in secrets

--- packages/api-client/src/v2/blocks/scalar-auth-selector-block/components/OAuth2.vue
+++ packages/api-client/src/v2/blocks/scalar-auth-selector-block/components/OAuth2.vue
@@ -234,21 +234,31 @@ const handleSecretLocationUpdate = (value: string): void =>
   <template v-else>
     <DataTableRow>
       <RequestAuthDataTableInput
-        v-if="'authorizationUrl' in flow"
+        v-if="flow['x-scalar-secret-auth-url']"
         containerClass="border-r-0"
         :environment
-        :modelValue="flow.authorizationUrl"
+        :modelValue="flow['x-scalar-secret-auth-url']"
         placeholder="https://galaxy.scalar.com/authorize"
-        @update:modelValue="(v) => handleOauth2Update({ authorizationUrl: v })">
+        @update:modelValue="
+          (v) => {
+            handleOauth2SecretsUpdate({ 'x-scalar-secret-auth-url': v })
+            handleOauth2Update({ authorizationUrl: v })
+          }
+        ">
         Auth URL
       </RequestAuthDataTableInput>
 
       <RequestAuthDataTableInput
-        v-if="'tokenUrl' in flow"
+        v-if="flow['x-scalar-secret-token-url']"
         :environment
-        :modelValue="flow.tokenUrl"
+        :modelValue="flow['x-scalar-secret-token-url']"
         placeholder="https://galaxy.scalar.com/token"
-        @update:modelValue="(v) => handleOauth2Update({ tokenUrl: v })">
+        @update:modelValue="
+          (v) => {
+            handleOauth2SecretsUpdate({ 'x-scalar-secret-token-url': v })
+            handleOauth2Update({ tokenUrl: v })
+          }
+        ">
         Token URL
       </RequestAuthDataTableInput>
     </DataTableRow>
@@ -259,8 +269,9 @@ const handleSecretLocationUpdate = (value: string): void =>
         :modelValue="flow['x-scalar-secret-redirect-uri']"
         placeholder="https://galaxy.scalar.com/callback"
         @update:modelValue="
-          (v) =>
+          (v) => {
             handleOauth2SecretsUpdate({ 'x-scalar-secret-redirect-uri': v })
+          }
         ">
         Redirect URL
       </RequestAuthDataTableInput>
--- packages/workspace-store/src/schemas/extensions/security/x-scalar-security-secrets.ts
+++ packages/workspace-store/src/schemas/extensions/security/x-scalar-security-secrets.ts
@@ -37,6 +37,42 @@ export type XScalarSecretRefreshToken = {
 }
 
 /**
+ * OAuth auth url
+ *
+ * We should not export this when exporting the document
+ */
+export const XScalarAuthUrlSchema = Type.Object({
+  'x-scalar-secret-auth-url': Type.Optional(Type.String()),
+})
+
+/**
+ * OAuth auth url
+ *
+ * We should not export this when exporting the document
+ */
+export type XScalarAuthUrl = {
+  'x-scalar-secret-auth-url'?: string
+}
+
+/**
+ * OAuth token url
+ *
+ * We should not export this when exporting the document
+ */
+export const XScalarTokenUrlSchema = Type.Object({
+  'x-scalar-secret-token-url': Type.Optional(Type.String()),
+})
+
+/**
+ * OAuth token url
+ *
+ * We should not export this when exporting the document
+ */
+export type XScalarTokenUrl = {
+  'x-scalar-secret-token-url'?: string
+}
+
+/**
  * Username and password for HTTP authentication
  *
  * We should not export this when exporting the document
