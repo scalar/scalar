2241c2dd40e674cda13cbbeca0e70a5d88f65944
Hans Pagel <mail@hanspagel.com>
2026-03-12 14:32:07 +0100

    fix: broken alias

--- examples/vue-ssr/vite.config.ts
+++ examples/vue-ssr/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('../../src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
     dedupe: ['vue'],
   },
