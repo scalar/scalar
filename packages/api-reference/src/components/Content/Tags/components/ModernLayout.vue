dbb441031a4a57ac559edd4d24f64871afad105d
DemonHa <hh360284@gmail.com>
2026-03-12 20:31:39 -0400

    remove extra lazy component?

--- packages/api-reference/src/components/Content/Tags/components/ModernLayout.vue
+++ packages/api-reference/src/components/Content/Tags/components/ModernLayout.vue
@@ -3,7 +3,6 @@ import type { WorkspaceEventBus } from '@scalar/workspace-store/events'
 import type { TraversedTag } from '@scalar/workspace-store/schemas/navigation'
 import { computed, useId } from 'vue'
 
-import Lazy from '@/components/Lazy/Lazy.vue'
 import { SectionContainer } from '@/components/Section'
 import ShowMoreButton from '@/components/ShowMoreButton.vue'
 
@@ -29,25 +28,22 @@ const hasChildren = computed(() => (tag?.children?.length ?? 0) > 0)
 <template>
   <SectionContainer
     :aria-labelledby="headerId"
-    class="tag-section-container divide-y"
+    class="tag-section-container"
     role="region">
-    <!-- Lazy load this part -->
-    <Lazy :id="tag.id">
-      <TagSection
-        v-if="moreThanOneDefaultTag"
-        :eventBus="eventBus"
-        :headerId="headerId"
-        :isCollapsed="isCollapsed"
-        :isLoading="isLoading"
-        :tag="tag" />
-      <ShowMoreButton
-        v-if="isCollapsed && moreThanOneTag && hasChildren"
-        :id="tag.id"
-        :aria-label="`Show all ${tag.title} endpoints`"
-        @click="
-          () => eventBus?.emit('toggle:nav-item', { id: tag.id, open: true })
-        " />
-    </Lazy>
+    <TagSection
+      v-if="moreThanOneDefaultTag"
+      :eventBus="eventBus"
+      :headerId="headerId"
+      :isCollapsed="isCollapsed"
+      :isLoading="isLoading"
+      :tag="tag" />
+    <ShowMoreButton
+      v-if="isCollapsed && moreThanOneTag && hasChildren"
+      :id="tag.id"
+      :aria-label="`Show all ${tag.title} endpoints`"
+      @click="
+        () => eventBus?.emit('toggle:nav-item', { id: tag.id, open: true })
+      " />
 
     <!-- We cannot use v-else due to the Lazy wrapper, but its the opposite of above -->
     <div
