599d10f58bb55eb896b0119a8121a6815129168b
Brynn Hawker <brynn@hawker.me>
2026-03-16 14:48:56 -0700

    feat(api-reference): use div for label instead of input

--- packages/api-reference/src/features/ask-agent-button/AskAgentButton.vue
+++ packages/api-reference/src/features/ask-agent-button/AskAgentButton.vue
@@ -17,12 +17,13 @@ function handleSubmit() {
 <template>
   <form
     v-if="agentContext?.agentEnabled.value"
-    class="agent-button-container flex"
+    class="agent-button-container"
     @click="inputRef?.focus()"
     @submit.prevent="handleSubmit()">
     <ScalarIconSparkle
       class="size-3 shrink-0"
       weight="fill" />
+    <div class="ask-agent-scalar-input-label">Ask AI Agent</div>
     <input
       ref="inputRef"
       v-model="message"
@@ -39,45 +40,7 @@ function handleSubmit() {
   </form>
 </template>
 <style scoped>
-.ask-agent-scalar-input {
-  color: var(--scalar-color-1);
-  appearance: none;
-  border: none;
-  white-space: nowrap;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-weight: var(--scalar-regular);
-  font-size: var(--scalar-small);
-  line-height: 22px;
-  font-family: var(--scalar-font);
-  position: relative;
-  cursor: pointer;
-  box-sizing: border-box;
-  outline: none;
-  outline-offset: 2px;
-  field-sizing: content;
-  max-width: 88px;
-}
-.ask-agent-scalar-input:focus {
-  cursor: text;
-  width: 100% !important;
-  max-width: calc(100% - 50px);
-}
-.ask-agent-scalar-input-not-empty {
-  width: 100% !important;
-  height: 100%;
-  field-sizing: border-box;
-  cursor: text;
-  max-width: calc(100% - 50px);
-}
-.ask-agent-scalar-input::placeholder {
-  color: var(--scalar-color-1);
-  font-family: var(--scalar-font);
-}
-.ask-agent-scalar-input:focus::placeholder {
-  color: var(--scalar-color-2);
-}
+/** Container */
 .agent-button-container {
   position: relative;
   color: var(--scalar-color-1);
@@ -88,7 +51,6 @@ function handleSubmit() {
   padding: 1px 6px;
   margin-right: 4px;
   border-radius: var(--scalar-radius);
-  gap: 4px;
   z-index: 2;
   height: 100%;
 }
@@ -97,13 +59,50 @@ function handleSubmit() {
 }
 .agent-button-container:has(.ask-agent-scalar-input-not-empty),
 .agent-button-container:focus-within {
-  width: calc(100% - 4px);
-  height: calc(100% - 4px);
   position: absolute;
-  left: 2px;
-  top: 2px;
-  border-radius: var(--scalar-radius);
+  inset: 2px;
+  border-radius: var(--scalar-radius-lg);
+  margin-right: 0;
+  height: auto;
 }
+.agent-button-container:has(.ask-agent-scalar-input:focus-visible) {
+  outline-style: solid;
+}
+/** Input */
+.ask-agent-scalar-input {
+  width: 0px;
+  opacity: 0;
+  font-size: 0px;
+  border: none;
+}
+.agent-button-container:has(.ask-agent-scalar-input-not-empty)
+  .ask-agent-scalar-input,
+.agent-button-container:focus-within .ask-agent-scalar-input {
+  width: 100%;
+  font-size: inherit;
+  opacity: 1;
+  line-height: 20px;
+  outline: none;
+  padding-inline: 4px;
+}
+.ask-agent-scalar-input::placeholder {
+  color: var(--scalar-color-2);
+  font-family: inherit;
+}
+/** Input label */
+.ask-agent-scalar-input-label {
+  /** Shown by default */
+  color: var(--scalar-color-1);
+  font-weight: var(--scalar-semibold);
+  margin-left: 4px;
+}
+.agent-button-container:has(.ask-agent-scalar-input-not-empty)
+  .ask-agent-scalar-input-label,
+.agent-button-container:focus-within .ask-agent-scalar-input-label {
+  /** Hidden when input is focused */
+  display: none;
+}
+/** Send button */
 .ask-agent-scalar-send {
   background: var(--scalar-color-blue);
   color: #fff;
@@ -114,18 +113,16 @@ function handleSubmit() {
   justify-content: center;
   align-items: center;
   border-radius: var(--scalar-radius);
-  margin-left: auto;
+  outline-offset: 1px;
+  /* Hidden by default */
   display: none;
-}
-.ask-agent-scalar-send:hover {
-  background: color-mix(
-    in srgb,
-    var(--scalar-color-blue),
-    transparent 10%
-  ) !important;
 }
 .agent-button-container:has(.ask-agent-scalar-input-not-empty)
   .ask-agent-scalar-send {
+  /** Show when input is not empty */
   display: flex;
+}
+.ask-agent-scalar-send:hover {
+  background: color-mix(in srgb, var(--scalar-color-blue), transparent 10%);
 }
 </style>
