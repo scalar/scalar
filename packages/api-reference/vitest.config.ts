6aedcb5c74e0e525dca58e2239aeec4c2244ddd5
DemonHa <hh360284@gmail.com>
2026-03-17 00:19:56 -0400

    fix test cases in references

--- packages/api-reference/vitest.config.ts
+++ packages/api-reference/vitest.config.ts
@@ -11,6 +11,12 @@ export default mergeConfig(
       environment: 'jsdom',
       root: fileURLToPath(new URL('./', import.meta.url)),
       setupFiles: './src/vitest.setup.ts',
+      alias: [
+        {
+          find: /^monaco-editor$/,
+          replacement: __dirname + '/node_modules/monaco-editor/esm/vs/editor/editor.api',
+        },
+      ],
     },
   }),
 )
