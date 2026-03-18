e126a4965ede702c298a7381ea6dca79a86b72ca
Redis Stasa <91160178+DemonHa@users.noreply.github.com>
2026-03-18 16:40:22 -0400

    chore(api-client): allow client version override (#8505)

--- packages/api-client/src/v2/features/operation/Operation.vue
+++ packages/api-client/src/v2/features/operation/Operation.vue
@@ -18,6 +18,7 @@ import { computed, toValue } from 'vue'
 
 import { OperationBlock } from '@/v2/blocks/operation-block'
 import type { ExtendedScalarCookie } from '@/v2/blocks/request-block/RequestBlock.vue'
+import { APP_VERSION } from '@/v2/constants'
 import type { RouteProps } from '@/v2/features/app/helpers/routes'
 import { mapHiddenClientsConfig } from '@/v2/features/modal/helpers/map-hidden-clients-config'
 import type { ModalProps } from '@/v2/features/modal/Modal.vue'
@@ -174,9 +175,6 @@ const environments = computed(() => {
 const httpClients = computed(() =>
   mapHiddenClientsConfig(toValue(options)?.hiddenClients),
 )
-
-// eslint-disable-next-line no-undef
-const APP_VERSION = PACKAGE_VERSION
 </script>
 
 <template>
--- packages/api-client/src/vite-env.d.ts
+++ packages/api-client/src/vite-env.d.ts
@@ -5,6 +5,8 @@ import type { useAppState } from './v2/features/app/app-state'
 declare global {
   /** The version number taken from the package.json */
   const PACKAGE_VERSION: string
+  /** The version number taken from the package.json to override the package version (e.g. for seprate deployment flow) */
+  const OVERRIDE_PACKAGE_VERSION: string | undefined
 
   interface Window {
     dataDumpWorkspace: () => WorkspaceStore | null
