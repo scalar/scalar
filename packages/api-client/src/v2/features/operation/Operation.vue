70a1b6f02ec7f5fcdf275cb9fa614c5a4b184374
DemonHa <hh360284@gmail.com>
2026-03-17 23:54:27 -0400

    make it reactive

--- packages/api-client/src/v2/features/operation/Operation.vue
+++ packages/api-client/src/v2/features/operation/Operation.vue
@@ -11,7 +11,7 @@ export default {}
 </script>
 
 <script setup lang="ts">
-import { computed, toValue } from 'vue'
+import { computed, toRef, toValue } from 'vue'
 
 import { OperationBlock } from '@/v2/blocks/operation-block'
 import type { RouteProps } from '@/v2/features/app/helpers/routes'
@@ -19,41 +19,30 @@ import { mapHiddenClientsConfig } from '@/v2/features/modal/helpers/map-hidden-c
 import type { ModalProps } from '@/v2/features/modal/Modal.vue'
 import { useOperationRequestContext } from '@/v2/features/operation/use-operation-request-context'
 
-const {
-  document,
-  layout,
-  eventBus,
-  path,
-  method,
-  exampleName,
-  options,
-  workspaceStore,
-  plugins,
-  documentSlug,
-} = defineProps<
+const props = defineProps<
   RouteProps & {
     /** Subset of config options for the modal */
     options?: ModalProps['options']
   }
 >()
 
-/** Derive environment, server, operation, auth, and executeRequest from workspace + document + path + method */
+/** Pass refs so context updates when route/operation/example changes */
 const requestContext = useOperationRequestContext({
-  workspaceStore,
-  document,
-  path,
-  method,
-  documentSlug,
-  layout,
-  exampleKey: exampleName,
-  eventBus,
-  plugins,
-  options,
+  workspaceStore: toRef(props, 'workspaceStore'),
+  document: toRef(props, 'document'),
+  path: toRef(props, 'path'),
+  method: toRef(props, 'method'),
+  documentSlug: toRef(props, 'documentSlug'),
+  layout: toRef(props, 'layout'),
+  exampleKey: computed(() => props.exampleName ?? 'default'),
+  eventBus: toRef(props, 'eventBus'),
+  plugins: toRef(props, 'plugins'),
+  options: toRef(props, 'options'),
 })
 
 /** Temporarily use the old config.hiddenClients until we migrate to the new httpClients config */
 const httpClients = computed(() =>
-  mapHiddenClientsConfig(toValue(options)?.hiddenClients),
+  mapHiddenClientsConfig(toValue(props.options)?.hiddenClients),
 )
 
 // eslint-disable-next-line no-undef
@@ -64,39 +53,47 @@ const APP_VERSION = PACKAGE_VERSION
   <!-- Operation exists -->
   <template
     v-if="
-      path &&
-      method &&
-      exampleName &&
+      props.path &&
+      props.method &&
+      props.exampleName &&
       requestContext.operation.value &&
-      document
+      props.document
     ">
     <OperationBlock
       :activeEnvironment="requestContext.activeEnvironment.value"
       :appVersion="APP_VERSION"
       :authMeta="requestContext.authMeta.value"
-      :documentSecurity="document?.security ?? []"
+      :documentSecurity="props.document?.security ?? []"
       :documentSelectedSecurity="requestContext.documentSelectedSecurity.value"
-      :documentUrl="document?.['x-scalar-original-source-url']"
+      :documentUrl="props.document?.['x-scalar-original-source-url']"
       :environment="requestContext.environment.value"
       :environments="requestContext.environments.value"
-      :eventBus
-      :exampleKey="exampleName"
+      :eventBus="props.eventBus"
+      :exampleKey="props.exampleName"
       :executeRequest="requestContext.executeRequest"
       :globalCookies="requestContext.globalCookies.value"
-      :hideClientButton="toValue(options)?.hideClientButton ?? false"
-      :history="workspaceStore.history.getHistory(documentSlug, path, method)"
+      :hideClientButton="toValue(props.options)?.hideClientButton ?? false"
+      :history="
+        props.workspaceStore.history.getHistory(
+          props.documentSlug,
+          props.path,
+          props.method,
+        )
+      "
       :httpClients
-      :layout
-      :method
+      :layout="props.layout"
+      :method="props.method"
       :operation="requestContext.operation.value"
       :operationSelectedSecurity="
         requestContext.operationSelectedSecurity.value
       "
-      :path
-      :plugins="plugins"
+      :path="props.path"
+      :plugins="props.plugins"
       :proxyUrl="requestContext.proxyUrl.value"
       :securitySchemes="requestContext.securitySchemes.value"
-      :selectedClient="workspaceStore.workspace['x-scalar-default-client']"
+      :selectedClient="
+        props.workspaceStore.workspace['x-scalar-default-client']
+      "
       :server="requestContext.server.value"
       :serverMeta="requestContext.serverMeta.value"
       :servers="requestContext.servers.value" />
