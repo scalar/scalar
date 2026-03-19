ed8d1c96c20aab0193d04357db8a94eb8428ac3a
Hans Pagel <mail@hanspagel.com>
2026-03-19 12:26:00 +0100

    chore: clean up

--- packages/blocks/vite.config.ts
+++ packages/blocks/vite.config.ts
@@ -1,19 +1,37 @@
-import { findEntryPoints } from '@scalar/build-tooling'
-import { alias, createViteBuildOptions } from '@scalar/build-tooling/vite'
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'node:path'
+
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vitest/config'
 
+import { createExternalsFromPackageJson, createLibEntry, findEntryPoints } from '../../tooling/scripts/vite-lib-config'
+
+const external = createExternalsFromPackageJson(resolve(import.meta.dirname, 'package.json'))
+const entryPaths = await findEntryPoints()
+const entry = createLibEntry(entryPaths, import.meta.dirname)
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
   resolve: {
-    alias: alias(import.meta.url),
-    dedupe: ['vue'],
-  },
-  build: createViteBuildOptions({
-    entry: await findEntryPoints({ allowCss: true }),
-    options: {
-      ssr: false,
+    alias: {
+      '@': resolve(import.meta.dirname, './src'),
     },
-  }),
+  },
+  plugins: [vue(), tailwindcss()],
+  build: {
+    outDir: './dist',
+    ssr: false,
+    sourcemap: true,
+    lib: {
+      formats: ['es'],
+      cssFileName: 'style',
+      entry,
+    },
+    rolldownOptions: {
+      treeshake: {
+        moduleSideEffects: (id) => id.includes('.css'),
+      },
+      external,
+    },
+  },
+  test: {},
 })
