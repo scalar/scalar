08ac0c6e056c7739598b6f8fafdde32c66416190
Hans Pagel <mail@hanspagel.com>
2026-03-19 12:00:55 +0100

    chore: restore api client v1 type

--- packages/oas-utils/src/entities/spec/parameters.ts
+++ packages/oas-utils/src/entities/spec/parameters.ts
@@ -44,7 +44,6 @@ export const oasParameterSchema = z.object({
   /** Defaulted according to @url https://spec.openapis.org/oas/v3.1.0#parameter-object */
   style: parameterStyleSchema.optional(),
   explode: z.boolean().optional(),
-  allowReserved: z.boolean().optional(),
   example: z.unknown().optional(),
   examples: z
     .union([
