1c3dc8b028bc1abe3b5f20af09827620903a1c9c
DemonHa <hh360284@gmail.com>
2026-03-23 16:36:27 -0400

    chore: add exports

---
+++ packages/validation/src/index.ts
@@ -0,0 +1,20 @@
+export { coerce } from './coerce'
+export {
+  type Schema,
+  any,
+  array,
+  boolean,
+  evaluate,
+  literal,
+  notDefined,
+  nullable,
+  number,
+  object,
+  optional,
+  record,
+  recursive,
+  string,
+  union,
+} from './schema'
+export type { Static } from './types'
+export { validate } from './validate'
