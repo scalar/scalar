2d4d503b4ac438dc70f07d1e8ff97e96aab6e74f
Brynn Hawker <brynn@hawker.me>
2026-03-31 13:24:22 -0700

    feat(api-reference): style classic tag groups

--- packages/api-reference/src/components/Content/Tags/components/ClassicLayout.vue
+++ packages/api-reference/src/components/Content/Tags/components/ClassicLayout.vue
@@ -21,17 +21,22 @@ const { tag, isCollapsed } = defineProps<{
   <SectionContainerAccordion
     :aria-label="tag.title"
     class="tag-section"
+    :class="{ 'tag-section-group': tag.isGroup }"
     :modelValue="!isCollapsed"
     @update:modelValue="
       (value) => eventBus?.emit('toggle:nav-item', { id: tag.id, open: value })
     ">
     <template #title>
-      <SectionHeader class="tag-name">
+      <SectionHeader
+        class="tag-name"
+        :class="{ 'tag-group-name': tag.isGroup }">
         <Anchor
           @copyAnchorUrl="
             () => eventBus?.emit('copy-url:nav-item', { id: tag.id })
           ">
-          <SectionHeaderTag :level="2">
+          <SectionHeaderTag
+            class="tag-group-name-header"
+            :level="2">
             {{ tag.title }}
           </SectionHeaderTag>
         </Anchor>
@@ -49,11 +54,37 @@ const { tag, isCollapsed } = defineProps<{
 .tag-section {
   margin-bottom: 48px;
 }
+
 .tag-name {
   text-transform: capitalize;
 }
 .tag-description {
   padding-bottom: 4px;
   text-align: left;
+}
+
+/* Tag Groups */
+.tag-section-group .tag-group-name {
+  gap: 12px;
+  align-self: stretch;
+  grid-template-columns: auto 1fr;
+}
+.tag-group-name:after {
+  content: '';
+  display: block;
+  height: 1px;
+  align-self: center;
+  background: var(--scalar-border-color);
+}
+.tag-group-name:has(* > *:hover),
+.tag-group-name:has(:focus-visible) {
+  gap: 32px;
+}
+.tag-section-group .tag-section {
+  padding-inline: 0;
+  margin-bottom: 24px;
+}
+.tag-section-group .tag-section:last-of-type {
+  margin-bottom: 0;
 }
 </style>
