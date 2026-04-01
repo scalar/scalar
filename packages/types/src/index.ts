5b7e76869210b5d33c6eadc5f871ac87318a1072
Hans Pagel <mail@hanspagel.com>
2026-04-01 15:01:42 +0200

    refactor(types): reorder named exports for clarity and maintain compatibility with CommonJS

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,18 +1,6 @@
 /**
  * We should not use these exports anymore, but we need them for commonjs compatibility.
  */
-
-// biome-ignore lint/performance/noReExportAll: leave this to avoid copy exports
-export {
-  apiClientConfigurationSchema,
-  apiClientPluginSchema,
-  apiReferenceConfigurationSchema,
-  apiReferenceConfigurationWithSourceSchema,
-  hooksSchema,
-  htmlRenderingConfigurationSchema,
-  isConfigurationWithSources,
-  sourceConfigurationSchema,
-} from './api-reference/index'
 export type {
   AnyApiReferenceConfiguration,
   ApiClientConfiguration,
@@ -30,5 +18,15 @@ export type {
   SourceConfiguration,
   SpecificationExtension,
   ViewComponent,
+} from './api-reference/index'
+export {
+  apiClientConfigurationSchema,
+  apiClientPluginSchema,
+  apiReferenceConfigurationSchema,
+  apiReferenceConfigurationWithSourceSchema,
+  hooksSchema,
+  htmlRenderingConfigurationSchema,
+  isConfigurationWithSources,
+  sourceConfigurationSchema,
 } from './api-reference/index'
 export { XScalarStability } from './legacy/index'
