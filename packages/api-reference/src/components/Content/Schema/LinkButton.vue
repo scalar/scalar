d0f27fa87942c8528ca38382bc06e2b1e5b488b8
DemonHa <hh360284@gmail.com>
2026-04-09 23:20:54 -0400

    clean up

--- packages/api-reference/src/components/Content/Schema/LinkButton.vue
+++ packages/api-reference/src/components/Content/Schema/LinkButton.vue
@@ -1,22 +1,7 @@
-<script lang="ts" setup>
-defineEmits<{
-  (e: 'click', event: MouseEvent): void
-}>()
-</script>
 <template>
   <button
-    class="property-example-label"
-    type="button"
-    @click="$emit('click', $event)">
+    class="text-c-3 hover:text-c-1 underline"
+    type="button">
     <slot />
   </button>
 </template>
-<style scoped>
-.property-example-label:hover {
-  color: var(--scalar-color-1);
-}
-.property-example-label {
-  color: var(--scalar-color-3);
-  border-bottom: var(--scalar-border-width) dotted currentColor;
-}
-</style>
