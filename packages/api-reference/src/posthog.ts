518efc892bf906784e2964f72c1ef2de48d8b45d
Liam Debell <liam@scalar.com>
2026-04-13 09:38:05 +0100

    fix(analytics): SSR check

--- packages/api-reference/src/posthog.ts
+++ packages/api-reference/src/posthog.ts
@@ -1,18 +1,25 @@
 import ph from 'posthog-js'
 import { type Ref, watch } from 'vue'
 
-const posthog = ph.init('phc_3elIjSOvGOo5aEwg6krzIY9IcQiRubsBtglOXsQ4Uu4', {
-  api_host: 'https://magic.scalar.com',
-  ui_host: 'https://us.posthog.com',
-  defaults: '2025-11-30',
-  opt_out_capturing_by_default: true,
-})
+const posthog =
+  typeof window !== 'undefined'
+    ? ph.init('phc_3elIjSOvGOo5aEwg6krzIY9IcQiRubsBtglOXsQ4Uu4', {
+        api_host: 'https://magic.scalar.com',
+        ui_host: 'https://us.posthog.com',
+        defaults: '2025-11-30',
+        opt_out_capturing_by_default: true,
+      })
+    : null
 
-posthog.register({
+posthog?.register({
   product: 'api-reference',
 })
 
 export const usePosthog = (enabled: Ref<boolean>) => {
+  if (!posthog) {
+    return
+  }
+
   watch(
     enabled,
     (value) => {
