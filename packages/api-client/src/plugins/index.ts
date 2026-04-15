477c829b7fc5ebc1c6f6a33c37175fa2359d3cbf
Hans Pagel <mail@hanspagel.com>
2026-04-15 12:24:26 +0200

    fix: broken imports/exports

--- packages/api-client/src/plugins/index.ts
+++
@@ -1,7 +0,0 @@
-export { PLUGIN_MANAGER_SYMBOL, usePluginManager } from './hooks/usePluginManager'
-export {
-  type ApiClientPlugin,
-  type PluginManager,
-  createPluginManager,
-} from './plugin-manager'
-export { PostHogClientPlugin } from './posthog'
