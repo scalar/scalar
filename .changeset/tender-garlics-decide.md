44c0e198a4909ed239bd9868bcda4108b35d5049
Brynn Hawker <brynn@hawker.me>
2026-04-15 23:22:49 -0700

    feat(api-client): support header for web layout

---
+++ .changeset/tender-garlics-decide.md
@@ -0,0 +1,5 @@
+---
+'@scalar/api-client': patch
+---
+
+feat(api-client): support header for web layout
--- packages/api-client/src/v2/features/app/App.vue
+++ packages/api-client/src/v2/features/app/App.vue
@@ -201,7 +201,9 @@ const routerViewProps = computed<RouteProps>(() => {
         app.workspace.activeWorkspace.value !== null &&
         !app.loading.value
       ">
-      <div class="relative flex h-dvh w-dvw flex-1 flex-col">
+      <div
+        class="relative flex w-dvw flex-col"
+        :class="layout === 'web' ? 'min-h-0' : 'h-dvh'">
         <SidebarToggle
           v-model="app.sidebar.isOpen.value"
           class="absolute top-4 left-3 z-[60] md:hidden" />
@@ -227,7 +229,7 @@ const routerViewProps = computed<RouteProps>(() => {
             </template>
           </AppSidebar>
 
-          <div class="flex flex-1 flex-col">
+          <div class="flex min-h-0 flex-1 flex-col">
             <!-- App Tabs -->
             <DesktopTabs
               v-if="layout === 'desktop'"
