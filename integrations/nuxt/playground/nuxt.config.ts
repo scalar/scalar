a595ff23ede0de33af7b7e92590de171a1669169
Amrit <amrit@scalar.com>
2026-04-16 13:05:43 -0700

    chore: undo changes to playground

--- integrations/nuxt/playground/nuxt.config.ts
+++ integrations/nuxt/playground/nuxt.config.ts
@@ -1,5 +1,3 @@
-import { type NuxtConfig } from 'nuxt/config'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-11',
@@ -8,18 +6,9 @@ export default defineNuxtConfig({
 
   modules: ['../src/module'],
 
-  scalar: {
-    spec: {
-      url: 'https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.yaml',
-    },
-    pathRouting: {
-      basePath: '/_scalar',
-    },
-  },
-
   nitro: {
     experimental: {
       openAPI: true,
     },
   },
-} as NuxtConfig)
+})
