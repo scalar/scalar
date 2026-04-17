7d5bbf2f539f1d92a4debf90a64da6d9347ca47f
DemonHa <hh360284@gmail.com>
2026-04-16 21:04:33 -0400

    fix(workspace-store): resolve selected security schemes using selectedIndex

--- packages/workspace-store/src/request-example/context/security/get-security-schemes.ts
+++ packages/workspace-store/src/request-example/context/security/get-security-schemes.ts
@@ -11,15 +11,13 @@ import type { SecurityRequirementObject } from '@/schemas/v3.1/strict/security-r
  */
 export const getSecuritySchemes = (
   securitySchemes: MergedSecuritySchemes,
-  selectedSecurity: SecurityRequirementObject[],
+  selectedSecurity: SecurityRequirementObject,
 ): SecuritySchemeObjectSecret[] =>
-  selectedSecurity.flatMap((scheme) =>
-    objectKeys(scheme).flatMap((key) => {
+    objectKeys(selectedSecurity).flatMap((key) => {
       const scheme = getResolvedRef(securitySchemes?.[key])
       if (scheme) {
         return scheme
       }
 
       return []
-    }),
-  ) ?? []
+    })
