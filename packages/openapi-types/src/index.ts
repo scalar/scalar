3f01b67dfe10e36dffba447ff29673828f68c3d7
Amrit <amrit@scalar.com>
2026-04-20 15:29:47 -0700

    feat: export new types

--- packages/openapi-types/src/index.ts
+++ packages/openapi-types/src/index.ts
@@ -1,2 +1,1 @@
-export type { OpenAPI, OpenAPIV2, OpenAPIV3, OpenAPIV3_1, OpenAPIV3_2 } from './openapi-types'
-export type { IJsonSchema } from './openapi-types'
+export type { IJsonSchema, OpenAPI, OpenAPIV2, OpenAPIV3, OpenAPIV3_1, OpenAPIV3_2 } from './openapi-types'
--- packages/openapi-types/src/openapi-types.ts
+++ packages/openapi-types/src/openapi-types.ts
@@ -15,7 +15,16 @@ type AnyOtherAttribute = {
   [key: string]: any
 }
 
-// biome-ignore lint/style/noNamespace: We want it to be a module here.
+/**
+ * OpenAPI types
+ *
+ * @deprecated we have deprecated these older types in favor of the new stricter ones. You may continue using
+ * them for now but we would recommend you to migrate to the newer stricter types.
+ *
+ * Check the documentation for more information.
+ *
+ * biome-ignore lint/style/noNamespace: We want it to be a module here.
+ */
 export namespace OpenAPI {
   // OpenAPI extensions can be declared using generics
   // e.g.:
