61a423eeee5d35b12040e72eb70a39d53da766c6
Amrit <amrit@scalar.com>
2026-04-30 23:44:37 -0700

    current team

--- projects/scalar-app/entrypoints/web/src/app-state.ts
+++ projects/scalar-app/entrypoints/web/src/app-state.ts
@@ -1,14 +1,14 @@
-import { router } from '@web/router'
-
 import { createAppState } from '@scalar/api-client/v2/features/app'
+import { router } from '@web/router'
 
 import { useStateData } from '@/hooks/use-state-data'
 
-const { customThemes, fallbackThemeSlug } = useStateData()
+const { currentTeam, customThemes, fallbackThemeSlug } = useStateData()
 
 /** Initialize the app state with router */
 export const appState = await createAppState({
   router,
+  currentTeam,
   customThemes,
   fallbackThemeSlug,
   telemetryDefault: true,
