764a6d3986c82cede875ca3381b96f4c91647861
Amrit <amrit@scalar.com>
2026-05-11 10:18:28 -0700

    fix:types

--- projects/scalar-app/entrypoints/web/src/app-state.ts
+++ projects/scalar-app/entrypoints/web/src/app-state.ts
@@ -3,13 +3,12 @@ import { router } from '@web/router'
 import { createAppState } from '@/features/app'
 import { useStateData } from '@/hooks/use-state-data'
 
-const { currentTeam, fallbackThemeSlug, isCurrentTeamLoading } = useStateData()
+const { currentTeam, isCurrentTeamLoading } = useStateData()
 
 /** Initialize the app state with router */
 export const appState = await createAppState({
   router,
   currentTeam,
   isCurrentTeamLoading,
-  fallbackThemeSlug,
   telemetryDefault: true,
 })
