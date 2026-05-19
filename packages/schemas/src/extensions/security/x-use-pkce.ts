6ba2497bf99f7d5bc26c88d33bab07878a4905eb
DemonHa <hh360284@gmail.com>
2026-05-19 13:49:44 -0400

    fix some tests

--- packages/schemas/src/extensions/security/x-use-pkce.ts
+++ packages/schemas/src/extensions/security/x-use-pkce.ts
@@ -2,7 +2,7 @@ import { literal, object, union } from '@scalar/validation'
 
 export const XusePkce = object(
   {
-    'x-usePkce': union([literal('SHA-256'), literal('plain'), literal('no')], {
+    'x-usePkce': union([literal('no'), literal('SHA-256'), literal('plain')], {
       typeComment: 'PKCE mode for the OAuth2 authorization code flow',
     }),
   },
