d23fbfd3227320b09518c40b46c29c144f46877e
Amrit <amrit@scalar.com>
2026-05-20 11:29:18 -0700

    chore: added tests

---
+++ projects/scalar-app/test/request-scripts-csp.e2e.ts
@@ -0,0 +1,9 @@
+import { test } from '@playwright/test'
+
+import { runRequestScriptsCspScenario } from './helpers/request-scripts-csp-scenario'
+
+test.describe('request-scripts-csp.e2e', () => {
+  test('pre-request and post-response scripts run under the app content security policy', async ({ page }) => {
+    await runRequestScriptsCspScenario(page)
+  })
+})
