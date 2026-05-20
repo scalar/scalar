2009e577b68ba0942ef72c0e33cfd5ee84ba8057
Amrit <amrit@scalar.com>
2026-05-20 12:39:29 -0700

    chore: pr fix

--- projects/scalar-app/test/request-scripts-csp.e2e.ts
+++ projects/scalar-app/test/request-scripts-csp.e2e.ts
@@ -3,6 +3,8 @@ import { test } from '@playwright/test'
 import { runRequestScriptsCspScenario } from './helpers/request-scripts-csp-scenario'
 
 test.describe('request-scripts-csp.e2e', () => {
+  test.setTimeout(120_000)
+
   test('pre-request and post-response scripts run under the app content security policy', async ({ page }) => {
     await runRequestScriptsCspScenario(page)
   })
