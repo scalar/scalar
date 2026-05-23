0760958531e13f11f0d0398ea5f426d3c86acf77
DemonHa <hh360284@gmail.com>
2026-05-23 01:57:55 -0400

    feat: add WebSocket session transport and plugin hooks for AsyncAPI

---
+++ packages/api-client/src/v2/blocks/channel-operation-block/index.ts
@@ -0,0 +1,19 @@
+export {
+  type ConnectWebSocketData,
+  type ConnectWebSocketFailureCode,
+  type ConnectWebSocketOptions,
+  type ConnectWebSocketResult,
+  WEBSOCKET_CONNECTION_FAILED,
+  WEBSOCKET_CONNECTION_FAILED_MESSAGE,
+  connectWebSocket,
+} from './helpers/connect-websocket'
+export {
+  type WebSocketCloseInfo,
+  type WebSocketConnectOptions,
+  type WebSocketFrame,
+  type WebSocketFrameOpcode,
+  type WebSocketSession,
+  type WebSocketSessionCallbacks,
+  type WebSocketSessionState,
+  createWebSocketSession,
+} from './helpers/websocket-session'
