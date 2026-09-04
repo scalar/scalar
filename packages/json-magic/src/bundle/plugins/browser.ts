f1d88b20c476370cc34d17f14a6a7d2881805253
Hans Pagel <mail@hanspagel.com>
2026-09-04 12:51:17 +0200

    fix(json-magic): isolate browser fetch plugin

--- packages/json-magic/src/bundle/plugins/browser.ts
+++ packages/json-magic/src/bundle/plugins/browser.ts
@@ -1,4 +1,4 @@
 // biome-ignore lint/performance/noBarrelFile: entrypoint
-export { fetchUrls } from './fetch-urls'
+export { fetchUrls } from './fetch-urls/browser'
 export { parseJson } from './parse-json'
 export { parseYaml } from './parse-yaml'
