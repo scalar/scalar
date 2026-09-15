444f2e896cb780914da41a41a0e158f8200c6048
Hans Pagel <hanspagel@users.noreply.github.com>
2026-09-15 18:56:35 +0200

    test(openapi-validator): check duplicate required properties (#10158)

--- packages/openapi-validator/tests/openapi3-examples/3.0/fail/duplicateRequired.test.ts
+++ packages/openapi-validator/tests/openapi3-examples/3.0/fail/duplicateRequired.test.ts
@@ -3,12 +3,16 @@ import { describe, expect, it } from 'vitest'
 import { validate } from '../../../../src/index'
 import duplicateRequired from './duplicateRequired.yaml?raw'
 
-describe.todo('duplicateRequired', () => {
-  it('returns an error', async () => {
+describe('duplicateRequired', () => {
+  it('rejects duplicate required property names', async () => {
     const result = await validate(duplicateRequired)
 
-    expect(result.errors?.[0]?.message).toBe('something something duplicate required properties')
-    expect(result.errors?.length).toBe(1)
+    expect(result.errors).toStrictEqual([
+      {
+        message: 'uniqueItems must NOT have duplicate items (items ## 1 and 0 are identical)',
+        path: '/components/schemas/test/required',
+      },
+    ])
     expect(result.valid).toBe(false)
   })
 })
